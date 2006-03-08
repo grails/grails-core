1187ae7045d10ccf4237bfb82aca3bb75b684c46
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-03-08 18:57:58 +0000

    corrected getPropertyName method to use short name representation as it should
    git-svn-id: https://svn.codehaus.org/grails/trunk@752 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/commons/org/codehaus/groovy/grails/commons/AbstractGrailsClass.java
+++ src/commons/org/codehaus/groovy/grails/commons/AbstractGrailsClass.java
@@ -114,7 +114,7 @@ public abstract class AbstractGrailsClass implements GrailsClass {
 	}
 
     public String getPropertyName() {
-        return GrailsClassUtils.getPropertyNameRepresentation(getClazz());
+        return GrailsClassUtils.getPropertyNameRepresentation(getName());
     }
 
     public String getPackageName() {
