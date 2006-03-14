742b03e0e09e74b3144a3e74b01f42903be1a1a0
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-03-14 13:40:20 +0000

    fixed silly mistake in logic of matches constraint
    git-svn-id: https://svn.codehaus.org/grails/trunk@764 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/commons/org/codehaus/groovy/grails/validation/ConstrainedProperty.java
+++ src/commons/org/codehaus/groovy/grails/validation/ConstrainedProperty.java
@@ -781,7 +781,7 @@ public class ConstrainedProperty   {
         }
 
         protected void processValidate(Object propertyValue, Errors errors) {
-            if(propertyValue.toString().matches( regex )) {
+            if(!propertyValue.toString().matches( regex )) {
                 Object[] args = new Object[] { constraintPropertyName, constraintOwningClass, propertyValue, regex  };
                 super.rejectValue(errors,MATCHES_CONSTRAINT + INVALID_SUFFIX,args,getDefaultMessage(DEFAULT_DOESNT_MATCH_MESSAGE_CODE, args));
             }
