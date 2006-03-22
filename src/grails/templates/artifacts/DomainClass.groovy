6cdb323de25872de85f1b378be0feda777f7256d
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-03-22 18:04:35 +0000

    added default implementations of equals and hashCode to domain class template
    git-svn-id: https://svn.codehaus.org/grails/trunk@791 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/grails/templates/artifacts/DomainClass.groovy
+++ src/grails/templates/artifacts/DomainClass.groovy
@@ -2,5 +2,14 @@ class @domain.class.name@ {
 	@Property Long id
 	@Property Long version
 
-    String toString() { "${this.class.name} :  $id" }	
+    String toString() { "${this.class.name} :  $id" }
+
+    boolean equals(other) {
+        if(other instanceof @domain.class.name@) {
+            return (id == other.id)
+        }
+        return false
+    }
+
+    int hashCode() { id }
 }	
