2a34eb76ea66573ce88f5b1d4cc195c8d69af704
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-03-29 13:24:21 +0000

    removed equals/hashCode methods for now
    git-svn-id: https://svn.codehaus.org/grails/trunk@818 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/grails/templates/artifacts/DomainClass.groovy
+++ src/grails/templates/artifacts/DomainClass.groovy
@@ -3,13 +3,4 @@ class @domain.class.name@ {
 	@Property Long version
 
     String toString() { "${this.class.name} :  $id" }
-
-    boolean equals(other) {
-        if(other instanceof @domain.class.name@) {
-            return (id == other.id)
-        }
-        return false
-    }
-
-    int hashCode() { id }
 }	
