9cc92851f9612d03c1cf73a39c9db9417e73425a
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-04-03 10:40:24 +0000

    added license and javadoc
    git-svn-id: https://svn.codehaus.org/grails/trunk@844 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/web/org/codehaus/groovy/grails/web/metaclass/TagLibMetaClass.java
+++ src/web/org/codehaus/groovy/grails/web/metaclass/TagLibMetaClass.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codehaus.groovy.grails.web.metaclass;
 
 import groovy.lang.GroovyObject;
@@ -15,7 +30,13 @@ import org.apache.commons.logging.LogFactory;
 import org.codehaus.groovy.grails.commons.metaclass.PropertyAccessProxyMetaClass;
 import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes;
 import org.codehaus.groovy.runtime.InvokerHelper;
-
+/**
+ * <p>Special meta class for tag libraries that allows tag libraries to call
+ * tags within other libraries without the need for inheritance
+ *
+ * @author Graeme Rocher
+ * @since Apr 3, 20056
+ */
 public class TagLibMetaClass extends PropertyAccessProxyMetaClass {
 	private static final Log LOG = LogFactory.getLog(TagLibMetaClass.class);
 	
