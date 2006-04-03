8391f3eaa56054cff21fb6b14c7ff1dc5d9593e0
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-04-03 10:39:02 +0000

    tests for tag library meta class changes
    git-svn-id: https://svn.codehaus.org/grails/trunk@843 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ test/commons/org/codehaus/groovy/grails/MockApplicationContext.java
@@ -0,0 +1,136 @@
+package org.codehaus.groovy.grails;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.MessageSourceResolvable;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.core.io.Resource;
+
+public class MockApplicationContext implements ApplicationContext {
+
+	Date startupDate = new Date();
+	Map beans = new HashMap();
+	
+	public void registerMockBean(String name, Object instance) {
+		beans.put(name,instance);
+	}
+	public ApplicationContext getParent() {
+		throw new UnsupportedOperationException("Method not supported by implementation");	
+	}
+
+	public String getDisplayName() {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public long getStartupDate() {
+		return startupDate.getTime();
+	}
+
+	public void publishEvent(ApplicationEvent event) {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public boolean containsBeanDefinition(String beanName) {	
+		return beans.containsKey(beanName);
+	}
+
+	public int getBeanDefinitionCount() {
+		return beans.size();
+	}
+
+	public String[] getBeanDefinitionNames() {
+		return (String[])beans.keySet().toArray(new String[beans.keySet().size()]);
+	}
+
+	public String[] getBeanDefinitionNames(Class type) {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public String[] getBeanNamesForType(Class type) {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public String[] getBeanNamesForType(Class type, boolean includePrototypes,
+			boolean includeFactoryBeans) {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public Map getBeansOfType(Class type) throws BeansException {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public Map getBeansOfType(Class type, boolean includePrototypes,
+			boolean includeFactoryBeans) throws BeansException {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public Object getBean(String name) throws BeansException {
+		if(!beans.containsKey(name))throw new NoSuchBeanDefinitionException("No bean found for name " + name);
+		return beans.get(name);
+	}
+
+	public Object getBean(String name, Class requiredType)
+			throws BeansException {
+		if(!beans.containsKey(name))throw new NoSuchBeanDefinitionException("No bean found for name " + name);
+		if(beans.get(name).getClass() != requiredType)throw new NoSuchBeanDefinitionException("No bean found for name " + name);
+		
+		return beans.get(name);
+	}
+
+	public boolean containsBean(String name) {
+		return beans.containsKey(name);
+	}
+
+	public boolean isSingleton(String name)
+			throws NoSuchBeanDefinitionException {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public Class getType(String name) throws NoSuchBeanDefinitionException {
+		if(!beans.containsKey(name))throw new NoSuchBeanDefinitionException("No bean found for name " + name);
+		
+		return beans.get(name).getClass();
+	}
+
+	public String[] getAliases(String name)
+			throws NoSuchBeanDefinitionException {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public BeanFactory getParentBeanFactory() {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public String getMessage(String code, Object[] args, String defaultMessage,
+			Locale locale) {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public String getMessage(String code, Object[] args, Locale locale)
+			throws NoSuchMessageException {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public String getMessage(MessageSourceResolvable resolvable, Locale locale)
+			throws NoSuchMessageException {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public Resource[] getResources(String locationPattern) throws IOException {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+	public Resource getResource(String location) {
+		throw new UnsupportedOperationException("Method not supported by implementation");
+	}
+
+}
--- test/web/org/codehaus/groovy/grails/web/metaclass/TagLibMetaClassTests.java
+++ test/web/org/codehaus/groovy/grails/web/metaclass/TagLibMetaClassTests.java
@@ -1,44 +1,40 @@
-package org.codehaus.groovy.grails.web.servlet;
+package org.codehaus.groovy.grails.web.metaclass;
 
+import groovy.lang.Closure;
 import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyObject;
 
+import java.util.HashMap;
 import java.util.Map;
-
-import junit.framework.TestCase;
 
 import org.codehaus.groovy.grails.MockApplicationContext;
 import org.codehaus.groovy.grails.commons.DefaultGrailsApplication;
 import org.codehaus.groovy.grails.commons.GrailsApplication;
-import org.codehaus.groovy.grails.web.metaclass.ControllerDynamicMethods;
+import org.codehaus.groovy.grails.web.servlet.DefaultGrailsApplicationAttributes;
+import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes;
 import org.codehaus.groovy.grails.web.servlet.mvc.SimpleGrailsControllerHelper;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockServletContext;
 
-public class GrailsApplicationAttributesTests extends TestCase {
+import junit.framework.TestCase;
 
-	/*
-	 * Test method for 'org.codehaus.groovy.grails.web.servlet.DefaultGrailsApplicationAttributes.getTemplateUri(String, ServletRequest)'
-	 */
-	public void testGetTemplateUri() {
-		 GrailsApplicationAttributes attrs = new DefaultGrailsApplicationAttributes(new MockServletContext());
-		 
-		 assertEquals("/WEB-INF/grails-app/views/_test.gsp",attrs.getTemplateUri("/test", new MockHttpServletRequest()));
-		 assertEquals("/WEB-INF/grails-app/views/shared/_test.gsp",attrs.getTemplateUri("/shared/test", new MockHttpServletRequest()));
-	}
-
-	public void testGetTagLibForTag() throws Exception {
+public class TagLibMetaClassTests extends TestCase {
+	
+	
+	public void testInvokeOneTagLibFromOther() throws Exception {
 		GroovyClassLoader gcl = new GroovyClassLoader();
         Class controllerClass = gcl.parseClass( "class TestController {\n" +
 									                "@Property list = {\n" +
 									                "}\n" +
 									                "}" );
         Class tagLibClass1 = gcl.parseClass( "class FirstTagLib {\n" +
-								                "@Property firstTag = {\n" +
+								                "@Property firstTag = { attrs ->\n" +
+								                "attrs.remove('test')" +
 								                "}\n" +
 								                "}" ); 
         Class tagLibClass2 = gcl.parseClass( "class SecondTagLib {\n" +
-                "@Property secondTag = {\n" +
+                "@Property secondTag = { attrs ->\n" +
+                	"firstTag(attrs)\n" +                	
                 "}\n" +
                 "}" );        
         
@@ -53,13 +49,14 @@ public class GrailsApplicationAttributesTests extends TestCase {
 		new ControllerDynamicMethods(controller,helper,request,null);
 		
 		request.setAttribute(GrailsApplicationAttributes.CONTROLLER,controller );
-		GroovyObject tagLib1 = attrs.getTagLibraryForTag(request,"firstTag");
-		assertNotNull(tagLib1);
-		assertNotNull(request.getAttribute(GrailsApplicationAttributes.TAG_CACHE));
-		Map tagCache = (Map)request.getAttribute(GrailsApplicationAttributes.TAG_CACHE);
-		
-		assertNotNull(tagCache.get("firstTag"));
-		
+		GroovyObject tagLib2 = attrs.getTagLibraryForTag(request,"secondTag");
+		assertNotNull(tagLib2);
+
+		Closure secondTag = (Closure)tagLib2.getProperty("secondTag");
+		Map tagAttrs = new HashMap();
+		tagAttrs.put("test","test");
+		secondTag.call(new Object[]{tagAttrs});
+		assertFalse(tagAttrs.containsKey("test"));
 		
 	}
 	
