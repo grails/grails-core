cf6ea0865f1e5dcdfccf88e7313e534dbd6d09b7
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-04-19 10:41:02 +0000

    fixed test case following change to mapper
    git-svn-id: https://svn.codehaus.org/grails/trunk@855 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/web/org/codehaus/groovy/grails/web/sitemesh/GrailsLayoutDecoratorMapperTests.java
+++ test/web/org/codehaus/groovy/grails/web/sitemesh/GrailsLayoutDecoratorMapperTests.java
@@ -25,7 +25,7 @@ public class GrailsLayoutDecoratorMapperTests extends TestCase {
 		Page page = parser.parse( new StringReader(html) ); 
 		Decorator d = m.getDecorator(request, page);
 		
-		assertEquals("/WEB-INF/grails-app/views/layouts/test.gsp", d.getPage());
+		assertEquals("/WEB-INF/grails-app/views/layouts/test.gsp", d.getURIPath());
 		assertEquals("test", d.getName());
 		
 	}
