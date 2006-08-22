95a6095d940f9c76c63ab4322d6fd6c2008cf4d2
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-08-22 20:19:27 +0000

    Patch from Jason to allow iteration over a proxy
    git-svn-id: https://svn.codehaus.org/grails/trunk@1138 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/commons/grails/util/ExtendProxy.java
+++ src/commons/grails/util/ExtendProxy.java
@@ -19,32 +19,51 @@ import org.codehaus.groovy.runtime.InvokerHelper;
 
 import groovy.lang.MissingPropertyException;
 import groovy.util.Proxy;
-/** 
+
+import java.util.Collection;
+
+/**
  * Extends the Groovy Proxy implementation and adds proxying of property getters/setters
  * 
 * @author Graeme Rocher
+* @author Jonathan Carlson
+ * 
 * @since Oct 20, 2005
 */
 public class ExtendProxy extends Proxy {
 
-	public Object getProperty(String property) {
-		try {			
-			return super.getProperty(property);
-		}
-		catch(MissingPropertyException mpe) {
-			return InvokerHelper.getMetaClass(getAdaptee()).getProperty(getAdaptee(),property);
-		}
-	}
+    public Object getProperty(String property) {
+        try {
+            return super.getProperty(property);
+        }
+        catch(MissingPropertyException mpe) {
+            return InvokerHelper.getMetaClass(getAdaptee()).getProperty(getAdaptee(),property);
+        }
+    }
 
-	public void setProperty(String property, Object newValue) {
-		try {			
-			super.setProperty(property,newValue);
-		}
-		catch(MissingPropertyException mpe) {
-			InvokerHelper.getMetaClass(getAdaptee()).setProperty(getAdaptee(),property,newValue);
-		}
-	}
-	
-	
+    public void setProperty(String property, Object newValue) {
+        try {
+            super.setProperty(property,newValue);
+        }
+        catch(MissingPropertyException mpe) {
+            InvokerHelper.getMetaClass(getAdaptee()).setProperty(getAdaptee(),property,newValue);
+        }
+    }
+
+    /**
+     * This method allows transparent iterating over an adapted collection.
+     * The previous behavior was to return the adaptee instead of the iterator()
+     * when iterator()was called so this keeps that behavior for non-collections.
+     * @return either an Iterator (if adaptee is a collection) or this.
+     *
+     */
+    public Object iterator() {
+        if(getAdaptee() instanceof Collection) {
+           return ((Collection)getAdaptee()).iterator();
+        }
+        else {
+            return getAdaptee();
+        }
+    }
 
 }
