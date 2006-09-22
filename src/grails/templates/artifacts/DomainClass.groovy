3b8221ed24b7fa8b615ffcc12e5e90d25a0ffa85
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-09-22 20:53:49 +0000

    removed equals/hashCode(). GRAILS-313
    git-svn-id: https://svn.codehaus.org/grails/trunk@1303 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/grails/templates/artifacts/DomainClass.groovy
+++ src/grails/templates/artifacts/DomainClass.groovy
@@ -2,19 +2,5 @@ class @domain.class.name@ {
 	Long id
 	Long version
 
-    String toString() { "${this.class.name} :  $id" }
-	
-	boolean equals(other) {
-		if(other?.is(this))return true
-		if(!(other instanceof @domain.class.name@)) return false
-		
-		if(!id || !other?.id || id!=other?.id) return false
-		
-		return true
-	}
-	
-	int hashCode() {
-		int hashCode = 0
-		hashCode = 29 * (hashCode + ( !id ? 0 : id ^ (id >>> 32) ) )
-	}
+    String toString() { "${this.class.name} :  $id" }	
 }	
