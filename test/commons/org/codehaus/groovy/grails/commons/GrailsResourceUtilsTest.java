f8f1c290b522e83ceaa72e5afaaed42c549d6b7b
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-09-26 14:06:47 +0000

    Implemented property GORM compile time injection greatly simplifying GORM. Fixes GRAILS-139
    git-svn-id: https://svn.codehaus.org/grails/trunk@1337 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ test/commons/org/codehaus/groovy/grails/commons/GrailsResourceUtilsTest.java
@@ -0,0 +1,38 @@
+package org.codehaus.groovy.grails.commons;
+
+import java.net.URL;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+import junit.framework.TestCase;
+
+public class GrailsResourceUtilsTest extends TestCase {
+
+	private static final String TEST_URL = "file:///test/grails/app/grails-app/domain/Test.groovy";
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testIsDomainClass() throws Exception {
+		URL testUrl = new URL(GrailsResourceUtilsTest.TEST_URL);
+		
+		assertTrue(GrailsResourceUtils.isDomainClass(testUrl));
+	}
+
+	public void testGetClassNameResource() throws Exception {
+		Resource r = new UrlResource(new URL(TEST_URL));
+		
+		assertEquals("Test", GrailsResourceUtils.getClassName(r));
+	}
+
+	public void testGetClassNameString() {
+		assertEquals("Test", GrailsResourceUtils.getClassName(TEST_URL));
+	}
+
+	public void testIsGrailsPath() {
+		assertTrue(GrailsResourceUtils.isGrailsPath(TEST_URL));
+	}
+
+}
