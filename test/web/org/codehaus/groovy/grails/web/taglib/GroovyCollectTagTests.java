bed202c8bec947644768514c69ad38ef5318568e
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-09-28 13:53:32 +0000

    corrected test case
    git-svn-id: https://svn.codehaus.org/grails/trunk@1381 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/web/org/codehaus/groovy/grails/web/taglib/GroovyCollectTagTests.java
+++ test/web/org/codehaus/groovy/grails/web/taglib/GroovyCollectTagTests.java
@@ -62,7 +62,7 @@ public class GroovyCollectTagTests extends TestCase {
 		tag.setAttributes(attrs);
 		assertFalse(tag.attributes.isEmpty());
 		tag.doStartTag();		
-		assertEquals("myObj.collect {it.name}.each {" + System.getProperty("line.separator"),sw.toString());
+		assertEquals("myObj.collect {it.name}.each { it ->" + System.getProperty("line.separator"),sw.toString());
 	}
 
 }
