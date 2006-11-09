bd7cd2a0b9b582578f3875c61dc709f7d28c8bdc
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-11-09 09:22:10 +0000

    inheritance querying test case
    git-svn-id: https://svn.codehaus.org/grails/trunk@1694 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ test/groovy/org/codehaus/groovy/grails/orm/hibernate/InheritanceQueryingTests.groovy
@@ -0,0 +1,51 @@
+package org.codehaus.groovy.grails.orm.hibernate;
+
+
+class InheritanceQueryingTests extends AbstractGrailsHibernateTests {
+
+	void testPolymorphicQuerying() {
+		def cityClass = ga.getGrailsDomainClass("City")
+		def countryClass = ga.getGrailsDomainClass("Country")
+		def locationClass = ga.getGrailsDomainClass("Location")
+		
+		def city = cityClass.newInstance()
+		city.properties = [code: "LON", name: "London", longitude: 49.1, latitude:
+			53.1]
+		def location = locationClass.newInstance()
+		location.properties = [code: "XX", name: "The World"]
+		def country = countryClass.newInstance()
+		country.properties = [code: "UK", name: "United Kingdom", population: 10000000]
+		                      
+		country.save(true)
+		city.save(true)
+		location.save(true)
+		
+		assertEquals 1, cityClass.clazz.findAll().size() 
+		assertEquals 1, countryClass.clazz.findAll().size() 
+		assertEquals 3, locationClass.clazz.findAll().size() 
+	
+	}
+	void onSetUp() {
+		this.gcl.parseClass('''
+class City extends Location {
+    BigDecimal latitude
+    BigDecimal longitude
+}         
+class Country extends Location {
+    int population
+}	
+class Location {
+	Long id
+	Long version
+    String name
+    String code
+}	
+
+'''
+		)
+	}	
+	
+	void onTearDown() {
+		
+	}
+}
