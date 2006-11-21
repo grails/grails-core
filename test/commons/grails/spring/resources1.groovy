aa0cb51c5d0f901a346ea817db3a0f1bec69bc2a
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2006-11-21 21:06:05 +0000

    moved here, not picked up on build server
    git-svn-id: https://svn.codehaus.org/grails/trunk@1876 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ test/commons/grails/spring/resources1.groovy
@@ -0,0 +1,9 @@
+import org.apache.commons.dbcp.BasicDataSource 
+beans {
+	dataSource(BasicDataSource) {
+		driverClassName = "org.hsqldb.jdbcDriver"
+		url = "jdbc:hsqldb:mem:grailsDB"
+		username = "sa"
+		password = ""		
+	}
+}
