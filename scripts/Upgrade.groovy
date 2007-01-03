583c1790f068f832cf3f4b0a87d2fa0df6d6bed0
marcpalmer <marcpalmer@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-01-03 17:15:06 +0000

    Fixes most problems with upgrade. Taglibs, Layouts, Views, CSS etc will no longer overwrite. However if you WANT it to overwrite you must remove your existing Taglibs (for example)
    Caveat: the logs still overwrite (see comment in source) 
    
    GRAILS-552
    
    git-svn-id: https://svn.codehaus.org/grails/trunk@2337 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- scripts/Upgrade.groovy
+++ scripts/Upgrade.groovy
@@ -33,33 +33,53 @@ task( upgrade: "main upgrade task") {
 	
 	Ant.sequential {  
         delete(dir:"${basedir}/tmp", failonerror:false)
-		copy(todir:"${basedir}/web-app", overwrite:false) {
+		copy(todir:"${basedir}/web-app") {
 			fileset(dir:"${grailsHome}/src/war") {
 				include(name:"**/**")
 				exclude(name:"WEB-INF/**")
+                present(present:"srconly", targetdir:"${basedir}/web-app")
 			} 
 		}
 		copy(file:"${grailsHome}/src/war/WEB-INF/sitemesh.xml",
 			 tofile:"${basedir}/web-app/WEB-INF/sitemesh.xml", overwrite:true)
 		copy(file:"${grailsHome}/src/war/WEB-INF/applicationContext.xml",
-			 tofile:"${basedir}/web-app/WEB-INF/applicationContext.xml", overwrite:true)		
-		copy(file:"${grailsHome}/src/war/WEB-INF/log4j.properties",
-			 tofile:"${basedir}/grails-app/conf/log4j.development.properties", overwrite:false)
-		copy(file:"${grailsHome}/src/war/WEB-INF/log4j.properties",
-			 tofile:"${basedir}/grails-app/conf/log4j.test.properties", overwrite:false)
-		copy(file:"${grailsHome}/src/war/WEB-INF/log4j.properties",
-			 tofile:"${basedir}/grails-app/conf/log4j.production.properties", overwrite:false)	
+			 tofile:"${basedir}/web-app/WEB-INF/applicationContext.xml", overwrite:true)
+
+		// These will still overwrite as the src name differs from target and the src name is not in target
+		copy(tofile:"${basedir}/grails-app/conf/log4j.development.properties") {
+		    fileset(file:"${grailsHome}/src/war/WEB-INF/log4j.properties") {
+                present(present:"srconly", targetdir:"${basedir}/grails-app/conf")
+            }
+        }
+		copy(tofile:"${basedir}/grails-app/conf/log4j.test.properties") {
+		    fileset(file:"${grailsHome}/src/war/WEB-INF/log4j.properties") {
+                present(present:"srconly", targetdir:"${basedir}/grails-app/conf")
+            }
+        }
+		copy(tofile:"${basedir}/grails-app/conf/log4j.production.properties") {
+		    fileset(file:"${grailsHome}/src/war/WEB-INF/log4j.properties") {
+                present(present:"srconly", targetdir:"${basedir}/grails-app/conf")
+            }
+        }
 			
-		copy(todir:"${basedir}/grails-app", overwrite:false) {
-			fileset(dir:"${grailsHome}/src/grails/grails-app")
-		} 
+		copy(todir:"${basedir}/grails-app") {
+			fileset(dir:"${grailsHome}/src/grails/grails-app") {
+                present(present:"srconly", targetdir:"${basedir}/grails-app")
+            }
+		}
+
 		copy(todir:"${basedir}/grails-app/taglib") {
-			fileset(dir:"${grailsHome}/src/grails/grails-app/taglib")
-		}		
+			fileset(dir:"${grailsHome}/src/grails/grails-app/taglib") {
+                present(present:"srconly", targetdir:"${basedir}/grails-app/taglib")
+            }
+		}
 						   	 
-		copy(file:"${grailsHome}/src/war/WEB-INF/web${servletVersion}.template.xml", 
-			 tofile:"${basedir}/web-app/WEB-INF/web.template.xml", overwrite:false) 
-			
+		copy(tofile:"${basedir}/web-app/WEB-INF/web.template.xml") {
+		    fileset(file:"${grailsHome}/src/war/WEB-INF/web${servletVersion}.template.xml") {
+                //present(present:"srconly", targetdir:"${basedir}/web-app/WEB-INF")
+            }
+        }
+
 		if(servletVersion != "2.3") {
 			replace(file:"${basedir}/web-app/index.jsp", token:"http://java.sun.com/jstl/core",
 					value:"http://java.sun.com/jsp/jstl/core")
