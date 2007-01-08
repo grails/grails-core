b89f4b6207fd80915a122df5721e0b6205febef3
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-01-08 16:24:44 +0000

    path from marcel for GRAILS-507
    git-svn-id: https://svn.codehaus.org/grails/trunk@2369 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ test/groovy/org/codehaus/groovy/grails/cli/InstallTemplatesTests.groovy
@@ -0,0 +1,44 @@
+package org.codehaus.groovy.grails.cli;
+
+import gant.Gant
+
+class InstallTemplatesTests extends AbstractCliTests {
+	
+	
+	void testInstallTemplatesCreatesTemplates() {				
+		Gant.main(["-f", "scripts/CreateApp.groovy"] as String[])
+
+    def templatesDirectory = "${appBase}/templates/"
+        
+		assertFalse "${templatesDirectory} exists, but should not", new File(templatesDirectory).exists()
+
+		Gant.main(["-f", "scripts/InstallTemplates.groovy"] as String[])
+
+		assertTrue "${templatesDirectory} does not exist", new File(templatesDirectory).exists()
+		
+		// expected templates to be installed
+		def templates = [
+		  "/artifacts/BootStrap.groovy",
+		  "/artifacts/Controller.groovy",
+		  "/artifacts/DataSource.groovy",
+		  "/artifacts/DomainClass.groovy",
+		  "/artifacts/Job.groovy",
+		  "/artifacts/Service.groovy",
+		  "/artifacts/TagLib.groovy",
+		  "/artifacts/Tests.groovy",
+		  "/artifacts/WebTest.groovy",
+		  "/scaffolding/Controller.groovy",
+		  "/scaffolding/create.gsp",
+		  "/scaffolding/edit.gsp",
+		  "/scaffolding/list.gsp",
+		  "/scaffolding/show.gsp"
+		]
+		
+		for (t in templates) {
+      assertTrue "${templatesDirectory}${t} does not exist", new File(templatesDirectory + t).exists()
+    } 
+		
+		
+	}
+
+}
