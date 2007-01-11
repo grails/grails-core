fedf1fe94c016f4ca53c56d218dd6b4d016cbcf2
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-01-11 09:48:56 +0000

    patch from marcel for GRAILS-507
    git-svn-id: https://svn.codehaus.org/grails/trunk@2416 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ scripts/InstallTemplates.groovy
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Gant script that installs artifact and scaffolding templates
+ * 
+ * @author Marcel Overdijk
+ *
+ * @since 0.4
+ */
+
+import org.codehaus.groovy.grails.commons.GrailsClassUtils as GCU
+
+Ant.property(environment:"env")                             
+grailsHome = Ant.antProject.properties."env.GRAILS_HOME"    
+
+includeTargets << new File ( "${grailsHome}/scripts/Init.groovy" )  
+
+task ('default': "Installs the artifact and scaffolding templates") {
+
+	targetDir = "${basedir}/templates"
+	overwrite = false
+
+	// only if template dir already exists in, ask to overwrite templates
+	if (new File(targetDir).exists()) {
+		Ant.input(addProperty: "${args}.template.overwrite", message: "Overwrite existing templates? [y/n]")
+		if (Ant.antProject.properties."${args}.template.overwrite" == "y")
+			overwrite = true
+	}
+	else {
+		Ant.mkdir(dir: targetDir)
+	}
+
+	Ant.copy(todir: targetDir, overwrite: overwrite) {
+		// copy artifact and scaffolding templates
+		fileset(dir: "${grailsHome}/src/grails/templates", includes: "artifacts/*, scaffolding/*")
+	}
+	
+	println "Templates installed successfully"   
+}
