2cec231eff7dcd0cf3724af37c7e5094d576b860
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-01-12 12:14:11 +0000

    moved to test dir
    git-svn-id: https://svn.codehaus.org/grails/trunk@2457 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ test/persistence/org/codehaus/groovy/grails/domain/AddRelatedDynamicMethodTests.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.grails.domain;
+
+import groovy.lang.GroovyClassLoader;
+import groovy.lang.GroovyObject;
+import groovy.lang.MissingMethodException;
+
+import java.util.Set;
+import java.util.SortedSet;
+
+import junit.framework.TestCase;
+
+import org.codehaus.groovy.grails.commons.DefaultGrailsApplication;
+import org.codehaus.groovy.grails.commons.GrailsApplication;
+import org.codehaus.groovy.grails.commons.GrailsDomainClass;
+import org.codehaus.groovy.grails.metaclass.AbstractAddRelatedDynamicMethod;
+import org.codehaus.groovy.grails.metaclass.AddRelatedDynamicMethod;
+
+/**
+ * Tests the relationship management method
+ *
+ * @author Graeme Rocher
+ *         <p/>
+ *         Date: Sep 19, 2006
+ *         Time: 11:07:14 PM
+ */
+public class AddRelatedDynamicMethodTests extends TestCase {
+
+    public void testInvoke() throws Exception {
+       GroovyClassLoader gcl = new GroovyClassLoader();
+       gcl.parseClass("class Book {\n" +
+                                    "\tLong id\n" +
+                                    "\tLong version\n" +
+                                    "\n" +
+                                    "\tAuthor author\n" +
+                                    "}\n" +
+                                    "class Author {\n" +
+                                    "\tLong id\n" +
+                                    "\tLong version\n" +
+                                    "\tdef relatesToMany = [books:Book]\n" +
+                                    "\tSet books\n" +
+                                    "}");
+
+
+        GroovyObject book = (GroovyObject)gcl.loadClass("Book",false,true).newInstance();
+        GroovyObject author = (GroovyObject)gcl.loadClass("Author",false,true).newInstance();
+
+        GrailsApplication ga = new DefaultGrailsApplication(new Class[]{book.getClass(),author.getClass()},gcl);
+        GrailsDomainClass authorDC = ga.getGrailsDomainClass("Author");
+
+        AbstractAddRelatedDynamicMethod ardm = new AddRelatedDynamicMethod(authorDC.getPropertyByName("books"));
+
+        try {
+            ardm.invoke(author,new Object[0]);
+            fail("Should have thrown missing method exception!");
+        } catch (MissingMethodException e) {
+            // expected
+        }
+
+        try {
+            ardm.invoke(author,new Object[]{"blah"});
+            fail("Should have thrown missing method exception!");
+        } catch (MissingMethodException e) {
+            // expected
+        }
+
+        ardm.invoke(author, new Object[]{book});
+
+        assertEquals(author,book.getProperty("author"));
+        Set books = (Set)author.getProperty("books");
+        assertNotNull(books);
+        assertEquals(1,books.size());
+        assertTrue(books.contains(book));
+    }
+
+    public void testInvokeWithSortedSet() throws Exception {
+        GroovyClassLoader gcl = new GroovyClassLoader();
+        gcl.parseClass("class Book implements Comparable {\n" +
+                                     "\tLong id\n" +
+                                     "\tLong version\n" +
+                                     "\n" +
+                                     "\tAuthor author\n" +
+                                     "int compareTo(other) { 1 }" +
+                                     "}\n" +
+                                     "class Author {\n" +
+                                     "\tLong id\n" +
+                                     "\tLong version\n" +
+                                     "\tdef relatesToMany = [books:Book]\n" +
+                                     "\tSortedSet books\n" +
+                                     "}");
+
+
+         GroovyObject book = (GroovyObject)gcl.loadClass("Book",false,true).newInstance();
+         GroovyObject author = (GroovyObject)gcl.loadClass("Author",false,true).newInstance();
+
+         GrailsApplication ga = new DefaultGrailsApplication(new Class[]{book.getClass(),author.getClass()},gcl);
+         GrailsDomainClass authorDC = ga.getGrailsDomainClass("Author");
+
+         AbstractAddRelatedDynamicMethod ardm = new AddRelatedDynamicMethod(authorDC.getPropertyByName("books"));
+
+         try {
+             ardm.invoke(author,new Object[0]);
+             fail("Should have thrown missing method exception!");
+         } catch (MissingMethodException e) {
+             // expected
+         }
+
+         try {
+             ardm.invoke(author,new Object[]{"blah"});
+             fail("Should have thrown missing method exception!");
+         } catch (MissingMethodException e) {
+             // expected
+         }
+
+         ardm.invoke(author, new Object[]{book});
+
+         assertEquals(author,book.getProperty("author"));
+         SortedSet books = (SortedSet)author.getProperty("books");
+         assertNotNull(books);
+
+    }
+}
