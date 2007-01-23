1a9a62693eb4b3dc83582c6915ce0fb300a4f031
brownj <brownj@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-01-23 00:38:03 +0000

    uncomment test methods and make changes to assertions
    git-svn-id: https://svn.codehaus.org/grails/trunk@2681 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/groovy/org/codehaus/groovy/grails/web/taglib/ApplicationTagLibTests.groovy
+++ test/groovy/org/codehaus/groovy/grails/web/taglib/ApplicationTagLibTests.groovy
@@ -6,29 +6,20 @@ import org.codehaus.groovy.grails.commons.metaclass.ExpandoMetaClass
 class ApplicationTagLibTests extends AbstractGrailsTagTests {
 
 	void testCreateLinkURLEncoding() {
-    /*
-
 		StringWriter sw = new StringWriter();
 
 		withTag("createLink", sw) { tag ->
 
-		    println grailsApplication.allClasses.inspect()
 			// test URL encoding. Params unordered to have to try one test at a time
 			def attrs = [action:'testAction', controller: 'testController',
 			    params:['name':'Marc Palmer']]
 			tag.call( attrs )
 
-			println "output: ${sw}"
-
-			assertEquals '<a href="/testController/testAction?name=Marc%20Palmer"></a>"', sw.toString()
-		}*/
-
+			assertEquals '/testController/testAction?name=Marc+Palmer', sw.toString()
+		}
 	}
 
 	void testCreateLinkURLEncodingWithHTMLChars() {
-/*
-        InvokerHelper.getInstance().getMetaRegistry().removeMetaClass(String.class)
-
 		StringWriter sw = new StringWriter();
 
 		withTag("createLink", sw) { tag ->
@@ -38,12 +29,8 @@ class ApplicationTagLibTests extends AbstractGrailsTagTests {
 			    params:['email':'<marc@anyware.co.uk>']]
 			tag.call( attrs )
 
-			println "output: ${sw}"
-
-			assertEquals '<a href="/testController/testAction?email=%3Cmarc%40anyware%2Eco%2Euk%3E"></a>"', sw.toString()
+			assertEquals '/testController/testAction?email=%3Cmarc%40anyware.co.uk%3E', sw.toString()
 		}
-
-*/
 	}
 
 }
