d314f92196f797057fd2d2724b592f8174731506
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-01-30 22:38:39 +0000

    a more concise script template
    git-svn-id: https://svn.codehaus.org/grails/trunk@2968 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/grails/templates/artifacts/Script.groovy
+++ src/grails/templates/artifacts/Script.groovy
@@ -1,7 +1,6 @@
 import org.codehaus.groovy.grails.commons.GrailsClassUtils as GCU
 
-Ant.property(environment:"env")                             
-grailsHome = Ant.antProject.properties."env.GRAILS_HOME"    
+grailsHome = Ant.project.properties."environment.GRAILS_HOME"
 
 includeTargets << new File ( "${grailsHome}/scripts/Init.groovy" )  
 
