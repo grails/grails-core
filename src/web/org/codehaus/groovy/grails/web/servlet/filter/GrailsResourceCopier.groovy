69b1cc54b3722859d2b4f46c61e4d032560bc6bb
marceloverdijk <marceloverdijk@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-02-06 21:20:59 +0000

    GRAILS-721: applied native2ascii on message bundle when reloading
    git-svn-id: https://svn.codehaus.org/grails/trunk@3059 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/web/org/codehaus/groovy/grails/web/servlet/filter/GrailsResourceCopier.groovy
+++ src/web/org/codehaus/groovy/grails/web/servlet/filter/GrailsResourceCopier.groovy
@@ -47,9 +47,10 @@ class GrailsResourceCopier implements ResourceCopier {
     public void copyResourceBundles() {
 	    def ant = new AntBuilder()
         if(new File("${basedir}/grails-app/i18n").exists()) {
-            ant.copy(todir:"${destdir}/WEB-INF/grails-app/i18n",failonerror:false) {
-                fileset(dir:"${basedir}/grails-app/i18n",includes:"**")
-            }
+        	ant.native2ascii(src:"${basedir}/grails-app/i18n",
+					 dest:"${destdir}/WEB-INF/grails-app/i18n",
+					 includes:"**",
+					 encoding:"UTF-8")
         }
     }
  
