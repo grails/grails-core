a0aa9283b85edf80e19b50bbb646635ba0aa0f8c
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-02-09 09:49:17 +0000

    changed to simply delegate to GrailsUtils
    git-svn-id: https://svn.codehaus.org/grails/trunk@3088 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/groovy/org/codehaus/groovy/grails/plugins/support/GrailsPluginUtils.groovy
+++ src/groovy/org/codehaus/groovy/grails/plugins/support/GrailsPluginUtils.groovy
@@ -6,16 +6,7 @@ class GrailsPluginUtils {
 
 	static grailsVersion = null
 	static getGrailsVersion() {
-		if(grailsVersion)return grailsVersion
-
-		grailsVersion = GrailsUtil.getGrailsVersion()
-		if(!grailsVersion)return 0.1
-		if(grailsVersion.endsWith("-SNAPSHOT"))
-			grailsVersion = grailsVersion[0..-10].toBigDecimal()
-		else {
-			grailsVersion.toBigDecimal()
-		}
-		return grailsVersion
+		return GrailsUtil.getGrailsVersion()
 	}
 
 }
