3293f77783601c1826727927b67318a16e5456a4
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-02-28 15:35:58 +0000

    test for GRAILS-849
    git-svn-id: https://svn.codehaus.org/grails/trunk@3277 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ test/groovy/org/codehaus/groovy/grails/web/codecs/Base64CodecTests.groovy
@@ -0,0 +1,38 @@
+package org.codehaus.groovy.grails.web.codecs
+
+import org.springframework.core.io.*
+
+class Base64CodecTests extends GroovyTestCase{
+    def GroovyObject codec
+    def resourceLoader = new DefaultResourceLoader()
+    
+	void setUp() {
+        GroovyClassLoader gcl = new GroovyClassLoader();
+        Class clazz = gcl.parseClass(resourceLoader.getResource('file:./src/grails/grails-app/utils/Base64Codec.groovy').inputStream);
+        codec = (GroovyObject)clazz.newInstance()
+    }
+	void testEncode() {
+        // this test was taken from Dierk Konig's Groovy in action book
+        byte[] data = new byte[256]
+        for (i in 0..255) {data[i] = i}
+        String result = codec.encode(data)
+
+        assertTrue(result.startsWith('AAECAwQFBg'))
+        assertTrue(result.endsWith('r7/P3+/w=='))
+        
+        //make sure encoding null returns null
+        assertEquals(codec.encode(null), null)
+	}
+	void testDecode() {
+        String data = 'd2hhdA=='
+        byte[] result = codec.decode(data)
+        
+        assertEquals(119, result[0])
+        assertEquals(104, result[1])
+        assertEquals(97, result[2])
+        assertEquals(116, result[3])
+        
+        //make sure decoding null returns null
+        assertEquals(codec.decode(null), null)
+	}
+}
