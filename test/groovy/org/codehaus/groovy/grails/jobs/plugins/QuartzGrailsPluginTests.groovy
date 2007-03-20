bc75f0b149c96041cd3e663e884044065c158dc6
marcpalmer <marcpalmer@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-03-20 12:23:31 +0000

    Stopped jobs running throughout tests. GRAILS-945
    git-svn-id: https://svn.codehaus.org/grails/trunk@3469 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/groovy/org/codehaus/groovy/grails/jobs/plugins/QuartzGrailsPluginTests.groovy
+++ test/groovy/org/codehaus/groovy/grails/jobs/plugins/QuartzGrailsPluginTests.groovy
@@ -41,7 +41,7 @@ class CronDefaultJob  {
 }
 """)
 	}
-	
+
 	void testJobsPlugin() {
 		def pluginClass = gcl.loadClass("org.codehaus.groovy.grails.jobs.plugins.QuartzGrailsPlugin")
 		
@@ -49,39 +49,45 @@ class CronDefaultJob  {
 		
 		def springConfig = new DefaultRuntimeSpringConfiguration(ctx)
 		springConfig.servletContext = createMockServletContext()
-		
-		plugin.doWithRuntimeConfiguration(springConfig)
-		
-		def appCtx = springConfig.getApplicationContext()
-		
-		assert appCtx.containsBean("SimpleJob")
-		assert appCtx.containsBean("SimpleJobJobDetail")
-		assert appCtx.containsBean("SimpleJobTrigger")
-		assert appCtx.containsBean("SimpleDefaultJob")
-		assert appCtx.containsBean("SimpleDefaultJobJobDetail")
-		assert appCtx.containsBean("SimpleDefaultJobTrigger")
-		assert appCtx.containsBean("CronJob")
-		assert appCtx.containsBean("CronJobJobDetail")
-		assert appCtx.containsBean("CronJobTrigger")
-		assert appCtx.containsBean("CronDefaultJob")
-		assert appCtx.containsBean("CronDefaultJobJobDetail")
-		assert appCtx.containsBean("CronDefaultJobTrigger")
-		
-		// test if properties of jobs are set as expected
-		// note that the Spring api does not have getters for startDelay, group and concurrent properties
-		// so these cannot be tested
-		
-		SimpleTriggerBean simpleJobTrigger = (SimpleTriggerBean)appCtx.getBean("SimpleJobTrigger")
-		assertEquals(1000, simpleJobTrigger.getRepeatInterval())
-		
-		SimpleTriggerBean simpleDefaultJobTrigger = (SimpleTriggerBean)appCtx.getBean("SimpleDefaultJobTrigger")
-		assertEquals(DefaultGrailsTaskClass.DEFAULT_TIMEOUT, simpleDefaultJobTrigger.getRepeatInterval())
-		
-		CronTriggerBean cronJobTrigger = (CronTriggerBean)appCtx.getBean("CronJobTrigger")
-		assertEquals("0 15 10 ? * 6#3", cronJobTrigger.getCronExpression())
-		
-		CronTriggerBean cronDefaultJobTrigger = (CronTriggerBean)appCtx.getBean("CronDefaultJobTrigger")
-		assertEquals(DefaultGrailsTaskClass.DEFAULT_CRON_EXPRESSION, cronDefaultJobTrigger.getCronExpression())
+
+        def appCtx
+
+		try {
+            plugin.doWithRuntimeConfiguration(springConfig)
+
+            appCtx = springConfig.getApplicationContext()
+
+            assert appCtx.containsBean("SimpleJob")
+            assert appCtx.containsBean("SimpleJobJobDetail")
+            assert appCtx.containsBean("SimpleJobTrigger")
+            assert appCtx.containsBean("SimpleDefaultJob")
+            assert appCtx.containsBean("SimpleDefaultJobJobDetail")
+            assert appCtx.containsBean("SimpleDefaultJobTrigger")
+            assert appCtx.containsBean("CronJob")
+            assert appCtx.containsBean("CronJobJobDetail")
+            assert appCtx.containsBean("CronJobTrigger")
+            assert appCtx.containsBean("CronDefaultJob")
+            assert appCtx.containsBean("CronDefaultJobJobDetail")
+            assert appCtx.containsBean("CronDefaultJobTrigger")
+
+            // test if properties of jobs are set as expected
+            // note that the Spring api does not have getters for startDelay, group and concurrent properties
+            // so these cannot be tested
+
+            SimpleTriggerBean simpleJobTrigger = (SimpleTriggerBean)appCtx.getBean("SimpleJobTrigger")
+            assertEquals(1000, simpleJobTrigger.getRepeatInterval())
+
+            SimpleTriggerBean simpleDefaultJobTrigger = (SimpleTriggerBean)appCtx.getBean("SimpleDefaultJobTrigger")
+            assertEquals(DefaultGrailsTaskClass.DEFAULT_TIMEOUT, simpleDefaultJobTrigger.getRepeatInterval())
+
+            CronTriggerBean cronJobTrigger = (CronTriggerBean)appCtx.getBean("CronJobTrigger")
+            assertEquals("0 15 10 ? * 6#3", cronJobTrigger.getCronExpression())
+
+            CronTriggerBean cronDefaultJobTrigger = (CronTriggerBean)appCtx.getBean("CronDefaultJobTrigger")
+            assertEquals(DefaultGrailsTaskClass.DEFAULT_CRON_EXPRESSION, cronDefaultJobTrigger.getCronExpression())
+        } finally {
+    	    appCtx?.getBean('quartzScheduler').shutdown()
+        }
 	}
 	
 	void testIntegerAndLongParameters() {
