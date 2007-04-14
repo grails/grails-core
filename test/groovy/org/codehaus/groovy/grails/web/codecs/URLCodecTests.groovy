530699735a3ab31b1244b886c75dd079fcba2fa8
brownj <brownj@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-04-14 17:47:57 +0000

    remove java-isms in favor of groovy-isms... static types, casts, semicolons, double quotes
    git-svn-id: https://svn.codehaus.org/grails/trunk@3786 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/groovy/org/codehaus/groovy/grails/web/codecs/URLCodecTests.groovy
+++ test/groovy/org/codehaus/groovy/grails/web/codecs/URLCodecTests.groovy
@@ -6,15 +6,15 @@ import org.springframework.web.context.request.*
 import org.codehaus.groovy.grails.web.servlet.mvc.*
 
 class URLCodecTests extends GroovyTestCase {
-    def GroovyObject codec
+    def codec
     def resourceLoader = new DefaultResourceLoader()
 
 	void setUp() {
-        GroovyClassLoader gcl = new GroovyClassLoader();
-        Class clazz = gcl.parseClass(resourceLoader.getResource('file:./src/grails/grails-app/utils/URLCodec.groovy').inputStream);
+        def gcl = new GroovyClassLoader()
+        def clazz = gcl.parseClass(resourceLoader.getResource('file:./src/grails/grails-app/utils/URLCodec.groovy').inputStream)
         def webRequest = new GrailsWebRequest(new MockHttpServletRequest(), new MockHttpServletResponse(), new MockServletContext())
         RequestContextHolder.setRequestAttributes(webRequest)
-        codec = (GroovyObject)clazz.newInstance()
+        codec = clazz.newInstance()
     }
 
     void tearDown() {
@@ -23,14 +23,14 @@ class URLCodecTests extends GroovyTestCase {
 
 	void testEncode() {
 	    println codec.encode('The @string \"foo-bar\"')
-	    assertEquals("My+test+string",codec.encode('My test string'))
+	    assertEquals('My+test+string',codec.encode('My test string'))
 	    // Some unsafe characters
-	    assertEquals("The+%40string+%22foo-bar%22",codec.encode('The @string \"foo-bar\"'))
+	    assertEquals('The+%40string+%22foo-bar%22',codec.encode('The @string \"foo-bar\"'))
 	}
 
 	void testDecode() {
-	    assertEquals('My test string',codec.decode("My+test+string"))
+	    assertEquals('My test string',codec.decode('My+test+string'))
 	    // Some unsafe characters
-	    assertEquals('The @string \"foo-bar\"',codec.decode("The+%40string+%22foo-bar%22"))
+	    assertEquals('The @string \"foo-bar\"',codec.decode('The+%40string+%22foo-bar%22'))
 	}
 }
