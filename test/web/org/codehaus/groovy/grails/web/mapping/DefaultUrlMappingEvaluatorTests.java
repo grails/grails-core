aa87676fd9621a66e18362a8b05257dc2dd4dc58
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-04-23 10:59:49 +0000

    a test i forgot to commit earlier
    git-svn-id: https://svn.codehaus.org/grails/trunk@3936 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ test/web/org/codehaus/groovy/grails/web/mapping/DefaultUrlMappingEvaluatorTests.java
@@ -0,0 +1,97 @@
+package org.codehaus.groovy.grails.web.mapping;
+
+import grails.util.GrailsWebUtil;
+import groovy.lang.Binding;
+import groovy.lang.Closure;
+import groovy.lang.GroovyShell;
+import groovy.lang.Script;
+import junit.framework.TestCase;
+import org.codehaus.groovy.grails.validation.ConstrainedProperty;
+import org.codehaus.groovy.grails.validation.Constraint;
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest;
+
+import java.util.List;
+
+public class DefaultUrlMappingEvaluatorTests extends TestCase {
+	
+	public void testNewMethod () throws Exception {
+		GroovyShell shell = new GroovyShell ();
+		Binding binding = new Binding();
+		Script script = (Script) shell.parse (
+				"mappings = {\n" + 
+				"	\"/$controller/$action?/$id?\" { \n" + 
+				"		constraints {\n" + 
+				"			id(matches:/\\d+/)\n" + 
+				"		}\n" + 
+				"	}\n" + 
+				"}\n");
+
+		script.setBinding(binding);
+		script.run();
+		
+		Closure closure = (Closure) binding.getVariable("mappings");
+		DefaultUrlMappingEvaluator evaluator = new DefaultUrlMappingEvaluator ();
+		List mappings = evaluator.evaluateMappings(closure);
+
+		assertEquals(1, mappings.size());
+		
+		UrlMapping mapping = (UrlMapping) mappings.get(0);
+		
+		assertNull(mapping.getActionName());
+		assertNull(mapping.getControllerName());
+		assertEquals("(*)",mapping.getUrlData().getTokens()[0]);
+		assertEquals("(*)",mapping.getUrlData().getTokens()[1]);
+		assertEquals("(*)",mapping.getUrlData().getTokens()[2]);
+		
+		assertNotNull(mapping.getConstraints());
+		
+		assertTrue(makeSureMatchesConstraintExistsOnId(mapping));
+		
+		GrailsWebRequest r = GrailsWebUtil.bindMockWebRequest();
+		
+		assertEquals("mycontroller",mapping.match("/mycontroller").getControllerName());
+		assertNull(mapping.match("/mycontroller").getActionName());
+		assertNull(mapping.match("/mycontroller").getId());
+		
+		assertEquals("test",mapping.match("/mycontroller/test").getActionName());
+		assertNull(mapping.match("/mycontroller/test").getId());
+		assertEquals("234", mapping.match("/blog/test/234").getId());
+	}
+
+	public void testOldMethod () throws Exception {
+		GroovyShell shell = new GroovyShell ();
+		Script script = (Script) shell.parse (
+				"mappings {\n" + 
+				"	\"/$controller/$action?/$id?\" { \n" + 
+				"		constraints {\n" + 
+				"			id(matches:/\\d+/)\n" + 
+				"		}\n" + 
+				"	}\n" + 
+				"}\n");
+
+		DefaultUrlMappingEvaluator evaluator = new DefaultUrlMappingEvaluator ();
+		List mappings = evaluator.evaluateMappings(script.getClass());
+		assertEquals(1, mappings.size());
+		assertNull(((UrlMapping) mappings.get(0)).getActionName());
+		assertNull(((UrlMapping) mappings.get(0)).getControllerName());
+		assertEquals("(*)",((UrlMapping) mappings.get(0)).getUrlData().getTokens()[0]);
+		assertEquals("(*)",((UrlMapping) mappings.get(0)).getUrlData().getTokens()[1]);
+		assertEquals("(*)",((UrlMapping) mappings.get(0)).getUrlData().getTokens()[2]);
+	}
+
+	private boolean makeSureMatchesConstraintExistsOnId(UrlMapping mapping) {
+		ConstrainedProperty [] props = mapping.getConstraints();
+		for (int i = 0; i < props.length; i++) {
+			if ("id".equals(props[i].getPropertyName())) {
+				Constraint [] constraints = (Constraint[]) props[i].getAppliedConstraints().toArray(new Constraint[0]);
+				for (int j = 0; j < constraints.length; j++) {
+					if (constraints[j].getClass().getName().endsWith("MatchesConstraint")) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+}
