442638133f6c297ceed54066cb78745f37fe6843
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-05-21 10:44:17 +0000

    added doWIthDynamicMethods closure to template
    git-svn-id: https://svn.codehaus.org/grails/trunk@4240 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- scripts/CreatePlugin.groovy
+++ scripts/CreatePlugin.groovy
@@ -50,9 +50,12 @@ class ${pluginName}GrailsPlugin {
 	def doWithApplicationContext = { applicationContext ->
 		// TODO Implement post initialization spring config (optional)		
 	}
-	def doWithWebDescriptor = {
+	def doWithWebDescriptor = { xml ->
 		// TODO Implement additions to web.xml (optional)
 	}	                                      
+	def doWithDynamicMethods = { ctx ->
+		// TODO Implement additions to web.xml (optional)
+	}	
 	def onChange = { event ->
 		// TODO Implement code that is executed when this class plugin class is changed  
 		// the event contains: event.application and event.applicationContext objects
