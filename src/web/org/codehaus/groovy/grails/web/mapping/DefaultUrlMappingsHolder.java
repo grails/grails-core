d053e3e37b54c031423982dcc97681ad12da03b2
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-05-30 10:01:56 +0000

    changed return type based on interface changes
    git-svn-id: https://svn.codehaus.org/grails/trunk@4349 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/web/org/codehaus/groovy/grails/web/mapping/DefaultUrlMappingsHolder.java
+++ src/web/org/codehaus/groovy/grails/web/mapping/DefaultUrlMappingsHolder.java
@@ -102,7 +102,7 @@ public class DefaultUrlMappingsHolder implements UrlMappingsHolder {
         return this.mappings;
     }
 
-    public UrlMapping getReverseMapping(final String controller, final String action, Map params) {
+    public UrlCreator getReverseMapping(final String controller, final String action, Map params) {
         if(params == null) params = Collections.EMPTY_MAP;
 
         UrlMapping mapping = (UrlMapping)mappingsLookup.get(new UrlMappingKey(controller, action, params.keySet()));
