f49abd0f05d6d1181846a0628f9f23d0bb5cd82a
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-05-31 11:11:49 +0000

    fix for GRAILS-1166
    git-svn-id: https://svn.codehaus.org/grails/trunk@4384 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- bin/startGrails.bat
+++ bin/startGrails.bat
@@ -103,7 +103,7 @@ set CMD_LINE_ARGS=%$
 
 :execute
 @rem Setup the command line
-set STARTER_CLASSPATH=%GROOVY_HOME%\lib\groovy-starter.jar
+set STARTER_CLASSPATH=%GRAILS_HOME%\lib\groovy-starter.jar
 
 if exist "%USERPROFILE%/.groovy/init.bat" call "%USERPROFILE%/.groovy/init.bat"
 
