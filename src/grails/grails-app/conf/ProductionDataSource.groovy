f2c8f2df343b419bef638b35141a90d6e204bb6a
leebutts <leebutts@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-06-18 23:28:37 +0000

    GRAILS-775
    git-svn-id: https://svn.codehaus.org/grails/trunk@4690 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/grails/grails-app/conf/ProductionDataSource.groovy
+++ src/grails/grails-app/conf/ProductionDataSource.groovy
@@ -1,7 +1,7 @@
 class ProductionDataSource {
    boolean pooling = true
    String dbCreate = "update" // one of 'create', 'create-drop','update'
-   String url = "jdbc:hsqldb:file:prodDb"
+   String url = "jdbc:hsqldb:file:prodDb;shutdown=true"
    String driverClassName = "org.hsqldb.jdbcDriver"
    String username = "sa"
    String password = ""
