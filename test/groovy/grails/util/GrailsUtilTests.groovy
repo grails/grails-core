4b81707dc25da56d971e19e456b8d8f42e9a6f59
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-06-20 01:15:35 +0000

    updated build for new src/config source folder
    git-svn-id: https://svn.codehaus.org/grails/trunk@4708 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/groovy/grails/util/GrailsUtilTests.groovy
+++ test/groovy/grails/util/GrailsUtilTests.groovy
@@ -8,7 +8,7 @@ class GrailsUtilTests extends GroovyTestCase {
     void testGrailsVersion() {
 
         
-        assertEquals "0.5.6", grails.util.GrailsUtil.getGrailsVersion()
+        assertEquals "0.6-SNAPSHOT", grails.util.GrailsUtil.getGrailsVersion()
     }
 
 }
