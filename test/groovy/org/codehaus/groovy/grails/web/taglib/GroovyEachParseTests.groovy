0934ee1f0c0b820909bdd7e8ea9045bd7e633f77
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-07-23 13:24:44 +0000

    fix for failing test
    git-svn-id: https://svn.codehaus.org/grails/trunk@4948 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/groovy/org/codehaus/groovy/grails/web/taglib/GroovyEachParseTests.groovy
+++ test/groovy/org/codehaus/groovy/grails/web/taglib/GroovyEachParseTests.groovy
@@ -3,7 +3,7 @@ package org.codehaus.groovy.grails.web.taglib;
 import org.codehaus.groovy.grails.web.pages.*;
 
 class GroovyEachParseTests extends ParseTests {
-
+                              
 	void testEachOutput() {
 		String output = parseCode("myTest", """
 <g:each var="t" in="${'blah'}">
@@ -16,37 +16,41 @@ import org.codehaus.groovy.grails.web.taglib.*
 
 class myTest extends GroovyPage {
 public Object run() {
-out.print('\\n')
 "blah".each { t ->
-out.print('\\n')
-}
-out.print('\\n')
 }
 }
-"""),trimAndRemoveCR(output) )
+}"""),trimAndRemoveCR(output) )
 
-				
+				               
 	}
 		
 		void testEachOutputNoVar() {
 			String output = parseCode("myTest2", """
 	<g:each in="${'blah'}">
 	</g:each>
-	""");	
+	""");	             
 		
 			assertEquals(trimAndRemoveCR("""import org.codehaus.groovy.grails.web.pages.GroovyPage
 import org.codehaus.groovy.grails.web.taglib.*
 
 class myTest2 extends GroovyPage {
 public Object run() {
-out.print('\\n\\t')
+out.print(STATIC_HTML_CONTENT_0)
 "blah".each { 
-out.print('\\n\\t')
+out.print(STATIC_HTML_CONTENT_1)
 }
-out.print('\\n\\t')
+out.print(STATIC_HTML_CONTENT_2)
 }
-}
-"""),trimAndRemoveCR(output) )
+static final STATIC_HTML_CONTENT_1 = '''\\t
+'''
+
+static final STATIC_HTML_CONTENT_2 = '''\\t
+'''
+
+static final STATIC_HTML_CONTENT_0 = '''\\t
+'''
+
+}"""),trimAndRemoveCR(output) )
 					
 		}	
 		
@@ -61,14 +65,22 @@ import org.codehaus.groovy.grails.web.taglib.*
 
 class myTest2 extends GroovyPage {
 public Object run() {
-out.print('\\n\\t')
+out.print(STATIC_HTML_CONTENT_0)
 "blah".eachWithIndex { t,i ->
-out.print('\\n\\t')
+out.print(STATIC_HTML_CONTENT_1)
 }
-out.print('\\n\\t')
+out.print(STATIC_HTML_CONTENT_2)
 }
-}
-"""),trimAndRemoveCR(output) )
+static final STATIC_HTML_CONTENT_1 = '''\\t
+'''
+
+static final STATIC_HTML_CONTENT_2 = '''\\t
+'''
+
+static final STATIC_HTML_CONTENT_0 = '''\\t
+'''
+
+}"""),trimAndRemoveCR(output) )
 		}
 
 }
