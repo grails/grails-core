3c4fa3bffd135f508c4b20c24ab54627cf716ed8
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-08-02 09:12:39 +0000

    fix for failing test.
    git-svn-id: https://svn.codehaus.org/grails/trunk@5016 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/web/org/codehaus/groovy/grails/web/pages/ParseTests.java
+++ test/web/org/codehaus/groovy/grails/web/pages/ParseTests.java
@@ -101,8 +101,7 @@ public class ParseTests extends TestCase {
                      "invokeTag('form','tt',[:],body1)\n" +
                      "out.print(STATIC_HTML_CONTENT_1)\n" +
                      "}\n" +
-                     "static final STATIC_HTML_CONTENT_1 = '''</tbody>\n" +
-                     "'''\n" +
+                     "static final STATIC_HTML_CONTENT_1 = '''</tbody>'''\n" +
                      "\n" +
                      "static final STATIC_HTML_CONTENT_0 = '''<tbody>'''\n" +
                      "\n" +
