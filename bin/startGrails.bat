30915bd7fccb53b663f152623314fc5297a89102
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-08-10 19:14:35 +0000

    added base dir argument
    git-svn-id: https://svn.codehaus.org/grails/trunk@5166 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- bin/startGrails.bat
+++ bin/startGrails.bat
@@ -138,6 +138,7 @@ set TOOLS_JAR=%JAVA_HOME%\lib\tools.jar
 if "%JAVA_OPTS%" == "" set JAVA_OPTS="-Xmx128m"
 set JAVA_OPTS=%JAVA_OPTS% -Dprogram.name="%PROGNAME%"
 set JAVA_OPTS=%JAVA_OPTS% -Dgrails.home="%GRAILS_HOME%"
+set JAVA_OPTS=%JAVA_OPTS% -Dbase.dir="."
 set JAVA_OPTS=%JAVA_OPTS% -Dtools.jar="%TOOLS_JAR%"
 set JAVA_OPTS=%JAVA_OPTS% -Dgroovy.starter.conf="%STARTER_CONF%"
 
