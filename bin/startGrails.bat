2d4d9106a16d85fa215ae3f58645e7f2eab83a7e
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-08-10 14:29:17 +0000

    updated windows batch file
    git-svn-id: https://svn.codehaus.org/grails/trunk@5158 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- bin/startGrails.bat
+++ bin/startGrails.bat
@@ -112,7 +112,7 @@ set CMD_LINE_ARGS=%$
 
 :execute
 @rem Setup the command line
-set STARTER_CLASSPATH=%GRAILS_HOME%\lib\groovy-starter.jar
+set STARTER_CLASSPATH=%GRAILS_HOME%\lib\groovy-all-1.1-beta-3-SNAPSHOT.jar
 
 if exist "%USERPROFILE%/.groovy/init.bat" call "%USERPROFILE%/.groovy/init.bat"
 
