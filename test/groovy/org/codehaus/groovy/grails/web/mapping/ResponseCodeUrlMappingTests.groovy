f1423dfeac58a0a463a6fd7c493d1c4323612f97
mike.aizatsky <mike.aizatsky@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-09-04 18:21:02 +0000

    Normalized file & class names to get rid of continuous compilation
    git-svn-id: https://svn.codehaus.org/grails/trunk@5405 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ test/groovy/org/codehaus/groovy/grails/web/mapping/ResponseCodeUrlMappingTests.groovy
@@ -0,0 +1,35 @@
+/**
+ * @author mike
+ */
+package org.codehaus.groovy.grails.web.mapping
+
+import org.codehaus.groovy.grails.web.servlet.mvc.AbstractGrailsControllerTests
+import org.springframework.core.io.ByteArrayResource
+
+class ResponseCodeUrlMappingTests extends AbstractGrailsControllerTests {
+    def topLevelMapping = '''
+mappings {
+    "404"{
+        controller = "sustem"
+        action = "notfound"
+    }
+}
+'''
+    def holder
+
+
+    void setUp() {
+        super.setUp()
+        def res = new ByteArrayResource(topLevelMapping.bytes)
+
+        def evaluator = new DefaultUrlMappingEvaluator()
+        def mappings = evaluator.evaluateMappings(res)
+
+        holder = new DefaultUrlMappingsHolder(mappings)
+    }
+
+
+    void testSimple() {
+        assertNotNull holder
+    }
+}
