2aab05b601756b4e1e275a35a6d725eeb2ba0af0
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-09-20 12:26:45 +0000

    new grails doc command that generates groovydocs and javadocs
    git-svn-id: https://svn.codehaus.org/grails/trunk@5519 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ scripts/Doc.groovy
@@ -0,0 +1,73 @@
+/*
+* Copyright 2004-2005 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
+ * @author Graeme Rocher
+ * @since 1.0
+ * 
+ * Created: Sep 20, 2007
+ */
+Ant.property(environment:"env")
+grailsHome = Ant.antProject.properties."env.GRAILS_HOME"
+
+includeTargets << new File ( "${grailsHome}/scripts/Compile.groovy" )
+
+javadocDir = "${basedir}/docs/api"
+groovydocDir = "${basedir}/docs/gapi"
+docEncoding = "UTF-8"
+docSourceLevel = "1.5"
+links = [
+            'http://java.sun.com/j2se/1.5.0/docs/api/'
+        ]
+
+
+
+
+task ('default': "Produces documentation for a Grails project") {
+    compile()
+    depends(javadoc, groovydoc)
+}
+
+task(setupDoc:"Sets up the doc directories") {
+    Ant.mkdir(dir:"${basedir}/docs")
+    Ant.mkdir(dir:javadocDir)
+    Ant.mkdir(dir:groovydocDir)
+}
+task(groovydoc:"Produces groovydoc documentation") {
+    Ant.taskdef(classpathref:"grails.classpath",name:"groovydoc", classname:"org.codehaus.groovy.ant.Groovydoc")
+    Ant.groovydoc(destdir:groovydocDir, sourcepath:".", use:"true", windowtitle:grailsAppName,'private':"true")
+}
+task(javadoc:"Produces javadoc documentation") {
+   depends(setupDoc)
+   Ant.javadoc( access:"protected",
+                destdir:javadocDir,
+                encoding:docEncoding,
+                classpathref:"grails.classpath",
+                use:"yes",
+                windowtitle:grailsAppName,
+                docencoding:docEncoding,
+                charset:docEncoding,
+                source:docSourceLevel,
+                useexternalfile:"yes",
+                breakiterator:"true",
+                linksource:"yes",
+                maxmemory:"128m") {
+       fileset(dir:"${basedir}/src/java")
+       for(i in links) {
+           link(href:i)
+       }
+   }
+}
