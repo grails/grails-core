e2c6fa292e8f89b7d1f68aecc9804f24ac3ed243
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-09-21 11:54:30 +0000

    updates to default log4j settings to output only errors by default
    git-svn-id: https://svn.codehaus.org/grails/trunk@5535 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/grails/grails-app/conf/Config.groovy
+++ src/grails/grails-app/conf/Config.groovy
@@ -1,3 +1,5 @@
+// enabled native2ascii conversion of i18n properties files
+grails.enable.native2ascii = true
 // log4j configuration
 log4j {
     appender.stdout = "org.apache.log4j.ConsoleAppender"
@@ -5,16 +7,16 @@ log4j {
  	appender.'stdout.layout.ConversionPattern'='[%r] %c{2} %m%n'
     rootLogger="error,stdout"
     logger {
-        grails="info,stdout"
+        grails="error,stdout"
         org {
-            codehaus.groovy.grails.web.servlet="info,stdout"  //  controllers
-			codehaus.groovy.grails.web.pages="info,stdout" //  GSP
-        	codehaus.groovy.grails.web.sitemesh="info,stdout" //  layouts
-        	codehaus.groovy.grails."web.mapping.filter"="info,stdout" // URL mapping
-        	codehaus.groovy.grails."web.mapping"="info,stdout" // URL mapping
+            codehaus.groovy.grails.web.servlet="error,stdout"  //  controllers
+			codehaus.groovy.grails.web.pages="error,stdout" //  GSP
+        	codehaus.groovy.grails.web.sitemesh="error,stdout" //  layouts
+        	codehaus.groovy.grails."web.mapping.filter"="error,stdout" // URL mapping
+        	codehaus.groovy.grails."web.mapping"="error,stdout" // URL mapping
             codehaus.groovy.grails.commons="info,stdout" // core / classloading
-            codehaus.groovy.grails.plugins="info,stdout" // plugins
-            codehaus.groovy.grails.orm.hibernate="info,stdout" // hibernate integration
+            codehaus.groovy.grails.plugins="error,stdout" // plugins
+            codehaus.groovy.grails.orm.hibernate="error,stdout" // hibernate integration
             springframework="off,stdout"
             hibernate="off,stdout"
         }
@@ -29,3 +31,4 @@ log4j {
 		}
     }
 }
+
--- src/grails/templates/artifacts/Config.groovy
+++ src/grails/templates/artifacts/Config.groovy
@@ -7,40 +7,28 @@ log4j {
  	appender.'stdout.layout.ConversionPattern'='[%r] %c{2} %m%n'
     rootLogger="error,stdout"
     logger {
-	        org {
-	            springframework="off,stdout"
-	            hibernate="off,stdout"
-	        }
-     }
- 	additivity.'default' = false
-
-}
-environments {
-	development {
-		log4j {
-    	    logger {
-		        grails="info,stdout"
-		        org {
-		            codehaus.groovy.grails {
-						web.servlet="info,stdout"  //  controllers
-						web.pages="info,stdout" //  GSP
-						web.sitemesh="info,stdout" //  layouts
-						this."web.mapping.filter"="info,stdout" // URL mapping
-						this."web.mapping"="info,stdout" // URL mapping
-						commons="info,stdout" // core / classloading
-						plugins="info,stdout" // plugins
-						orm.hibernate="info,stdout" // hibernate integration
-					}
-		        }
-		    }
-		    additivity {
-				grails=false
-				org {
-		           codehaus.groovy.grails=false
-		           springframework=false
-				   hibernate=false
-				}
-		    }
+        grails="error,stdout"
+        org {
+            codehaus.groovy.grails.web.servlet="error,stdout"  //  controllers
+			codehaus.groovy.grails.web.pages="error,stdout" //  GSP
+        	codehaus.groovy.grails.web.sitemesh="error,stdout" //  layouts
+        	codehaus.groovy.grails."web.mapping.filter"="error,stdout" // URL mapping
+        	codehaus.groovy.grails."web.mapping"="error,stdout" // URL mapping
+            codehaus.groovy.grails.commons="info,stdout" // core / classloading
+            codehaus.groovy.grails.plugins="error,stdout" // plugins
+            codehaus.groovy.grails.orm.hibernate="error,stdout" // hibernate integration
+            springframework="off,stdout"
+            hibernate="off,stdout"
+        }
+    }
+	additivity.'default' = false
+    additivity {
+		grails=false
+		org {
+           codehaus.groovy.grails=false
+           springframework=false
+		   hibernate=false
 		}
-	}
+    }
 }
+
