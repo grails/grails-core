8a97b85075104cab8b2749e90d775d55b49fa4ba
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-11-28 11:54:52 +0000

    fix for GRAILS-1745
    git-svn-id: https://svn.codehaus.org/grails/trunk@6113 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/grails/grails-app/conf/Config.groovy
+++ src/grails/grails-app/conf/Config.groovy
@@ -33,17 +33,41 @@ appender.stdout = org.apache.log4j.ConsoleAppender
 appender.stdout.layout=org.apache.log4j.PatternLayout
 appender.stdout.layout.ConversionPattern=[%r] %c{2} %m%n
 rootLogger=error,stdout
+
+# This logger is for your own application artefact logs
+# Artefacts are logged by their type and optionally class name i.e:
+#
+# log4j.logger.grails.app.controller.HelloController=debug, stdout
+# ...will control the logs from just that controller
+#
+# log4j.logger.grails.app.domain=trace, stdout
+# ...will control the logs for all domain classes
+#
+# At the time of writing, supported artefact type ids include:
+# domain (aka Domain Class), service, dataSource,
+# controller, tagLib, urlMappings, codec, bootstrap
+#
+# The default "info" level for all artefacts is set here
+log4j.logger.grails.app=error
+
+# This logger is for Grails' public APIs within the grails. package
 logger.grails=error
-org.codehaus.groovy.grails.web.servlet=error
-org.codehaus.groovy.grails.web.errors=error
-org.codehaus.groovy.grails.web.pages=error
-org.codehaus.groovy.grails.web.sitemesh=error
-org.codehaus.groovy.grails.web.mapping.filter=error
-org.codehaus.groovy.grails.web.mapping=error
-org.codehaus.groovy.grails.commons=info
-org.codehaus.groovy.grails.plugins=error
-org.codehaus.groovy.grails.orm.hibernate=error
-org.springframework=error
-org.hibernate=error
+
+# This logger is useful if you just want to see what Grails
+# configures with Spring at runtime. Setting to debug will show
+# each bean that is configured
+log4j.logger.org.codehaus.groovy.grails.commons.spring=error
+
+# This logger covers all of Grails' internals
+# Enable to see whats going on underneath.
+log4j.logger.org.codehaus.groovy.grails=error
+
+# Enable this logger to log Hibernate output
+# handy to see its database interaction activity
+log4j.logger.org.hibernate=error
+
+# Enable this logger to see what Spring does, occasionally useful
+#log4j.logger.org.springframework=error
+
 '''
 
