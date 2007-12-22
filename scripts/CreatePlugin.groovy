fd7a2cc2a279db470243d7569fe20938853659b8
marceloverdijk <marceloverdijk@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-12-22 13:36:39 +0000

    Fixed wrong todo comment for doWithDynamicMethods. Also put blank line between hooks.
    git-svn-id: https://svn.codehaus.org/grails/trunk@6394 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- scripts/CreatePlugin.groovy
+++ scripts/CreatePlugin.groovy
@@ -41,29 +41,34 @@ target( createPlugin: "The implementation target")  {
  	new File("${basedir}/${pluginName}GrailsPlugin.groovy") <<
 """
 class ${pluginName}GrailsPlugin {
-	def version = 0.1
-	def dependsOn = [:]
+    def version = 0.1
+    def dependsOn = [:]
 	
-	def doWithSpring = {
-		// TODO Implement runtime spring config (optional)
-	}   
-	def doWithApplicationContext = { applicationContext ->
-		// TODO Implement post initialization spring config (optional)		
-	}
-	def doWithWebDescriptor = { xml ->
-		// TODO Implement additions to web.xml (optional)
-	}	                                      
-	def doWithDynamicMethods = { ctx ->
-		// TODO Implement additions to web.xml (optional)
-	}	
-	def onChange = { event ->
-		// TODO Implement code that is executed when this class plugin class is changed  
-		// the event contains: event.application and event.applicationContext objects
-	}                                                                                  
-	def onApplicationChange = { event ->
-		// TODO Implement code that is executed when any class in a GrailsApplication changes
-		// the event contain: event.source, event.application and event.applicationContext objects
-	}
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+   
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)		
+    }
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional)
+    }
+	                                      
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+	
+    def onChange = { event ->
+        // TODO Implement code that is executed when this class plugin class is changed  
+        // the event contains: event.application and event.applicationContext objects
+    }
+                                                                                  
+    def onApplicationChange = { event ->
+        // TODO Implement code that is executed when any class in a GrailsApplication changes
+        // the event contain: event.source, event.application and event.applicationContext objects
+    }
 }
 """
     new File("${basedir}/scripts/_Install.groovy") <<
