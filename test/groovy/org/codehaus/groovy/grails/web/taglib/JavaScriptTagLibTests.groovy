6846469b3d4a6ae72ce6377a4214a888e9e958d6
pledbrook <pledbrook@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2008-01-01 23:02:18 +0000

    Reformatted JavaScriptTagLibTests for consistency.
    git-svn-id: https://svn.codehaus.org/grails/trunk@6398 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/groovy/org/codehaus/groovy/grails/web/taglib/JavaScriptTagLibTests.groovy
+++ test/groovy/org/codehaus/groovy/grails/web/taglib/JavaScriptTagLibTests.groovy
@@ -1,21 +1,15 @@
 package org.codehaus.groovy.grails.web.taglib;
 
-import groovy.lang.Closure;
-import groovy.lang.GroovyObject;
 
-import org.codehaus.groovy.grails.commons.*
-import org.codehaus.groovy.grails.web.servlet.*;
-import org.springframework.web.util.*;
-
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.*;
+import org.codehaus.groovy.grails.commons.UrlMappingsArtefactHandler
+import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
+import org.springframework.web.util.WebUtils;
 
 
 public class JavaScriptTagLibTests extends AbstractGrailsTagTests {
 
     void onInit() {
-                def urlMappingsClass = gcl.parseClass('''\
+        def urlMappingsClass = gcl.parseClass('''\
 class TestUrlMappings {
     static mappings = {
         "/$controller/$action?/$id?" {}
@@ -32,26 +26,26 @@ class TestUrlMappings {
         assertOutputEquals("new Ajax.Request('/bar/foo',{asynchronous:false,evalScripts:true});", template)
     }
 
-	public void testPrototypeRemoteFunction() throws Exception {
-		StringWriter sw = new StringWriter()
-		PrintWriter pw = new PrintWriter(sw)
+    public void testPrototypeRemoteFunction() throws Exception {
+        StringWriter sw = new StringWriter()
+        PrintWriter pw = new PrintWriter(sw)
 
-		withTag("remoteFunction",pw) { tag ->
-			GroovyObject tagLibrary = (GroovyObject)tag.getOwner()
-			def request = tagLibrary.getProperty("request")
-			def includedLibrary = ['prototype']
-			request.setAttribute("org.codehaus.grails.INCLUDED_JS_LIBRARIES", includedLibrary)
+        withTag("remoteFunction", pw) {tag ->
+            GroovyObject tagLibrary = (GroovyObject) tag.getOwner()
+            def request = tagLibrary.getProperty("request")
+            def includedLibrary = ['prototype']
+            request.setAttribute("org.codehaus.grails.INCLUDED_JS_LIBRARIES", includedLibrary)
 
-			def attrs = [action:'action',controller:'test']
-			tag.call(attrs)
-			assertEquals("new Ajax.Request('/test/action',{asynchronous:true,evalScripts:true});",sw.toString())
+            def attrs = [action: 'action', controller: 'test']
+            tag.call(attrs)
+            assertEquals("new Ajax.Request('/test/action',{asynchronous:true,evalScripts:true});", sw.toString())
 
-			sw.getBuffer().delete(0,sw.getBuffer().length())
-			attrs = [action:'action',controller:'test',update:[success:'updateMe'],options:[insertion:'Insertion.Bottom']]
-			tag.call(attrs)
-			assertEquals("new Ajax.Updater({success:'updateMe'},'/test/action',{asynchronous:true,evalScripts:true,insertion:Insertion.Bottom});",sw.toString())
-		}
-	}
+            sw.getBuffer().delete(0, sw.getBuffer().length())
+            attrs = [action: 'action', controller: 'test', update: [success: 'updateMe'], options: [insertion: 'Insertion.Bottom']]
+            tag.call(attrs)
+            assertEquals("new Ajax.Updater({success:'updateMe'},'/test/action',{asynchronous:true,evalScripts:true,insertion:Insertion.Bottom});", sw.toString())
+        }
+    }
 
 
 
@@ -61,15 +55,15 @@ class TestUrlMappings {
         StringWriter sw = new StringWriter()
         PrintWriter pw = new PrintWriter(sw)
 
-        withTag("remoteField",pw) { tag ->
-            GroovyObject tagLibrary = (GroovyObject)tag.getOwner()
+        withTag("remoteField", pw) {tag ->
+            GroovyObject tagLibrary = (GroovyObject) tag.getOwner()
             def request = tagLibrary.getProperty("request")
             def includedLibrary = ['prototype']
             request.setAttribute("org.codehaus.grails.INCLUDED_JS_LIBRARIES", includedLibrary)
 
-            def attrs = [controller:'test',action:'changeTitle',update:'titleDiv',name:'title',value:'testValue']
-            tag.call(attrs) { "body" }
-            assertEquals("<input type=\"text\" name=\"title\" value=\"testValue\" onkeyup=\"new Ajax.Updater('titleDiv','/test/changeTitle',{asynchronous:true,evalScripts:true,parameters:'value='+this.value});\" />",sw.toString())
+            def attrs = [controller: 'test', action: 'changeTitle', update: 'titleDiv', name: 'title', value: 'testValue']
+            tag.call(attrs) {"body"}
+            assertEquals("<input type=\"text\" name=\"title\" value=\"testValue\" onkeyup=\"new Ajax.Updater('titleDiv','/test/changeTitle',{asynchronous:true,evalScripts:true,parameters:'value='+this.value});\" />", sw.toString())
         }
 
     }
@@ -81,140 +75,140 @@ class TestUrlMappings {
         StringWriter sw = new StringWriter()
         PrintWriter pw = new PrintWriter(sw)
 
-        withTag("remoteLink",pw) { tag ->
-            GroovyObject tagLibrary = (GroovyObject)tag.getOwner()
+        withTag("remoteLink", pw) {tag ->
+            GroovyObject tagLibrary = (GroovyObject) tag.getOwner()
             def request = tagLibrary.getProperty("request")
             def includedLibrary = ['prototype']
             request.setAttribute("org.codehaus.grails.INCLUDED_JS_LIBRARIES", includedLibrary)
 
-            def attrs = [controller:'person',action:'show',params:[var1:'0'],update:'async']
-            tag.call(attrs) { "Show async" }
+            def attrs = [controller: 'person', action: 'show', params: [var1: '0'], update: 'async']
+            tag.call(attrs) {"Show async"}
             println sw.toString()
-            assertEquals("<a href=\"/people/details/0\" onclick=\"new Ajax.Updater('async','/people/details/0',{asynchronous:true,evalScripts:true,parameters:'var1=0'});return false;\">Show async</a>",sw.toString())
+            assertEquals("<a href=\"/people/details/0\" onclick=\"new Ajax.Updater('async','/people/details/0',{asynchronous:true,evalScripts:true,parameters:'var1=0'});return false;\">Show async</a>", sw.toString())
         }
     }
 
-     public void testPluginAwareJSSrc (){
+    public void testPluginAwareJSSrc() {
         StringWriter sw = new StringWriter()
         PrintWriter pw = new PrintWriter(sw)
-        withTag("javascript",pw) { tag ->
-           setupPluginController(tag)
-           def attrs = [src:'lib.js']
-           tag.call(attrs) { }
-           assertEquals("<script type=\"text/javascript\" src=\"/myapp/plugins/myplugin/js/lib.js\"></script>" + System.getProperty("line.separator"),sw.toString())
+        withTag("javascript", pw) {tag ->
+            setupPluginController(tag)
+            def attrs = [src: 'lib.js']
+            tag.call(attrs) {}
+            assertEquals("<script type=\"text/javascript\" src=\"/myapp/plugins/myplugin/js/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
         }
-     }
+    }
 
     public void testPluginAwareJSSrcTrailingSlash() {
         StringWriter sw = new StringWriter()
         PrintWriter pw = new PrintWriter(sw)
-        withTag("javascript",pw) { tag ->
-           setupPluginController(tag)
-           setRequestContext('/otherapp/')
-           def attrs = [src:'lib.js']
-           tag.call(attrs) { }
-           assertEquals("<script type=\"text/javascript\" src=\"/otherapp/plugins/myplugin/js/lib.js\"></script>" + System.getProperty("line.separator"),sw.toString())
-        }
-   }
-
-   public void testPluginAwareJSLib (){
-        StringWriter sw = new StringWriter()
-        PrintWriter pw = new PrintWriter(sw)
-        withTag("javascript",pw) { tag ->
+        withTag("javascript", pw) {tag ->
             setupPluginController(tag)
-           def attrs = [library:'lib']
-           tag.call(attrs) {  }
-           assertEquals("<script type=\"text/javascript\" src=\"/myapp/plugins/myplugin/js/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
+            setRequestContext('/otherapp/')
+            def attrs = [src: 'lib.js']
+            tag.call(attrs) {}
+            assertEquals("<script type=\"text/javascript\" src=\"/otherapp/plugins/myplugin/js/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
         }
     }
 
-    public void testJSSrc (){
+    public void testPluginAwareJSLib() {
         StringWriter sw = new StringWriter()
         PrintWriter pw = new PrintWriter(sw)
-        withTag("javascript",pw) { tag ->
-           def attrs = [src:'lib.js']
-           setRequestContext()
-           tag.call(attrs) { }
-           assertEquals("<script type=\"text/javascript\" src=\"/myapp/js/lib.js\"></script>" + System.getProperty("line.separator"),sw.toString())
+        withTag("javascript", pw) {tag ->
+            setupPluginController(tag)
+            def attrs = [library: 'lib']
+            tag.call(attrs) {}
+            assertEquals("<script type=\"text/javascript\" src=\"/myapp/plugins/myplugin/js/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
         }
-     }
+    }
+
+    public void testJSSrc() {
+        StringWriter sw = new StringWriter()
+        PrintWriter pw = new PrintWriter(sw)
+        withTag("javascript", pw) {tag ->
+            def attrs = [src: 'lib.js']
+            setRequestContext()
+            tag.call(attrs) {}
+            assertEquals("<script type=\"text/javascript\" src=\"/myapp/js/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
+        }
+    }
 
     public void testJSSrcTrailingSlash() {
         StringWriter sw = new StringWriter()
         PrintWriter pw = new PrintWriter(sw)
-        withTag("javascript",pw) { tag ->
-           def attrs = [src:'lib.js']
-           setRequestContext('/otherapp/')
-           tag.call(attrs) { }
-           assertEquals("<script type=\"text/javascript\" src=\"/otherapp/js/lib.js\"></script>" + System.getProperty("line.separator"),sw.toString())
+        withTag("javascript", pw) {tag ->
+            def attrs = [src: 'lib.js']
+            setRequestContext('/otherapp/')
+            tag.call(attrs) {}
+            assertEquals("<script type=\"text/javascript\" src=\"/otherapp/js/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
         }
-   }
+    }
 
-    public void testJSSrcWithNoController (){
+    public void testJSSrcWithNoController() {
         StringWriter sw = new StringWriter()
         PrintWriter pw = new PrintWriter(sw)
-        withTag("javascript",pw) { tag ->
-           def attrs = [src:'lib.js']
-           setRequestContext()
-           request.setAttribute(GrailsApplicationAttributes.CONTROLLER, null);
-           tag.call(attrs) { }
-           assertEquals("<script type=\"text/javascript\" src=\"/myapp/js/lib.js\"></script>" + System.getProperty("line.separator"),sw.toString())
+        withTag("javascript", pw) {tag ->
+            def attrs = [src: 'lib.js']
+            setRequestContext()
+            request.setAttribute(GrailsApplicationAttributes.CONTROLLER, null);
+            tag.call(attrs) {}
+            assertEquals("<script type=\"text/javascript\" src=\"/myapp/js/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
         }
-   }
+    }
 
-   public void testJSLib (){
+    public void testJSLib() {
         StringWriter sw = new StringWriter()
         PrintWriter pw = new PrintWriter(sw)
-        withTag("javascript",pw) { tag ->
-           def attrs = [library:'lib']
-           setRequestContext()
-           tag.call(attrs) {  }
-           assertEquals("<script type=\"text/javascript\" src=\"/myapp/js/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
+        withTag("javascript", pw) {tag ->
+            def attrs = [library: 'lib']
+            setRequestContext()
+            tag.call(attrs) {}
+            assertEquals("<script type=\"text/javascript\" src=\"/myapp/js/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
         }
-     }
+    }
 
     public void testJSLibTrailingSlash() {
         StringWriter sw = new StringWriter()
         PrintWriter pw = new PrintWriter(sw)
-        withTag("javascript",pw) { tag ->
-           def attrs = [library:'lib']
-           setRequestContext('/otherapp/')
-           tag.call(attrs) {  }
-           assertEquals("<script type=\"text/javascript\" src=\"/otherapp/js/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
+        withTag("javascript", pw) {tag ->
+            def attrs = [library: 'lib']
+            setRequestContext('/otherapp/')
+            tag.call(attrs) {}
+            assertEquals("<script type=\"text/javascript\" src=\"/otherapp/js/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
         }
     }
 
-     public void testJSWithBody (){
+    public void testJSWithBody() {
         StringWriter sw = new StringWriter()
         PrintWriter pw = new PrintWriter(sw)
-        withTag("javascript",pw) { tag ->
-           setRequestContext()
-           tag.call([:]) { "do.this();" }
-           assertEquals("<script type=\"text/javascript\">"+ System.getProperty("line.separator") + "do.this();"+ System.getProperty("line.separator")+"</script>" + System.getProperty("line.separator"), sw.toString())
-        }
-    }
-
-
-    public void testJSLibWithBase (){
-        StringWriter sw = new StringWriter()
-        PrintWriter pw = new PrintWriter(sw)
-        withTag("javascript",pw) { tag ->
-           def attrs = [library:'lib', base:'http://testserver/static/']
-           setRequestContext()
-           tag.call(attrs) {  }
-           assertEquals("<script type=\"text/javascript\" src=\"http://testserver/static/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
+        withTag("javascript", pw) {tag ->
+            setRequestContext()
+            tag.call([:]) {"do.this();"}
+            assertEquals("<script type=\"text/javascript\">" + System.getProperty("line.separator") + "do.this();" + System.getProperty("line.separator") + "</script>" + System.getProperty("line.separator"), sw.toString())
         }
     }
 
 
-    public void testJSSrcWithBase (){
+    public void testJSLibWithBase() {
         StringWriter sw = new StringWriter()
         PrintWriter pw = new PrintWriter(sw)
-        withTag("javascript",pw) { tag ->
-           def attrs = [src:'mylib.js', base:'http://testserver/static/']
-           setRequestContext()
-           tag.call(attrs) {  }
-           assertEquals("<script type=\"text/javascript\" src=\"http://testserver/static/mylib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
+        withTag("javascript", pw) {tag ->
+            def attrs = [library: 'lib', base: 'http://testserver/static/']
+            setRequestContext()
+            tag.call(attrs) {}
+            assertEquals("<script type=\"text/javascript\" src=\"http://testserver/static/lib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
+        }
+    }
+
+
+    public void testJSSrcWithBase() {
+        StringWriter sw = new StringWriter()
+        PrintWriter pw = new PrintWriter(sw)
+        withTag("javascript", pw) {tag ->
+            def attrs = [src: 'mylib.js', base: 'http://testserver/static/']
+            setRequestContext()
+            tag.call(attrs) {}
+            assertEquals("<script type=\"text/javascript\" src=\"http://testserver/static/mylib.js\"></script>" + System.getProperty("line.separator"), sw.toString())
         }
     }
 
@@ -226,22 +220,21 @@ class TestUrlMappings {
         request.setAttribute(WebUtils.INCLUDE_CONTEXT_PATH_ATTRIBUTE, path)
     }
 
-    def setupPluginController(tag){
-        GroovyObject tagLibrary = (GroovyObject)tag.getOwner()
+    def setupPluginController(tag) {
+        GroovyObject tagLibrary = (GroovyObject) tag.getOwner()
         def request = tagLibrary.getProperty("request")
         setRequestContext()
-        request.setAttribute(GrailsApplicationAttributes.CONTROLLER, new Expando(pluginContextPath:"plugins/myplugin"));
+        request.setAttribute(GrailsApplicationAttributes.CONTROLLER, new Expando(pluginContextPath: "plugins/myplugin"));
     }
 
-
     public void testEscapeJavascript() throws Exception {
-		StringWriter sw = new StringWriter();
-		PrintWriter pw = new PrintWriter(sw);
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
 
-		withTag("escapeJavascript",pw) { tag ->
+        withTag("escapeJavascript", pw) {tag ->
 
-	        tag.call( "This is some \"text\" to be 'escaped'", Collections.EMPTY_MAP );
-	        assertEquals("This is some \\\"text\\\" to be \\'escaped\\'",sw.toString());
-		}
+            tag.call("This is some \"text\" to be 'escaped'", Collections.EMPTY_MAP);
+            assertEquals("This is some \\\"text\\\" to be \\'escaped\\'", sw.toString());
+        }
     }
 }
