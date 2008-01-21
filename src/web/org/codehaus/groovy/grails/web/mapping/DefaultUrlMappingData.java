21681f970fb03df5e112023ac4861a113a558a87
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2008-01-21 22:41:28 +0000

    fix for failing tests
    git-svn-id: https://svn.codehaus.org/grails/trunk@6526 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/web/org/codehaus/groovy/grails/web/mapping/DefaultUrlMappingData.java
+++ src/web/org/codehaus/groovy/grails/web/mapping/DefaultUrlMappingData.java
@@ -100,7 +100,7 @@ public class DefaultUrlMappingData implements UrlMappingData {
     }
 
     public boolean isOptional(int index) {
-        if(optionalTokens.size() >= index) return true;
+        if(index >= optionalTokens.size()) return true;
         return optionalTokens.get(index).equals(Boolean.TRUE);
     }
 }
