73f6074a51acae7a53b71181766d51090a8f89c4
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2008-01-31 16:58:40 +0000

    fix for GRAILS-1566
    git-svn-id: https://svn.codehaus.org/grails/trunk@6637 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/web/org/codehaus/groovy/grails/web/servlet/mvc/SimpleGrailsControllerHelper.java
+++ src/web/org/codehaus/groovy/grails/web/servlet/mvc/SimpleGrailsControllerHelper.java
@@ -43,6 +43,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.util.WebUtils;
+import org.springframework.web.util.UrlPathHelper;
 import org.springframework.webflow.context.servlet.ServletExternalContext;
 import org.springframework.webflow.execution.support.ApplicationView;
 import org.springframework.webflow.execution.support.ExternalRedirect;
@@ -265,12 +266,14 @@ public class SimpleGrailsControllerHelper implements GrailsControllerHelper {
             ServletExternalContext externalContext = new ServletExternalContext(getServletContext(), request, response) {
                 public String getDispatcherPath() {
                     String forwardURI = (String)request.getAttribute(WebUtils.FORWARD_REQUEST_URI_ATTRIBUTE);
-                    if(!StringUtils.isBlank(forwardURI)) {
-                        forwardURI = forwardURI.substring(1);
-                        forwardURI = forwardURI.substring(forwardURI.indexOf('/'), forwardURI.length());
+                    UrlPathHelper pathHelper = new UrlPathHelper();
+                    String contextPath = pathHelper.getContextPath(request);
+                    if(forwardURI.startsWith(contextPath)) {
+                        return forwardURI.substring(contextPath.length(),forwardURI.length());
+                    }
+                    else {
                         return forwardURI;
                     }
-                    return super.getDispatcherPath();
                 }
             };
 
