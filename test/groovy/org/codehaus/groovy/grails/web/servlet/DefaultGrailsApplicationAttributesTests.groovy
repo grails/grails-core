b219a2ed07b8d17b6d353a95de0074c7440229e6
brownj <brownj@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2008-03-24 03:36:07 +0000

    GRAILS-1912 more test coverage
    git-svn-id: https://svn.codehaus.org/grails/trunk@6827 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/groovy/org/codehaus/groovy/grails/web/servlet/DefaultGrailsApplicationAttributesTests.groovy
+++ test/groovy/org/codehaus/groovy/grails/web/servlet/DefaultGrailsApplicationAttributesTests.groovy
@@ -5,18 +5,24 @@ import org.codehaus.groovy.grails.web.metaclass.ControllerDynamicMethods
 
 class DefaultGrailsApplicationAttributesTests extends GroovyTestCase {
 
-    void testGetTemplateUriWithNestedRelativePath() {
-        def grailsApplicationAttributes = new DefaultGrailsApplicationAttributes(null)
+    def grailsApplicationAttributes
+    def request
 
+    void setUp() {
+        grailsApplicationAttributes = new DefaultGrailsApplicationAttributes(null)
         def controller = new Expando()
-        controller."${ControllerDynamicMethods.CONTROLLER_URI_PROPERTY}" = 'foo'
-
-        def request = new MockHttpServletRequest()
+        controller."${ControllerDynamicMethods.CONTROLLER_URI_PROPERTY}" = 'mycontroller'
+        request = new MockHttpServletRequest()
         request.setAttribute(GrailsApplicationAttributes.CONTROLLER, controller)
-
-        String templateUri = grailsApplicationAttributes.getTemplateUri('one/two/three', request)
-
-        assertEquals 'wrong template uri', 'foo/one/two/_three.gsp', templateUri
     }
-    
+
+    void testGetTemplateUriWithNestedRelativePath() {
+        def templateUri = grailsApplicationAttributes.getTemplateUri('one/two/three', request)
+        assertEquals 'wrong template uri', 'mycontroller/one/two/_three.gsp', templateUri
+    }
+
+    void testGetTemplateUriWithRelativePath() {
+        def templateUri = grailsApplicationAttributes.getTemplateUri('bar', request)
+        assertEquals 'wrong template uri', 'mycontroller/_bar.gsp', templateUri
+    }
 }
