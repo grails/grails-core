ea765246ce79028efb62ffd0b9e239e9520be993
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2008-03-27 19:06:04 +0000

    updated version number to 1.0.3 snapshot
    git-svn-id: https://svn.codehaus.org/grails/trunk@6844 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/groovy/grails/util/GrailsUtilTests.groovy
+++ test/groovy/grails/util/GrailsUtilTests.groovy
@@ -7,7 +7,7 @@ class GrailsUtilTests extends GroovyTestCase {
 
     void testGrailsVersion() {
 
-        assertEquals "1.0.2", grails.util.GrailsUtil.getGrailsVersion()
+        assertEquals "1.0.3-SNAPSHOT", grails.util.GrailsUtil.getGrailsVersion()
     }
 
 }
