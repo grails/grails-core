aa47d0d4bdc482f52553dd200bdde68630595f81
brownj <brownj@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2008-11-01 01:08:15 +0000

    GRAILS-2856 adding create-filters target
    git-svn-id: https://svn.codehaus.org/grails/trunk@7493 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ scripts/CreateFilters.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Gant script that creates a Grails filters class
+ * 
+ * @author Jeff Brown
+ *
+ * @since 1.0.4
+ */
+
+import org.codehaus.groovy.grails.commons.GrailsClassUtils as GCU
+
+Ant.property(environment:"env")                             
+grailsHome = Ant.antProject.properties."env.GRAILS_HOME"    
+
+includeTargets << new File ( "${grailsHome}/scripts/Init.groovy" )  
+includeTargets << new File( "${grailsHome}/scripts/CreateIntegrationTest.groovy")
+
+target ('default': "Creates a new filters class") {
+    depends(checkVersion)
+
+	typeName = "Filters"
+	artifactName = "Filters" 	
+	artifactPath = "grails-app/conf"
+	
+	Ant.mkdir(dir:"${basedir}/grails-app/conf")
+	
+	createArtifact()
+	createTestSuite() 
+}
