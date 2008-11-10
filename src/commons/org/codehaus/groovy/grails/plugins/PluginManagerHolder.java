f980765531cdcb83fbb93f58c215cbf09b5bb9ec
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2008-11-10 13:51:21 +0000

    fix for GRAILS-3128
    git-svn-id: https://svn.codehaus.org/grails/trunk@7578 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- src/commons/org/codehaus/groovy/grails/plugins/PluginManagerHolder.java
+++ src/commons/org/codehaus/groovy/grails/plugins/PluginManagerHolder.java
@@ -4,53 +4,53 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 package org.codehaus.groovy.grails.plugins;
 
 /**
  * Manages a thread bound plugin manager instance
- * 
+ *
  * @author Graeme Rocher
  * @since 0.4
  *
  */
 public abstract class PluginManagerHolder {
 
-	private static final ThreadLocal pluginManagerHolder = new InheritableThreadLocal();
-	
+	private static GrailsPluginManager gpm = null;
+
 	/**
 	 * Bind the given GrailsPluginManager instance to the current Thread
 	 * @param pluginManager The GrailsPluginManager to expose
 	 */
 	public static void setPluginManager(GrailsPluginManager pluginManager) {
-		pluginManagerHolder.set(pluginManager);
+		gpm = pluginManager;
 	}
-	
+
 	/**
-	 * Retrieves the GrailsPluginManager bound to the current Thread 
+	 * Retrieves the GrailsPluginManager bound to the current Thread
 	 * @return The GrailsPluginManager or null
 	 */
 	public static GrailsPluginManager getPluginManager() {
-		return (GrailsPluginManager)pluginManagerHolder.get();
+		return gpm;
 	}
-	
+
 	/**
 	 * Retrieves the bound GrailsPluginManager that resides in the current Thread
-	 * @return The GrailsPluginManager 
+	 * @return The GrailsPluginManager
 	 * @throws IllegalStateException When there is no bound GrailsPluginManager
 	 */
 	public static GrailsPluginManager currentPluginManager() {
 		GrailsPluginManager current = getPluginManager();
 		if(current == null)
-			throw new IllegalStateException("No thread-bound PluginManager");
+			throw new IllegalStateException("No PluginManager set");
 		return current;
 	}
 }
