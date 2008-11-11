6ec3b9a0d1b103efca120ad02892fab8a6feb45f
pledbrook <pledbrook@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2008-11-11 09:43:15 +0000

    Implemented GRAILS-2946: the "generate-all" command can now accept a "*" argument which tells it to generate views and controllers for all the application's domain classes. You can also run it with no argument and then just press return when prompted for the name.
    git-svn-id: https://svn.codehaus.org/grails/trunk@7593 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- scripts/GenerateAll.groovy
+++ scripts/GenerateAll.groovy
@@ -23,16 +23,8 @@
  */
 
 import org.codehaus.groovy.grails.commons.GrailsClassUtils as GCU
-import groovy.text.SimpleTemplateEngine
 import org.codehaus.groovy.grails.commons.spring.GrailsRuntimeConfigurator;
 import org.codehaus.groovy.grails.scaffolding.*
-import org.springframework.mock.web.MockServletContext
-import org.springframework.core.io.Resource
-import org.codehaus.groovy.grails.commons.ApplicationHolder;
-
-
-Ant.property(environment:"env")                             
-grailsHome = Ant.antProject.properties."env.GRAILS_HOME"    
 
 includeTargets << grailsScript ( "Bootstrap" )
     
@@ -40,47 +32,90 @@ generateViews = true
 generateController = true
 
 target ('default': "Generates a CRUD interface (controller + views) for a domain class") {
-	depends( checkVersion, packageApp )
-	typeName = "Domain Class"
-	promptForName()
-	generateAll()
+    depends( checkVersion, packageApp )
+    typeName = "Domain Class"
+    promptForName()
+    generateAll()
 }            
 
 target(generateAll:"The implementation target") {
-	                                     
-	rootLoader.addURL(classesDir.toURI().toURL())
+
+    rootLoader.addURL(classesDir.toURI().toURL())
     loadApp()
-    
+
+    templateGenerator = new DefaultGrailsTemplateGenerator()
+
     def name = args.trim()
+    if (!name || name == "*") {
+        uberGenerate()
+    }
+    else {
+        generateForOne(name: name)
+    }
+}
+
+target(generateForOne: "Generates controllers and views for only one domain class.") { Map args ->
+    def name = args["name"]
     name = name.indexOf('.') > -1 ? name : GCU.getClassNameRepresentation(name)
     def domainClass = grailsApp.getDomainClass(name)
-	
-	if(!domainClass) {
-   		println "Domain class not found in grails-app/domain, trying hibernate mapped classes..."		
-		try {
-			def config = new GrailsRuntimeConfigurator(grailsApp, appCtx)  
-			appCtx = config.configure(appCtx.servletContext)     			
-		}   
-		catch(Exception e) {
-			println e.message
-			e.printStackTrace()
-		}
-		domainClass = grailsApp.getDomainClass(name)  
-	}
 
-   if(domainClass) {
-		def generator = new DefaultGrailsTemplateGenerator()                                        
-		if(generateViews) {
-			event("StatusUpdate", ["Generating views for domain class ${domainClass.fullName}"])				
-			generator.generateViews(domainClass, basedir)
-		}                                                                                       
-		if(generateController) {
-			event("StatusUpdate", ["Generating controller for domain class ${domainClass.fullName}"])		
-			generator.generateController(domainClass, basedir)				
-		}
-		event("StatusFinal", ["Finished generation for domain class ${domainClass.fullName}"])
-	}                                                
-	else {
-		event("StatusFinal", ["No domain class found for name ${name}. Please try again and enter a valid domain class name"])		
-	}
+    if(!domainClass) {
+        println "Domain class not found in grails-app/domain, trying hibernate mapped classes..."
+        doRuntimeConfig(grailsApp, appCtx)
+        domainClass = grailsApp.getDomainClass(name)
+    }
+
+    if(domainClass) {
+        generateForDomainClass(domainClass)
+        event("StatusFinal", ["Finished generation for domain class ${domainClass.fullName}"])
+    }
+    else {
+        event("StatusFinal", ["No domain class found for name ${name}. Please try again and enter a valid domain class name"])
+    }
+}
+
+target(uberGenerate: "Generates controllers and views for all domain classes.") {
+    rootLoader.addURL(classesDir.toURI().toURL())
+    loadApp()
+
+    def domainClasses = grailsApp.domainClasses
+
+    if (!domainClasses) {
+        println "No domain classes found in grails-app/domain, trying hibernate mapped classes..."
+        doRuntimeConfig(grailsApp, appCtx)
+        domainClasses = grailsApp.domainClasses
+    }
+
+   if (domainClasses) {
+        def generator = new DefaultGrailsTemplateGenerator()
+        domainClasses.each { domainClass ->
+            generateForDomainClass(domainClass)
+        }
+        event("StatusFinal", ["Finished generation for domain classes"])
+    }
+    else {
+        event("StatusFinal", ["No domain classes found"])
+    }
+}
+
+def doRuntimeConfig(app, ctx) {
+    try {
+        def config = new GrailsRuntimeConfigurator(app, ctx)
+        appCtx = config.configure(ctx.servletContext)
+    }
+    catch (Exception e) {
+        println e.message
+        e.printStackTrace()
+    }
+}
+
+def generateForDomainClass(domainClass) {
+    if(generateViews) {
+        event("StatusUpdate", ["Generating views for domain class ${domainClass.fullName}"])
+        templateGenerator.generateViews(domainClass, basedir)
+    }
+    if(generateController) {
+        event("StatusUpdate", ["Generating controller for domain class ${domainClass.fullName}"])
+        templateGenerator.generateController(domainClass, basedir)
+    }
 }
