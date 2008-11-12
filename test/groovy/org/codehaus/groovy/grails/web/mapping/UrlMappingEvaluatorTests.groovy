e350ea4605c30f5e3dd0473c21c93a9e7088537c
pledbrook <pledbrook@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2008-11-12 09:46:33 +0000

    Reformatted a test.
    git-svn-id: https://svn.codehaus.org/grails/trunk@7601 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- test/groovy/org/codehaus/groovy/grails/web/mapping/UrlMappingEvaluatorTests.groovy
+++ test/groovy/org/codehaus/groovy/grails/web/mapping/UrlMappingEvaluatorTests.groovy
@@ -41,83 +41,78 @@ mappings {
 '''
     
     void testEvaluateMappings() {
-         def res = new ByteArrayResource(mappingScript.bytes)
+        def res = new ByteArrayResource(mappingScript.bytes)
 
-         def evaluator = new DefaultUrlMappingEvaluator()
-         def mappings = evaluator.evaluateMappings(res)
+        def evaluator = new DefaultUrlMappingEvaluator()
+        def mappings = evaluator.evaluateMappings(res)
 
-         assert mappings
-         assertEquals 7, mappings.size()
+        assert mappings
+        assertEquals 7, mappings.size()
 
-         def m1 = mappings[0]
-         assertEquals "/(*)/(*)?/(*)?/(*)?", m1.urlData.urlPattern
-         assertEquals 4, m1.urlData.tokens.size()
-
-
-         def info = m1.match("/myentry/2007/04/28")
-
-         assertEquals "myentry", info.id
-         assertEquals "2007", info.parameters.year
-         assertEquals "04", info.parameters.month
-         assertEquals "28", info.parameters.day
-         assertEquals "blog", info.controllerName
-         assertEquals "show", info.actionName
+        def m = mappings[0]
+        assertEquals "/(*)/(*)?/(*)?/(*)?", m.urlData.urlPattern
+        assertEquals 4, m.urlData.tokens.size()
 
 
-         assert m1.match("/myentry/2007/04/28")
-         assert m1.match("/myentry/2007/04")
-         assert m1.match("/myentry/2007")
-         assert m1.match("/myentry")
+        def info = m.match("/myentry/2007/04/28")
+
+        assertEquals "myentry", info.id
+        assertEquals "2007", info.parameters.year
+        assertEquals "04", info.parameters.month
+        assertEquals "28", info.parameters.day
+        assertEquals "blog", info.controllerName
+        assertEquals "show", info.actionName
 
 
-         def m2 = mappings[1]
+        assert m.match("/myentry/2007/04/28")
+        assert m.match("/myentry/2007/04")
+        assert m.match("/myentry/2007")
+        assert m.match("/myentry")
 
-         info = m2.match("/product/MacBook")
-         assert info
-         assertEquals "MacBook", info.parameters.name
 
-         assert !m2.match("/product")
-         assert !m2.match("/foo/bar")
-         assert !m2.match("/product/MacBook/foo")      
-         
-         def m4 = mappings[3]
-         info = m4.match("/author/Brown/Jeff")
-         assert info
-         assertEquals "Brown", info.parameters.lastName
-         assertEquals "Jeff", info.parameters.firstName
-         assertEquals "show", info.actionName
-         assertEquals "author", info.controllerName
-         
-         // first name too long
-         assert !m4.match("/author/Lang/Johnny")
-         
-         // both names too long
-         assert !m4.match("/author/Winter/Johnny")
-         
-         // last name too long
-         assert !m4.match("/author/Winter/Edgar")
-         
-         def m5 = mappings[4]
-         info = m5.match("/music/Rush/Hemispheres")
-         assert info
-         assertEquals "Rush", info.parameters.band
-         assertEquals "Hemispheres", info.parameters.album
-         assertEquals "show", info.actionName
-         assertEquals "music", info.controllerName
+        m = mappings[1]
+        info = m.match("/product/MacBook")
+        assert info
+        assertEquals "MacBook", info.parameters.name
 
-         def m6 = mappings[5]
-         info = m6.match("/myFiles/something-hello.txt")
-         assert info
-         assertEquals "files", info.controllerName
-         assertEquals "hello", info.parameters.fname
-         assertEquals "txt", info.parameters.fext
+        assert !m.match("/product")
+        assert !m.match("/foo/bar")
+        assert !m.match("/product/MacBook/foo")
+
+        m = mappings[3]
+        info = m.match("/author/Brown/Jeff")
+        assert info
+        assertEquals "Brown", info.parameters.lastName
+        assertEquals "Jeff", info.parameters.firstName
+        assertEquals "show", info.actionName
+        assertEquals "author", info.controllerName
+
+        // first name too long
+        assert !m.match("/author/Lang/Johnny")
+
+        // both names too long
+        assert !m.match("/author/Winter/Johnny")
+
+        // last name too long
+        assert !m.match("/author/Winter/Edgar")
+
+        info = mappings[4].match("/music/Rush/Hemispheres")
+        assert info
+        assertEquals "Rush", info.parameters.band
+        assertEquals "Hemispheres", info.parameters.album
+        assertEquals "show", info.actionName
+        assertEquals "music", info.controllerName
+
+        info = mappings[5].match("/myFiles/something-hello.txt")
+        assert info
+        assertEquals "files", info.controllerName
+        assertEquals "hello", info.parameters.fname
+        assertEquals "txt", info.parameters.fext
 
         // Test the double-wildcard, "**".
-         def m = mappings[6]
-         info = m.match("/long/path/to/some/file")
-         assert info
-         assertEquals "path/to/some/file", info.parameters.path
-         assertEquals "files", info.controllerName
+        info = mappings[6].match("/long/path/to/some/file")
+        assert info
+        assertEquals "path/to/some/file", info.parameters.path
+        assertEquals "files", info.controllerName
     }
 }
-
