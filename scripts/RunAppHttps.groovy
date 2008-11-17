356165cea651b97d488126ce7f4226a7fd85c10a
pledbrook <pledbrook@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2008-11-17 06:09:03 +0000

    Added missing import to RunAppHttps.groovy.
    git-svn-id: https://svn.codehaus.org/grails/trunk@7650 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- scripts/RunAppHttps.groovy
+++ scripts/RunAppHttps.groovy
@@ -1,18 +1,23 @@
 /*
- * Copyright 2004-2005 the original author or authors.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2004-2005 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import org.codehaus.groovy.grails.plugins.PluginManagerHolder
+import org.mortbay.jetty.Connector
+import org.mortbay.jetty.security.SslSocketConnector
+import sun.security.tools.KeyTool
 
 /**
  * Gant script that executes Grails using an embedded Jetty server with
@@ -22,14 +27,6 @@
  *
  * @since 0.4
  */
-
-import org.codehaus.groovy.grails.commons.GrailsClassUtils as GCU
-import org.mortbay.jetty.*
-import org.mortbay.jetty.nio.*
-import org.mortbay.jetty.handler.*
-import org.mortbay.jetty.webapp.*
-import org.mortbay.jetty.security.*
-import sun.security.tools.*
 
 Ant.property(environment: "env")
 grailsHome = Ant.antProject.properties."env.GRAILS_HOME"
