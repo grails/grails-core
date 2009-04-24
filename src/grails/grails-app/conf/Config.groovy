c5f4a8d8330fa7d80d92ea2fc653d291b2dba2ea
Graeme Rocher <graeme.rocher@gmail.com>
2009-04-24 11:20:00 +0100

    fixed weird quotes inside package names

--- src/grails/grails-app/conf/Config.groovy
+++ src/grails/grails-app/conf/Config.groovy
@@ -58,8 +58,8 @@ log4j = {
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
 	       'org.codehaus.groovy.grails.web.pages', //  GSP
 	       'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-	       'org.codehaus.groovy.grails."web.mapping.filter', // URL mapping
-	       'org.codehaus.groovy.grails."web.mapping', // URL mapping
+	       'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+	       'org.codehaus.groovy.grails.web.mapping', // URL mapping
 	       'org.codehaus.groovy.grails.commons', // core / classloading
 	       'org.codehaus.groovy.grails.plugins', // plugins
 	       'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
