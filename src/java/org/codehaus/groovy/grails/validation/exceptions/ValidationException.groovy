cf60791ff212542fc0fc2ac25e2a998b4063cacb
Jeff Brown <jeff@jeffandbetsy.net>
2009-07-09 21:39:06 +0200

    GRAILS-4343 fill in ValidationException

--- src/java/org/codehaus/groovy/grails/validation/exceptions/ValidationException.groovy
+++ src/java/org/codehaus/groovy/grails/validation/exceptions/ValidationException.groovy
@@ -1,5 +1,17 @@
 package org.codehaus.groovy.grails.validation.exceptions
 
-public class ValidationException {
+import org.codehaus.groovy.grails.exceptions.GrailsException
+
+/**
+ * An exception thrown when validation fails during a .save()
+ *
+ * @author Jeff Brown
+ * @since 1.2
+ */
+class ValidationException extends GrailsException {
+
+    public ValidationException(String msg) {
+        super(msg)
+    }
 
 }
