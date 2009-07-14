4cb33489a475a78d6b90e22699ef0d5d36d636b7
Graeme Rocher <graeme.rocher@gmail.com>
2009-07-14 10:07:24 +0200

    changed to deregister JDBC drivers

--- src/java/org/codehaus/groovy/grails/web/context/GrailsContextLoaderListener.java
+++ src/java/org/codehaus/groovy/grails/web/context/GrailsContextLoaderListener.java
@@ -3,8 +3,16 @@
  */
 package org.codehaus.groovy.grails.web.context;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.ContextLoaderListener;
+
+import javax.servlet.ServletContextEvent;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
 
 /**
  * Extends the Spring default ContextLoader to load GrailsApplicationContext
@@ -14,8 +22,27 @@ import org.springframework.web.context.ContextLoaderListener;
  */
 public class GrailsContextLoaderListener extends ContextLoaderListener {
 
+    private static final Log LOG = LogFactory.getLog(GrailsContextLoaderListener.class);
+
 	protected ContextLoader createContextLoader() {
 		return new GrailsContextLoader();
 	}
 
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+        deregisterJDBCDrivers();
+        super.contextDestroyed(event);
+    }
+
+    private void deregisterJDBCDrivers() {
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+            try {
+                DriverManager.deregisterDriver(driver);
+            } catch (SQLException e) {
+                LOG.error("Error deregisetring JDBC driver ["+driver+"]: " + e.getMessage(), e);
+            }
+        }
+    }
 }
