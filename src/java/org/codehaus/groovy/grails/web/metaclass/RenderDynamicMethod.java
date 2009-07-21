5220cb24dd3e7a4edc0b06fecd1ce5890f225e9c
Graeme Rocher <graeme.rocher@gmail.com>
2009-07-21 11:10:26 +0200

    fix for GRAILS-4738

--- src/java/org/codehaus/groovy/grails/web/metaclass/RenderDynamicMethod.java
+++ src/java/org/codehaus/groovy/grails/web/metaclass/RenderDynamicMethod.java
@@ -116,7 +116,7 @@ public class RenderDynamicMethod extends AbstractDynamicMethodInvocation {
             String text = arguments[0].toString();
             renderView = renderText(text, response);
         } else if (arguments[0] instanceof Closure) {
-            setContentType(response, TEXT_HTML, DEFAULT_ENCODING, true);
+            setContentType(response, TEXT_HTML, gspEncoding, true);
             Closure closure = (Closure) arguments[arguments.length - 1];
             renderView = renderMarkup(closure, response);
         } else if (arguments[0] instanceof Map) {
