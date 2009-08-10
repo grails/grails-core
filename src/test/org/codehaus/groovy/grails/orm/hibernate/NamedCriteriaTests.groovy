36bced153288b1769e56125f628a8dc8968aac4b
Jeff Brown <jeff.brown@springsource.com>
2009-08-10 10:53:49 -0500

    GRAILS-4940 - start filling in some code to support named queries

---
+++ src/test/org/codehaus/groovy/grails/orm/hibernate/NamedCriteriaTests.groovy
@@ -0,0 +1,62 @@
+package org.codehaus.groovy.grails.orm.hibernate
+
+/**
+ * @author Jeff Brown
+ */
+class NamedCriteriaTests extends AbstractGrailsHibernateTests {
+
+    protected void onSetUp() {
+        gcl.parseClass('''
+
+class Book {
+   Long id
+   Long version
+   String title
+   Date datePublished
+
+   static namedQueries = {
+       recentBooks {
+           def now = new Date()
+           gt 'datePublished', now - 365
+       }
+   }
+
+}
+''')
+    }
+
+    void testList() {
+        def bookClass = ga.getDomainClass("Book").clazz
+
+        def now = new Date()
+        assert bookClass.newInstance(title: "Some New Book",
+                datePublished: now - 10).save(flush: true)
+        assert bookClass.newInstance(title: "Some Old Book",
+                datePublished: now - 900).save(flush: true)
+
+        session.clear()
+
+        def books = bookClass.recentBooks.list()
+
+        assertEquals 1, books?.size()
+        assertEquals 'Some New Book', books[0].title
+    }
+
+    void testInvokingDirectly() {
+        def bookClass = ga.getDomainClass("Book").clazz
+
+        def now = new Date()
+        assert bookClass.newInstance(title: "Some New Book",
+                datePublished: now - 10).save(flush: true)
+        assert bookClass.newInstance(title: "Some Old Book",
+                datePublished: now - 900).save(flush: true)
+
+        session.clear()
+
+        def books = bookClass.recentBooks()
+
+        assertEquals 1, books?.size()
+        assertEquals 'Some New Book', books[0].title
+    }
+
+}
