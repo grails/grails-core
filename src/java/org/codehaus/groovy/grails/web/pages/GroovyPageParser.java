5242d87c5f0fdd3aebaef0956c685da5dc3b7e2a
Graeme Rocher <graeme.rocher@gmail.com>
2009-09-15 14:09:06 +0200

    Fix for GRAILS-5022 "Unable to resolve domain class when doing grails war" GSPs now no longer in packages, because it was causing issues with GSPs being able to see domain classes etc.

--- src/java/org/codehaus/groovy/grails/web/pages/GroovyPageParser.java
+++ src/java/org/codehaus/groovy/grails/web/pages/GroovyPageParser.java
@@ -315,10 +315,6 @@ public class GroovyPageParser implements Tokens {
 		this.precompileMode = precompileMode;
 
 		out = new GSPWriter(target, this);
-		if (packageName != null) {
-			out.println("package " + packageName);
-			out.println();
-		}
 		page();
 		finalPass = true;
 		scan.reset();
@@ -644,6 +640,11 @@ public class GroovyPageParser implements Tokens {
 		if (finalPass) {
 			out.println();
 			out.print("class ");
+            if (packageName != null) {
+                out.print(packageName);
+                out.print('_');
+            }
+
 			out.print(className);
 			out.println(" extends GroovyPage {");
 
