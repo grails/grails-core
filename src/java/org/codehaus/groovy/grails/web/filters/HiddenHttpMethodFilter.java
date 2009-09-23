29c5e1d8bcb11707db25ae00aeb756a5b3d166aa
Graeme Rocher <graeme.rocher@gmail.com>
2009-09-23 17:24:12 +0200

    new hidden http method filter impl for dealing with REST requests in a browser. Based on Spring 3 implementation.

--- src/java/org/codehaus/groovy/grails/web/filters/HiddenHttpMethodFilter.java
+++ src/java/org/codehaus/groovy/grails/web/filters/HiddenHttpMethodFilter.java
@@ -14,9 +14,93 @@
  */
 package org.codehaus.groovy.grails.web.filters;
 
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
+ * Based off the Spring implementation, but this one works with Grails params object
+ * and also deals with the X-HTTP-Method-Override HTTP header.
+ *
+ * @see org.springframework.web.filter.HiddenHttpMethodFilter
+ *
  * @author Graeme Rocher
- * @since 1.1
+ * @since 1.2
  */
-public class HiddenHttpMethodFilter {
+public class HiddenHttpMethodFilter extends OncePerRequestFilter{
+
+    /** Default method parameter: <code>_method</code> */
+    public static final String DEFAULT_METHOD_PARAM = "_method";
+
+    private String methodParam = DEFAULT_METHOD_PARAM;
+    public static final String HEADER_X_HTTP_METHOD_OVERRIDE = "X-HTTP-Method-Override";
+
+
+    /**
+     * Set the parameter name to look for HTTP methods.
+     * @see #DEFAULT_METHOD_PARAM
+     */
+    public void setMethodParam(String methodParam) {
+        Assert.hasText(methodParam, "'methodParam' must not be empty");
+        this.methodParam = methodParam;
+    }
+
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        if("POST".equalsIgnoreCase(request.getMethod())) {
+            String httpMethod = getHttpMethodOverride(request);
+            if(StringUtils.hasLength(httpMethod))
+                filterChain.doFilter(new HttpMethodRequestWrapper(httpMethod, request), response);
+            else
+                filterChain.doFilter(request, response);
+        }
+        else {
+            filterChain.doFilter(request, response);
+        }
+
+    }
+
+    protected String getHttpMethodOverride(HttpServletRequest request) {
+        GrailsWebRequest webRequest = GrailsWebRequest.lookup(request);
+        String httpMethod = null;
+        if(webRequest != null) {
+            final Object val = webRequest.getParams().get(methodParam);
+            if(val!=null)
+                httpMethod = val.toString();
+        }
+        else {
+            httpMethod = request.getParameter(methodParam);
+        }
+        if(httpMethod == null) {
+            httpMethod = request.getHeader(HEADER_X_HTTP_METHOD_OVERRIDE);
+        }
+        return httpMethod;
+    }
+
+    /**
+     * Simple {@link HttpServletRequest} wrapper that returns the supplied method for
+     * {@link HttpServletRequest#getMethod()}.
+     */
+    private static class HttpMethodRequestWrapper extends HttpServletRequestWrapper {
+
+        private final String method;
+
+        public HttpMethodRequestWrapper(String method, HttpServletRequest request) {
+            super(request);
+            this.method = method;
+        }
+
+        public String getMethod() {
+            return this.method;
+        }
+    }
+    
 }
--- src/test/org/codehaus/groovy/grails/web/filters/HiddenHttpMethodFilterTests.groovy
+++ src/test/org/codehaus/groovy/grails/web/filters/HiddenHttpMethodFilterTests.groovy
@@ -1,9 +1,79 @@
+
 package org.codehaus.groovy.grails.web.filters
+
+import org.springframework.mock.web.MockServletContext;
+
+import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes;
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest;
+
+import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.mock.web.MockHttpServletResponse
+import javax.servlet.FilterChain
+
 /**
  * @author Graeme Rocher
  * @since 1.1
  */
 
-public class HiddenHttpMethodFilterTests {
+public class HiddenHttpMethodFilterTests extends GroovyTestCase{
 
+	void testDefaultCase() {
+	  def filter = new HiddenHttpMethodFilter()
+      def req = new MockHttpServletRequest()
+      def res = new MockHttpServletResponse()
+      req.setMethod("POST")
+      String method  
+      filter.doFilter(req, res, { req2, res2 ->
+            method = req2.method
+      } as FilterChain)
+
+      assertEquals "POST", method		
+	}
+	
+    void testWithParameter() {
+       def filter = new HiddenHttpMethodFilter()
+       def req = new MockHttpServletRequest()
+       def res = new MockHttpServletResponse()
+       req.addParameter("_method", "DELETE")
+       req.setMethod("POST")
+       String method  
+       filter.doFilter(req, res, { req2, res2 ->
+            method = req2.method
+       } as FilterChain)
+
+       assertEquals "DELETE", method
+    }
+
+    void testWithParamsObject() {
+        def filter = new HiddenHttpMethodFilter()
+        def req = new MockHttpServletRequest()
+        def res = new MockHttpServletResponse()
+       	GrailsWebRequest webRequest = new GrailsWebRequest(req, res, new MockServletContext())
+        req.setAttribute(GrailsApplicationAttributes.WEB_REQUEST, webRequest)
+        
+        webRequest.params['_method'] = 'DELETE'
+       // req.addParameter("_method", "DELETE")
+        req.setMethod("POST")
+        String method  
+        filter.doFilter(req, res, { req2, res2 ->
+             method = req2.method
+        } as FilterChain)
+
+        assertEquals "DELETE", method
+    }
+
+    void testWithHeader() {
+        def filter = new HiddenHttpMethodFilter()
+        def req = new MockHttpServletRequest()
+        req.addHeader(HiddenHttpMethodFilter.HEADER_X_HTTP_METHOD_OVERRIDE, "DELETE")
+        def res = new MockHttpServletResponse()
+       // req.addParameter("_method", "DELETE")
+        req.setMethod("POST")
+        String method  
+        filter.doFilter(req, res, { req2, res2 ->
+             method = req2.method
+        } as FilterChain)
+
+        assertEquals "DELETE", method
+    }
 }
