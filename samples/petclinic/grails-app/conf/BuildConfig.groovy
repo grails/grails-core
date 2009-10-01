7c0b4f3434043ad35aa6795d991c93e2fb1f02f8
Graeme Rocher <graeme.rocher@gmail.com>
2009-10-01 12:04:34 +0200

    updated sample for M3 release

---
+++ samples/petclinic/grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,12 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir	= "target/test-reports"
+grails.project.dependency.resolution = {
+    inherits "global" // inherit Grails' default dependencies
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsHome()
+        mavenLocal()
+        mavenCentral()
+    }
+}
