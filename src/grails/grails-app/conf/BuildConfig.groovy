6d91bc1d4c8d47cc1d1daa497c736979db6773f8
Graeme Rocher <graeme.rocher@gmail.com>
2009-10-30 11:27:21 +0100

    added missing quote from commented out repo

--- src/grails/grails-app/conf/BuildConfig.groovy
+++ src/grails/grails-app/conf/BuildConfig.groovy
@@ -15,7 +15,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/
+        //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
