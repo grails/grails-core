5b685febe1666f6972cfce53941121664092a7cb
Graeme Rocher <graeme.rocher@gmail.com>
2009-11-09 11:04:48 +0100

    fix for failing test

--- src/test/grails/util/PluginBuildSettingsTests.groovy
+++ src/test/grails/util/PluginBuildSettingsTests.groovy
@@ -68,7 +68,7 @@ public class PluginBuildSettingsTests extends GroovyTestCase{
         PluginBuildSettings pluginSettings = createPluginBuildSettings()
         def scripts = pluginSettings.getAvailableScripts()
 
-        assertEquals 40, scripts.size()
+        assertEquals 41, scripts.size()
     }
 
     void testGetPluginScripts() {
