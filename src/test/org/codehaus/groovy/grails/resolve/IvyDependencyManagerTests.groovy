ee866746e5e62b65b6e1d705e1f0e8145878423a
Graeme Rocher <graeme.rocher@gmail.com>
2009-11-12 15:28:50 +0100

    fix for failing test

--- src/test/org/codehaus/groovy/grails/resolve/IvyDependencyManagerTests.groovy
+++ src/test/org/codehaus/groovy/grails/resolve/IvyDependencyManagerTests.groovy
@@ -322,8 +322,8 @@ public class IvyDependencyManagerTests extends GroovyTestCase{
         def grailsVersion = getCurrentGrailsVersion()
         manager.parseDependencies(IvyDependencyManager.getDefaultDependencies(grailsVersion))
 
-        assertEquals 53, manager.listDependencies('runtime').size()
-        assertEquals 56, manager.listDependencies('test').size()
+        assertEquals 51, manager.listDependencies('runtime').size()
+        assertEquals 54, manager.listDependencies('test').size()
         assertEquals 18, manager.listDependencies('build').size()
         assertEquals 3, manager.listDependencies('provided').size()
 
