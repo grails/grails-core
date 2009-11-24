d3c3d0b5d3221abe513da9f6f035e1c407d47ee2
Graeme Rocher <graeme.rocher@gmail.com>
2009-11-24 15:31:46 +0100

    fix for failing tests

--- src/java/org/codehaus/groovy/grails/web/util/TypeConvertingMap.groovy
+++ src/java/org/codehaus/groovy/grails/web/util/TypeConvertingMap.groovy
@@ -1,13 +1,28 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codehaus.groovy.grails.web.util
 /**
- * An abstract map that provies methods to converting
+ * An category for use with maps that want type conversion capabilities
  * 
  * @author Graeme Rocher
  * @since 1.2
  */
 
 @Category(Map.class)
-abstract class TypeConvertingMap implements Map{
+abstract class TypeConvertingMap {
     /**
      * Helper method for obtaining integer value from parameter
      * @param name The name of the parameter
