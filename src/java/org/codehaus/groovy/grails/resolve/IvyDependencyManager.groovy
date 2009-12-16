31bb69e223d7182392685d10a988b8bcc0d93dcd
Graeme Rocher <graeme.rocher@gmail.com>
2009-12-16 16:34:06 +0100

    Upgrade to Spring 3 final

--- src/java/org/codehaus/groovy/grails/resolve/IvyDependencyManager.groovy
+++ src/java/org/codehaus/groovy/grails/resolve/IvyDependencyManager.groovy
@@ -252,7 +252,7 @@ public class IvyDependencyManager implements DependencyResolver, DependencyDefin
                        "org.grails:grails-web:$grailsVersion",
                        "org.slf4j:slf4j-api:1.5.8",
                        "org.slf4j:slf4j-log4j12:1.5.8",
-                       "org.springframework:org.springframework.test:3.0.0.RC3"
+                       "org.springframework:org.springframework.test:3.0.0.RELEASE"
 
                 // dependencies needed during development, but not for deployment
                 provided "javax.servlet:servlet-api:2.5",
@@ -284,22 +284,22 @@ public class IvyDependencyManager implements DependencyResolver, DependencyDefin
                          "org.grails:grails-resources:$grailsVersion",
                          "org.grails:grails-spring:$grailsVersion",
                          "org.grails:grails-web:$grailsVersion",
-                         "org.springframework:org.springframework.core:3.0.0.RC3",
-                         "org.springframework:org.springframework.aop:3.0.0.RC3",
-                         "org.springframework:org.springframework.aspects:3.0.0.RC3",
-                         "org.springframework:org.springframework.asm:3.0.0.RC3",
-                         "org.springframework:org.springframework.beans:3.0.0.RC3",
-                         "org.springframework:org.springframework.context:3.0.0.RC3",
-                         "org.springframework:org.springframework.context.support:3.0.0.RC3",
-                         "org.springframework:org.springframework.expression:3.0.0.RC3",
-                         "org.springframework:org.springframework.instrument:3.0.0.RC3",
-                         "org.springframework:org.springframework.jdbc:3.0.0.RC3",
-                         "org.springframework:org.springframework.jms:3.0.0.RC3",
-                         "org.springframework:org.springframework.orm:3.0.0.RC3",
-                         "org.springframework:org.springframework.oxm:3.0.0.RC3",
-                         "org.springframework:org.springframework.transaction:3.0.0.RC3",
-                         "org.springframework:org.springframework.web:3.0.0.RC3",
-                         "org.springframework:org.springframework.web.servlet:3.0.0.RC3",
+                         "org.springframework:org.springframework.core:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.aop:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.aspects:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.asm:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.beans:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.context:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.context.support:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.expression:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.instrument:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.jdbc:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.jms:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.orm:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.oxm:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.transaction:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.web:3.0.0.RELEASE",
+                         "org.springframework:org.springframework.web.servlet:3.0.0.RELEASE",
                          "org.slf4j:slf4j-api:1.5.8") {
                         transitive = false
                 }
@@ -308,7 +308,7 @@ public class IvyDependencyManager implements DependencyResolver, DependencyDefin
                 // dependencies needed for running tests
                 test "junit:junit:3.8.2",
                      "org.grails:grails-test:$grailsVersion",
-                     "org.springframework:org.springframework.test:3.0.0.RC3"
+                     "org.springframework:org.springframework.test:3.0.0.RELEASE"
 
                 // dependencies needed at runtime only
                 runtime "org.aspectj:aspectjweaver:1.6.6",
