0c7b95d97259fdeafd2a63056cea3dea7b7dac8a
Graeme Rocher <graeme.rocher@gmail.com>
2009-12-18 12:16:32 +0100

    changed version back to snapshot

--- src/test/grails/util/GrailsUtilTests.java
+++ src/test/grails/util/GrailsUtilTests.java
@@ -53,7 +53,7 @@ public class GrailsUtilTests extends TestCase {
     }
 
     public void testGrailsVersion() {
-        assertEquals("1.2.0.RC2", GrailsUtil.getGrailsVersion());
+        assertEquals("1.2.0.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     protected void tearDown() throws Exception {
