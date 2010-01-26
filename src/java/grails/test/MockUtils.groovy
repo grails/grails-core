7917ee88ca1c8a1b24f8b643744905a0b8802dda
Jeff Brown <jeff@jeffandbetsy.net>
2010-01-26 09:44:38 -0600

    fix failing test

--- src/java/grails/test/MockUtils.groovy
+++ src/java/grails/test/MockUtils.groovy
@@ -37,7 +37,8 @@ import org.springframework.beans.SimpleTypeConverter
 import org.springframework.mock.web.MockHttpSession
 import org.springframework.validation.Errors
 import org.springframework.web.context.request.RequestContextHolder
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.View
 
 import grails.validation.ValidationException
 
@@ -197,7 +198,7 @@ class MockUtils {
             else if (map["view"] != null) {
                 assert map["text"] == null : "'text' cannot be used with 'view' in render"
 
-                modelAndView["view"] = map["view"]
+                modelAndView["view"] =  [getName: {map["view"]}] as View
                 modelAndView["model"] = map["model"]
             }
             else if (map["text"] != null) {
