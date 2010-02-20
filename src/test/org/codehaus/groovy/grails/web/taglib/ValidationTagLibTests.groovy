dbdeac3b65233febabc1b086655af555def08ec7
Burt Beckwith <burt@burtbeckwith.com>
2010-02-20 01:37:50 -0500

    GRAILS-5618 fix based on 9add1ea4a9a5057911dbf1ad50bc4d15fdd5dbeb and ca9c6a1b59014f3a44971c7535b98522b475bcec from niclupien

--- src/test/org/codehaus/groovy/grails/web/taglib/ValidationTagLibTests.groovy
+++ src/test/org/codehaus/groovy/grails/web/taglib/ValidationTagLibTests.groovy
@@ -1,14 +1,11 @@
-package org.codehaus.groovy.grails.web.taglib;
+package org.codehaus.groovy.grails.web.taglib
 
-import java.util.Locale;
+import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
 
+import org.springframework.context.MessageSourceResolvable
+import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.validation.Errors
 import org.springframework.validation.FieldError
-import org.springframework.util.StringUtils
-import org.springframework.web.context.request.RequestContextHolder
-import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
-import org.springframework.context.MessageSourceResolvable
-import org.springframework.context.i18n.LocaleContextHolder;
 
 class ValidationTagLibTests extends AbstractGrailsTagTests {
 
@@ -36,22 +33,21 @@ class Article {
 
     void testFieldValueWithClassAndPropertyNameLookupFromBundle() {
         def domain = ga.getDomainClass("Book")
-        
+
         LocaleContextHolder.setLocale(Locale.US)
         messageSource.addMessage("Book.label", Locale.US, "Reading Material")
         messageSource.addMessage("Book.title.label", Locale.US, "Subject")
         def b = domain.newInstance()
         b.validate()
-        assert b.hasErrors()
+        assertTrue b.hasErrors()
 
         def template = '<g:fieldError bean="${book}" field="title" />'
 
         webRequest.currentRequest.addPreferredLocale(Locale.US)
         assertOutputEquals 'Property [Subject] of class [Reading Material] cannot be null', template, [book:b]
-
     }
 
-   void testFieldValueWithShortClassAndPropertyNameLookupFromBundle() {
+    void testFieldValueWithShortClassAndPropertyNameLookupFromBundle() {
         def domain = ga.getDomainClass("Book")
 
         LocaleContextHolder.setLocale(Locale.US)
@@ -59,20 +55,19 @@ class Article {
         messageSource.addMessage("book.title.label", Locale.US, "Subject")
         def b = domain.newInstance()
         b.validate()
-        assert b.hasErrors()
+        assertTrue b.hasErrors()
 
         def template = '<g:fieldError bean="${book}" field="title" />'
 
         webRequest.currentRequest.addPreferredLocale(Locale.US)
         assertOutputEquals 'Property [Subject] of class [Reading Material] cannot be null', template, [book:b]
-
     }
 
     void testRenderErrorTag() {
         def domain = ga.getDomainClass("Book")
         def b = domain.newInstance()
         b.validate()
-        assert b.hasErrors()
+        assertTrue b.hasErrors()
 
         def template = '''<g:fieldError bean="${book}" field="title" />'''
 
@@ -82,7 +77,6 @@ class Article {
         b.validate()
         assertOutputEquals '', template, [book:b]
         assertOutputEquals '', template, [book:domain.newInstance()]
-
     }
 
     void testFieldValueHtmlEscaping() {
@@ -101,14 +95,14 @@ class Article {
     void testFieldValueTag() {
         def b = ga.getDomainClass("Book").newInstance()
         b.properties = [publisherURL:"a_bad_url"]
-        assert b.hasErrors()
+        assertTrue b.hasErrors()
 
         def template = '''<g:fieldValue bean="${book}" field="publisherURL" />'''
 
         assertOutputEquals("a_bad_url", template, [book:b])
 
         b.properties = [publisherURL:"http://google.com"]
-        assert !b.hasErrors()
+        assertFalse b.hasErrors()
 
         assertOutputEquals("http://google.com", template, [book:b])
     }
@@ -123,7 +117,7 @@ class Article {
         def template = '<g:fieldValue bean="${book}" field="usPrice" />'
         assertOutputEquals("1,045.99", template, [book:b])
 
-        webRequest.currentRequest.removeAttribute(GrailsApplicationAttributes.PROPERTY_REGISTRY) 
+        webRequest.currentRequest.removeAttribute(GrailsApplicationAttributes.PROPERTY_REGISTRY)
         // And then with German.
         webRequest.currentRequest.addPreferredLocale(Locale.GERMANY)
         assertOutputEquals("1.045,99", template, [book:b])
@@ -137,11 +131,31 @@ class Article {
         assertOutputEquals("1.045,456", template, [book:b])
     }
 
+    void testFieldValueTagWithFrenchLocaleInTextField() {
+        def b = ga.getDomainClass("Book").newInstance()
+        b.properties = [publisherURL:"http://google.com", usPrice: 1045.99]
+
+        String template = '''<g:textField name="usPrice" value="${fieldValue(bean: book, field: 'usPrice')}" />'''
+
+        // First test with English.
+        webRequest.currentRequest.addPreferredLocale(Locale.US)
+
+        assertOutputEquals '<input type="text" name="usPrice" value="1,045.99" id="usPrice" />',
+                template, [book:b]
+
+        webRequest.currentRequest.removeAttribute(GrailsApplicationAttributes.PROPERTY_REGISTRY)
+        // And then with French.
+        webRequest.currentRequest.addPreferredLocale(Locale.FRENCH)
+
+        assertOutputEquals '<input type="text" name="usPrice" value="1&nbsp;045,99" id="usPrice" />',
+                template, [book:b]
+    }
+
     void testHasErrorsTag() {
         def b = ga.getDomainClass("Book").newInstance()
         b.validate()
 
-        assert b.hasErrors()
+        assertTrue b.hasErrors()
 
         def template = '''<g:hasErrors bean="${book}">success</g:hasErrors>'''
 
@@ -151,7 +165,7 @@ class Article {
         b.title = "Groovy In Action"
         b.publisherURL = new URL("http://canoo.com/gia")
         b.validate()
-        assert b.hasErrors()
+        assertTrue b.hasErrors()
         assertOutputEquals("success", '''<g:hasErrors bean="${book}" field="releaseDate">success</g:hasErrors>''', [book:b])
         assertOutputEquals("success", '''<g:hasErrors model="[book:book]" field="releaseDate">success</g:hasErrors>''', [book:b])
         assertOutputEquals("", '''<g:hasErrors bean="${book}" field="title">success</g:hasErrors>''', [book:b])
@@ -163,18 +177,18 @@ class Article {
         b.releaseDate = new Date()
         b.usPrice = 10.99
 
-        assert b.validate()
-        assert !b.hasErrors()
+        assertTrue b.validate()
+        assertFalse b.hasErrors()
 
         assertOutputEquals("", template, [book:b])
 
-        b = ga.getDomainClass("Book").newInstance();
+        b = ga.getDomainClass("Book").newInstance()
         b.title = "Groovy In Action"
         b.publisherURL = new URL("http://canoo.com/gia")
         b.releaseDate = new Date()
         b.usPrice = 10.99
         b.validate()
-        def a = ga.getDomainClass("Article").newInstance();
+        def a = ga.getDomainClass("Article").newInstance()
         a.validate()
         assertOutputEquals("success", '''<g:hasErrors bean="${article}">success</g:hasErrors>''', [book:b,article:a])
         assertOutputEquals("success", '''<g:hasErrors bean="${article}" field="title">success</g:hasErrors>''', [book:b,article:a])
@@ -193,12 +207,11 @@ class Article {
         def b = ga.getDomainClass("Book").newInstance()
         b.validate()
 
-        assert b.hasErrors()
+        assertTrue b.hasErrors()
 
         def template = '''<g:eachError var="err" bean="${book}">${err.field}|</g:eachError>'''
 
         def result = applyTemplate(template, [book:b])
-        println result
         assertTrue result.contains("title|")
         assertTrue result.contains("releaseDate|")
         assertTrue result.contains("publisherURL|")
@@ -214,7 +227,7 @@ class Article {
         def b = ga.getDomainClass("Book").newInstance()
         b.validate()
 
-        assert b.hasErrors()
+        assertTrue b.hasErrors()
 
         def template = '''<g:renderErrors bean="${book}" />'''
 
@@ -231,8 +244,8 @@ class Article {
         b.releaseDate = new Date()
         b.usPrice = 10.99
 
-        assert b.validate()
-        assert !b.hasErrors()
+        assertTrue b.validate()
+        assertFalse b.hasErrors()
 
         assertOutputEquals("<ul></ul>", template, [book:b])
     }
@@ -241,8 +254,8 @@ class Article {
         def b = ga.getDomainClass("Book").newInstance()
         b.validate()
 
-        assert b.hasErrors()
-        request.setAttribute("bookInstance", b);
+        assertTrue b.hasErrors()
+        request.setAttribute("bookInstance", b)
 
         def template = '''<g:renderErrors as="list" />'''
 
@@ -258,15 +271,13 @@ class Article {
         def b = ga.getDomainClass("Book").newInstance()
         b.validate()
 
-        assert b.hasErrors()
+        assertTrue b.hasErrors()
 
         def template = '''<g:renderErrors bean="${book}" as="xml" />'''
 
         def result = applyTemplate(template,[book:b])
 
-        println result
         def xml = new XmlSlurper().parseText(result)
-
 
         assertEquals 4, xml.error.size()
         assertEquals "Book", xml.error[0].@object.text()
@@ -275,23 +286,23 @@ class Article {
     }
 
     void testHasErrorsWithRequestAttributes() {
-		StringWriter sw = new StringWriter();
+        StringWriter sw = new StringWriter()
 
-		withTag("hasErrors", sw) { tag ->
+        withTag("hasErrors", sw) { tag ->
 
             def mockErrors = [hasErrors:{true}]
-            
-	        request.setAttribute("somethingErrors", mockErrors as Errors);
 
-			// test when no message found it returns code
-			def attrs = [:]
-			tag.call( attrs, { "error found"} )
+            request.setAttribute("somethingErrors", mockErrors as Errors)
 
-			assertEquals "error found", sw.toString()
-		}
-	}
+            // test when no message found it returns code
+            def attrs = [:]
+            tag.call( attrs, { "error found"} )
 
-	void testMessageTagWithError() {
+            assertEquals "error found", sw.toString()
+        }
+    }
+
+    void testMessageTagWithError() {
         def error = new FieldError("foo", "bar",1, false, ["my.error.code"] as String[], null, "This is default")
         def template = '<g:message error="${error}" />'
 
@@ -309,10 +320,10 @@ class Article {
 
     }
 
-	void testMessageTagWithBlankButExistingMessageBundleValue() {
-	    
-	    messageSource.addMessage( "test.blank.message", Locale.ENGLISH, "")
-	    
+    void testMessageTagWithBlankButExistingMessageBundleValue() {
+
+        messageSource.addMessage( "test.blank.message", Locale.ENGLISH, "")
+
         def template = '<g:message code="test.blank.message" />'
 
         assertOutputEquals("", template, [:])
@@ -329,5 +340,4 @@ class Article {
 
         assertOutputEquals("The Default Message", template, [message: resolvable])
     }
-
 }
