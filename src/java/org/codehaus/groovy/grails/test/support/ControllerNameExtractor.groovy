565768eb4fbaf987749b21513c6ebb74132f4eff
Burt Beckwith <burt@burtbeckwith.com>
2010-03-13 11:10:03 -0500

    GRAILS-5998 using GrailsNameUtils.getPropertyName to properly convert class name to controller name

--- src/java/org/codehaus/groovy/grails/test/support/ControllerNameExtractor.groovy
+++ src/java/org/codehaus/groovy/grails/test/support/ControllerNameExtractor.groovy
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,17 +22,17 @@ class ControllerNameExtractor {
 
     static String extractControllerNameFromTestClassName(String testClassName, String[] testClassSuffixes) {
         if (!testClassSuffixes) testClassSuffixes = [''] as String[]
-        
+
         def matchingTail
-        testClassSuffixes.find { 
+        testClassSuffixes.find {
             def tail = "Controller$it"
             if (testClassName.endsWith(tail)) matchingTail = tail }
-        
+
         if (matchingTail) {
             GrailsNameUtils.getPropertyName(testClassName[0..(testClassName.size() - matchingTail.size() - 1)])
-        } else {
+        }
+        else {
             null
         }
-    } 
-    
+    }
 }
