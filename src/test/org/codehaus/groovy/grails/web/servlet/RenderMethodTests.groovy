grails/grails-core

commit fe441ac8c4f375798658c62f583447ddc58624b6

CharSequence (previously String & GString only) support to RenderDynamicMethod. Added test for GString & StreamCharBuffer .

src/java/org/codehaus/groovy/grails/web/metaclass/RenderDynamicMethod.java
@@ -115,9 +115,9 @@ public class RenderDynamicMethod extends AbstractDynamicMethodInvocation {
 
         boolean renderView = true;
         GroovyObject controller = (GroovyObject) target;
-        if ((arguments[0] instanceof String) || (arguments[0] instanceof GString)) {
+        if (arguments[0] instanceof CharSequence) {
             setContentType(response, TEXT_HTML, DEFAULT_ENCODING,true);
-            String text = arguments[0].toString();
+            CharSequence text = (CharSequence)arguments[0];
             renderView = renderText(text, response);
         } else if (arguments[0] instanceof Closure) {
             setContentType(response, TEXT_HTML, gspEncoding, true);
@@ -163,11 +163,12 @@ public class RenderDynamicMethod extends AbstractDynamicMethodInvocation {
                 } else {
                     renderView = renderMarkup(callable, response);
                 }
-            } else if (arguments[arguments.length - 1] instanceof String) {
-                String text = (String) arguments[arguments.length - 1];
+            } else if (arguments[arguments.length - 1] instanceof CharSequence) {
+            	CharSequence text = (CharSequence) arguments[arguments.length - 1];
                 renderView = renderText(text, out);
             } else if (argMap.containsKey(ARGUMENT_TEXT)) {
-                String text = argMap.get(ARGUMENT_TEXT).toString();
+            	Object textArg = argMap.get(ARGUMENT_TEXT);
+            	CharSequence text = (textArg instanceof CharSequence) ? ((CharSequence)textArg) : textArg.toString();
                 renderView = renderText(text, out);
             } else if (argMap.containsKey(ARGUMENT_VIEW)) {
 
@@ -197,7 +198,7 @@ public class RenderDynamicMethod extends AbstractDynamicMethodInvocation {
         String templateName = argMap.get(ARGUMENT_TEMPLATE).toString();
         String contextPath = getContextPath(webRequest, argMap);
 
-        String var = (String) argMap.get(ARGUMENT_VAR);
+        String var = String.valueOf(argMap.get(ARGUMENT_VAR));
         // get the template uri
         String templateUri = webRequest.getAttributes().getTemplateURI(controller, templateName);
 
@@ -386,7 +387,7 @@ public class RenderDynamicMethod extends AbstractDynamicMethodInvocation {
         return renderView;
     }
 
-    private boolean renderText(String text, HttpServletResponse response) {
+    private boolean renderText(CharSequence text, HttpServletResponse response) {
         try {
             PrintWriter writer = response.getWriter();
             return renderText(text, writer);
@@ -396,9 +397,13 @@ public class RenderDynamicMethod extends AbstractDynamicMethodInvocation {
         }
     }
 
-    private boolean renderText(String text, Writer writer) {
+    private boolean renderText(CharSequence text, Writer writer) {
         try {
-            writer.write(text);
+        	if(writer instanceof PrintWriter) {
+        		((PrintWriter)writer).print(text);
+        	} else {
+        		writer.write(text.toString());
+        	}
             return false;
         } catch (IOException e) {
             throw new ControllerExecutionException(e.getMessage(), e);

src/java/org/codehaus/groovy/grails/web/sitemesh/GrailsRoutablePrintWriter.java
@@ -42,12 +42,14 @@ public class GrailsRoutablePrintWriter extends GrailsPrintWriter {
             } catch (IOException e) {
                 setError();
             }
+            super.out = destination;
         }
         return destination;
     }
 
     public void updateDestination(DestinationFactory factory) {
         destination = null;
+        super.out = destination;
         this.factory = factory;
     }
 

src/test/org/codehaus/groovy/grails/web/servlet/mvc/RenderDynamicMethodTests.groovy
@@ -29,6 +29,16 @@ class RenderDynamicMethodTests extends AbstractGrailsControllerTests {
 		        render "text"
 		   }
 
+		   def renderStreamCharBuffer = {
+				def writer = new org.codehaus.groovy.grails.web.pages.FastStringWriter()
+				writer.write("text")
+				render writer.buffer
+		   }
+
+		   def renderGString = {
+				render "${'te' + 'xt'}"
+		   }
+
 		   def renderTextWithContentType = {
 		        render(text:"<foo>bar</foo>",contentType:"text/xml", encoding:"utf-16")
            }
@@ -77,7 +87,23 @@ class RenderDynamicMethodTests extends AbstractGrailsControllerTests {
         assertEquals "text", response.contentAsString
     }
 
-    void testRenderTextWithContentType() {
+	void testRenderStreamCharBuffer() {
+		def testCtrl = ga.getControllerClass("TestController").newInstance()
+		
+		testCtrl.renderStreamCharBuffer()
+		assertEquals "text/html;charset=utf-8", response.contentType
+		assertEquals "text", response.contentAsString
+	}
+	
+	void testRenderGString() {
+		def testCtrl = ga.getControllerClass("TestController").newInstance()
+		
+		testCtrl.renderGString()
+		assertEquals "text/html;charset=utf-8", response.contentType
+		assertEquals "text", response.contentAsString
+	}
+		
+	void testRenderTextWithContentType() {
         def testCtrl = ga.getControllerClass("TestController").newInstance()
 
         testCtrl.renderTextWithContentType()

commit 0115780b49d39e7be6dd12acf0b3b7018cdf05d6

GRAILS-6035 - fixed the render method so it supports the use of the implicit 'it' variable in a template.
The problem was a small regression introduced with http://github.com/grails/grails-core/commit/fe441ac8c4f375798658c62f583447ddc58624b6.  

The problem is that code like this:

String var = (String) argMap.get(ARGUMENT_VAR);

Was changed to this:

String var = String.valueOf(argMap.get(ARGUMENT_VAR))

The problem with this is if argMap.get(...) returns null, String.valueOf(...) will turn that into the string "null" so "null" became the value of var.

src/java/org/codehaus/groovy/grails/web/metaclass/RenderDynamicMethod.java
@@ -198,7 +198,11 @@ public class RenderDynamicMethod extends AbstractDynamicMethodInvocation {
         String templateName = argMap.get(ARGUMENT_TEMPLATE).toString();
         String contextPath = getContextPath(webRequest, argMap);
 
-        String var = String.valueOf(argMap.get(ARGUMENT_VAR));
+        String var = null;
+        if(argMap.containsKey(ARGUMENT_VAR)) {
+        	var = String.valueOf(argMap.get(ARGUMENT_VAR));
+        }
+        
         // get the template uri
         String templateUri = webRequest.getAttributes().getTemplateURI(controller, templateName);
 

src/test/org/codehaus/groovy/grails/web/servlet/RenderMethodTests.groovy
@@ -143,6 +143,34 @@ class RenderMethodTests extends AbstractGrailsControllerTests {
         assertEquals "text/html;charset=utf-8", response.contentType
 	}
 
+    void testRenderTemplateWithCollectionUsingImplicitITVariable() {
+    	def mockController = ga.getControllerClass('RenderController').newInstance()
+		
+		request.setAttribute GrailsApplicationAttributes.CONTROLLER, mockController
+		def resourceLoader = new MockStringResourceLoader()
+		resourceLoader.registerMockResource '/render/_peopleTemplate.gsp', '${it.firstName} ${it.middleName}<br/>'
+		appCtx.groovyPagesTemplateEngine.resourceLoader = resourceLoader
+		webRequest.controllerName = 'render'
+		mockController.renderTemplateWithCollection.call()
+		
+		def resopnse = mockController.response
+		assertEquals 'Jacob Ray<br/>Zachary Scott<br/>', response.contentAsString
+	}
+
+	void testRenderTemplateWithCollectionUsingExplicitVariableName() {
+		def mockController = ga.getControllerClass('RenderController').newInstance()
+		
+		request.setAttribute GrailsApplicationAttributes.CONTROLLER, mockController
+		def resourceLoader = new MockStringResourceLoader()
+		resourceLoader.registerMockResource '/render/_peopleTemplate.gsp', '${person.firstName} ${person.middleName}<br/>'
+		appCtx.groovyPagesTemplateEngine.resourceLoader = resourceLoader
+		webRequest.controllerName = 'render'
+		mockController.renderTemplateWithCollectionAndExplicitVarName.call()
+		
+		def resopnse = mockController.response
+		assertEquals 'Jacob Ray<br/>Zachary Scott<br/>', response.contentAsString
+	}
+	
 	void testRenderTemplateWithContentType() {
        def mockController = ga.getControllerClass("RenderController").newInstance()
 
@@ -199,6 +227,14 @@ class RenderController {
 	def renderTemplate = {
 		render(template:"testTemplate", model:[hello:"world"])
 	}
+	def renderTemplateWithCollection = {
+		def people = [[firstName: 'Jacob', middleName: 'Ray'], [firstName: 'Zachary', middleName: 'Scott']]
+   		render(template:"peopleTemplate", collection: people)
+   	}
+	def renderTemplateWithCollectionAndExplicitVarName = {
+    		def people = [[firstName: 'Jacob', middleName: 'Ray'], [firstName: 'Zachary', middleName: 'Scott']]
+       		render(var: 'person', template:"peopleTemplate", collection: people)
+	}			
 	def renderXmlTemplate = {
 		render(template:"xmlTemplate",contentType:"text/xml")
 	}