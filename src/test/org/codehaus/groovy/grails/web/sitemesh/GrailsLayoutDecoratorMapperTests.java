883fd46016d52949abfd5d782aba89b610242171
Jeff Brown <jeff@jeffandbetsy.net>
2010-03-22 06:35:55 -0500

    GRAILS-1640 - add support for an application level default sitemesh layout.  The default is grails-app/views/layout/application.gsp.  The name of the default layout may be overridden by defining grails.sitemesh.default.layout in Config.groovy.

--- src/test/org/codehaus/groovy/grails/web/sitemesh/GrailsLayoutDecoratorMapperTests.java
+++ src/test/org/codehaus/groovy/grails/web/sitemesh/GrailsLayoutDecoratorMapperTests.java
@@ -3,11 +3,14 @@ package org.codehaus.groovy.grails.web.sitemesh;
 import grails.util.GrailsWebUtil;
 import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyObject;
+import groovy.util.ConfigObject;
+import groovy.util.ConfigSlurper;
 
 import javax.servlet.ServletContext;
 
 import junit.framework.TestCase;
 
+import org.codehaus.groovy.grails.commons.ConfigurationHolder;
 import org.codehaus.groovy.grails.support.MockApplicationContext;
 import org.codehaus.groovy.grails.web.pages.DefaultGroovyPagesUriService;
 import org.codehaus.groovy.grails.web.pages.GroovyPagesUriService;
@@ -59,6 +62,71 @@ public class GrailsLayoutDecoratorMapperTests extends TestCase {
 		
 	}
 	
+	public void testDecoratedByApplicationConvention() throws Exception {
+        GrailsWebRequest webRequest = buildMockRequest();
+        MockApplicationContext appCtx = (MockApplicationContext)webRequest.getApplicationContext();
+        appCtx.registerMockResource("WEB-INF/grails-app/views/layouts/application.gsp", "<html><body><h1>Default Layout</h1><g:layoutBody /></body></html>");
+
+        MockHttpServletRequest request = new MockHttpServletRequest("GET", "orders/list");
+		ServletContext context = webRequest.getServletContext();
+		GroovyClassLoader gcl = new GroovyClassLoader();
+
+		// create mock controller
+		GroovyObject controller = (GroovyObject)gcl.parseClass("class FooController {\n" +
+				"def controllerName = 'foo'\n" +
+				"def actionUri = '/foo/fooAction'\n" +
+		"}").newInstance();
+
+		request.setAttribute(GrailsApplicationAttributes.CONTROLLER, controller);
+        GrailsLayoutDecoratorMapper m = new GrailsLayoutDecoratorMapper();
+		Config c = new Config(new MockServletConfig(context));
+		m.init(c, null, null);
+		HTMLPageParser parser = new HTMLPageParser();
+		String html = "<html><head><title>Foo title</title></head><body>here is the body</body></html>";
+
+		Page page = parser.parse( html.toCharArray() );
+		Decorator d = m.getDecorator(request, page);
+		assertNotNull(d);
+		assertEquals("/WEB-INF/grails-app/views/layouts/application.gsp", d.getPage());
+		assertEquals("application", d.getName());
+	}
+
+	public void testOverridingDefaultTemplateViaConfig() throws Exception {
+		try {
+	        ConfigObject config = new ConfigSlurper().parse("grails.sitemesh.default.layout='otherApplication'");
+	        ConfigurationHolder.setConfig(config);
+			GrailsWebRequest webRequest = buildMockRequest();
+			MockApplicationContext appCtx = (MockApplicationContext)webRequest.getApplicationContext();
+			appCtx.registerMockResource("WEB-INF/grails-app/views/layouts/application.gsp", "<html><body><h1>Default Layout</h1><g:layoutBody /></body></html>");
+			appCtx.registerMockResource("WEB-INF/grails-app/views/layouts/otherApplication.gsp", "<html><body><h1>Other Default Layout</h1><g:layoutBody /></body></html>");
+
+			MockHttpServletRequest request = new MockHttpServletRequest("GET", "orders/list");
+			ServletContext context = webRequest.getServletContext();
+			GroovyClassLoader gcl = new GroovyClassLoader();
+
+			// create mock controller
+			GroovyObject controller = (GroovyObject)gcl.parseClass("class FooController {\n" +
+					"def controllerName = 'foo'\n" +
+					"def actionUri = '/foo/fooAction'\n" +
+			"}").newInstance();
+
+			request.setAttribute(GrailsApplicationAttributes.CONTROLLER, controller);
+			GrailsLayoutDecoratorMapper m = new GrailsLayoutDecoratorMapper();
+			Config c = new Config(new MockServletConfig(context));
+			m.init(c, null, null);
+			HTMLPageParser parser = new HTMLPageParser();
+			String html = "<html><head><title>Foo title</title></head><body>here is the body</body></html>";
+
+			Page page = parser.parse( html.toCharArray() );
+			Decorator d = m.getDecorator(request, page);
+			assertNotNull(d);
+			assertEquals("/WEB-INF/grails-app/views/layouts/otherApplication.gsp", d.getPage());
+			assertEquals("otherApplication", d.getName());
+		} finally {
+			ConfigurationHolder.setConfig(null);
+		}
+	}
+
 	public void testDecoratedByControllerConvention() throws Exception {
         GrailsWebRequest webRequest = buildMockRequest();
         MockApplicationContext appCtx = (MockApplicationContext)webRequest.getApplicationContext();
