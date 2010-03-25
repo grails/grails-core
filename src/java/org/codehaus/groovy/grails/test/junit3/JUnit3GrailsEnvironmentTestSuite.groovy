1b0e6c6ec9f42a0f42dd792d55dce23b4d1dc7a6
Burt Beckwith <burt@burtbeckwith.com>
2010-03-24 21:53:51 -0400

    GRAILS-5622 fix for 1.2.x; manually applied 7d1e5c3e248d7395099b5085efffb0720dbc1a26, for some reason couldn't cherry-pick

--- src/java/org/codehaus/groovy/grails/test/junit3/JUnit3GrailsEnvironmentTestSuite.groovy
+++ src/java/org/codehaus/groovy/grails/test/junit3/JUnit3GrailsEnvironmentTestSuite.groovy
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 
 package org.codehaus.groovy.grails.test.junit3
 
+import grails.util.GrailsNameUtils
 import junit.framework.Test
 import junit.framework.TestResult
 import junit.framework.TestSuite
@@ -31,22 +32,22 @@ import org.codehaus.groovy.grails.test.support.GrailsTestTransactionInterceptor
 import org.codehaus.groovy.grails.test.support.ControllerNameExtractor
 
 /**
- * A Grails specific test suite that runs tests in a “Grails” environment. That is,
+ * A Grails specific test suite that runs tests in a "Grails" environment. That is,
  * subjects the tests to autowiring of dependencies (by name), establishes a request like environment,
  * and inside a rollback only transaction (unless the test is not transactional).
- * 
+ *
  * @see GrailsTestAutowirer
  * @see GrailsTestRequestEnvironmentInterceptor
  * @see GrailsTestTransactionInterceptor
  */
 class JUnit3GrailsEnvironmentTestSuite extends TestSuite {
-    
+
     GrailsTestAutowirer autowirer
     GrailsTestRequestEnvironmentInterceptor requestEnvironmentInterceptor
     GrailsTestTransactionInterceptor transactionInterceptor
-    
-	final JUnit3GrailsTestTypeMode mode
-	
+
+    final JUnit3GrailsTestTypeMode mode
+
     /**
      * @see TestSuite(Class)
      */
@@ -65,7 +66,7 @@ class JUnit3GrailsEnvironmentTestSuite extends TestSuite {
         requestEnvironmentInterceptor = new GrailsTestRequestEnvironmentInterceptor(applicationContext)
         transactionInterceptor = new GrailsTestTransactionInterceptor(applicationContext)
     }
-    
+
     void runTest(Test test, TestResult result) {
         if (mode.autowire) {
             autowirer.autowire(test)
@@ -84,6 +85,7 @@ class JUnit3GrailsEnvironmentTestSuite extends TestSuite {
         if (mode.wrapInRequestEnvironment) {
             def controllerName = ControllerNameExtractor.extractControllerNameFromTestClassName(test.class.name, JUnit3GrailsTestType.TESTS_SUFFIX)
             if (controllerName) {
+                controllerName = GrailsNameUtils.getPropertyName(controllerName)
                 inRequestRunner = { requestEnvironmentInterceptor.doInRequestEnvironment(controllerName, inTransactionRunner) }
             } else {
                 inRequestRunner = { requestEnvironmentInterceptor.doInRequestEnvironment(inTransactionRunner) }
@@ -91,7 +93,7 @@ class JUnit3GrailsEnvironmentTestSuite extends TestSuite {
         } else {
             inRequestRunner = inTransactionRunner
         }
-        
+
         inRequestRunner()
     }
 }
