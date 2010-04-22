b3878bdd1ef32c8e219d4c6bc1a8ff4a5b604049
Burt Beckwith <burt@burtbeckwith.com>
2010-04-22 14:06:10 -0400

    GRAILS-6179 was using the wrong variable name

--- scripts/TestApp.groovy
+++ scripts/TestApp.groovy
@@ -49,39 +49,39 @@ TEST_PHASE_AND_TYPE_SEPARATOR = ':'
 target('default': "Run a Grails applications unit tests") {
     depends(checkVersion, configureProxy, parseArguments, cleanTestReports)
 
-    // The test targeting patterns 
+    // The test targeting patterns
     def testTargeters = []
-    
+
     // The params that target a phase and/or type
     def phaseAndTypeTargeters = []
-    
+
     // Separate the type/phase targeters from the test targeters
-    for(p in argsMap["params"]) { 
+    for(p in argsMap["params"]) {
         def destination = p.contains(TEST_PHASE_AND_TYPE_SEPARATOR) ? phaseAndTypeTargeters : testTargeters
         destination << p
     }
 
     // If we are targeting tests, set testNames (from _GrailsTest)
     if (testTargeters) testNames = testTargeters
-    
+
     // treat pre 1.2 phase targeting args as '[phase]:' for backwards compatibility
     for(type in ["unit", "integration", "functional", "other"]) {
         if (argsMap[type]) {
-            phaseAndTypeTargeters << "${it}${TEST_PHASE_AND_TYPE_SEPARATOR}"
+            phaseAndTypeTargeters << "${type}${TEST_PHASE_AND_TYPE_SEPARATOR}"
             argsMap.remove(type) // these are not test "options"
         }
     }
-    
+
     // process the phaseAndTypeTargeters, populating the targetPhasesAndTypes map from _GrailsTest
     for(t in phaseAndTypeTargeters) {
         def parts = t.split(TEST_PHASE_AND_TYPE_SEPARATOR, 2)
         def targetPhase = parts[0] ?: TEST_PHASE_WILDCARD
         def targetType = parts[1] ?: TEST_TYPE_WILDCARD
-        
+
         if (!targetPhasesAndTypes.containsKey(targetPhase)) targetPhasesAndTypes[targetPhase] = []
         targetPhasesAndTypes[targetPhase] << targetType
     }
-    
+
     // Any switch style args are "test options" (from _GrailsTest)
     for(a in argsMap) {
         if (a.key != 'params') {
