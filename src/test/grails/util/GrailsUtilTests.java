11a59274975443abdc481df7f162a17d227a3684
Jeff Brown <jeff@jeffandbetsy.net>
2010-05-17 06:44:27 -0500

    set version number to 1.3.1

--- src/test/grails/util/GrailsUtilTests.java
+++ src/test/grails/util/GrailsUtilTests.java
@@ -36,7 +36,7 @@ public class GrailsUtilTests extends TestCase {
 
 
     public void testGrailsVersion() {
-        assertEquals("1.3.1.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("1.3.1", GrailsUtil.getGrailsVersion());
     }
 
     protected void tearDown() throws Exception {
