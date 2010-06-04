2068af219e905ebcfbcfc5ca69a03d62c1e1af96
Graeme Rocher <graeme.rocher@gmail.com>
2010-06-04 14:17:52 +0200

    test for GRAILS-6247, marked as not yet implemented

---
+++ src/test/org/codehaus/groovy/grails/web/servlet/mvc/ControllerInheritanceTests.groovy
@@ -0,0 +1,41 @@
+package org.codehaus.groovy.grails.web.servlet.mvc
+
+class ControllerInheritanceTests extends AbstractGrailsControllerTests {
+	
+	@Override
+	protected void onSetUp() {
+		gcl.parseClass '''
+class ControllerInheritanceFooBaseController {
+
+	protected void bar(){ 
+		println('bar in base class') 
+	}
+}
+		
+class ControllerInheritanceFooController extends ControllerInheritanceFooBaseController {
+
+	@Override
+	protected void bar(){
+		println "bar in subclass"
+		super.bar() 
+		println "bar after subclass"
+	}
+
+	def index = { 
+		bar() 
+		render 'hello' 
+	}
+}		
+		'''
+	}
+
+	// test for GRAILS-6247
+	void testCallSuperMethod() {
+		
+		if(notYetImplemented()) return
+		
+		def controller = ga.getControllerClass("ControllerInheritanceFooController").newInstance()
+		
+		controller.index()
+	}
+}
