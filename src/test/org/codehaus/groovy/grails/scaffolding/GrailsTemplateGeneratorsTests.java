be56264f5c962bc508a35419eb463911e2c0e9cb
Burt Beckwith <burt@burtbeckwith.com>
2010-06-12 17:06:48 -0400

    fixing failing tests

--- src/test/org/codehaus/groovy/grails/scaffolding/GrailsTemplateGeneratorsTests.java
+++ src/test/org/codehaus/groovy/grails/scaffolding/GrailsTemplateGeneratorsTests.java
@@ -17,33 +17,42 @@ package org.codehaus.groovy.grails.scaffolding;
 import grails.util.BuildSettings;
 import grails.util.BuildSettingsHolder;
 import groovy.lang.GroovyClassLoader;
-import junit.framework.TestCase;
-import org.codehaus.groovy.grails.commons.DefaultGrailsDomainClass;
-import org.codehaus.groovy.grails.commons.GrailsClassUtils;
-import org.codehaus.groovy.grails.commons.GrailsDomainClass;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
 
 import java.io.File;
 import java.io.StringWriter;
 import java.util.Map;
 
+import junit.framework.TestCase;
+
+import org.codehaus.groovy.grails.commons.DefaultGrailsDomainClass;
+import org.codehaus.groovy.grails.commons.GrailsClassUtils;
+import org.codehaus.groovy.grails.commons.GrailsDomainClass;
+import org.codehaus.groovy.grails.plugins.MockGrailsPluginManager;
+import org.codehaus.groovy.grails.plugins.PluginManagerHolder;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
 /**
  * @author Graeme Rocher
- * @since 09-Feb-2006
  */
+@SuppressWarnings("unchecked")
 public class GrailsTemplateGeneratorsTests extends TestCase {
 
+    @Override
     protected void setUp() {
         BuildSettings buildSettings = new BuildSettings(new File("."));
         BuildSettingsHolder.setSettings(buildSettings);
+        MockGrailsPluginManager pluginManager = new MockGrailsPluginManager();
+        PluginManagerHolder.setPluginManager(pluginManager);
+        pluginManager.registerMockPlugin(DefaultGrailsTemplateGeneratorTests.fakeHibernatePlugin);
     }
 
+    @Override
     protected void tearDown() {
         BuildSettingsHolder.setSettings(null);
+        PluginManagerHolder.setPluginManager(null);
     }
 
-    
     public void testGenerateController() throws Exception {
         GrailsTemplateGenerator generator;
 
@@ -55,22 +64,20 @@ public class GrailsTemplateGeneratorsTests extends TestCase {
         GrailsDomainClass domainClass = new DefaultGrailsDomainClass(dc);
 
         File generatedFile = new File("test/grails-app/controllers/TestController.groovy");
-        if(generatedFile.exists()) {
-        	generatedFile.delete();
+        if (generatedFile.exists()) {
+            generatedFile.delete();
         }
-
 
         StringWriter sw = new StringWriter();
         generator.generateController(domainClass,sw);
 
-        
         String text = sw.toString();
 
         Class controllerClass = gcl.parseClass(text);
         BeanWrapper bean = new BeanWrapperImpl(controllerClass.newInstance());
-        
+
         assertEquals("TestController", controllerClass.getName());
-        
+
         assertTrue(bean.isReadableProperty("list"));
         assertTrue(bean.isReadableProperty("update"));
         assertTrue(bean.isReadableProperty("create"));
@@ -78,14 +85,14 @@ public class GrailsTemplateGeneratorsTests extends TestCase {
         assertTrue(bean.isReadableProperty("show"));
         assertTrue(bean.isReadableProperty("edit"));
         assertTrue(bean.isReadableProperty("delete"));        
-        
+
         Object propertyValue = GrailsClassUtils.getStaticPropertyValue(controllerClass, "allowedMethods");
         assertTrue("allowedMethods property was the wrong type", propertyValue instanceof Map);
         Map map = (Map) propertyValue;
         assertTrue("allowedMethods did not contain the delete action", map.containsKey("delete"));
         assertTrue("allowedMethods did not contain the save action", map.containsKey("save"));
         assertTrue("allowedMethods did not contain the update action", map.containsKey("update"));
-        
+
         assertEquals("allowedMethods had incorrect value for delete action", "POST", map.get("delete"));
         assertEquals("allowedMethods had incorrect value for save action", "POST", map.get("save"));
         assertEquals("allowedMethods had incorrect value for update action", "POST", map.get("update"));
@@ -98,27 +105,23 @@ public class GrailsTemplateGeneratorsTests extends TestCase {
 
         generator = new DefaultGrailsTemplateGenerator();
 
-        Class dc = gcl.parseClass("class Test { " +
-                                        "\n Long id;" +
-                                        "\n Long version;" +
-                                        "\n String name;" +
-                                        "\n TimeZone tz;" +
-                                        "\n Locale locale;" +
-                                        "\n Currency currency;" +
-                                        "\n Boolean active;" +
-                                        "\n Date age  }");
+        Class dc = gcl.parseClass(
+                "class Test { " +
+                "\n Long id;" +
+                "\n Long version;" +
+                "\n String name;" +
+                "\n TimeZone tz;" +
+                "\n Locale locale;" +
+                "\n Currency currency;" +
+                "\n Boolean active;" +
+                "\n Date age  }");
         GrailsDomainClass domainClass = new DefaultGrailsDomainClass(dc);
 
         generator.generateViews(domainClass,"test");
 
-        File showFile = new File("test/grails-app/views/test/show.gsp");
-        assertTrue(showFile.exists());
-        File listFile = new File("test/grails-app/views/test/list.gsp");
-        assertTrue(listFile.exists());
-        File editFile = new File("test/grails-app/views/test/edit.gsp");
-        assertTrue(editFile.exists());
-        File createFile = new File("test/grails-app/views/test/create.gsp");
-        assertTrue(createFile.exists());
+        assertTrue(new File("test/grails-app/views/test/show.gsp").exists());
+        assertTrue(new File("test/grails-app/views/test/list.gsp").exists());
+        assertTrue(new File("test/grails-app/views/test/edit.gsp").exists());
+        assertTrue(new File("test/grails-app/views/test/create.gsp").exists());
     }
-
 }
