db55e6ea4853efe89aa14f3962263261ae304cde
Burt Beckwith <burt@burtbeckwith.com>
2010-06-16 04:43:16 -0400

    GRAILS-6405

---
+++ src/test/org/codehaus/groovy/grails/compiler/injection/GrailsASTUtilsTests.groovy
@@ -0,0 +1,25 @@
+package org.codehaus.groovy.grails.compiler.injection
+
+import org.codehaus.groovy.ast.ClassNode
+
+/**
+ * @author Burt Beckwith
+ */
+class GrailsASTUtilsTests extends GroovyTestCase {
+    
+    void testGetFurthestParent() {
+        def fooNode = new ClassNode(Foo)
+        def barNode = new ClassNode(Bar)
+        def bazNode = new ClassNode(Baz)
+        
+        assertEquals Foo.name, GrailsASTUtils.getFurthestParent(fooNode).name
+        assertEquals Foo.name, GrailsASTUtils.getFurthestParent(barNode).name
+        assertEquals Foo.name, GrailsASTUtils.getFurthestParent(bazNode).name
+    }
+}
+
+class Foo {}
+
+class Bar extends Foo {}
+
+class Baz extends Bar {}
