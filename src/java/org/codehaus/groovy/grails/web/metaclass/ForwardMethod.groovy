fddb87000c171e54a63aed054db2012dcf8df733
Jeff Brown <jeff@jeffandbetsy.net>
2010-08-13 20:09:20 -0500

    GRAILS-6618 - only clear the params if there are any

--- src/java/org/codehaus/groovy/grails/web/metaclass/ForwardMethod.groovy
+++ src/java/org/codehaus/groovy/grails/web/metaclass/ForwardMethod.groovy
@@ -44,7 +44,11 @@ class ForwardMethod {
     }
 
     String forward(HttpServletRequest request, HttpServletResponse response, Map params) {
-		request.parameterMap?.clear()
+		def paramMap = request.parameterMap
+		if(paramMap) {
+			paramMap.clear()
+		}
+
         def urlInfo = new ForwardUrlMappingInfo()
 
         GrailsWebRequest webRequest = GrailsWebRequest.lookup(request)
