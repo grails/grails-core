ec21c9b5b79a15bebb67cb8a4d33dd0004fb2af7
Alexander Zeillinger <aaz@uenterprise.de>
2010-09-04 04:53:53 -0700

    fixed typo (GRAILS-6505)

--- src/grails/grails-app/conf/DataSource.groovy
+++ src/grails/grails-app/conf/DataSource.groovy
@@ -14,7 +14,7 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:mem:devDB"
+            url = "jdbc:hsqldb:mem:devDb"
         }
     }
     test {
