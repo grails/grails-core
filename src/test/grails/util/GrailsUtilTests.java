9ff5a3a316f82f833f61eb89ed44c6cf9a6ced12
Jeff Brown <jeff@jeffandbetsy.net>
2010-10-11 12:40:14 -0500

    set version to 1.4.0.BUILD-SNAPSHOT

--- src/test/grails/util/GrailsUtilTests.java
+++ src/test/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("1.3.6.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("1.4.0.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @Override
