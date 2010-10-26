430bca8b2e2f796e4dffb4031615a769fcaeda43
Luke Daley <ld@ldaley.com>
2010-10-26 13:37:39 +1000

    GRAILS-6874 - support using the hibernate persistence interceptor in a nested fashion.
    I suspect this was always the intended style of usage, but the implementation was bugged.

---
+++ src/test/org/codehaus/groovy/grails/orm/hibernate/support/HibernatePersistenceContextInterceptorTests.groovy
@@ -0,0 +1,62 @@
+package org.codehaus.groovy.grails.orm.hibernate.support
+
+import org.codehaus.groovy.grails.orm.hibernate.AbstractGrailsHibernateTests
+import org.springframework.transaction.support.TransactionSynchronizationManager
+import org.springframework.orm.hibernate3.SessionFactoryUtils
+import org.springframework.orm.hibernate3.SessionHolder
+import org.hibernate.Session
+import org.hibernate.SessionFactory
+import java.util.concurrent.CountDownLatch
+
+/**
+ * @author Luke Daley
+ * @since 1.4
+ */
+class HibernatePersistenceContextInterceptorTests extends AbstractGrailsHibernateTests {
+
+    protected void setUp() {
+        super.setUp()
+        
+        // We are going to manage the session ourselves through the interceptor
+        def holder = TransactionSynchronizationManager.getResource(sessionFactory)
+        def s = holder.session
+        TransactionSynchronizationManager.unbindResource(sessionFactory)
+        SessionFactoryUtils.releaseSession(s, sessionFactory)       
+    }
+
+    void testSimpleLifecycle() {
+        def interceptor = getInterceptor()
+        assertEquals("interceptor open", false, interceptor.open)
+        interceptor.init()
+        assertEquals("interceptor open", true, interceptor.open)
+        interceptor.destroy()
+        assertEquals("interceptor open", false, interceptor.open)
+        interceptor.init()
+        assertEquals("interceptor open", true, interceptor.open)
+        interceptor.destroy()
+        assertEquals("interceptor open", false, interceptor.open)
+    }
+
+    void testNestedLifecycle() {
+        def interceptor = getInterceptor()
+        assertEquals("interceptor open", false, interceptor.open)
+        
+        interceptor.init()
+        assertEquals("interceptor open", true, interceptor.open)
+        interceptor.init()
+        assertEquals("interceptor open", true, interceptor.open)
+        interceptor.destroy()
+        assertEquals("interceptor open", true, interceptor.open)
+        interceptor.destroy()
+        assertEquals("interceptor open", false, interceptor.open)
+        
+        interceptor.init()
+        assertEquals("interceptor open", true, interceptor.open)
+        interceptor.destroy()
+        assertEquals("interceptor open", false, interceptor.open)
+    }
+    
+    protected getInterceptor() {
+        appCtx.persistenceInterceptor
+    }
+}
