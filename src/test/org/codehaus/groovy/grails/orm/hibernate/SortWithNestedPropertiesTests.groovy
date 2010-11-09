03633523e74ec5106358a71d0fc18b0d09f06e74
trygvea <trygve.amundsen@gmail.com>
2010-11-09 22:21:43 +0100

    whitespace and formatting

--- src/test/org/codehaus/groovy/grails/orm/hibernate/SortWithNestedPropertiesTests.groovy
+++ src/test/org/codehaus/groovy/grails/orm/hibernate/SortWithNestedPropertiesTests.groovy
@@ -4,53 +4,52 @@ package org.codehaus.groovy.grails.orm.hibernate
  * Test for GRAILS-3911
  */
 class SortWithNestedPropertiesTests extends AbstractGrailsHibernateTests {
-	
-	def bookClass
+    
+    def bookClass
 
     protected void onSetUp() {
         gcl.parseClass '''
-class Book {
-    Long id
-    Long version
-    String title
-    Author author
-    String publisher = 'Manning'
-    static namedQueries = {
-        manningBooks {
-            eq('publisher', 'Manning')
-        }
-    }
-    
-}
-
-class Author {
-    Long id
-    Long version
-    String name
-    Person person
-}
-
-class Person {
-    Long id
-    Long version
-    String name
-}
-'''
+            class Book {
+                Long id
+                Long version
+                String title
+                Author author
+                String publisher = 'Manning'
+                static namedQueries = {
+                    manningBooks {
+                        eq('publisher', 'Manning')
+                    }
+                }
+            }
+            
+            class Author {
+                Long id
+                Long version
+                String name
+                Person person
+            }
+            
+            class Person {
+                Long id
+                Long version
+                String name
+            }
+        '''
     }
 
     protected void setUp() {
-		super.setUp()
-		
-		def personClass = ga.getDomainClass('Person').clazz
-		def authorClass = ga.getDomainClass('Author').clazz
-		bookClass = ga.getDomainClass('Book').clazz
-		['C','A','b','a','c','B'].eachWithIndex { name, i ->
-			def person = personClass.newInstance(id:i, version:1, name:name).save(flush:true)
-			def author = authorClass.newInstance(id:i, version:1, name:name, person:person).save(flush:true)
-			bookClass.newInstance(id:i, version:1, title:'foo', author:author).save(flush:true)
-		}
-	}
-	
+        super.setUp()
+        
+        def personClass = ga.getDomainClass('Person').clazz
+        def authorClass = ga.getDomainClass('Author').clazz
+        bookClass = ga.getDomainClass('Book').clazz
+        ['C','A','b','a','c','B'].eachWithIndex { name, i ->
+            def person = personClass.newInstance(id:i, version:1, name:name).save(flush:true)
+            def author = authorClass.newInstance(id:i, version:1, name:name, person:person).save(flush:true)
+            bookClass.newInstance(id:i, version:1, title:'foo', author:author).save(flush:true)
+        }
+    }
+    
     void testListPersistentMethod() {
         assertEquals( ['A','a','b','B','C','c'], bookClass.list(sort:'author.name').author.name )
         assertEquals( ['A','B','C','a','b','c'], bookClass.list(sort:'author.name', ignoreCase:false).author.name )
@@ -71,9 +70,9 @@ class Person {
     void testFindByPersistentMethod() {
         assertEquals( 'A', bookClass.findByPublisher('Manning', [sort:'author.name']).author.name )
     }
-	
-	void testDeepSort() {
+    
+    void testDeepSort() {
         assertEquals( ['A','a','b','B','C','c'], bookClass.list(sort:'author.person.name').author.person.name )
-	}
+    }
 
 }
