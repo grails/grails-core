fa1a19d44011ac1b253437d324b9939488b7f979
Graeme Rocher <graeme.rocher@gmail.com>
2010-12-03 10:55:58 +0100

    fix for GRAILS-6215 "Array-indexed binding to Set associations is apparently meant to work but doesn't in 1.2.x and 1.3"
    Implemented by using a custom version of ListOrderedSet that actually implements the List interface to wrap the underlying set

---
+++ src/test/org/codehaus/groovy/grails/web/binding/DataBindingWithUnidirectionalAssociationTests.groovy
@@ -0,0 +1,65 @@
+package org.codehaus.groovy.grails.web.binding
+
+import org.codehaus.groovy.grails.orm.hibernate.AbstractGrailsHibernateTests;
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsParameterMap 
+import org.springframework.mock.web.MockHttpServletRequest 
+
+class DataBindingWithUnidirectionalAssociationTests extends AbstractGrailsHibernateTests{
+	
+	@Override
+	protected void onSetUp() {
+		gcl.parseClass '''
+import grails.persistence.*
+
+@Entity		
+class Author {
+
+    String name
+    
+    static constraints = {
+    }
+}
+
+@Entity
+class Book {
+    
+    String title
+    
+    static hasMany = [authors:Author]
+    
+    static constraints = {
+    }
+}
+		
+		
+		'''
+	}
+	
+	void testBindToNewInstance() {
+		def Author = ga.getDomainClass("Author").clazz
+		def Book = ga.getDomainClass("Book").clazz
+
+		def id1 = Author.newInstance(name:"Stephen King").save(flush:true).id
+		def id2 = Author.newInstance(name:"James Patterson").save(flush:true).id
+		
+		def book = Book.newInstance(title:"The Stand")
+		
+		def request = new MockHttpServletRequest()
+		request.addParameter("authors[0].id","$id1")
+		request.addParameter("authors[1].id","$id2")
+		
+		def params = new GrailsParameterMap(request)
+
+		book.properties = params
+		book.save(flush:true)
+		
+		session.clear()
+		
+		book = Book.findByTitle("The Stand")
+		
+		assert 2 == book.authors.size()
+		assert book.authors.find { it.name == "Stephen King" } != null
+
+	}
+
+}
