38657155dbd355e0a424e2a55847a70e9423fd4c
Jeff Brown <jeff@jeffandbetsy.net>
2011-02-15 15:35:11 -0600

    Upgrade to Groovy 1.7.8

--- src/java/org/codehaus/groovy/grails/resolve/IvyDependencyManager.groovy
+++ src/java/org/codehaus/groovy/grails/resolve/IvyDependencyManager.groovy
@@ -250,7 +250,7 @@ class IvyDependencyManager extends AbstractIvyDependencyManager implements Depen
                          "javax.servlet:jsp-api:2.1"
 
                 // dependencies needed for compilation
-                "${compileTimeDependenciesMethod}"("org.codehaus.groovy:groovy-all:1.7.8-SNAPSHOT") {
+                "${compileTimeDependenciesMethod}"("org.codehaus.groovy:groovy-all:1.7.8") {
                     excludes 'jline'
                 }
 
