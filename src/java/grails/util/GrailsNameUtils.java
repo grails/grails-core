9b1f8d6f5c864ba265e6794b129ff4bb562d113b
Michael Frankfurter <micfra@michael-pd01.(none)>
2011-02-17 00:29:12 +0100

    Fixed GRAILS-7280: added check if token is empty so StringIndexOutOfBounds is avoided in general which is the root cause of the error mentioned in the ticket.

--- src/java/grails/util/GrailsNameUtils.java
+++ src/java/grails/util/GrailsNameUtils.java
@@ -86,7 +86,7 @@ public class GrailsNameUtils {
             String[] tokens = name.split("[^\\w\\d]");
             for (String token1 : tokens) {
                 String token = token1.trim();
-                if (token.length() == null) continue;
+                if (token.length() == 0) continue;
                 buf.append(token.substring(0, 1).toUpperCase(Locale.ENGLISH))
                    .append(token.substring(1));
             }
