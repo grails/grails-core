df94f654b6466bdf2803312bb0617edbfb1df46b
Antony Jones <ant@interrobang.(none)>
2011-02-22 13:25:32 +0000

    Improved MessageTagTests before I begin work on the message tag.

--- src/test/org/codehaus/groovy/grails/web/taglib/MessageTagTests.groovy
+++ src/test/org/codehaus/groovy/grails/web/taglib/MessageTagTests.groovy
@@ -2,43 +2,53 @@ package org.codehaus.groovy.grails.web.taglib;
 
 class MessageTagTests extends AbstractGrailsTagTests {
 
+    private StringWriter sw
+
+    void setUp() {
+        super.setUp();
+        sw = new StringWriter();
+    }
+
     void testMessageTagInTemplate() {
         def template = '<g:message code="test.code" />'
         messageSource.addMessage("test.code", new Locale("en"), "hello world!")
-
         assertOutputEquals 'hello world!', template
     }
 
-    void testMessageTag() {
-        StringWriter sw = new StringWriter();
+    void testMessageTagWitMissingMessage() {
 
         withTag("message", sw) { tag ->
-
-            // test when no message found it returns code
-            def attrs = [code:"test.code"]
-            def result=tag.call( attrs )
+            Map attrs = [code:"test.code"]
+            def result = tag.call( attrs )
             assertEquals "test.code", result
+        }
 
-            // now test that when there is a message it finds it
+    }
+
+    void testMessageTagWithMessage() {
+
+        withTag("message", sw) { tag ->
             messageSource.addMessage("test.code", new Locale("en"), "hello world!")
-            result = tag.call( attrs )
+            def result = tag.call( attrs )
             assertEquals "hello world!", result
+        }
 
-            // now test with arguments
+    }
+
+    void testMessageTagWithArguments() {
+
+        withTag("message", sw) { tag ->
             messageSource.addMessage("test.args", new Locale("en"), "hello {0}!")
-            attrs = [code:"test.args", args:["fred"]]
-
-            result = tag.call(attrs)
-
+            def attrs = [code:"test.args", args:["fred"]]
+            def result = tag.call(attrs)
             assertEquals "hello fred!", result
         }
+
     }
 
     void testMessageTagWithCodec() {
-        StringWriter sw = new StringWriter();
 
         withTag("message", sw) { tag ->
-
             def attrs = [code:"test.code", encodeAs:'HTML']
             messageSource.addMessage("test.code", new Locale("en"), ">>&&")
             def result=tag.call( attrs )
