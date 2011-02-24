395bf9564fcc757ff22926e73c06df3cb0f6b791
Peter Ledbrook <peter@cacoethes.co.uk>
2011-02-24 16:59:39 +0000

    Removed some unused doc templates.

--- grails-docs/src/main/groovy/grails/doc/DocPublisher.groovy
+++ grails-docs/src/main/groovy/grails/doc/DocPublisher.groovy
@@ -163,9 +163,6 @@ class DocPublisher {
                 fileset(dir: style)
             }
         }
-        ant.copy(todir: "${refDocsDir}/ref") {
-            fileset(dir: "${docResources}/style/ref")
-        }
 
         def comparator = [compare: {o1, o2 ->
             def idx1 = o1.name[0..o1.name.indexOf(' ') - 1]
