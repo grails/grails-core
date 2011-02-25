ba3e2d2dc17ee2d217f36d71ac760e4bfcf4666d
Jeff Brown <jeff@jeffandbetsy.net>
2011-02-25 13:58:34 -0600

    extract some test support classes out to be top level classes

---
+++ grails-test-suite-persistence/src/test/groovy/org/codehaus/groovy/grails/orm/hibernate/NamedCriteriaPublication.groovy
@@ -0,0 +1,78 @@
+package org.codehaus.groovy.grails.orm.hibernate
+
+import grails.persistence.Entity
+
+@Entity
+class NamedCriteriaPublication {
+   Long id
+   Long version
+   String title
+   Date datePublished
+   Boolean paperback = true
+
+   static namedQueries = {
+       aPaperback  {
+          eq 'paperback', true
+       }
+
+       lastPublishedBefore { date ->
+           uniqueResult = true
+           le 'datePublished', date
+           order 'datePublished', 'desc'
+       }
+       recentPublications {
+           def now = new Date()
+           gt 'datePublished', now - 365
+       }
+
+       publicationsWithBookInTitle {
+           like 'title', '%Book%'
+       }
+
+       recentPublicationsByTitle { title ->
+           recentPublications()
+           eq 'title', title
+       }
+
+       latestBooks {
+           maxResults(10)
+           order("datePublished", "desc")
+       }
+
+       publishedBetween { start, end ->
+           between 'datePublished', start, end
+       }
+
+        publishedAfter { date ->
+           gt 'datePublished', date
+        }
+
+       paperbackOrRecent {
+           or {
+                def now = new Date()
+                gt 'datePublished', now - 365
+                paperbacks()
+           }
+       }
+
+       paperbacks {
+          eq 'paperback', true
+       }
+
+       paperbackAndRecent {
+           paperbacks()
+           recentPublications()
+       }
+
+       thisWeeksPaperbacks() {
+           paperbacks()
+           def today = new Date()
+           publishedBetween(today - 7, today)
+       }
+
+       queryThatNestsMultipleLevels {
+           // this nested query will call other nested queries
+           thisWeeksPaperbacks()
+       }
+   }
+}
---
+++ grails-test-suite-persistence/src/test/groovy/org/codehaus/groovy/grails/orm/hibernate/NamedCriteriaPublicationSubclassWithoutNamedQueries.groovy
@@ -0,0 +1,7 @@
+package org.codehaus.groovy.grails.orm.hibernate
+
+import grails.persistence.Entity
+
+@Entity
+class NamedCriteriaPublicationSubclassWithoutNamedQueries extends NamedCriteriaPublication {
+}
