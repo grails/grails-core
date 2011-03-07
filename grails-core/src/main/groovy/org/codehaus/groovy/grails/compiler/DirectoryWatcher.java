9fb97f6188079eadd8fa3c401532e5b8e130936c
Graeme Rocher <graeme.rocher@gmail.com>
2011-03-07 12:34:18 +0100

    refactored compiler and file watching logic into utility classes

---
+++ grails-core/src/main/groovy/org/codehaus/groovy/grails/compiler/DirectoryWatcher.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2011 SpringSource
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codehaus.groovy.grails.compiler;
+
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Utility class to watch directories for changes
+ *
+ * @author Graeme Rocher
+ * @since 1.4
+ */
+public class DirectoryWatcher extends Thread {
+
+    private File[] directories;
+    private String[] extensions;
+    private List<FileChangeListener> listeners = new ArrayList<FileChangeListener>();
+
+    private Map<File, Long> lastModifiedMap = new ConcurrentHashMap<File, Long>();
+    private boolean active = true;
+    private long sleepTime = 3000;
+
+    public DirectoryWatcher(File[] directories, String[] extensions) {
+        this.directories = directories;
+        this.extensions = extensions;
+        setDaemon(true);
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public void setSleepTime(long sleepTime) {
+        this.sleepTime = sleepTime;
+    }
+
+    private void initializeLastModifiedCache(File[] directories, final String[] extensions) {
+
+        for (File directory : directories) {
+            cacheFilesForDirectory(directory, extensions);
+        }
+
+    }
+
+    private void cacheFilesForDirectory(File directory, String[] extensions) {
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if(isValidFileToMonitor(file.getName(),extensions))
+                lastModifiedMap.put(file, file.lastModified());
+            else if(file.isDirectory()) {
+               cacheFilesForDirectory(file, extensions);
+            }
+        }
+    }
+
+    private boolean isValidFileToMonitor(String name, String[] extensions) {
+        for (String extension : extensions) {
+            if (name.endsWith(extension)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void run() {
+        initializeLastModifiedCache(directories, extensions);
+        while(active) {
+            int count = 0;
+            Set<File> files = lastModifiedMap.keySet();
+            for (File file : files) {
+                long currentLastModified = file.lastModified();
+                Long cachedTime = lastModifiedMap.get(file);
+                if(currentLastModified > cachedTime) {
+                    lastModifiedMap.put(file, currentLastModified);
+                    for (FileChangeListener listener : listeners) {
+                        listener.onChange(file);
+                    }
+                }
+            }
+            try {
+                count++;
+                if(count > 3) {
+                    count = 0;
+                    initializeLastModifiedCache(directories, extensions);
+                }
+                sleep(sleepTime);
+            } catch (InterruptedException e) {
+                // ignore
+            }
+        }
+    }
+
+
+    public void addListener(FileChangeListener listener ) {
+        this.listeners.add(listener);
+    }
+
+    public static interface FileChangeListener {
+        public abstract void onChange(File file);
+    }
+}
---
+++ grails-core/src/main/groovy/org/codehaus/groovy/grails/compiler/GrailsProjectCompiler.groovy
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2011 SpringSource
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codehaus.groovy.grails.compiler
+
+import org.codehaus.groovy.control.CompilationUnit
+import org.codehaus.groovy.control.CompilerConfiguration
+import org.springframework.core.io.Resource
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver
+
+/**
+ *
+ * Encapsulates the compilation logic required for a Grails application
+ *
+ * @author Graeme Rocher
+ * @since 1.4
+ */
+class GrailsProjectCompiler {
+
+    public static final List<String> EXCLUDED_PATHS =  ["views", "i18n", "conf"]
+    private static final String CLASSPATH_REF = "grails.compile.classpath"
+    private static final List<String> PLUGIN_EXCLUDE_PATHS = ["views", "i18n"] // conf gets special handling
+
+    private AntBuilder ant
+    private List<String> srcDirectories
+    private File pluginDescriptor
+    private CompilerConfiguration config
+    private ClassLoader classLoader
+
+    boolean verbose = false
+    boolean isPluginProject = false
+    Map javaOptions = [classpathref:CLASSPATH_REF, encoding:"UTF-8", debug:"yes"]
+    String basedir
+    String srcdir
+    String encoding = "UTF-8"
+    Resource[] pluginSourceDirectories
+
+
+    GrailsProjectCompiler(String basedir, String srcdir, Resource[] pluginSrcDirectories, CompilerConfiguration config, ClassLoader rootLoader = Thread.currentThread().getContextClassLoader()) {
+        this.basedir = basedir
+        this.srcdir = srcdir
+        this.pluginSourceDirectories = pluginSrcDirectories
+        this.classLoader = rootLoader
+        this.pluginDescriptor = new File(basedir).listFiles().find { it.name.endsWith("GrailsPlugin.groovy") }
+        this.config = config
+        isPluginProject = pluginDescriptor != null
+        srcDirectories = [ "${basedir}/grails-app/conf",
+                           "${basedir}/grails-app/conf/spring",
+                           "${srcdir}/groovy",
+                           "${srcdir}/java"]
+
+        def excludedPaths =  EXCLUDED_PATHS
+
+        for (dir in new File("${basedir}/grails-app").listFiles()) {
+            if (!excludedPaths.contains(dir.name) && dir.isDirectory()) {
+                srcDirectories << "${dir}"
+            }
+        }
+    }
+
+    AntBuilder getAnt() {
+        if(this.ant == null) ant = new AntBuilder()
+        return ant
+    }
+
+    void setAnt(AntBuilder ant) {
+        this.ant = ant
+    }
+
+    /**
+     * Compiles project sources to the given target directory
+     *
+     * @param targetDir The target directory to compile to
+     */
+    void compile(targetDir) {
+
+        def compilerPaths = { String classpathId ->
+            for(srcPath in srcDirectories) {
+               if(new File(srcPath).exists()) {
+                   src(path:srcPath)
+               }
+            }
+            javac(javaOptions)
+        }
+
+        def classesDirPath = new File(targetDir.toString())
+        ant.mkdir(dir:classesDirPath)
+        String classpathId = "grails.compile.classpath"
+        ant.groovyc(destdir:classesDirPath,
+                    classpathref:classpathId,
+                    encoding:encoding,
+                    verbose: verbose,
+                    listfiles: verbose,
+                    compilerPaths.curry(classpathId))
+
+        // If this is a plugin project, the descriptor is not included
+        // in the compiler's source path. So, we manually compile it now.
+        if (isPluginProject) compilePluginDescriptor(pluginDescriptor, classesDirPath)
+    }
+
+    /**
+     * Compiles plugin sources files to the given target directory
+     *
+     * @param targetDir The target directory to compile to
+     */
+    void compilePlugins(targetDir) {
+        def classesDirPath = targetDir
+        ant.mkdir(dir:classesDirPath)
+
+        // First compile the plugins so that we can exclude any
+        // classes that might conflict with the project's.
+        def classpathId = CLASSPATH_REF
+
+        def excludedPaths = PLUGIN_EXCLUDE_PATHS // conf gets special handling
+        def pluginResources = pluginSourceDirectories?.findAll {
+            !excludedPaths.contains(it.file.name) && it.file.isDirectory()
+        }
+
+        if (pluginResources) {
+            // Only perform the compilation if there are some plugins
+            // installed or otherwise referenced.
+            ant.groovyc(destdir:classesDirPath,
+                        classpathref:classpathId,
+                        encoding:encoding,
+                        verbose: verbose,
+                        listfiles: verbose) {
+                for (dir in pluginResources.file) {
+                    src(path:"${dir}")
+                }
+                exclude(name: "**/BootStrap.groovy")
+                exclude(name: "**/BuildConfig.groovy")
+                exclude(name: "**/Config.groovy")
+                exclude(name: "**/*DataSource.groovy")
+                exclude(name: "**/UrlMappings.groovy")
+                exclude(name: "**/resources.groovy")
+                javac(javaOptions)
+            }
+        }
+    }
+
+    /**
+     * Compiles a given plugin descriptor file - *GrailsPlugin.groovy.
+     */
+    protected compilePluginDescriptor(File descriptor, File classesDir) {
+        def className = descriptor.name - '.groovy'
+        def classFile = new File(classesDir, "${className}.class")
+
+        if (descriptor.lastModified() > classFile.lastModified()) {
+            ant.echo(message: "Compiling plugin descriptor...")
+            config.setTargetDirectory(classesDir)
+            def unit = new CompilationUnit(config, null, new GroovyClassLoader(classLoader))
+            unit.addSource(descriptor)
+            unit.compile()
+        }
+    }
+
+    /**
+     * Starts a daemon thread that recompiles sources when they change
+     */
+    DirectoryWatcher startCompilerDaemon(classesDir, pluginClassesDir) {
+        def allDirectories = []
+        allDirectories.addAll(srcDirectories.collect { new File(it) })
+        allDirectories.addAll( pluginSourceDirectories.findAll { Resource r -> r.file.isDirectory() }.collect { it.file } )
+        DirectoryWatcher watcher = new DirectoryWatcher(allDirectories as File[], ['.groovy', '.java'] as String[] )
+        watcher.addListener({
+           compilePlugins(pluginClassesDir)
+           compile(classesDir)
+        } as DirectoryWatcher.FileChangeListener)
+        watcher.start()
+        return watcher
+    }
+}
