f52d7c28301e9c17488e54fc8f79c23dc1939454
Graeme Rocher <graeme.rocher@gmail.com>
2011-03-10 10:32:04 +0100

    improved with the ability to lookup src/java and src/groovy class names

--- grails-core/src/main/groovy/org/codehaus/groovy/grails/commons/GrailsResourceUtils.java
+++ grails-core/src/main/groovy/org/codehaus/groovy/grails/commons/GrailsResourceUtils.java
@@ -15,6 +15,12 @@
  */
 package org.codehaus.groovy.grails.commons;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.codehaus.groovy.grails.exceptions.GrailsConfigurationException;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -24,12 +30,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.codehaus.groovy.grails.exceptions.GrailsConfigurationException;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 
 /**
  * Utility methods for working with Grails resources and URLs that represent artifacts
@@ -65,12 +65,12 @@ public class GrailsResourceUtils {
      Domain path is always matched against the normalized File representation of an URL and
     can therefore work with slashes as separators.
      */
-    public static Pattern DOMAIN_PATH_PATTERN = Pattern.compile(".+/"+GRAILS_APP_DIR+"/domain/(.+)\\.groovy");
+    public static Pattern DOMAIN_PATH_PATTERN = Pattern.compile(".+/"+GRAILS_APP_DIR+"/domain/(.+)\\.(groovy|java)");
 
     /*
      This pattern will match any resource within a given directory inside grails-app
      */
-    public static Pattern RESOURCE_PATH_PATTERN = Pattern.compile(".+?/"+GRAILS_APP_DIR+"/(.+?)/(.+?\\.groovy)");
+    public static Pattern RESOURCE_PATH_PATTERN = Pattern.compile(".+?/"+GRAILS_APP_DIR+"/(.+?)/(.+?\\.(groovy|java))");
 
     public static Pattern SPRING_SCRIPTS_PATH_PATTERN = Pattern.compile(".+?/"+GRAILS_APP_DIR+"/conf/spring/(.+?\\.groovy)");
 
@@ -89,6 +89,8 @@ public class GrailsResourceUtils {
     public static final Pattern GRAILS_RESOURCE_PATTERN_FOURTH_MATCH;
     public static final Pattern GRAILS_RESOURCE_PATTERN_FIFTH_MATCH;
     public static final Pattern GRAILS_RESOURCE_PATTERN_SIXTH_MATCH;
+    public static final Pattern GRAILS_RESOURCE_PATTERN_SEVENTH_MATCH;
+    public static final Pattern GRAILS_RESOURCE_PATTERN_EIGHTH_MATCH;
 
     static {
         String fs = File.separator;
@@ -96,6 +98,8 @@ public class GrailsResourceUtils {
 
         GRAILS_RESOURCE_PATTERN_FIRST_MATCH = Pattern.compile(createGrailsResourcePattern(fs, GRAILS_APP_DIR +fs+ "conf" +fs + "spring"));
         GRAILS_RESOURCE_PATTERN_THIRD_MATCH = Pattern.compile(createGrailsResourcePattern(fs, GRAILS_APP_DIR +fs +"\\w+"));
+        GRAILS_RESOURCE_PATTERN_SEVENTH_MATCH = Pattern.compile(createGrailsResourcePattern(fs, "src" + fs + "java"));
+        GRAILS_RESOURCE_PATTERN_EIGHTH_MATCH = Pattern.compile(createGrailsResourcePattern(fs, "src" + fs + "groovy" ));
         GRAILS_RESOURCE_PATTERN_FIFTH_MATCH = Pattern.compile(createGrailsResourcePattern(fs, "grails-tests"));
         fs = "/";
         GRAILS_RESOURCE_PATTERN_SECOND_MATCH = Pattern.compile(createGrailsResourcePattern(fs, GRAILS_APP_DIR +fs+ "conf" +fs + "spring"));
@@ -107,6 +111,8 @@ public class GrailsResourceUtils {
         GRAILS_RESOURCE_PATTERN_FIRST_MATCH,
         GRAILS_RESOURCE_PATTERN_SECOND_MATCH,
         GRAILS_RESOURCE_PATTERN_THIRD_MATCH,
+        GRAILS_RESOURCE_PATTERN_SEVENTH_MATCH,
+        GRAILS_RESOURCE_PATTERN_EIGHTH_MATCH,
         GRAILS_RESOURCE_PATTERN_FOURTH_MATCH,
         GRAILS_RESOURCE_PATTERN_FIFTH_MATCH,
         GRAILS_RESOURCE_PATTERN_SIXTH_MATCH
@@ -114,7 +120,7 @@ public class GrailsResourceUtils {
     private static final Log LOG = LogFactory.getLog(GrailsResourceUtils.class);
 
     private static String createGrailsResourcePattern(String separator, String base) {
-        return ".+" + separator + base + separator + "(.+)\\.groovy";
+        return ".+" + separator + base + separator + "(.+)\\.(groovy|java)";
     }
 
     /**
