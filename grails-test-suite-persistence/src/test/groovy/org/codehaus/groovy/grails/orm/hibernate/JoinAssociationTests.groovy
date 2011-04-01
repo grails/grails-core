508fea170aba3e1d677965d836c76ac766407b77
Graeme Rocher <graeme.rocher@gmail.com>
2011-04-01 16:28:35 +0200

    additional test

--- grails-test-suite-persistence/src/test/groovy/org/codehaus/groovy/grails/orm/hibernate/JoinAssociationTests.groovy
+++ grails-test-suite-persistence/src/test/groovy/org/codehaus/groovy/grails/orm/hibernate/JoinAssociationTests.groovy
@@ -39,16 +39,9 @@ class Role {
     }
 
 
+    // test for GRAILS-7087
     void testObtainCorrectResultsViaJoin() {
-        def User = ga.getDomainClass("User").clazz
-        def user = User.newInstance(name:'Name')
-        user.save(flush:true)
-
-        user.addToRoles(user:user, name:'Role1')
-        user.addToRoles(user:user, name:'Role2')
-        user.save(flush:true)
-
-        session.clear()
+        def User = createData()
 
         def users = User.createCriteria().list{
             roles{
@@ -60,16 +53,10 @@ class Role {
         assertEquals 2, users.head().roles.size()
     }
 
+
+    // test for GRAILS-7087
     void testObtainCorrectResultsViaLeftJoin() {
-        def User = ga.getDomainClass("User").clazz
-        def user = User.newInstance(name:'Name')
-        user.save(flush:true)
-
-        user.addToRoles(user:user, name:'Role1')
-        user.addToRoles(user:user, name:'Role2')
-        user.save(flush:true)
-
-        session.clear()
+        Class User = createData()
 
         def users = User.createCriteria().list{
             roles(org.hibernate.criterion.CriteriaSpecification.LEFT_JOIN){
@@ -79,5 +66,50 @@ class Role {
 
         assertEquals 1, users.size()
         assertEquals 1, users.head().roles.size()
+    }
+
+    private Class createData() {
+        def User = ga.getDomainClass("User").clazz
+        def user = User.newInstance(name: 'Name')
+        user.save(flush: true)
+
+        user.addToRoles(user: user, name: 'Role1')
+        user.addToRoles(user: user, name: 'Role2')
+        user.save(flush: true)
+
+        session.clear()
+        return User
+    }
+
+    // test for GRAILS-3045
+    void testObtainCorrectResultWithDistinctPaginationAndJoin() {
+        def User = ga.getDomainClass("User").clazz
+
+        (1..30).each {
+            def user = User.newInstance(name: "User $it")
+            user.save(flush: true)
+
+            if(it%2) {
+                user.addToRoles(user: user, name: 'Role1')
+            }
+            else {
+                user.addToRoles(user: user, name: 'Role2')
+            }
+
+        }
+
+        session.flush()
+        session.clear()
+
+
+        def results = User.createCriteria().listDistinct {
+            roles{
+                eq('name', 'Role1')
+            }
+            order 'id', 'asc'
+            maxResults 10
+        }
+
+        assert results.size() == 10
     }
 }
