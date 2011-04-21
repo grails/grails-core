d4702cbe6d297715031cae50dc594deb6f62f834
Jeff Brown <jeff@jeffandbetsy.net>
2011-04-21 10:23:37 -0500

    GRAILS-7417 - add test for equality of JSONObjects returned from JSON.parse(String)

--- grails-test-suite-uber/src/test/groovy/org/codehaus/groovy/grails/web/json/JSONObjectTests.groovy
+++ grails-test-suite-uber/src/test/groovy/org/codehaus/groovy/grails/web/json/JSONObjectTests.groovy
@@ -17,6 +17,19 @@ class JSONObjectTests extends GroovyTestCase {
         JSONObject j = new JSONObject();
         j.put('test', 1)
         assert j.containsValue(1)
-
+    }
+    
+    void testEqualityOfJSONObjectsReturnedFromConverter() {
+        // GRAILS-7417
+        
+        def input = '''{"message":"mockMessage","errors":[{"field":"name","errorMessage":"mockMessage","errorType":"validation","fieldValue":null}],
+        "object":{"nest2":{"p2":"val2","p1":"val1"},"nest1":{"p2":"val2","p1":"val1"},"name":null},"success":false}'''
+        
+        JSONObject j1 = grails.converters.JSON.parse(input)
+        JSONObject j2 = grails.converters.JSON.parse(input)
+        
+        assertNotSame j1, j2
+        assertEquals j1, j2
+        assertTrue j1 == j2
     }
 }
