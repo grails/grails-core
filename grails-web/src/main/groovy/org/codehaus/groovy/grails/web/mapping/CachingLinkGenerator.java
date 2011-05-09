17dfded0614e47adcf95252f9394b6c23394ebd5
Graeme Rocher <graeme.rocher@gmail.com>
2011-05-09 11:57:01 +0200

    Fixes remaining failing functional tests. CachingLinkGenerator was erroneously caching links with only actions, resulting in incorrect links being generated on certain pages. In addition the previous cache key generation was using DGM.toMapString(map), but this uses StringBuffer to build the map, which is synchronized and bad for performance. This fixes that by providing an alternative implementation that uses StringBuilder.

--- grails-web/src/main/groovy/org/codehaus/groovy/grails/web/mapping/CachingLinkGenerator.java
+++ grails-web/src/main/groovy/org/codehaus/groovy/grails/web/mapping/CachingLinkGenerator.java
@@ -18,6 +18,7 @@ package org.codehaus.groovy.grails.web.mapping;
 
 import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap;
 import org.codehaus.groovy.grails.web.taglib.GroovyPageAttributes;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
 
 import java.util.Collection;
 import java.util.Map;
@@ -34,6 +35,14 @@ public class CachingLinkGenerator extends DefaultLinkGenerator {
     public static final String LINK_PREFIX = "link";
     public static final String RESOURCE_PREFIX = "resource";
     public static final String USED_ATTRIBUTES_SUFFIX = "-used-attributes";
+    public static final String EMPTY_MAP_STRING = "[:]";
+    private static final String OPENING_BRACKET = "[";
+    private static final String CLOSING_BRACKET = "]";
+    private static final String COMMA_SEPARATOR = ", ";
+    private static final String KEY_VALUE_SEPARATOR = ":";
+    private static final String THIS_MAP = "(this Map)";
+    private static final String URL_ATTRIBUTE = "url";
+    private static final String URI_ATTRIBUTE = "uri";
 
     private Map<String, Object> linkCache;
 
@@ -60,14 +69,75 @@ public class CachingLinkGenerator extends DefaultLinkGenerator {
     }
 
     @Override
-    public String link(Map attrs, String encoding) {
-        final String key = LINK_PREFIX + attrs;
-        Object resourceLink = linkCache.get(key);
-        if(resourceLink == null) {
-            resourceLink = super.link(attrs, encoding);
-            linkCache.put(key, resourceLink);
+    public String link(Map attrs, String encoding)
+    {
+        if(!isCacheable(attrs)) {
+            return super.link(attrs, encoding);
         }
-        return resourceLink.toString();
+        else {
+            final String key = LINK_PREFIX + createKey(attrs);
+            Object resourceLink = linkCache.get(key);
+            if(resourceLink == null) {
+                resourceLink = super.link(attrs, encoding);
+                linkCache.put(key, resourceLink);
+            }
+            return resourceLink.toString();
+        }
+    }
+
+    private boolean isCacheable(Map attrs) {
+        Object urlAttr = attrs.get(URL_ATTRIBUTE);
+        if(urlAttr instanceof Map) {
+            return isCacheable((Map) urlAttr);
+        }
+        else {
+            return attrs.get(UrlMapping.CONTROLLER) != null ||
+                    attrs.get(UrlMapping.ACTION) != null ||
+                    urlAttr != null ||
+                    attrs.get(URI_ATTRIBUTE) != null;
+        }
+    }
+
+    // Based on DGM toMapString, but with StringBuilder instead of StringBuffer
+    private String createKey(Map map) {
+        if (map.isEmpty()) {
+            return EMPTY_MAP_STRING;
+        }
+        StringBuilder buffer = new StringBuilder(OPENING_BRACKET);
+        boolean first = true;
+        for (Object o : map.entrySet()) {
+            Map.Entry entry = (Map.Entry) o;
+            Object value = entry.getValue();
+            if(value == null) continue;
+            first = appendCommaIfNotFirst(buffer, first);
+            Object key = entry.getKey();
+            if(UrlMapping.ACTION.equals(key) && map.get(UrlMapping.CONTROLLER) == null) {
+                appendKeyValue(buffer, map, UrlMapping.CONTROLLER, getRequestStateLookupStrategy().getControllerName());
+                appendCommaIfNotFirst(buffer, false);
+            }
+            appendKeyValue(buffer, map, key, value);
+        }
+        buffer.append(CLOSING_BRACKET);
+        return buffer.toString();
+    }
+
+    private boolean appendCommaIfNotFirst(StringBuilder buffer, boolean first) {
+        if (first) {
+            first = false;
+        } else {
+            buffer.append(COMMA_SEPARATOR);
+        }
+        return first;
+    }
+
+    private void appendKeyValue(StringBuilder buffer, Map map, Object key, Object value) {
+        buffer.append(key)
+              .append(KEY_VALUE_SEPARATOR);
+        if (value == map) {
+            buffer.append(THIS_MAP  );
+        } else {
+            buffer.append(DefaultGroovyMethods.toString(value));
+        }
     }
 
     @Override
