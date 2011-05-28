6a0b5b1b01d760408a60dd3079d8f754a262d394
Burt Beckwith <burt@burtbeckwith.com>
2011-05-28 15:15:57 -0400

    updated controller template to have consistent logic and be more DRY when looking up i18n messages

--- grails-resources/src/grails/templates/scaffolding/Controller.groovy
+++ grails-resources/src/grails/templates/scaffolding/Controller.groovy
@@ -1,4 +1,5 @@
 <%=packageName ? "package ${packageName}\n\n" : ''%>import grails.converters.JSON
+import org.springframework.dao.DataIntegrityViolationException
 
 class ${className}Controller {
 
@@ -8,93 +9,102 @@ class ${className}Controller {
         redirect(action: "list", params: params)
     }
 
-	def list = {
-		params.max = Math.min(params.max ? params.int('max') : 10, 100)
-		[${propertyName}List: ${className}.list(params), ${propertyName}Total: ${className}.count()]
-	}
+    def list = {
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
+        [${propertyName}List: ${className}.list(params), ${propertyName}Total: ${className}.count()]
+    }
 
     def create = {
-        def ${propertyName} = new ${className}()
-        ${propertyName}.properties = params
-        return [${propertyName}: ${propertyName}]
+        [${propertyName}: new ${className}(params)]
     }
 
     def save = {
         def ${propertyName} = new ${className}(params)
-        if (${propertyName}.save(flush: true)) {
-            flash.message = message(code: 'default.created.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), ${propertyName}.id])
-            redirect(action: "show", id: ${propertyName}.id)
-        }
-        else {
+        if (!${propertyName}.save(flush: true)) {
             render(view: "create", model: [${propertyName}: ${propertyName}])
+            return
         }
+
+        flash.message = lookupMessage('default.created.message', ${propertyName}.id)
+        redirect(action: "show", id: ${propertyName}.id)
     }
 
     def show = {
         def ${propertyName} = ${className}.get(params.id)
         if (!${propertyName}) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
+            flash.message = lookupNotFoundMessage(params.id)
             redirect(action: "list")
+            return
         }
-        else {
-            [${propertyName}: ${propertyName}]
-        }
+
+        [${propertyName}: ${propertyName}]
     }
 
     def edit = {
         def ${propertyName} = ${className}.get(params.id)
         if (!${propertyName}) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
+            flash.message = lookupNotFoundMessage(params.id)
             redirect(action: "list")
+            return
         }
-        else {
-            return [${propertyName}: ${propertyName}]
-        }
+
+        [${propertyName}: ${propertyName}]
     }
 
     def update = {
         def ${propertyName} = ${className}.get(params.id)
-        if (${propertyName}) {
-            if (params.version) {
-                def version = params.version.toLong()
-                if (${propertyName}.version > version) {
-                    <% def lowerCaseName = grails.util.GrailsNameUtils.getPropertyName(className) %>
-                    ${propertyName}.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: '${domainClass.propertyName}.label', default: '${className}')] as Object[], "Another user has updated this ${className} while you were editing")
-                    render(view: "edit", model: [${propertyName}: ${propertyName}])
-                    return
-                }
-            }
-            ${propertyName}.properties = params
-            if (!${propertyName}.hasErrors() && ${propertyName}.save(flush: true)) {
-                flash.message = message(code: 'default.updated.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), ${propertyName}.id])
-                redirect(action: "show", id: ${propertyName}.id)
-            }
-            else {
-                render(view: "edit", model: [${propertyName}: ${propertyName}])
-            }
-        }
-        else {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
+        if (!${propertyName}) {
+            flash.message = lookupNotFoundMessage(params.id)
             redirect(action: "list")
+            return
         }
+
+        if (params.version) {
+            def version = params.version.toLong()
+            if (${propertyName}.version > version) {<% def lowerCaseName = grails.util.GrailsNameUtils.getPropertyName(className) %>
+                ${propertyName}.errors.rejectValue("version", "default.optimistic.locking.failure",
+                          [message(code: '${domainClass.propertyName}.label', default: '${className}')] as Object[],
+                          "Another user has updated this ${className} while you were editing")
+                render(view: "edit", model: [${propertyName}: ${propertyName}])
+                return
+            }
+        }
+
+        ${propertyName}.properties = params
+
+        if (!${propertyName}.save(flush: true)) {
+            render(view: "edit", model: [${propertyName}: ${propertyName}])
+            return
+        }
+
+        flash.message = lookupMessage('default.updated.message', ${propertyName}.id)
+        redirect(action: "show", id: ${propertyName}.id)
     }
 
     def delete = {
         def ${propertyName} = ${className}.get(params.id)
-        if (${propertyName}) {
-            try {
-                ${propertyName}.delete(flush: true)
-                flash.message = message(code: 'default.deleted.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
-                redirect(action: "list")
-            }
-            catch (org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = message(code: 'default.not.deleted.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
-                redirect(action: "show", id: params.id)
-            }
+        if (!${propertyName}) {
+            flash.message = lookupNotFoundMessage(params.id)
+            redirect(action: "list")
+            return
         }
-        else {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
+
+        try {
+            ${propertyName}.delete(flush: true)
+            flash.message = lookupMessage('default.deleted.message', params.id)
             redirect(action: "list")
         }
+        catch (DataIntegrityViolationException e) {
+            flash.message = lookupMessage('default.not.deleted.message', params.id)
+            redirect(action: "show", id: params.id)
+        }
+    }
+
+    private String lookupMessage(String code, id) {
+        message(code: code, args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), id])
+    }
+
+    private String lookupNotFoundMessage(id) {
+        lookupMessage('default.not.found.message', id)
     }
 }
