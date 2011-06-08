e399b6bf5529f447740d8ca534f08955f3265438
Burt Beckwith <burt@burtbeckwith.com>
2011-06-08 11:06:40 -0400

    test fix

--- grails-test-suite-uber/src/test/groovy/org/codehaus/groovy/grails/resolve/IvyDependencyManagerTests.groovy
+++ grails-test-suite-uber/src/test/groovy/org/codehaus/groovy/grails/resolve/IvyDependencyManagerTests.groovy
@@ -578,9 +578,9 @@ class IvyDependencyManagerTests extends GroovyTestCase {
 
         assertTrue("all default dependencies should be inherited", manager.dependencyDescriptors.every { it.inherited == true })
         assertEquals 53, manager.dependencyDescriptors.findAll { it.scope == 'compile'}.size()
-        assertEquals 15, manager.dependencyDescriptors.findAll { it.scope == 'runtime'}.size()
+        assertEquals 14, manager.dependencyDescriptors.findAll { it.scope == 'runtime'}.size()
         assertEquals 4, manager.dependencyDescriptors.findAll { it.scope == 'test'}.size()
-        assertEquals 23, manager.dependencyDescriptors.findAll { it.scope == 'build'}.size()
+        assertEquals 22, manager.dependencyDescriptors.findAll { it.scope == 'build'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'provided'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'docs'}.size()
     }
@@ -601,8 +601,8 @@ class IvyDependencyManagerTests extends GroovyTestCase {
         assertEquals 0, manager.dependencyDescriptors.findAll { it.scope == 'compile'}.size()
         assertEquals 0, manager.dependencyDescriptors.findAll { it.scope == 'runtime'}.size()
         assertEquals 4, manager.dependencyDescriptors.findAll { it.scope == 'test'}.size()
-        assertEquals 23, manager.dependencyDescriptors.findAll { it.scope == 'build'}.size()
-        assertEquals 71, manager.dependencyDescriptors.findAll { it.scope == 'provided'}.size()
+        assertEquals 22, manager.dependencyDescriptors.findAll { it.scope == 'build'}.size()
+        assertEquals 70, manager.dependencyDescriptors.findAll { it.scope == 'provided'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'docs'}.size()
 
         manager = new IvyDependencyManager("project", "0.1",settings)
@@ -614,9 +614,9 @@ class IvyDependencyManagerTests extends GroovyTestCase {
         }
 
         assertEquals 53, manager.dependencyDescriptors.findAll { it.scope == 'compile'}.size()
-        assertEquals 15, manager.dependencyDescriptors.findAll { it.scope == 'runtime'}.size()
+        assertEquals 14, manager.dependencyDescriptors.findAll { it.scope == 'runtime'}.size()
         assertEquals 4, manager.dependencyDescriptors.findAll { it.scope == 'test'}.size()
-        assertEquals 23, manager.dependencyDescriptors.findAll { it.scope == 'build'}.size()
+        assertEquals 22, manager.dependencyDescriptors.findAll { it.scope == 'build'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'provided'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'docs'}.size()
     }
