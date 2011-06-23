e25191270f0de69c3ec6447e7e6d077f6e2e6fb6
Jeff Brown <jeff@jeffandbetsy.net>
2011-06-22 21:56:36 -0500

    GRAILS-7637 - testing backreference assignment

---
+++ grails-test-suite-persistence/src/test/groovy/grails/test/mixin/domain/DomainClassUnitTestMixinTests.groovy
@@ -0,0 +1,33 @@
+package grails.test.mixin.domain
+
+import grails.persistence.Entity
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+
+@TestFor(Writer)
+@Mock(Publication)
+class DomainClassUnitTestMixinTests {
+
+    void testBackReferenceAssignment() {
+        def publication = new Publication(title: 'Some Paper')
+        def writer = new Writer(naem: 'Some Writer')
+        
+        writer.addToPublications(publication)
+        
+        assert publication.ghostWriter == null
+        assert writer.is(publication.writer)
+    }
+}
+
+@Entity
+class Writer {
+    String name
+    static hasMany = [publications: Publication]
+}
+
+@Entity
+class Publication {
+    String title
+    static belongsTo = [writer: Writer]
+    Writer ghostWriter
+}
