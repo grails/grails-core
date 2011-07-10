405a7d5dcef1996e7c55f921bf14ec9e3fc4963f
Jeff Brown <jeff@jeffandbetsy.net>
2011-07-10 15:44:12 -0700

    Removing code that was throwing an exception from an AST transform.  The error is related to default parameter values being used in controller action methods. How to handle default parameter values is still under discussion but this code change is an improvement over what was there before for now.

--- grails-core/src/main/groovy/org/codehaus/groovy/grails/compiler/GrailsCompilationException.java
+++
@@ -1,8 +0,0 @@
-package org.codehaus.groovy.grails.compiler;
-
-public class GrailsCompilationException extends RuntimeException {
-
-	public GrailsCompilationException(String message) {
-		super(message);
-	}
-}
