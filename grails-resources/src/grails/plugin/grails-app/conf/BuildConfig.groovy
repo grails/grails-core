7c1b4e2b65863afa29a42a77a819dba75dc2fc70
Graeme Rocher <graemerocher@Graeme-Rochers-iMac.local>
2011-07-20 12:44:25 +0200

    upgrade to release plugin 1.0 RC1

--- grails-resources/src/grails/plugin/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/plugin/grails-app/conf/BuildConfig.groovy
@@ -29,7 +29,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         build(":tomcat:$grailsVersion",
-              ":release:1.0.0.M2") {
+              ":release:1.0.0.RC1") {
             export = false
         }
     }
