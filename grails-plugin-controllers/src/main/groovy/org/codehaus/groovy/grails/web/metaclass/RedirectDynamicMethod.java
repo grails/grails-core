ad45d8bfbdfa4605168ca5975b6def8312f52158
Jeff Brown <jeff@jeffandbetsy.net>
2011-07-21 14:07:19 -0500

    don't pass null errors to controllerErrors.addAllErrors(...).  (AstEnhancedControllerUnitTestMixinTests was failing)

--- grails-plugin-controllers/src/main/groovy/org/codehaus/groovy/grails/web/metaclass/RedirectDynamicMethod.java
+++ grails-plugin-controllers/src/main/groovy/org/codehaus/groovy/grails/web/metaclass/RedirectDynamicMethod.java
@@ -128,7 +128,7 @@ public class RedirectDynamicMethod extends AbstractDynamicMethodInvocation {
         // if there are errors add it to the list of errors
         Errors controllerErrors = (Errors)controller.getProperty(ControllerDynamicMethods.ERRORS_PROPERTY);
         Errors errors = (Errors)argMap.get(ARGUMENT_ERRORS);
-        if (controllerErrors != null) {
+        if (controllerErrors != null && errors != null) {
             controllerErrors.addAllErrors(errors);
         }
         else {
