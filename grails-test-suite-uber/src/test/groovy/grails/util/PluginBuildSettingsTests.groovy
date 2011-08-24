7fba80910a2bfd17d89cc6135f8f195f7e1ae0d0
Graeme Rocher <graeme.rocher@gmail.com>
2011-08-24 10:27:56 +0200

    fix for GRAILS-7873 "PluginBuildSettings#getAvailableScripts() requires a grails home"

--- grails-test-suite-uber/src/test/groovy/grails/util/PluginBuildSettingsTests.groovy
+++ grails-test-suite-uber/src/test/groovy/grails/util/PluginBuildSettingsTests.groovy
@@ -108,6 +108,15 @@ class PluginBuildSettingsTests extends GroovyTestCase {
         assertEquals 6, pluginSettings.getArtefactResources().size()
     }
 
+    void testGetAvailableScriptsNoGrailsHome() {
+        final bs = new BuildSettings()
+        bs.grailsHome  = null
+        bs.baseDir = new File("foo")
+        def ps = new PluginBuildSettings(bs)
+
+        assert 0 == ps.availableScripts.size()
+
+    }
     void testGetAvailableScripts() {
         PluginBuildSettings pluginSettings = createPluginBuildSettings()
         def scripts = pluginSettings.getAvailableScripts()
