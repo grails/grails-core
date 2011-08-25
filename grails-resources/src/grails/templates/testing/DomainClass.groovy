dd62b8bf32860de102dd04db59c7e1a3e4a59592
Konstantinos Kostarellis <Konstantinos Kostarellis@gmail.com>
2011-08-25 18:32:09 +0200

    fixed link in javadoc, which was pointing to a non-existent class

--- grails-resources/src/grails/templates/testing/DomainClass.groovy
+++ grails-resources/src/grails/templates/testing/DomainClass.groovy
@@ -4,7 +4,7 @@ import grails.test.mixin.*
 import org.junit.*
 
 /**
- * See the API for {@link grails.test.mixin.domain.DomainUnitTestMixin} for usage instructions
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(@artifact.testclass@)
 class @artifact.name@ {
