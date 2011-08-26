09d9eba3755e5cfad8a0350f7881bc1f216b5423
Jeff Brown <jeff@jeffandbetsy.net>
2011-08-25 21:26:07 -0500

    fix failing test

--- grails-core/src/main/groovy/org/codehaus/groovy/grails/compiler/injection/GrailsASTUtils.java
+++ grails-core/src/main/groovy/org/codehaus/groovy/grails/compiler/injection/GrailsASTUtils.java
@@ -177,7 +177,7 @@ public class GrailsASTUtils {
     public static MethodNode addDelegateInstanceMethod(ClassNode classNode, Expression delegate, MethodNode declaredMethod, boolean thisAsFirstArgument) {
         Parameter[] parameterTypes = thisAsFirstArgument ? getRemainingParameterTypes(declaredMethod.getParameters()) : declaredMethod.getParameters();
         String methodName = declaredMethod.getName();
-        if (classNode.hasMethod(methodName, parameterTypes)) {
+        if (classNode.hasDeclaredMethod(methodName, parameterTypes)) {
             return null;
         }
         String propertyName = GrailsClassUtils.getPropertyForGetter(methodName);
