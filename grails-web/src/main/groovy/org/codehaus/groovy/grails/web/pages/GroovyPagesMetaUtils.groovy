7f73dd786f7383a733001ddfaaf26ef5dc1c5f04
Lari Hotari <lari.hotari@sagire.fi>
2011-09-20 18:16:02 +0300

    Fix and unit test for failing functional tests

--- grails-web/src/main/groovy/org/codehaus/groovy/grails/web/pages/GroovyPagesMetaUtils.groovy
+++ grails-web/src/main/groovy/org/codehaus/groovy/grails/web/pages/GroovyPagesMetaUtils.groovy
@@ -2,8 +2,8 @@ package org.codehaus.groovy.grails.web.pages
 
 import groovy.lang.MetaClass
 
-import org.codehaus.groovy.grails.commons.GrailsMetaClassUtils;
-import org.codehaus.groovy.runtime.InvokerHelper
+import org.codehaus.groovy.grails.commons.GrailsMetaClassUtils
+import org.springframework.web.context.request.RequestContextHolder as RCH
 
 class GroovyPagesMetaUtils {
 	public static void registerMethodMissingForGSP(Class gspClass, TagLibraryLookup gspTagLibraryLookup) {
@@ -23,6 +23,33 @@ class GroovyPagesMetaUtils {
 				return method.invoke(tagBean, args)
 			} else {
 				throw new MissingMethodException(name, mc.getTheClass(), args)
+			}
+		}
+		registerMethodMissingWorkaroundsForDefaultNamespace(mc, gspTagLibraryLookup)
+	}
+	
+	public static registerMethodMissingWorkaroundsForDefaultNamespace(MetaClass mc, TagLibraryLookup gspTagLibraryLookup) {
+		// hasErrors gets mixed up by hasErrors method without this metaclass modification
+		registerMethodMissingForTags(mc, gspTagLibraryLookup, GroovyPage.DEFAULT_NAMESPACE, 'hasErrors', false)
+	}
+	
+	// copied from /grails-plugin-controllers/src/main/groovy/org/codehaus/groovy/grails/web/plugins/support/WebMetaUtils.groovy
+	private static registerMethodMissingForTags(MetaClass mc, TagLibraryLookup gspTagLibraryLookup, String namespace, String name, boolean addAll) {
+		mc."$name" = {Map attrs, Closure body ->
+			GroovyPage.captureTagOutput(gspTagLibraryLookup, namespace, name, attrs, body, RCH.currentRequestAttributes())
+		}
+		mc."$name" = {Map attrs, CharSequence body ->
+			GroovyPage.captureTagOutput(gspTagLibraryLookup, namespace, name, attrs, new GroovyPage.ConstantClosure(body), RCH.currentRequestAttributes())
+		}
+		mc."$name" = {Map attrs ->
+			GroovyPage.captureTagOutput(gspTagLibraryLookup, namespace, name, attrs, null, RCH.currentRequestAttributes())
+		}
+		if(addAll) {
+			mc."$name" = {Closure body ->
+				GroovyPage.captureTagOutput(gspTagLibraryLookup, namespace, name, [:], body, RCH.currentRequestAttributes())
+			}
+			mc."$name" = {->
+				GroovyPage.captureTagOutput(gspTagLibraryLookup, namespace, name, [:], null, RCH.currentRequestAttributes())
 			}
 		}
 	}
