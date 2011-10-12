910081f8227775363bb9a2393cfee5a26b9ca23e
Jeff Brown <jeff@jeffandbetsy.net>
2011-10-12 13:15:14 -0500

    Upgrade groovy-1.8.3-SNAPSHOT -> groovy-1.8.3

--- grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
+++ grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
@@ -149,7 +149,7 @@ public class GrailsCoreDependencies {
 
                         // dependencies needed at compile time
                         ModuleRevisionId[] groovyDependencies = {
-                            ModuleRevisionId.newInstance("org.codehaus.groovy", "groovy-all", "1.8.3-SNAPSHOT")
+                            ModuleRevisionId.newInstance("org.codehaus.groovy", "groovy-all", "1.8.3")
                         };
                         registerDependencies(dependencyManager, compileTimeDependenciesMethod, groovyDependencies, "jline");
 
