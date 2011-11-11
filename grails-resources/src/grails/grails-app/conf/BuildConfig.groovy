a166bc968df7f566b6b8d98d00cb85ce2a13ebf9
Graeme Rocher <graeme.rocher@gmail.com>
2011-11-11 15:14:03 +0100

    made source level an optional configuration property to avoid warnings on JDK 1.7

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,6 @@ grails.servlet.version = "2.5" // Change depending on target container complianc
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.source.level = 1.6
 grails.project.target.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
