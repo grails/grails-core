54b29af8424f6be19a5f2043050cbeb41d85d76d
Konstantinos Kostarellis <kosta.grails@gmail.com>
2011-12-07 13:14:12 +0100

    upgrade to resources 1.1.4 (solving problem described in GRAILS-8431)
    - changed BuildConfig.groovy to resources 1.1.4
    - added resources 1.1.4 and webxml 1.4.1 to the build

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -39,7 +39,7 @@ grails.project.dependency.resolution = {
     plugins {
         compile ":hibernate:$grailsVersion"
         compile ":jquery:1.7"
-        compile ":resources:1.1.3"
+        compile ":resources:1.1.4"
 
         build ":tomcat:$grailsVersion"
 
