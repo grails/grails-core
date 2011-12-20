d62b85a0fb3e23e3ceeeb0a0beaeb220f6cfbdc6
Jeff Brown <jeff@jeffandbetsy.net>
2011-12-20 08:47:14 -0600

    Revert "GRAILS-8307 - Add support for parameterized sqlRestrictions"
    The new feature will be included in 2.1 but not 2.0.1
    This reverts commit 7ff5439fabc36cc3eb9eac0822e35ed5e971aa46.

--- grails-test-suite-persistence/src/test/groovy/org/codehaus/groovy/grails/orm/hibernate/HibernateCriteriaBuilderTests.groovy
+++ grails-test-suite-persistence/src/test/groovy/org/codehaus/groovy/grails/orm/hibernate/HibernateCriteriaBuilderTests.groovy
@@ -77,19 +77,8 @@ class HibernateCriteriaBuilderTests extends AbstractGrailsHibernateTests {
         results = domainClass.withCriteria {
             sqlRestriction "char_length(first_name) > 2"
         }
-        
-        // should retrieve homer, not bart, lisa and maggie
-        results = domainClass.withCriteria {
-            sqlRestriction 'char_length(first_name) > ? AND char_length(first_name) < ?', [4, 6]
-        }
 
-        
-        // should retrieve homer, bart and lisa, not maggie
-        results = domainClass.withCriteria {
-            sqlRestriction 'char_length(first_name) > ? AND char_length(first_name) < ?', [2, 6]
-        }
-        
-        assertEquals 3, results?.size()
+        assertEquals 4, results?.size()
     }
 
     void testOrderByProjection() {
