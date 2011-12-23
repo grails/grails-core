f8541d243f45939456e847112942a8b42138004f
Ben Doerr <tiggerizzy@bendoerr.me>
2011-12-23 11:43:38 -0500

    Additional test for MetaClassCleanupSpec
    Originally a blocker for us in the 2.0.0.RC1 release. Just wanted to add
    a test so it doesn't regress.

--- grails-plugin-testing/src/test/groovy/grails/test/mixin/MetaClassCleanupSpec.groovy
+++ grails-plugin-testing/src/test/groovy/grails/test/mixin/MetaClassCleanupSpec.groovy
@@ -13,9 +13,11 @@ class MetaClassCleanupSpec extends Specification {
     def "Test that meta classes are restored to prior state after test run"() {
         when:"A meta class is modified in the test"
             Author.metaClass.testMe = {-> "test"}
+            Author.metaClass.testToo = {-> "second"}
             def a = new Author()
-        then:"The method is available"
+        then:"The methods are available"
             a.testMe() == "test"
+            a.testToo() == "second"
     }
 
     @AfterClass
@@ -24,6 +26,12 @@ class MetaClassCleanupSpec extends Specification {
 
         try {
             a.testMe()
+            Assert.fail("Should have cleaned up meta class changes")
+        } catch (MissingMethodException) {
+        }
+        
+        try {
+            a.testToo()
             Assert.fail("Should have cleaned up meta class changes")
         } catch (MissingMethodException) {
         }
