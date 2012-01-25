636825d47ce238b8286551f3105d3a389274a64b
graemerocher <graeme.rocher@gmail.com>
2012-01-25 14:48:54 +0100

    fix for GRAILS-7851 "Grails 2.0.0M1 Domain Class named Environment"

---
+++ grails-test-suite-persistence/src/test/groovy/org/codehaus/groovy/grails/orm/hibernate/DomainClassNamedEnvironmentSpec.groovy
@@ -0,0 +1,49 @@
+package org.codehaus.groovy.grails.orm.hibernate
+
+import grails.persistence.Entity
+import org.codehaus.groovy.grails.commons.GrailsApplication
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: graemerocher
+ * Date: 1/25/12
+ * Time: 2:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class DomainClassNamedEnvironmentSpec extends GormSpec{
+    @Override
+    List getDomainClasses() {
+        [Environment]
+    }
+    
+    void "Test that a domain class named environment doesn't conflict with Spring's environment bean"() {
+        when:"A service is called that injects the environment"
+            def environmentService = applicationContext.getBean(EnvironmentService)
+        
+        then:"The service is usable"
+            environmentService.listEnvironments().size() == 0
+    }
+
+    @Override
+    protected void initializeApplication() {
+        grailsApplication.initialise()
+        domainClasses?.each { dc -> grailsApplication.addArtefact 'Domain', dc }
+        grailsApplication.addArtefact("Service", EnvironmentService)
+        grailsApplication.setApplicationContext(applicationContext)
+        parentCtx.registerMockBean(GrailsApplication.APPLICATION_ID, grailsApplication)
+
+    }
+
+
+}
+@Entity
+class Environment {
+    String name
+}
+
+class EnvironmentService {
+    Environment environment
+    List listEnvironments() {
+        Environment.list()
+    }
+}
