087a4b21f88133901e9583d61be765a7b562a096
graemerocher <graeme.rocher@gmail.com>
2012-02-09 12:10:54 +0100

    update to Groovy 1.8.6 snapshot and spring loaded 1.0.4

--- grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
+++ grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
@@ -180,7 +180,7 @@ public class GrailsCoreDependencies {
 
                         // dependencies needed at compile time
                         ModuleRevisionId[] groovyDependencies = {
-                            ModuleRevisionId.newInstance("org.codehaus.groovy", "groovy-all", "1.8.5")
+                            ModuleRevisionId.newInstance("org.codehaus.groovy", "groovy-all", "1.8.6-SNAPSHOT")
                         };
                         registerDependencies(dependencyManager, compileTimeDependenciesMethod, groovyDependencies, "jline");
 
