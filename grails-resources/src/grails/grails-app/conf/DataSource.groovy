8840318bee01f930f3fa73ad3f15da57f710f325
graemerocher <graeme.rocher@gmail.com>
2012-03-22 10:12:51 +0100

    disable query cache by default. Related to GRAILS-8888

--- grails-resources/src/grails/grails-app/conf/DataSource.groovy
+++ grails-resources/src/grails/grails-app/conf/DataSource.groovy
@@ -6,7 +6,7 @@ dataSource {
 }
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = true
+    cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 // environment specific settings
