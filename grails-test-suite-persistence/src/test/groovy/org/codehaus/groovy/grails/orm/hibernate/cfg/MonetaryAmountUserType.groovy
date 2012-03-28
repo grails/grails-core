1f38373f244cd612ddff89cf955e2953c85aeee6
Burt Beckwith <beckwithb@vmware.com>
2012-03-27 23:43:09 -0400

    created abstract AbstractUserType base class

--- grails-test-suite-persistence/src/test/groovy/org/codehaus/groovy/grails/orm/hibernate/cfg/MonetaryAmountUserType.groovy
+++ grails-test-suite-persistence/src/test/groovy/org/codehaus/groovy/grails/orm/hibernate/cfg/MonetaryAmountUserType.groovy
@@ -1,47 +1,40 @@
 package org.codehaus.groovy.grails.orm.hibernate.cfg
 
-import java.sql.*
-import org.hibernate.*
-import org.hibernate.usertype.UserType
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.sql.Types
+
+import org.codehaus.groovy.grails.orm.hibernate.AbstractUserType
 
 /**
  * Example multi-column Hibernate user type for use in testing.
  */
-class MonetaryAmountUserType implements UserType {
+class MonetaryAmountUserType extends AbstractUserType {
 
-    private static final int[] SQL_TYPES = [ Types.NUMERIC, Types.VARCHAR ] as int[]
+    private static final int[] SQL_TYPES = [Types.NUMERIC, Types.VARCHAR]
 
     int[] sqlTypes() { SQL_TYPES }
     Class returnedClass() { MonetaryAmount }
-    boolean equals(Object x, Object y) { x == y }
-    int hashCode(Object x) { x.hashCode() }
-    Object deepCopy(Object value) { value }
-    boolean isMutable() { false }
 
-    Object nullSafeGet(ResultSet resultSet, String[] names, owner) throws SQLException {
+    Object nullSafeGet(ResultSet rs, String[] names, owner) throws SQLException {
 
-        if (resultSet.wasNull()) return null
+        if (rs.wasNull()) return null
 
-        BigDecimal value = resultSet.getBigDecimal(names[0])
-        Currency currency = Currency.getInstance(resultSet.getString(names[1]))
+        BigDecimal value = rs.getBigDecimal(names[0])
+        Currency currency = Currency.getInstance(rs.getString(names[1]))
         new MonetaryAmount(value, currency)
     }
 
-    void nullSafeSet(PreparedStatement statement, amount, int index) throws SQLException {
-        if (value == null) {
-            statement.setNull(index, SQL_TYPES[index])
-            statement.setNull(index + 1, SQL_TYPES[index + 1])
+    void nullSafeSet(PreparedStatement ps, amount, int index) throws SQLException {
+        if (amount == null) {
+            ps.setNull(index, SQL_TYPES[index])
+            ps.setNull(index + 1, SQL_TYPES[index + 1])
         }
         else {
             String currencyCode = amount.currency.currencyCode
-            statement.setBigDecimal(index, amount.value)
-            statement.setString(index + 1, currencyCode)
+            ps.setBigDecimal(index, amount.value)
+            ps.setString(index + 1, currencyCode)
         }
     }
-
-    Serializable disassemble(Object value) { value }
-
-    Object assemble(Serializable cached, Object owner) { cached }
-
-    Object replace(Object original, Object target, Object owner) { original }
 }
