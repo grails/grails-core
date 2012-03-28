04ef5b874b7d8106391d0b8b4463fa979d52fbf1
Burt Beckwith <beckwithb@vmware.com>
2012-03-27 23:43:09 -0400

    created abstract AbstractUserType base class

---
+++ grails-test-suite-persistence/src/test/groovy/org/codehaus/groovy/grails/orm/hibernate/AbstractUserType.java
@@ -0,0 +1,37 @@
+package org.codehaus.groovy.grails.orm.hibernate;
+
+import java.io.Serializable;
+
+import org.hibernate.HibernateException;
+import org.hibernate.usertype.UserType;
+
+public abstract class AbstractUserType implements UserType {
+
+    public boolean equals(Object x, Object y) throws HibernateException {
+        return x == y;
+    }
+
+    public int hashCode(Object x) throws HibernateException {
+        return x.hashCode();
+    }
+
+    public Object deepCopy(Object value) throws HibernateException {
+        return value;
+    }
+
+    public boolean isMutable() {
+        return false;
+    }
+
+    public Serializable disassemble(Object value) throws HibernateException {
+        return (Serializable)value;
+    }
+
+    public Object assemble(Serializable cached, Object owner) throws HibernateException {
+        return cached;
+    }
+
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return original;
+    }
+}
