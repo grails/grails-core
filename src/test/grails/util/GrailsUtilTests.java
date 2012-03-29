e34faf191299840ac472621eeca4e019605f731d
graemerocher <graeme.rocher@gmail.com>
2012-03-29 09:22:44 +0200

    update version to 1.3.8

--- src/test/grails/util/GrailsUtilTests.java
+++ src/test/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("1.3.8.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("1.3.8", GrailsUtil.getGrailsVersion());
     }
 
     @Override
