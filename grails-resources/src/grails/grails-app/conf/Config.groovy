051145ee86c2e999c1f107c0b33b99c5de3b8c35
Burt Beckwith <beckwithb@vmware.com>
2012-04-02 19:20:33 -0400

    small comment tweak

--- grails-resources/src/grails/grails-app/conf/Config.groovy
+++ grails-resources/src/grails/grails-app/conf/Config.groovy
@@ -56,7 +56,7 @@ grails.web.disable.multipart=false
 // request parameters to mask when logging exceptions
 grails.exceptionresolver.params.exclude = ['password']
 
-// enable auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
+// configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
 grails.hibernate.cache.queries = false
 
 environments {
