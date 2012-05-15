6833c130b61e64bd56579e32d246070eb5b30e94
graemerocher <graeme.rocher@gmail.com>
2012-05-15 09:39:44 +0200

    use release plugin 2.0.1

--- grails-resources/src/grails/plugin/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/plugin/grails-app/conf/BuildConfig.groovy
@@ -28,7 +28,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         build(":tomcat:$grailsVersion",
-              ":release:2.0.0") {
+              ":release:2.0.1") {
             export = false
         }
     }
