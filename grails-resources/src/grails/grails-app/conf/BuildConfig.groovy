8b3fc594fcc3aef88bebc2c38d25b2a098faf738
Jeff Brown <jeff@jeffandbetsy.net>
2012-07-09 11:12:51 -0500

    Merge from 2.1.x branch

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -50,7 +50,7 @@ grails.project.dependency.resolution = {
         build ":tomcat:$grailsVersion"
 
         runtime ":database-migration:1.1"
-        
-        compile ':cache:1.0.0.RC1'
+
+        compile ':cache:1.0.0'
     }
 }
