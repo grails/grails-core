3bfed7bf3f76fb99aa30d2b6f070e9d5c2c54d4b
Burt Beckwith <beckwithb@vmware.com>
2012-07-24 18:53:09 -0400

    added sample grails.project.fork config

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -4,9 +4,12 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-// Use the following to control whether to fork a JVM to isolate classpaths in Grails.
-grails.project.fork.run=false
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+// uncomment (and adjust settings) to fork the JVM to isolate classpaths
+//grails.project.fork = [
+//   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
+//]
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -33,8 +36,9 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
+
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
 
         // runtime 'mysql:mysql-connector-java:5.1.20'
     }
