588a5164c6d25231016cc6bf0f38bc1d89d1b254
Bobby Warner <bobbywarner@gmail.com>
2012-07-25 22:01:09 -0500

    Updated to Groovy 1.8.7

--- grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
+++ grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
@@ -181,7 +181,7 @@ public class GrailsCoreDependencies {
 
                         // dependencies needed at compile time
                         ModuleRevisionId[] groovyDependencies = {
-                            ModuleRevisionId.newInstance("org.codehaus.groovy", "groovy-all", "1.8.6")
+                            ModuleRevisionId.newInstance("org.codehaus.groovy", "groovy-all", "1.8.7")
                         };
                         registerDependencies(dependencyManager, compileTimeDependenciesMethod, groovyDependencies, "jline");
 
