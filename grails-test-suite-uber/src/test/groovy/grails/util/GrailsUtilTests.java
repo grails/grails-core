ee7e69cab0186f74b5fbae00aed2e5375483202b
Graeme Rocher <graeme.rocher@gmail.com>
2012-09-11 09:47:45 +0200

    update version to 2.1.1 and Tomcat to 7.0.30

--- grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("2.1.1.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("2.1.1", GrailsUtil.getGrailsVersion());
     }
 
     @Override
