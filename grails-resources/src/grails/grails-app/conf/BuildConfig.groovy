46cdc5c0dd6d6d6df221036f666e07c21f2c1c5a
Graeme Rocher <graeme.rocher@gmail.com>
2012-09-11 13:48:04 +0200

    update to jquery 1.8.0

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -39,7 +39,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.7.2"
+        runtime ":jquery:1.8.0"
         runtime ":resources:1.1.6"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
