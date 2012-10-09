b1e54930eca21dbae95491765ae46159ce8ebaba
Graeme Rocher <graeme.rocher@gmail.com>
2012-10-09 12:55:20 +0200

    fix failing test

--- grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("2.2.0.RC1", GrailsUtil.getGrailsVersion());
+        assertEquals("2.2.0.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @Override
