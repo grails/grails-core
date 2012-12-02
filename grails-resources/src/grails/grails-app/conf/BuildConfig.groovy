4ec5a1efad1df8e9242c90862f498226eb267f38
Burt Beckwith <beckwithb@vmware.com>
2012-12-02 01:50:21 -0500

    need to use database-migration plugin 1.2.1 in 2.2+ because of http://jira.grails.org/browse/GPDATABASEMIGRATION-109

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -55,7 +55,7 @@ grails.project.dependency.resolution = {
 
         build ":tomcat:$grailsVersion"
 
-        runtime ":database-migration:1.2"
+        runtime ":database-migration:1.2.1"
 
         compile ':cache:1.0.1'
     }
