e75dc039a0b91644e9f89977ffcd3d3c77572327
Jeff Brown <jbrown@vmware.com>
2012-12-05 19:47:14 -0600

    2.1.3.BUILD-SNAPSHOT

--- grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("2.1.2", GrailsUtil.getGrailsVersion());
+        assertEquals("2.1.3.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @Override
