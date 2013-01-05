6836ab720de894ad2ec0ec4775f2aecd27d20fae
Bobby Warner <bobbywarner@gmail.com>
2013-01-05 11:05:22 -0600

    Database Migration was updated again

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -50,7 +50,7 @@ grails.project.dependency.resolution = {
 
         build ":tomcat:$grailsVersion"
 
-        runtime ":database-migration:1.3.1"
+        runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
     }
