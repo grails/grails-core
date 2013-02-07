fcfa3138ec0dc407daf1ea2c10b6e25f745e9d24
Graeme Rocher <graeme.rocher@gmail.com>
2013-02-07 14:33:08 +0100

    fix for GRAILS-9723 "--nojars stopped working (jars are included)"

--- scripts/_GrailsWar.groovy
+++ scripts/_GrailsWar.groovy
@@ -39,6 +39,7 @@ target (configureRunningScript: "Sets the currently running script, in case call
 
 target (war: "The implementation target") {
     depends(parseArguments, configureRunningScript, cleanWarFile, packageApp, compilegsp, configureWarName)
+    warCreator.includeJars = argsMap.nojars ? false : true
     warCreator.packageWar()
 }
 
