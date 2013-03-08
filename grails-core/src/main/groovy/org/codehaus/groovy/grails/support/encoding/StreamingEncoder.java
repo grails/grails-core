12218d8a4a7bd0b20401012cccf9148f64425593
Lari Hotari <lari.hotari@sagire.fi>
2013-03-08 13:40:39 +0200

    refactor EncodedWriter from StreamCharBuffer to grails-core as EncodedAppender

--- grails-core/src/main/groovy/org/codehaus/groovy/grails/support/encoding/StreamingEncoder.java
+++ grails-core/src/main/groovy/org/codehaus/groovy/grails/support/encoding/StreamingEncoder.java
@@ -1,8 +1,6 @@
 package org.codehaus.groovy.grails.support.encoding;
 
-import java.io.IOException;
-import java.io.Writer;
 
 public interface StreamingEncoder extends Encoder {
-    public void encodeToWriter(Object source, Writer writer) throws IOException;
+    public void encodeToStream(Object source, EncodedAppender appender);
 }
