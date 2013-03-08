655c21656debc0fbf6a2024f808704c90e2b6c51
Lari Hotari <lari.hotari@sagire.fi>
2013-03-08 11:42:14 +0200

    move DefaultEncodingState from GrailsWebRequest to separate file in grails-core

--- grails-core/src/main/groovy/org/codehaus/groovy/grails/support/encoding/EncodingState.java
+++ grails-core/src/main/groovy/org/codehaus/groovy/grails/support/encoding/EncodingState.java
@@ -5,7 +5,6 @@ import java.util.Set;
 public interface EncodingState {
     public Set<Encoder> getEncodersFor(CharSequence string);
     public boolean shouldEncodeWith(Encoder encoderToApply, CharSequence string);
-    public boolean isEncoderEquivalentToPrevious(Encoder encoderToApply, Encoder encoder);
     public boolean isEncodedWith(Encoder encoder, CharSequence string);
     public void registerEncodedWith(Encoder encoder, CharSequence escaped);
 }
