af5fa33723615356eb14a7c9300d1efc1ba77634
Lari Hotari <lari.hotari@sagire.fi>
2013-03-08 10:55:43 +0200

    Add initial support for RawCodec, refactor EncodingState impl

--- grails-core/src/main/groovy/org/codehaus/groovy/grails/support/encoding/EncodingState.java
+++ grails-core/src/main/groovy/org/codehaus/groovy/grails/support/encoding/EncodingState.java
@@ -3,7 +3,8 @@ package org.codehaus.groovy.grails.support.encoding;
 import java.util.Set;
 
 public interface EncodingState {
+    //public Set<Encoder> getEncodersFor(CharSequence string);
     public Set<String> getEncodingTagsFor(CharSequence string);
-    public boolean isEncodedWith(String encoding, CharSequence string);
-    public void registerEncodedWith(String encoding, CharSequence escaped);
+    public boolean isEncodedWith(Encoder encoder, CharSequence string);
+    public void registerEncodedWith(Encoder encoder, CharSequence escaped);
 }
