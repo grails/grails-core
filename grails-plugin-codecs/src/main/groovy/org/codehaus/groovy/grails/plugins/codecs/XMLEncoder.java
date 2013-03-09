3f7afe2e92bf04426ca7d779c63456ecfc0ba07e
Lari Hotari <lari.hotari@sagire.fi>
2013-03-09 16:10:40 +0200

    revise the "escaped" characters in XML/XHTML. Use &#160; instead of &nbsp; since it's also valid in xml.

--- grails-plugin-codecs/src/main/groovy/org/codehaus/groovy/grails/plugins/codecs/XMLEncoder.java
+++ grails-plugin-codecs/src/main/groovy/org/codehaus/groovy/grails/plugins/codecs/XMLEncoder.java
@@ -6,15 +6,18 @@ import java.util.Set;
 
 public class XMLEncoder extends AbstractCharReplacementEncoder {
     static final String XML_CODEC_NAME="EscapedXML";
-    private static final String ESCAPED_BACKTICK = "&#" + ((int) '`')  + ";";
-    private static final String ESCAPED_AT = "&#" + ((int) '@')  + ";";
-    private static final String ESCAPED_EQUAL = "&#" + ((int) '=')  + ";";
-    private static final String ESCAPED_PLUS = "&#" + ((int) '+')  + ";";
-    private static final String ESCAPED_APOS = "&#" + ((int) '\'')  + ";";
+    private static final String ESCAPED_APOS = "&#" + ((int) '\'')  + ";"; // html doesn't have apos, so use numeric entity
     private static final String ESCAPED_QUOTE = "&quot;";
     private static final String ESCAPED_GT = "&gt;";
     private static final String ESCAPED_LT = "&lt;";
     private static final String ESCAPED_AMP = "&amp;";
+    // some extras
+    private static final String ESCAPED_BACKSLASH = "&#" + ((int) '\\')  + ";";
+    private static final char NBSP=(char)160;
+    private static final String ESCAPED_NON_BREAKING_SPACE = "&#" + ((int) NBSP)  + ";";
+    private static final String ESCAPED_BACKTICK = "&#" + ((int) '`')  + ";";
+    private static final String ESCAPED_AT = "&#" + ((int) '@')  + ";"; // IE Javascript conditional compilation rules
+    
     private static final Set<String> equivalentCodecNames = new HashSet<String>(Arrays.asList(new String[]{"HTML4",HTMLCodec.CODEC_NAME}));
 
     @Override
@@ -28,10 +31,10 @@ public class XMLEncoder extends AbstractCharReplacementEncoder {
           case '>': return ESCAPED_GT;          
           case '"': return ESCAPED_QUOTE;
           case '\'': return  ESCAPED_APOS;
-          case '+': return ESCAPED_PLUS;
-          case '=': return ESCAPED_EQUAL;
-          case '@': return ESCAPED_AT;
+          case '\\': return  ESCAPED_BACKSLASH;
+          case '@': return ESCAPED_AT; 
           case '`': return ESCAPED_BACKTICK;
+          case NBSP: return ESCAPED_NON_BREAKING_SPACE;
       }
       return null;
     }
