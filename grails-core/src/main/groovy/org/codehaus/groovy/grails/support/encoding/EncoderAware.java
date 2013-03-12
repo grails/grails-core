4a3b73eb47a69169a760d912881c7754253364d8
Lari Hotari <lari.hotari@sagire.fi>
2013-03-12 07:07:57 +0200

    remove encoder setter from EncoderAware interface

--- grails-core/src/main/groovy/org/codehaus/groovy/grails/support/encoding/EncoderAware.java
+++ grails-core/src/main/groovy/org/codehaus/groovy/grails/support/encoding/EncoderAware.java
@@ -2,6 +2,5 @@ package org.codehaus.groovy.grails.support.encoding;
 
 public interface EncoderAware {
     public boolean isEncoderAware();
-    public void setEncoder(Encoder encoder);
     public Encoder getEncoder();
 }
