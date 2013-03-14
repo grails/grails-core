ce8c806aa97ca7bf47ba3455bf309febde3a945a
Bobby Warner <bobbywarner@gmail.com>
2013-03-14 12:06:34 -0500

    Updated Versions: Spring 3.2.2, log4j 1.2.17, AspectJ 1.7.2

--- grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
+++ grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
@@ -31,7 +31,7 @@ public class GrailsCoreDependencies {
     public final String grailsVersion;
     public final String servletVersion;
     protected final String groovyVersion = "2.1.1";
-    protected final String log4jVersion = "1.2.16";
+    protected final String log4jVersion = "1.2.17";
     protected final String h2Version = "1.3.164";
     protected final String ehcacheVersion = "2.4.6";
     protected final String jaxbVersion = "2.0";
