7f7d59eed215b63f5fceb07240b84b5f8b866618
Burt Beckwith <burt@burtbeckwith.com>
2013-03-19 18:30:03 -0400

    removed legacyResolve from plugin's BuildConfig.groovy since it's unlikely to be needed, and causes problems with older versions of Grails when the plugin's dependencies.groovy is parsed

--- grails-resources/src/grails/plugin/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/plugin/grails-app/conf/BuildConfig.groovy
@@ -10,7 +10,6 @@ grails.project.dependency.resolution = {
         // excludes 'ehcache'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
     repositories {
         grailsCentral()
         // uncomment the below to enable remote dependency resolution
