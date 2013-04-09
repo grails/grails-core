f779ca0232d689a576a720d1cce033287e12e949
Lari Hotari <lari.hotari@sagire.fi>
2013-04-09 08:05:30 +0300

    Override buildRequestAttributes also in GroovyPagesServlet see commit f0b0d18858 where the change was made for GrailsDispatcherServlet

--- grails-web/src/main/groovy/org/codehaus/groovy/grails/web/pages/GroovyPagesServlet.java
+++ grails-web/src/main/groovy/org/codehaus/groovy/grails/web/pages/GroovyPagesServlet.java
@@ -40,7 +40,9 @@ import org.codehaus.groovy.grails.web.sitemesh.GrailsLayoutDecoratorMapper;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.FrameworkServlet;
 import org.springframework.web.util.WebUtils;
 
@@ -80,6 +82,16 @@ public class GroovyPagesServlet extends FrameworkServlet implements PluginManage
         // use the root web application context always
         setContextAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
     }
+    
+    @Override
+    protected ServletRequestAttributes buildRequestAttributes(HttpServletRequest request, HttpServletResponse response, RequestAttributes previousAttributes) {
+        if(previousAttributes instanceof GrailsWebRequest) {
+            return null;
+        }
+        else {
+            return super.buildRequestAttributes(request, response, previousAttributes);
+        }
+    }    
 
     /**
      * The size of the buffer used when formulating the response
