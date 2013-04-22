c0249196ab48d3ae396ec6c5a1c9b9d1e91d3763
Bobby Warner <bobbywarner@gmail.com>
2013-04-22 09:14:19 -0500

    Added mysql update to plugins BuildConfig

--- grails-resources/src/grails/plugin/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/plugin/grails-app/conf/BuildConfig.groovy
@@ -23,8 +23,7 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.21'
+        // runtime 'mysql:mysql-connector-java:5.1.24'
     }
 
     plugins {
