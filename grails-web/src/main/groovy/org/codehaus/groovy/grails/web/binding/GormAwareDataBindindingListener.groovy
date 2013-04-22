97ffa323664a9efe358a53e61ba5ab1c74195e0f
Jeff Brown <jbrown@vmware.com>
2013-04-22 09:54:21 -0500

    cleanup GormAwareDataBindingListener
    Mark with @CompileStatic and reformat code

--- grails-web/src/main/groovy/org/codehaus/groovy/grails/web/binding/GormAwareDataBindindingListener.groovy
+++ grails-web/src/main/groovy/org/codehaus/groovy/grails/web/binding/GormAwareDataBindindingListener.groovy
@@ -1,5 +1,7 @@
 package org.codehaus.groovy.grails.web.binding
- 
+
+import groovy.transform.CompileStatic
+
 import org.codehaus.groovy.grails.validation.ConstrainedProperty
 import org.codehaus.groovy.runtime.MetaClassHelper
 import org.codehaus.groovy.runtime.metaclass.ThreadManagedMetaBeanProperty
@@ -9,68 +11,68 @@ import org.springframework.context.support.DefaultMessageSourceResolvable
 import org.springframework.validation.BindingResult
 import org.springframework.validation.Errors
 import org.springframework.validation.FieldError
-import org.springframework.validation.ObjectError
- 
+
+@CompileStatic
 class GormAwareDataBindindingListener extends
-            DataBindingListenerAdapter {
-        private final BindingResult tmpBindingResult;
-        private final Object object;
- 
-        GormAwareDataBindindingListener(BindingResult tmpBindingResult,
-                Object object) {
-            this.tmpBindingResult = tmpBindingResult;
-            this.object = object;
-        }
- 
-        @Override
-        public Boolean beforeBinding(Object obj, String propertyName, Object value) {
-            if("".equals(value)) {
-                def cps = resolveConstrainedProperties(obj)
-                if(cps != null) {
-                    ConstrainedProperty cp = cps[propertyName]
-                    if(cp && cp.isNullable()) {
-                        obj[propertyName] = null
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
- 
-        @Override
-        public void bindingError(BindingError error) {
-            Object[] o = getArgumentsForBindError(object.getClass().getName(), error.getPropertyName());
-            def codes = ['typeMismatch']
-            def cause = error.cause
-            def defaultMessage = cause ? cause.message : 'Data Binding Failed'
-            def fieldError = new FieldError("", error.getPropertyName(), error.getRejectedValue(), true, codes.toArray(new String[0]), o, defaultMessage);
-            tmpBindingResult.addError(fieldError);
-        }
-        
-        private Map resolveConstrainedProperties(Object object) {
-            Map constrainedProperties = null;
-                // is this dead code? , didn't remove in case it's used somewhere
-                MetaClass mc = GroovySystem.getMetaClassRegistry().getMetaClass(object.getClass());
-                MetaProperty metaProp = mc.getMetaProperty('constraints');
-                if (metaProp != null) {
-                    Object constrainedPropsObj = getMetaPropertyValue(metaProp, object);
-                    if (constrainedPropsObj instanceof Map) {
-                        constrainedProperties = (Map)constrainedPropsObj;
-                }
-            }
-            return constrainedProperties;
-        }
-    
-        private Object getMetaPropertyValue(MetaProperty metaProperty, Object delegate) {
-        if (metaProperty instanceof ThreadManagedMetaBeanProperty) {
-            return ((ThreadManagedMetaBeanProperty)metaProperty).getGetter().invoke(delegate, MetaClassHelper.EMPTY_ARRAY);
-        }
- 
-        return metaProperty.getProperty(delegate);
+DataBindingListenerAdapter {
+    private final BindingResult tmpBindingResult
+    private final Object object
+
+    GormAwareDataBindindingListener(BindingResult tmpBindingResult, Object object) {
+        this.tmpBindingResult = tmpBindingResult
+        this.object = object
     }
-        
-        protected Object[] getArgumentsForBindError(String objectName, String field) {
-            def codes = [objectName + Errors.NESTED_PATH_SEPARATOR + field, field] as String[]
-            [new DefaultMessageSourceResolvable(codes, field)] as Object[];
+
+    @Override
+    public Boolean beforeBinding(Object obj, String propertyName, Object value) {
+        if("".equals(value)) {
+            def cps = resolveConstrainedProperties(obj)
+            if(cps != null) {
+                ConstrainedProperty cp = (ConstrainedProperty)cps[propertyName]
+                if(cp && cp.isNullable()) {
+                    obj[propertyName] = null
+                    return false
+                }
+            }
         }
+        return true;
+    }
+
+    @Override
+    public void bindingError(BindingError error) {
+        Object[] o = getArgumentsForBindError(object.getClass().getName(), error.getPropertyName());
+        def codes = ['typeMismatch']
+        def cause = error.cause
+        def defaultMessage = cause ? cause.message : 'Data Binding Failed'
+        def fieldError = new FieldError("", error.getPropertyName(), error.getRejectedValue(), true, codes.toArray(new String[0]), o, defaultMessage);
+        tmpBindingResult.addError(fieldError);
+    }
+
+    private Map resolveConstrainedProperties(Object object) {
+        Map constrainedProperties = null
+        MetaClass mc = GroovySystem.getMetaClassRegistry().getMetaClass(object.getClass())
+        MetaProperty metaProp = mc.getMetaProperty('constraints')
+        if (metaProp != null) {
+            Object constrainedPropsObj = getMetaPropertyValue(metaProp, object)
+            if (constrainedPropsObj instanceof Map) {
+                constrainedProperties = (Map)constrainedPropsObj
+            }
+        }
+        constrainedProperties
+    }
+
+    private Object getMetaPropertyValue(MetaProperty metaProperty, Object delegate) {
+        if (metaProperty instanceof ThreadManagedMetaBeanProperty) {
+            return ((ThreadManagedMetaBeanProperty)metaProperty).getGetter().invoke(delegate, MetaClassHelper.EMPTY_ARRAY)
+        }
+
+        return metaProperty.getProperty(delegate)
+    }
+
+    protected Object[] getArgumentsForBindError(String objectName, String field) {
+        def codes = [
+            objectName + Errors.NESTED_PATH_SEPARATOR + field,
+            field] as String[]
+        [new DefaultMessageSourceResolvable(codes, field)] as Object[]
+    }
 }
