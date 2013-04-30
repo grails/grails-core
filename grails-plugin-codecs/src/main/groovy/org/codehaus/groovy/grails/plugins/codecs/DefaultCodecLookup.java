6eb806860f223caece828e8785b5d86a1391e270
Bobby Warner <bobbywarner@gmail.com>
2013-04-30 13:01:16 -0500

    Copyright cleanup across the project

--- grails-plugin-codecs/src/main/groovy/org/codehaus/groovy/grails/plugins/codecs/DefaultCodecLookup.java
+++ grails-plugin-codecs/src/main/groovy/org/codehaus/groovy/grails/plugins/codecs/DefaultCodecLookup.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codehaus.groovy.grails.plugins.codecs;
 
 import grails.util.GrailsNameUtils;
@@ -23,6 +38,10 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.OrderComparator;
 
+/**
+ * @author Lari Hotari
+ * @since 2.3
+ */
 public class DefaultCodecLookup implements GrailsApplicationAware, InitializingBean, CodecLookup {
     private static final String NONE_CODEC_NAME = "none";
     protected ApplicationContext applicationContext;
--- grails-test/src/main/groovy/org/codehaus/groovy/grails/test/event/GrailsTestEventConsoleReporter.groovy
+++ grails-test/src/main/groovy/org/codehaus/groovy/grails/test/event/GrailsTestEventConsoleReporter.groovy
@@ -1,4 +1,5 @@
-/* Copyright 2009 the original author or authors.
+/*
+ * Copyright 2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.codehaus.groovy.grails.test.event
 
 import grails.build.GrailsBuildListener
