13713efce2c1ea8ed285f728618ac82db1cd91a4
Jeff Brown <jbrown@gopivotal.com>
2013-05-09 10:42:01 -0500

    Revert "version 2.3.0.M1"
    This reverts commit 40350d0666b92e6890d1b5774eb5d528dd8593ee.

--- grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("2.3.0.M1", GrailsUtil.getGrailsVersion());
+        assertEquals("2.3.0.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @Override
