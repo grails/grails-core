7404419e072b24f37eb351f3cdf357b909541957
Graeme Rocher <graeme.rocher@gmail.com>
2013-05-17 09:42:17 +0200

    Missing abstract class from previous commit

---
+++ grails-plugin-url-mappings/src/test/groovy/org/codehaus/groovy/grails/web/mapping/AbstractUrlMappingsSpec.groovy
@@ -0,0 +1,23 @@
+package org.codehaus.groovy.grails.web.mapping
+
+import grails.web.CamelCaseUrlConverter
+import org.springframework.mock.web.MockServletContext
+import spock.lang.Specification
+
+/**
+ * @author Graeme Rocher
+ */
+abstract class AbstractUrlMappingsSpec extends Specification{
+
+    LinkGenerator getLinkGenerator(Closure mappings) {
+        def generator = new DefaultLinkGenerator("http://localhost", null)
+        generator.grailsUrlConverter = new CamelCaseUrlConverter()
+        generator.urlMappingsHolder = getUrlMappingsHolder mappings
+        return generator;
+    }
+    UrlMappingsHolder getUrlMappingsHolder(Closure mappings) {
+        def evaluator = new DefaultUrlMappingEvaluator(new MockServletContext())
+        def allMappings = evaluator.evaluateMappings mappings
+        return new DefaultUrlMappingsHolder(allMappings)
+    }
+}
