b9f0ef679f6b3e737906c91b07202e08172d4875
Jeff Brown <jbrown@gopivotal.com>
2013-05-22 14:33:53 -0500

    GRAILS-10092 - improve redirects to/from namespaced controllers
    When redirecting from a namespaces controller, if no namespace is specified for the redirect, the redirect will be directed to a controller in the same namespace as the one initiating the redirect.  To redirect from a namespaced controller to a non-namespaced controller, namespace:null must be specified.

---
+++ grails-test-suite-uber/src/test/groovy/org/codehaus/groovy/grails/web/servlet/mvc/beta/AnotherNamespacedController.groovy
@@ -0,0 +1,11 @@
+package org.codehaus.groovy.grails.web.servlet.mvc.beta
+
+class AnotherNamespacedController {
+
+    static namespace = 'secondary'
+    
+    def demo() {
+        render 'Rendered by the secondary AnotherNamespaced Controller'
+    }
+
+}
