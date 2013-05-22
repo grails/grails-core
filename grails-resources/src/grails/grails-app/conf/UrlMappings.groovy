4c2dceefc64147e43aaa7e644b365d8cb972643e
Graeme Rocher <graeme.rocher@gmail.com>
2013-05-23 00:23:25 +0200

    fix indentation

--- grails-resources/src/grails/grails-app/conf/UrlMappings.groovy
+++ grails-resources/src/grails/grails-app/conf/UrlMappings.groovy
@@ -2,12 +2,12 @@ class UrlMappings {
 
 	static mappings = {
         "/$controller/$action?/$id?(.${format})?"{
-			constraints {
-				// apply constraints here
-			}
-		}
+            constraints {
+                // apply constraints here
+            }
+        }
 
-		"/"(view:"/index")
-		"500"(view:'/error')
+        "/"(view:"/index")
+        "500"(view:'/error')
 	}
 }
