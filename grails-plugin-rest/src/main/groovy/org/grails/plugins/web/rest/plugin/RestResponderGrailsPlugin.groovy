5c0f4f899c7d2f0bbbf735f2c8df2c4c3deebe3b
Graeme Rocher <graeme.rocher@gmail.com>
2013-05-27 17:07:44 +0200

    fix failing test

--- grails-plugin-rest/src/main/groovy/org/grails/plugins/web/rest/plugin/RestResponderGrailsPlugin.groovy
+++ grails-plugin-rest/src/main/groovy/org/grails/plugins/web/rest/plugin/RestResponderGrailsPlugin.groovy
@@ -1,5 +1,6 @@
 package org.grails.plugins.web.rest.plugin
 
+import grails.util.GrailsUtil
 import org.codehaus.groovy.grails.plugins.web.api.ControllersMimeTypesApi
 import org.grails.plugins.web.rest.api.ControllersRestApi
 import org.grails.plugins.web.rest.render.DefaultRendererRegistry
@@ -8,6 +9,8 @@ import org.grails.plugins.web.rest.render.DefaultRendererRegistry
  * @author Graeme Rocher
  */
 class RestResponderGrailsPlugin {
+
+    def version = GrailsUtil.getGrailsVersion()
 
     def doWithSpring = {
         instanceControllersRestApi(ControllersRestApi, new DefaultRendererRegistry(), ref("instanceControllersApi"), new ControllersMimeTypesApi())
