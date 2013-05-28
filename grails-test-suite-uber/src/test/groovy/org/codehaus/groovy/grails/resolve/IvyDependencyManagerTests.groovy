264a4ce9c3c3b02a2c1f1b6625d5301fe0929f84
Graeme Rocher <graeme.rocher@gmail.com>
2013-05-28 09:45:29 +0200

    fix failing test

--- grails-test-suite-uber/src/test/groovy/org/codehaus/groovy/grails/resolve/IvyDependencyManagerTests.groovy
+++ grails-test-suite-uber/src/test/groovy/org/codehaus/groovy/grails/resolve/IvyDependencyManagerTests.groovy
@@ -644,7 +644,7 @@ class IvyDependencyManagerTests extends GroovyTestCase {
         }
 
         assertTrue("all default dependencies should be inherited", manager.dependencyDescriptors.every { it.inherited == true })
-        assertEquals 11, manager.dependencyDescriptors.findAll { it.scope == 'compile'}.size()
+        assertEquals 10, manager.dependencyDescriptors.findAll { it.scope == 'compile'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'runtime'}.size()
         assertEquals 2, manager.dependencyDescriptors.findAll { it.scope == 'test'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'build'}.size()
@@ -671,7 +671,7 @@ class IvyDependencyManagerTests extends GroovyTestCase {
         assertEquals 0, manager.dependencyDescriptors.findAll { it.scope == 'runtime'}.size()
         assertEquals 2, manager.dependencyDescriptors.findAll { it.scope == 'test'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'build'}.size()
-        assertEquals 15, manager.dependencyDescriptors.findAll { it.scope == 'provided'}.size()
+        assertEquals 14, manager.dependencyDescriptors.findAll { it.scope == 'provided'}.size()
         assertEquals 2, manager.dependencyDescriptors.findAll { it.scope == 'docs'}.size()
 
         manager = new IvyDependencyManager("project", "0.1",settings)
@@ -682,7 +682,7 @@ class IvyDependencyManagerTests extends GroovyTestCase {
             defaultDependencyClosure()
         }
 
-        assertEquals 11, manager.dependencyDescriptors.findAll { it.scope == 'compile'}.size()
+        assertEquals 10, manager.dependencyDescriptors.findAll { it.scope == 'compile'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'runtime'}.size()
         assertEquals 2, manager.dependencyDescriptors.findAll { it.scope == 'test'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'build'}.size()
