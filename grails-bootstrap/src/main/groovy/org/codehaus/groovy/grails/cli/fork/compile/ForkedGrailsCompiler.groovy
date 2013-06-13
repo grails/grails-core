5b5bf35db750670266f1c99eb72ba749e1440a49
Jeff Scott Brown <jbrown@gopivotal.com>
2013-06-13 07:27:07 -0500

    Revert "update copyright statements"
    This reverts commit 0c5f7afbb5d1ad3058915c75cad5c4e008130cd5.

--- grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/cli/fork/compile/ForkedGrailsCompiler.groovy
+++ grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/cli/fork/compile/ForkedGrailsCompiler.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 GoPivotal, Inc. All Rights Reserved
+ * Copyright 2012 SpringSource
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- grails-plugin-converters/src/main/groovy/org/codehaus/groovy/grails/plugins/converters/ConvertersGrailsPlugin.groovy
+++ grails-plugin-converters/src/main/groovy/org/codehaus/groovy/grails/plugins/converters/ConvertersGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2006 GoPivotal, Inc. All Rights Reserved
+ * Copyright 2004-2006 Graeme Rocher
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
