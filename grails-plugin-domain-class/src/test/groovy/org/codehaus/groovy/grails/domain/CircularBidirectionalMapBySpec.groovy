570e965cd65885b64959d37351ef75df6e2c05ef
Graeme Rocher <graeme.rocher@gmail.com>
2013-06-17 15:50:25 +0200

    fix for GRAILS-10115 "Incorrect bidirectional association mapping"

---
+++ grails-plugin-domain-class/src/test/groovy/org/codehaus/groovy/grails/domain/CircularBidirectionalMapBySpec.groovy
@@ -0,0 +1,81 @@
+package org.codehaus.groovy.grails.domain
+
+import grails.persistence.Entity
+import org.codehaus.groovy.grails.commons.DefaultGrailsApplication
+import org.codehaus.groovy.grails.commons.GrailsDomainClass
+import spock.lang.Specification
+
+/**
+ * @author Graeme Rocher
+ */
+class CircularBidirectionalMapBySpec extends Specification{
+
+    void "Test mapping for circular bidirectional association"() {
+
+        given:"A Grails application"
+            def application = new DefaultGrailsApplication([Person] as Class[], getClass().classLoader)
+            application.initialise()
+
+        when:"The domain instance is obtained"
+            GrailsDomainClass domainClass = application.getDomainClass(Person.name)
+
+        then:"The property mappings are correct"
+            !domainClass.getPropertyByName('father').bidirectional
+            !domainClass.getPropertyByName('friends').bidirectional
+    }
+
+    void "Test mapping for circular bidirectional association with 'none' as name"() {
+
+        given:"A Grails application"
+        def application = new DefaultGrailsApplication([Person2] as Class[], getClass().classLoader)
+        application.initialise()
+
+        when:"The domain instance is obtained"
+        GrailsDomainClass domainClass = application.getDomainClass(Person2.name)
+
+        then:"The property mappings are correct"
+        !domainClass.getPropertyByName('father').bidirectional
+        !domainClass.getPropertyByName('friends').bidirectional
+    }
+}
+
+@Entity
+class Person {
+
+    String name
+    Person father
+
+    static hasMany = [
+        friends: Person
+    ]
+
+    static mappedBy = [
+        father: null, friends:null
+    ]
+
+    static constraints = {
+        father (nullable: true)
+    }
+
+}
+
+
+@Entity
+class Person2 {
+
+    String name
+    Person father
+
+    static hasMany = [
+        friends: Person
+    ]
+
+    static mappedBy = [
+        father: 'none', friends:'none'
+    ]
+
+    static constraints = {
+        father (nullable: true)
+    }
+
+}
