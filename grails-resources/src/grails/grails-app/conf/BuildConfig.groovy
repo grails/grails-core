9c3c6962b4e7e6bf5b8db72fed830385f00a26e3
Graeme Rocher <graeme.rocher@gmail.com>
2013-06-27 14:38:18 +0200

    update Hibernate plugin versions

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -60,7 +60,7 @@ grails.project.dependency.resolution = {
         compile ':cache:1.0.1'
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.M3" // or ":hibernate4:4.1.11.BUILD-SNAPSHOT"
+        runtime ":hibernate:3.6.10.M4" // or ":hibernate4:4.1.11.M1"
         runtime ":database-migration:1.3.3"
         runtime ":jquery:1.10.0"
         runtime ":resources:1.2"
