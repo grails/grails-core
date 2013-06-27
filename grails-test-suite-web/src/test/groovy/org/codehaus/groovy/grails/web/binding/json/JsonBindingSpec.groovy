382c08e6a7de860c210b48d42ce96de492eebe63
Jeff Scott Brown <jbrown@gopivotal.com>
2013-06-27 09:54:44 -0500

    GRAILS-9190 - added a test
    The test is currently marked with @Ignore

--- grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/binding/json/JsonBindingSpec.groovy
+++ grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/binding/json/JsonBindingSpec.groovy
@@ -1,7 +1,8 @@
 package org.codehaus.groovy.grails.web.binding.json
 
-import grails.persistence.Entity
+import grails.artefact.Artefact
 import grails.test.mixin.TestFor
+import spock.lang.Ignore
 import spock.lang.Specification
 
 @TestFor(BindingController)
@@ -20,18 +21,51 @@ class JsonBindingSpec extends Specification {
         model.person.name == 'Douglas'
         model.person.age == 42
     }
+    
+    @Ignore
+    void 'Test binding nested collection elements'() {
+        given:
+        request.json = '''
+            {
+    "lastName": "Brown", 
+    "familyMembers": [
+        {"name": "Jake", "age": "12"}, 
+        {"name": "Zack", "age": "15"}
+    ]
 }
+'''
+    when:
+        def model = controller.createFamily()
+    then:
+        model.family.lastName == 'Brown'
+        
+        model.family.familyMembers.size() == 2
+        
+        model.family.familyMembers[0].name == 'Jake'
+        model.family.familyMembers[0].age == 12
 
-class BindingController {
-    def createPerson() {
-        def person = new Person()
-        person.properties = request
-        [person: person]
+        model.family.familyMembers[1].name == 'Zack'
+        model.family.familyMembers[1].age == 15
     }
 }
 
-@Entity
+@Artefact('Controller')
+class BindingController {
+    def createPerson(Person person) {
+        [person: person]
+    }
+    
+    def createFamily(Family family) {
+        [family: family]
+    }
+}
+
 class Person {
     String name
     Integer age
+}
+
+class Family {
+    String lastName
+    List<Person> familyMembers
 }
