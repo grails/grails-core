391818eb4df3b15a3eece5bdbe2d5d5cac6225c4
Graeme Rocher <graeme.rocher@gmail.com>
2013-06-27 12:25:07 +0200

    update version to 2.3.0.M2 and datastore version to 2.0.0.M4

--- grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("2.3.0.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("2.3.0.M2", GrailsUtil.getGrailsVersion());
     }
 
     @Override
