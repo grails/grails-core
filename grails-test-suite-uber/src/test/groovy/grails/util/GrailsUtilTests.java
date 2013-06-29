5647eda6e4f8407910b3d8e7ac43545a8fbdb0fa
Burt Beckwith <burt@burtbeckwith.com>
2013-06-28 20:06:00 -0400

    bumped version to 2.2.4

--- grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("2.2.3", GrailsUtil.getGrailsVersion());
+        assertEquals("2.2.4", GrailsUtil.getGrailsVersion());
     }
 
     @Override
