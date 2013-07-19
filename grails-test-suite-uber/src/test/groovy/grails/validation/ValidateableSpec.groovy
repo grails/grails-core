ee1b9893267df92ee9ba583c9c06d42f90e05722
Jeff Scott Brown <jbrown@gopivotal.com>
2013-07-19 11:24:44 -0500

    GRAILS-9162 - problem with FieldError.code
    Investigating why this fails on the build server.
    
    errors['name'] should be a FieldError object so errors['name'].code should be valid.
    
    GrailsMockErrors implements getAt(String) such that it returns the code, but GrailsMockErrors should not be in play for this test.
    
    Still investigating.

--- grails-test-suite-uber/src/test/groovy/grails/validation/ValidateableSpec.groovy
+++ grails-test-suite-uber/src/test/groovy/grails/validation/ValidateableSpec.groovy
@@ -24,7 +24,7 @@ class ValidateableSpec extends Specification {
         !validateable.validate()
         validateable.hasErrors()
         validateable.errors.errorCount == 1
-        validateable.errors['name'].code == 'matches.invalid'
+        validateable.errors.getFieldError('name').code == 'matches.invalid'
         
         when: 'the clearErrors() is called'
         validateable.clearErrors()
