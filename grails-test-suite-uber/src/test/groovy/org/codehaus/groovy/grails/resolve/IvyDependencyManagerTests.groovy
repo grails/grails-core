6997ad391c36a6a967df56e57b665a351b0c925c
Jeff Scott Brown <jbrown@gopivotal.com>
2013-07-19 09:08:23 -0500

    spock is no longer expressed as a test dependency in GrailsCoreDependencies

--- grails-test-suite-uber/src/test/groovy/org/codehaus/groovy/grails/resolve/IvyDependencyManagerTests.groovy
+++ grails-test-suite-uber/src/test/groovy/org/codehaus/groovy/grails/resolve/IvyDependencyManagerTests.groovy
@@ -684,7 +684,7 @@ class IvyDependencyManagerTests extends GroovyTestCase {
 
         assertEquals 10, manager.dependencyDescriptors.findAll { it.scope == 'compile'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'runtime'}.size()
-        assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'test'}.size()
+        assertEquals 2, manager.dependencyDescriptors.findAll { it.scope == 'test'}.size()
         assertEquals 3, manager.dependencyDescriptors.findAll { it.scope == 'build'}.size()
         assertEquals 1, manager.dependencyDescriptors.findAll { it.scope == 'provided'}.size()
         assertEquals 2, manager.dependencyDescriptors.findAll { it.scope == 'docs'}.size()
