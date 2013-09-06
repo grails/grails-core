5620509bbb004181ca2601ad5758ecdb03a045a2
Graeme Rocher <graeme.rocher@gmail.com>
2013-09-06 11:35:12 +0200

    update datastore version to 2.0.1 and Hibernate plugin versions

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -60,7 +60,7 @@ grails.project.dependency.resolution = {
         compile ':cache:1.1.1'
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10" // or ":hibernate4:4.1.11"
+        runtime ":hibernate:3.6.10.1" // or ":hibernate4:4.1.11.1"
         runtime ":database-migration:1.3.5"
         runtime ":jquery:1.10.2"
         runtime ":resources:1.2"
