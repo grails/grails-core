1be0fd33054570086e11e98debb71f762b5a423b
Jeff Scott Brown <jbrown@gopivotal.com>
2013-09-16 11:39:47 -0500

    GRAILS-10448 - introduce our own ExpressionEvaluationUtils for now

--- grails-web/src/main/groovy/org/codehaus/groovy/grails/web/taglib/jsp/JspInvokeGrailsTagLibTag.java
+++ grails-web/src/main/groovy/org/codehaus/groovy/grails/web/taglib/jsp/JspInvokeGrailsTagLibTag.java
@@ -48,7 +48,6 @@ import org.codehaus.groovy.runtime.DefaultGroovyMethods;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.context.ApplicationContext;
-//import org.springframework.web.util.ExpressionEvaluationUtils;
 
 /**
  * A tag that invokes a tag defined in a the Grails dynamic tag library. Authors of Grails tags
@@ -65,7 +64,6 @@ import org.springframework.context.ApplicationContext;
  * @since 16-Jan-2006
  */
 public class JspInvokeGrailsTagLibTag extends BodyTagSupport implements DynamicAttributes {
-
     private static final long serialVersionUID = 4688821761801666631L;
     private static final String ZERO_ARGUMENTS = "zeroArgumentsFlag";
     private static final String GROOVY_DEFAULT_ARGUMENT = "it";
@@ -112,9 +110,9 @@ public class JspInvokeGrailsTagLibTag extends BodyTagSupport implements DynamicA
                         while (m.find()) {
                             String attributeName = m.group(1);
                             String attributeValue = m.group(2);
-                            if (false /*ExpressionEvaluationUtils.isExpressionLanguage(attributeValue)*/) {
-//                                attributeMap.put(attributeName, ExpressionEvaluationUtils.evaluate(
-//                                        attributeName, attributeValue, Object.class, pageContext));
+                            if (ExpressionEvaluationUtils.isExpressionLanguage(attributeValue)) {
+                                attributeMap.put(attributeName, ExpressionEvaluationUtils.evaluate(
+                                        attributeName, attributeValue, Object.class, pageContext));
                             }
                             else {
                                 attributeMap.put(attributeName, attributeValue);
@@ -123,9 +121,9 @@ public class JspInvokeGrailsTagLibTag extends BodyTagSupport implements DynamicA
                         attributes.put(pd.getName(), attributeMap);
                     }
                     else {
-                        if (false /*ExpressionEvaluationUtils.isExpressionLanguage(propertyValue)*/) {
-//                            attributes.put(pd.getName(), ExpressionEvaluationUtils.evaluate(
-//                                    pd.getName(), propertyValue, Object.class, pageContext));
+                        if (ExpressionEvaluationUtils.isExpressionLanguage(propertyValue)) {
+                            attributes.put(pd.getName(), ExpressionEvaluationUtils.evaluate(
+                                    pd.getName(), propertyValue, Object.class, pageContext));
                         }
                         else {
                             attributes.put(pd.getName(), propertyValue);
@@ -339,9 +337,9 @@ public class JspInvokeGrailsTagLibTag extends BodyTagSupport implements DynamicA
                 while (m.find()) {
                     String attributeName = m.group(1);
                     String attributeValue = m.group(2);
-                    if (false /*ExpressionEvaluationUtils.isExpressionLanguage(attributeValue)*/) {
-//                        attributeMap.put(attributeName, ExpressionEvaluationUtils.evaluate(
-//                                attributeName, attributeValue, Object.class, pageContext));
+                    if (ExpressionEvaluationUtils.isExpressionLanguage(attributeValue)) {
+                        attributeMap.put(attributeName, ExpressionEvaluationUtils.evaluate(
+                                attributeName, attributeValue, Object.class, pageContext));
                     }
                     else {
                         attributeMap.put(attributeName, attributeValue);
@@ -350,8 +348,8 @@ public class JspInvokeGrailsTagLibTag extends BodyTagSupport implements DynamicA
                 attributes.put(localName, attributeMap);
             }
             else {
-                if (false /*ExpressionEvaluationUtils.isExpressionLanguage(stringValue)*/) {
-//                    attributes.put(localName,ExpressionEvaluationUtils.evaluate(localName,stringValue,Object.class,pageContext));
+                if (ExpressionEvaluationUtils.isExpressionLanguage(stringValue)) {
+                    attributes.put(localName,ExpressionEvaluationUtils.evaluate(localName,stringValue,Object.class,pageContext));
                 }
                 else {
                     attributes.put(localName,value);
