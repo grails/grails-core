b35fb67bef8e99c49e55ea65a2714783e3954827
Peter Ledbrook <peter@cacoethes.co.uk>
2013-09-21 19:42:23 -0700

    Test argument to create-multi-project-build for validity.
    Fixes GRAILS-10494. If the argument to `create-multi-project-build` didn't
    consist of 3 parts separated by colons, the command just threw an exception
    when trying to assign the result of `split()` to a tuple. I have added an
    explicit check for the 3 parts and simply print a message now and exit if
    the number of parts is incorrect.

--- scripts/CreateMultiProjectBuild_.groovy
+++ scripts/CreateMultiProjectBuild_.groovy
@@ -32,7 +32,15 @@ target(default: "Creates a multi-project build for Maven") {
 
     def generator = new MavenMultiProjectGenerator(grailsSettings)
     try {
-        def (group, name, version) = argsMap.params[0].split(":")
+        def coordinates = argsMap.params[0].split(":")
+        if (coordinates.size() != 3) {
+            println "You must specify groupId, artifactId and version in the argument - ${argsMap.params[0]} is invalid"
+            println()
+            println msg()
+            exit 1
+        }
+
+        def (group, name, version) = coordinates
         if(group && name && version) {
             generator.generate group, name, version      
             grailsConsole.addStatus "Multi-module Maven build configured."  
