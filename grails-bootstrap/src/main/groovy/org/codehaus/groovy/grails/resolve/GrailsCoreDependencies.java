64b2fedc58e7027891a051f7d7e98826afa09a4f
Jeff Scott Brown <jbrown@gopivotal.com>
2013-10-14 20:42:04 -0500

    Update to Groovy 2.1.8

--- grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
+++ grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
@@ -32,7 +32,7 @@ public class GrailsCoreDependencies {
 
     public final String grailsVersion;
     public final String servletVersion;
-    protected final String groovyVersion = "2.1.8-SNAPSHOT";
+    protected final String groovyVersion = "2.1.8";
     protected final String log4jVersion = "1.2.17";
     protected final String h2Version = "1.3.173";
     protected final String jaxbVersion = "2.0";
