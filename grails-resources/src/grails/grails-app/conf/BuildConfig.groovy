a05ea02bdb75f3a9b548c19aaf763bb0e679fc05
Graeme Rocher <graeme.rocher@gmail.com>
2013-10-17 08:49:33 +0200

    updated version to 2.3.1, updated spring loaded to 1.1.4 and scaffolding to 2.0.1

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -56,7 +56,7 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.42"
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.0"
+        compile ":scaffolding:2.0.1"
         compile ':cache:1.1.1'
 
         // plugins needed at runtime but not for compilation
