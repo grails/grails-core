04a1a37bb649d735ddbb9e222dd3e64117bb5d02
Graeme Rocher <graeme.rocher@gmail.com>
2013-10-31 14:06:41 +0100

    fix for GRAILS-10708

--- scripts/_GrailsTest.groovy
+++ scripts/_GrailsTest.groovy
@@ -89,7 +89,8 @@ target(allTests: "Runs the project's tests.") {
         if(grailsSettings.forkSettings.test instanceof Map) {
             forkedTestRunner.configure(grailsSettings.forkSettings.test)
         }
-        forkedTestRunner.fork(argsMap)
+        int exitCode = forkedTestRunner.fork(argsMap).waitFor()
+        exit( exitCode )
     }
     else {
         projectTestRunner.runAllTests(argsMap, false)
