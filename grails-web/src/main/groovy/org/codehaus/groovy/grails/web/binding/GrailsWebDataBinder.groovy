c6a09cd8ccfb73b2479a34bcc4dde9ff3153201c
Jeff Scott Brown <jbrown@gopivotal.com>
2013-10-31 07:35:13 -0500

    add @Override where applicable

--- grails-web/src/main/groovy/org/codehaus/groovy/grails/web/binding/GrailsWebDataBinder.groovy
+++ grails-web/src/main/groovy/org/codehaus/groovy/grails/web/binding/GrailsWebDataBinder.groovy
@@ -73,19 +73,23 @@ class GrailsWebDataBinder extends SimpleDataBinder {
         registerConverter new ByteArrayMultipartFileValueConverter()
     }
 
+    @Override
     void bind(obj, DataBindingSource source) {
         bind obj, source, null, getBindingIncludeList(obj), null, null
     }
 
+    @Override
     void bind(obj, DataBindingSource source, DataBindingListener listener) {
         bind obj, source, null, getBindingIncludeList(obj), null, listener
     }
 
+    @Override
     void bind(object, DataBindingSource source, String filter, List whiteList, List blackList, DataBindingListener listener) {
         def bindingResult = new BeanPropertyBindingResult(object, object.getClass().name)
         doBind object, source, filter, whiteList, blackList, listener, bindingResult
     }
 
+    @Override
     protected void doBind(object, DataBindingSource source, String filter, List whiteList, List blackList, DataBindingListener listener, errors) {
         BeanPropertyBindingResult bindingResult = (BeanPropertyBindingResult)errors
         def errorHandlingListener = new GrailsWebDataBindingListener(messageSource)
@@ -110,6 +114,7 @@ class GrailsWebDataBinder extends SimpleDataBinder {
         populateErrors(object, bindingResult)
     }
 
+    @Override
     void bind(obj, GPathResult gpath) {
         bind obj, new SimpleMapDataBindingSource(new GPathResultMap(gpath)), getBindingIncludeList(obj)
     }
@@ -511,6 +516,7 @@ class GrailsWebDataBinder extends SimpleDataBinder {
         }
     }
 
+    @Override
     protected preprocessValue(propertyValue) {
         if(propertyValue instanceof CharSequence) {
             String stringValue = propertyValue.toString()
@@ -525,6 +531,7 @@ class GrailsWebDataBinder extends SimpleDataBinder {
         propertyValue
     }
     
+    @Override
     protected addElementToCollection(obj, String propName, Class propertyType, propertyValue, boolean clearCollection) {
         def elementToAdd = propertyValue
         def referencedType = getReferencedTypeForCollection propName, obj
@@ -562,6 +569,7 @@ class GrailsWebDataBinder extends SimpleDataBinder {
         this.listeners.addAll Arrays.asList(listeners)
     }
 
+    @Override
     protected convert(Class typeToConvertTo, value) {
         if (value instanceof JSONObject.Null) {
             return null
@@ -627,6 +635,7 @@ class DataBindingEventMulticastListener implements DataBindingListener {
         bind
     }
 
+    @Override
     Boolean beforeBinding(obj, String propertyName, value, errors) {
         boolean bind = true
         for (DataBindingListener listener in listeners) {
@@ -641,6 +650,7 @@ class DataBindingEventMulticastListener implements DataBindingListener {
         bind
     }
 
+    @Override
     void afterBinding(obj, String propertyName, errors) {
         if(listeners) {
             for(DataBindingListener listener : listeners) {
@@ -653,6 +663,7 @@ class DataBindingEventMulticastListener implements DataBindingListener {
         }
     }
 
+    @Override
     void afterBinding(target, errors) {
         if(listeners) {
             for(DataBindingListener listener : listeners) {
@@ -665,6 +676,7 @@ class DataBindingEventMulticastListener implements DataBindingListener {
         }
     }
 
+    @Override
     void bindingError(BindingError error, errors) {
         if(listeners) {
             for(DataBindingListener listener : listeners) {
