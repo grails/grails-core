2c0024e86c7a552951eba7195dfc4b814238fac8
Kazuki YAMAMOTO <yam.kazuki@gmail.com>
2013-11-07 01:37:39 +0900

    GRAILS-10741 - readOnly attribute of @Resource not working

--- grails-plugin-rest/src/test/groovy/org/grails/plugins/web/rest/transform/ResourceTransformSpec.groovy
+++ grails-plugin-rest/src/test/groovy/org/grails/plugins/web/rest/transform/ResourceTransformSpec.groovy
@@ -1,7 +1,10 @@
 package org.grails.plugins.web.rest.transform
 
 import grails.web.Action
+import spock.lang.Issue
 import spock.lang.Specification
+
+import java.lang.reflect.Method
 
 /**
  * @author Graeme Rocher
@@ -27,15 +30,20 @@ class Book {
 
         then:"It exists"
             ctrl != null
-            ctrl.getDeclaredMethod("index", Integer)
-            ctrl.getDeclaredMethod("index", Integer).getAnnotation(Action)
-            ctrl.getDeclaredMethod("index")
-            ctrl.getDeclaredMethod("index").getAnnotation(Action)
-            ctrl.getDeclaredMethod("show", domain)
-            ctrl.getDeclaredMethod("edit", domain)
-            ctrl.getDeclaredMethod("create")
-            ctrl.getDeclaredMethod("save", domain)
-            ctrl.getDeclaredMethod("save")
+            getMethod(ctrl, "index", Integer)
+            getMethod(ctrl, "index", Integer).getAnnotation(Action)
+            getMethod(ctrl, "index")
+            getMethod(ctrl, "index").getAnnotation(Action)
+            getMethod(ctrl, "show", domain)
+            getMethod(ctrl, "edit", domain)
+            getMethod(ctrl, "create")
+            getMethod(ctrl, "save", domain)
+            getMethod(ctrl, "save")
+            getMethod(ctrl, "update", domain)
+            getMethod(ctrl, "update")
+            getMethod(ctrl, "delete", domain)
+            getMethod(ctrl, "delete")
+
             ctrl.scope == "singleton"
 
         when:"A link is added"
@@ -46,5 +54,59 @@ class Book {
         then:"The link is added to the available links"
             links[0].href == '/foo'
 
+    }
+
+    @Issue("GRAILS-10741")
+    void "Test that the resource transform creates a read only controller class"() {
+        given:"A parsed class with a @Resource annotation"
+        def gcl = new GroovyClassLoader()
+        gcl.parseClass('''
+import grails.rest.*
+import grails.persistence.*
+
+@Entity
+@Resource(formats=['html','xml'], readOnly=true)
+class Book {
+}
+''')
+
+        when:"The controller class is loaded"
+            def domain = gcl.loadClass("Book")
+            def ctrl = gcl.loadClass('BookController')
+
+        then:"It exists"
+            ctrl != null
+            getMethod(ctrl, "index", Integer)
+            getMethod(ctrl, "index", Integer).getAnnotation(Action)
+            getMethod(ctrl, "index")
+            getMethod(ctrl, "index").getAnnotation(Action)
+            getMethod(ctrl, "show", domain)
+            ctrl.scope == "singleton"
+
+        and:"It doesn't exist"
+            !getMethod(ctrl, "edit", domain)
+            !getMethod(ctrl, "create")
+            !getMethod(ctrl, "save", domain)
+            !getMethod(ctrl, "save")
+            !getMethod(ctrl, "update", domain)
+            !getMethod(ctrl, "update")
+            !getMethod(ctrl, "delete", domain)
+            !getMethod(ctrl, "delete")
+
+        when:"A link is added"
+            def book = domain.newInstance()
+            book.link(rel:'foos', href:"/foo")
+            def links = book.links()
+
+        then:"The link is added to the available links"
+            links[0].href == '/foo'
+    }
+
+    private Method getMethod(Class clazz, String methodName, Class[] paramTypes) {
+        try {
+            clazz.getDeclaredMethod(methodName, paramTypes)
+        } catch (NoSuchMethodException e) {
+            return null
+        }
     }
 }
