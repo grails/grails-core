be44d6c391db0e97cbb1ca01df0f26824bbeaa33
Bobby Warner <bobbywarner@gmail.com>
2013-11-21 14:14:00 -0600

    Updated jQuery plugin to 1.10.2.2 as the last point release had an issue

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -62,7 +62,7 @@ grails.project.dependency.resolution = {
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.4" // or ":hibernate4:4.1.11.4"
         runtime ":database-migration:1.3.8"
-        runtime ":jquery:1.10.2.1"
+        runtime ":jquery:1.10.2.2"
         runtime ":resources:1.2.1"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
