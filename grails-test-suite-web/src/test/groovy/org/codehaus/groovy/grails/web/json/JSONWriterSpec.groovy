36841cbf58aac8845b37147de8a10b5d93f23c52
Lari Hotari <lari.hotari@sagire.fi>
2013-11-30 10:19:28 +0200

    GRAILS-10799 related: Test null handling in JSONWriter

--- grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/json/JSONWriterSpec.groovy
+++ grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/json/JSONWriterSpec.groovy
@@ -1,5 +1,6 @@
 package org.codehaus.groovy.grails.web.json
 
+import groovy.transform.CompileStatic;
 import spock.lang.Ignore
 import spock.lang.Issue
 import spock.lang.Specification
@@ -19,4 +20,26 @@ class JSONWriterSpec extends Specification {
         '{"namespace":"alpha/beta"}' == writer.toString()
     }
 
+    void 'should handle nulls'() {
+        given:
+        def writer = new StringWriter()
+        def jsonWriter = new JSONWriter(writer)
+        when:
+        jsonWriter.array()
+        writeNumber(jsonWriter, null)
+        writeObject(jsonWriter, null)
+        jsonWriter.endArray()
+        then:
+        '[{"key":null},{"key":null}]' == writer.toString()
+    }
+
+    @CompileStatic
+    private writeNumber(JSONWriter jsonWriter, Number n) {
+        jsonWriter.object().key('key').value(n).endObject()
+    }
+
+    @CompileStatic
+    private writeObject(JSONWriter jsonWriter, Object o) {
+        jsonWriter.object().key('key').value(o).endObject()
+    }
 }
