ba8bdb263a1523194673b5e0eb35db4525f4bbce
Graeme Rocher <graeme.rocher@gmail.com>
2013-12-03 17:29:34 +0100

    update version to 2.3.4

--- grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("2.3.4.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("2.3.4", GrailsUtil.getGrailsVersion());
     }
 
     @Override
