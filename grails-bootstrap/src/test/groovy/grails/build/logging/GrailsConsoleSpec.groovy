4ffe72feacf0cde8af8b980007973a9269efe0a2
Tom Bujok <tom.bujok@gmail.com>
2013-12-07 15:27:21 +0100

    GRAILS-10753 Console Colors in Windows
    The GrailsConsole makes sure now that whenever ANSI escape codes are output to the console they are followed by the reset sequence, so that the console is not 'stateful' when it comes to the colouring, formatting, etc. It applies to the userInput as well, so that even if the prompt question is coloured the reset mark precedes the user's input. It ensures that if the process is killed in-between, the console is set to the default state.

---
+++ grails-bootstrap/src/test/groovy/grails/build/logging/GrailsConsoleSpec.groovy
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package grails.build.logging
+
+import jline.console.ConsoleReader
+import org.fusesource.jansi.Ansi
+import spock.lang.Specification
+
+import java.util.regex.Pattern
+
+/**
+ * Tests of the GrailsConsole.
+ *
+ * Only the following methods output ansi sequences or color codes directly:
+ * - outputMessage
+ * - error
+ * - userInput
+ *
+ * The goal of the test is to verify that the RESET sequence is written to the output after
+ * the invocation of the aforementioned methods
+ *
+ * @author Tom Bujok
+ * @since 2.3
+ */
+class GrailsConsoleSpec extends Specification {
+
+    static final String RESET = Pattern.quote(Ansi.ansi().reset().toString())
+
+    PrintStream out
+    GrailsConsole console
+    String output
+
+    def setup() {
+        InputStream systemIn = Mock(InputStream)
+        systemIn.read(* _) >> -1
+        out = Mock(PrintStream)
+
+        console = GrailsConsole.getInstance()
+        console.ansiEnabled = true
+        console.out = out
+        console.reader = new ConsoleReader(systemIn, out)
+
+        output = ""
+    }
+
+    def "outputMessage - verify the reset marker at the end of the output"() {
+        when:
+        console.outputMessage("MSG", 1)
+
+        then:
+        out./print.*/(* _) >> { def args -> output += args.join('') }
+        assert assertResetMarkAtTheEndOfOutput(output)
+    }
+
+    def "error - verify the reset marker at the end of the output"() {
+        when:
+        console.error("LABEL", "MSG")
+
+        then:
+        out./print.*/(* _) >> { def args -> output += args.join('') }
+        assert assertResetMarkAtTheEndOfOutput(output)
+    }
+
+    def "userInput - verify the reset marker at the end of the output"() {
+        when:
+        console.userInput("QUESTION")
+
+        then:
+        out./write.*/(* _) >> { def args -> output = new String(args[0], args[1], args[2]) }
+        assert assertResetMarkAtTheEndOfOutput(output)
+    }
+
+    def assertResetMarkAtTheEndOfOutput(def output) {
+        return output ==~ /.*$RESET/
+    }
+
+}
