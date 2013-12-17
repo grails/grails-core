c4bdf376bbddefcb5969f068782b16f134f10ac4
Jeff Scott Brown <jbrown@gopivotal.com>
2013-12-17 07:30:37 -0600

    Revert "GRAILS-10917 Undid formatting changes"
    This reverts commit 7539e726afb45d9dfa5bc7390fa10b0cb1cdd362.

--- grails-test-suite-web/src/test/groovy/grails/rest/web/RespondMethodSpec.groovy
+++ grails-test-suite-web/src/test/groovy/grails/rest/web/RespondMethodSpec.groovy
@@ -30,25 +30,24 @@ import spock.lang.Specification
 
 @TestFor(BookController)
 @Mock(Book)
-//@Ignore
 class RespondMethodSpec extends Specification{
 
     void setup() {
         def ga = grailsApplication
         ga.config.grails.mime.types =
             [ html: ['text/html','application/xhtml+xml'],
-            xml: ['text/xml', 'application/xml'],
-            text: 'text/plain',
-            js: 'text/javascript',
-            rss: 'application/rss+xml',
-            atom: 'application/atom+xml',
-            css: 'text/css',
-            csv: 'text/csv',
-            all: '*/*',
-            json: ['application/json','text/json'],
-            form: 'application/x-www-form-urlencoded',
-            multipartForm: 'multipart/form-data'
-        ]
+                xml: ['text/xml', 'application/xml'],
+                text: 'text/plain',
+                js: 'text/javascript',
+                rss: 'application/rss+xml',
+                atom: 'application/atom+xml',
+                css: 'text/css',
+                csv: 'text/csv',
+                all: '*/*',
+                json: ['application/json','text/json'],
+                form: 'application/x-www-form-urlencoded',
+                multipartForm: 'multipart/form-data'
+            ]
 
         defineBeans {
             mimeTypes(MimeTypesFactoryBean) {
@@ -60,111 +59,18 @@ class RespondMethodSpec extends Specification{
     @Ignore
     void "Test that the respond method produces the correct model for a domain instance and no specific content type"() {
         given:"A book instance"
-            def book = new Book(title: "The Stand").save(flush:true)
+        def book = new Book(title: "The Stand").save(flush:true)
 
         when:"The respond method is used to render a response"
-            webRequest.actionName = 'show'
-            controller.show(book)
-            def modelAndView = webRequest.request.getAttribute(GrailsApplicationAttributes.MODEL_AND_VIEW)
+        webRequest.actionName = 'show'
+        controller.show(book)
+        def modelAndView = webRequest.request.getAttribute(GrailsApplicationAttributes.MODEL_AND_VIEW)
 
         then:"A modelAndView and view is produced"
-            modelAndView != null
-            modelAndView instanceof ModelAndView
-            modelAndView.model.book == book
-            modelAndView.viewName == 'show'
-    }
-
-    @Ignore
-    void "Test that the respond method produces XML for a domain instance and a content type of XML"() {
-        given:"A book instance"
-            def book = new Book(title: "The Stand").save(flush:true)
-
-        when:"The respond method is used to render a response"
-            response.format = 'xml'
-
-            def result = controller.show(book)
-
-        then:"A modelAndView and view is produced"
-            result == null
-            response.contentType == 'text/xml'
-            response.xml.title.text() == 'The Stand'
-    }
-
-    @Ignore
-    void "Test that the respond method produces XML for a list of domains and a content type of XML"() {
-        given:"A book instance"
-            def book = new Book(title: "The Stand").save(flush:true)
-
-        when:"The respond method is used to render a response"
-            response.format = 'xml'
-            def result = controller.index()
-
-        then:"A modelAndView and view is produced"
-            result == null
-            response.contentType == 'text/xml'
-    }
-
-    @Ignore
-    void "Test that the respond method produces errors XML for a domain instance that has errors and a content type of XML"() {
-        given:"A book instance"
-            def book = new Book(title: "")
-            book.validate()
-
-        when:"The respond method is used to render a response"
-            response.format = 'xml'
-
-            def result = controller.show(book)
-
-        then:"A modelAndView and view is produced"
-            result == null
-            response.contentType == 'text/xml'
-            response.xml.error.message.text() == 'Property [title] of class [class grails.rest.web.Book] cannot be null'
-    }
-
-    @Ignore
-    void "Test that the respond method produces JSON for a domain instance and a content type of JSON"() {
-        given:"A book instance"
-            def book = new Book(title: "The Stand").save(flush:true)
-
-        when:"The respond method is used to render a response"
-            response.format = 'json'
-
-        def result = controller.show(book)
-
-        then:"A modelAndView and view is produced"
-            result == null
-            response.contentType == 'application/json'
-            response.json.title == 'The Stand'
-    }
-
-    @Ignore
-    void "Test that the respond method produces a 404 for a format not supported"() {
-        given:"A book instance"
-            def book = new Book(title: "The Stand").save(flush:true)
-
-        when:"The respond method is used to render a response"
-            response.format = 'xml'
-
-            def result = controller.showWithFormats(book.id)
-
-        then:"A modelAndView and view is produced"
-            response.status == 404
-    }
-
-    @Ignore
-    void "Test that the respond method produces JSON for an action that specifies explicit formats"() {
-        given:"A book instance"
-            def book = new Book(title: "The Stand").save(flush:true)
-
-        when:"The respond method is used to render a response"
-            response.format = 'json'
-
-            def result = controller.showWithFormats(book.id)
-
-        then:"A modelAndView and view is produced"
-            result == null
-            response.contentType == 'application/json'
-            response.json.title == 'The Stand'
+        modelAndView != null
+        modelAndView instanceof ModelAndView
+        modelAndView.model.book == book
+        modelAndView.viewName == 'show'
     }
 
     void "Test that the respond method produces the correct model for a domain instance and content type is HTML"() {
@@ -198,6 +104,99 @@ class RespondMethodSpec extends Specification{
         modelAndView instanceof ModelAndView
         modelAndView.model == [book: book, extra: true]
         modelAndView.viewName == 'showWithModel'
+    }
+
+    @Ignore
+    void "Test that the respond method produces XML for a domain instance and a content type of XML"() {
+        given:"A book instance"
+        def book = new Book(title: "The Stand").save(flush:true)
+
+        when:"The respond method is used to render a response"
+        response.format = 'xml'
+
+        def result = controller.show(book)
+
+        then:"A modelAndView and view is produced"
+        result == null
+        response.contentType == 'text/xml'
+        response.xml.title.text() == 'The Stand'
+    }
+
+    @Ignore
+    void "Test that the respond method produces XML for a list of domains and a content type of XML"() {
+        given:"A book instance"
+        def book = new Book(title: "The Stand").save(flush:true)
+
+        when:"The respond method is used to render a response"
+        response.format = 'xml'
+        def result = controller.index()
+
+        then:"A modelAndView and view is produced"
+        result == null
+        response.contentType == 'text/xml'
+    }
+
+    @Ignore
+    void "Test that the respond method produces errors XML for a domain instance that has errors and a content type of XML"() {
+        given:"A book instance"
+        def book = new Book(title: "")
+        book.validate()
+
+        when:"The respond method is used to render a response"
+        response.format = 'xml'
+
+        def result = controller.show(book)
+
+        then:"A modelAndView and view is produced"
+        result == null
+        response.contentType == 'text/xml'
+        response.xml.error.message.text() == 'Property [title] of class [class grails.rest.web.Book] cannot be null'
+    }
+
+    @Ignore
+    void "Test that the respond method produces JSON for a domain instance and a content type of JSON"() {
+        given:"A book instance"
+        def book = new Book(title: "The Stand").save(flush:true)
+
+        when:"The respond method is used to render a response"
+        response.format = 'json'
+
+        def result = controller.show(book)
+
+        then:"A modelAndView and view is produced"
+        result == null
+        response.contentType == 'application/json'
+        response.json.title == 'The Stand'
+    }
+
+    @Ignore
+    void "Test that the respond method produces a 404 for a format not supported"() {
+        given:"A book instance"
+        def book = new Book(title: "The Stand").save(flush:true)
+
+        when:"The respond method is used to render a response"
+        response.format = 'xml'
+
+        def result = controller.showWithFormats(book.id)
+
+        then:"A modelAndView and view is produced"
+        response.status == 404
+    }
+
+    @Ignore
+    void "Test that the respond method produces JSON for an action that specifies explicit formats"() {
+        given:"A book instance"
+        def book = new Book(title: "The Stand").save(flush:true)
+
+        when:"The respond method is used to render a response"
+        response.format = 'json'
+
+        def result = controller.showWithFormats(book.id)
+
+        then:"A modelAndView and view is produced"
+        result == null
+        response.contentType == 'application/json'
+        response.json.title == 'The Stand'
     }
 }
 
