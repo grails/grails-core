690028752c5c0cca7c71594a87ad06df9e338049
Lari Hotari <lari.hotari@sagire.fi>
2014-01-22 11:35:35 +0200

    test for checking that @ClassRule works in Spock

--- grails-test-suite-uber/src/test/groovy/grails/test/mixin/CallbackSpec.groovy
+++ grails-test-suite-uber/src/test/groovy/grails/test/mixin/CallbackSpec.groovy
@@ -1,13 +1,24 @@
 package grails.test.mixin
 
 import grails.test.mixin.support.GrailsUnitTestMixin
+
+import org.junit.ClassRule
+import org.junit.rules.TestRule
+
+import spock.lang.Ignore;
+import spock.lang.IgnoreRest
+import spock.lang.Shared;
 import spock.lang.Specification
 
 /**
  * @author Lari Hotari
  */
-@TestMixin(GrailsUnitTestMixin)
+//@TestMixin(GrailsUnitTestMixin)
 class CallbackSpec extends Specification {
+    static GrailsUnitTestMixin gutMixin = new GrailsUnitTestMixin()
+    static TestRule gutRuleStatic = gutMixin.newClassRule(CallbackSpec)
+    @Shared @ClassRule TestRule gutRule = gutRuleStatic
+    
     static doWithSpring = {
         myService(MyService)
     }
@@ -15,12 +26,23 @@ class CallbackSpec extends Specification {
     static doWithConfig(c) {
         c.myConfigValue = 'Hello'    
     }
+    
+    def getGrailsApplication() {
+        gutMixin.getGrailsApplication()
+    }
 
+    def "grailsApplication is not null"() {
+        expect:
+        grailsApplication != null
+    }
+    
+    @Ignore
     def "doWithSpring callback is executed"() {
         expect:
         grailsApplication.mainContext.getBean('myService') != null
     }
-    
+
+    @Ignore
     def "doWithConfig callback is executed"(){
         expect:
         config.myConfigValue == 'Hello'
