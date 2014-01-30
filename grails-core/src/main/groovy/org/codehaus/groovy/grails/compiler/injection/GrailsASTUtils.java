grails/grails-core

commit 6bffcd742ee7877fa1b5b9fc154237d9f712a884

remove dependency on commons-lang from GrailsASTUtils

grails-core/src/main/groovy/org/codehaus/groovy/grails/compiler/injection/GrailsASTUtils.java
@@ -42,10 +42,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Predicate;
-import org.apache.commons.collections.map.DefaultedMap;
-import org.apache.commons.lang.StringUtils;
 import org.codehaus.groovy.ast.ASTNode;
 import org.codehaus.groovy.ast.AnnotatedNode;
 import org.codehaus.groovy.ast.AnnotationNode;
@@ -88,11 +84,13 @@ import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.control.messages.SyntaxErrorMessage;
 import org.codehaus.groovy.grails.commons.GrailsClassUtils;
 import org.codehaus.groovy.grails.commons.GrailsDomainClassProperty;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
 import org.codehaus.groovy.runtime.MetaClassHelper;
 import org.codehaus.groovy.syntax.SyntaxException;
 import org.codehaus.groovy.syntax.Token;
 import org.codehaus.groovy.syntax.Types;
 import org.codehaus.groovy.transform.sc.StaticCompileTransformation;
+import org.springframework.util.StringUtils;
 
 /**
  * Helper methods for working with Groovy AST trees.
@@ -166,7 +164,7 @@ public class GrailsASTUtils {
      * @return true if the property exists in the ClassNode
      */
     public static boolean hasProperty(ClassNode classNode, String propertyName) {
-        if (classNode == null || StringUtils.isBlank(propertyName)) {
+        if (classNode == null || !StringUtils.hasText(propertyName)) {
             return false;
         }
 
@@ -622,8 +620,13 @@ public class GrailsASTUtils {
     }
 
     @SuppressWarnings("unchecked")
-    public static ClassNode nonGeneric(ClassNode type, ClassNode wildcardReplacement) {
-        return replaceGenericsPlaceholders(type, DefaultedMap.decorate(emptyGenericsPlaceHoldersMap, wildcardReplacement));
+    public static ClassNode nonGeneric(ClassNode type, final ClassNode wildcardReplacement) {
+        return replaceGenericsPlaceholders(type, DefaultGroovyMethods.withDefault(emptyGenericsPlaceHoldersMap, new Closure(this) {
+            @Override
+            public Object call(Object... args) {
+                return wildcardReplacement;
+            }
+        }));
     }
     
     public static ClassNode replaceGenericsPlaceholders(ClassNode type, Map<String, ClassNode> genericsPlaceholders) {
@@ -825,12 +828,12 @@ public class GrailsASTUtils {
      * @return true if classNode is marked with any of the annotations in annotationsToLookFor
      */
     public static boolean hasAnyAnnotations(final ClassNode classNode, final Class<? extends Annotation>... annotationsToLookFor) {
-        return CollectionUtils.exists(Arrays.asList(annotationsToLookFor), new Predicate() {
-            @SuppressWarnings({ "unchecked", "rawtypes" })
-            public boolean evaluate(Object object) {
-                return hasAnnotation(classNode, (Class)object);
+        for (Class<? extends Annotation> annotationClass : annotationsToLookFor) {
+            if(hasAnnotation(classNode, annotationClass)) {
+                return true;
             }
-        });
+        }
+        return false;
     }
 
     public static void addMethodIfNotPresent(ClassNode controllerClassNode, MethodNode methodNode) {

commit bd145765551af93f5ed354886ec30c977ef10d66

Fix problem caused by 6bffcd742ee7877fa1b5b9fc154
We were attempting to mutate an immutable Map

grails-core/src/main/groovy/org/codehaus/groovy/grails/compiler/injection/GrailsASTUtils.java
@@ -84,7 +84,6 @@ import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.control.messages.SyntaxErrorMessage;
 import org.codehaus.groovy.grails.commons.GrailsClassUtils;
 import org.codehaus.groovy.grails.commons.GrailsDomainClassProperty;
-import org.codehaus.groovy.runtime.DefaultGroovyMethods;
 import org.codehaus.groovy.runtime.MetaClassHelper;
 import org.codehaus.groovy.syntax.SyntaxException;
 import org.codehaus.groovy.syntax.Token;
@@ -621,15 +620,14 @@ public class GrailsASTUtils {
 
     @SuppressWarnings("unchecked")
     public static ClassNode nonGeneric(ClassNode type, final ClassNode wildcardReplacement) {
-        return replaceGenericsPlaceholders(type, DefaultGroovyMethods.withDefault(emptyGenericsPlaceHoldersMap, new Closure(GrailsASTUtils.class) {
-            @Override
-            public Object call(Object... args) {
-                return wildcardReplacement;
-            }
-        }));
+        return replaceGenericsPlaceholders(type, emptyGenericsPlaceHoldersMap, wildcardReplacement);
     }
     
     public static ClassNode replaceGenericsPlaceholders(ClassNode type, Map<String, ClassNode> genericsPlaceholders) {
+        return replaceGenericsPlaceholders(type, genericsPlaceholders, null);
+    }
+    
+    public static ClassNode replaceGenericsPlaceholders(ClassNode type, Map<String, ClassNode> genericsPlaceholders, ClassNode defaultPlaceholder) {
         if (type.isArray()) {
             return replaceGenericsPlaceholders(type.getComponentType(), genericsPlaceholders).makeArray();
         }
@@ -639,7 +637,12 @@ public class GrailsASTUtils {
         }
 
         if(type.isGenericsPlaceHolder() && genericsPlaceholders != null) {
-            ClassNode placeHolderType = genericsPlaceholders.get(type.getUnresolvedName());
+            final ClassNode placeHolderType;
+            if(genericsPlaceholders.containsKey(type.getUnresolvedName())) {
+                placeHolderType = genericsPlaceholders.get(type.getUnresolvedName());
+            } else {
+                placeHolderType = defaultPlaceholder;
+            }
             if(placeHolderType != null) {
                 return placeHolderType.getPlainNodeReference();
             } else {