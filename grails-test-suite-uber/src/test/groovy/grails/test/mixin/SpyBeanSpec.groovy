8731cb3a817b967f878d4a1eb2a3e406a90fb791
Lari Hotari <lhotari@gopivotal.com>
2014-02-03 18:44:28 +0200

    GRAILS-11007 Example of using Spock Spy on a bean

---
+++ grails-test-suite-uber/src/test/groovy/grails/test/mixin/SpyBeanSpec.groovy
@@ -0,0 +1,67 @@
+package grails.test.mixin
+
+import grails.test.mixin.support.GrailsUnitTestMixin
+import grails.test.runtime.FreshRuntime
+
+import org.codehaus.groovy.grails.commons.InstanceFactoryBean
+import org.codehaus.groovy.grails.plugins.databinding.DataBindingGrailsPlugin
+import org.grails.databinding.SimpleMapDataBindingSource
+import org.grails.databinding.converters.ValueConverter
+import org.junit.ClassRule
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * @author Lari Hotari
+ */
+@FreshRuntime
+@TestMixin(GrailsUnitTestMixin)
+class SpyBeanSpec extends Specification {
+    def myAddressValueConverter=Spy(MyAddressValueConverter)
+    def doWithSpring = {
+        def otherClosure=new DataBindingGrailsPlugin().doWithSpring.clone()
+        otherClosure.delegate=delegate
+        otherClosure.call()
+        myAddressValueConverter(InstanceFactoryBean, myAddressValueConverter, MyAddressValueConverter)
+    }
+    
+    def "it's possible to use Spy instances as beans as well"() {
+        given:
+        def binder=grailsApplication.mainContext.getBean("grailsWebDataBinder")
+        def person=new MyPerson()
+        when:
+        binder.bind person, [name:'Lari', address:'Espoo,Finland'] as SimpleMapDataBindingSource
+        then:
+        1 * myAddressValueConverter.canConvert('Espoo,Finland')
+        1 * myAddressValueConverter.convert('Espoo,Finland')
+        0 * myAddressValueConverter._
+        person.address.city=='Espoo'
+        person.address.country=='Finland'
+    }
+}
+
+class MyPerson {
+    String name
+    MyAddress address
+}
+
+class MyAddress {
+    String city
+    String country
+}
+
+class MyAddressValueConverter implements ValueConverter {
+    boolean canConvert(value) {
+        value instanceof String
+    }
+
+    def convert(value) {
+        def pieces = value.split(',')
+        new MyAddress(city: pieces[0], country: pieces[1])
+    }
+
+    Class<?> getTargetType() {
+        MyAddress
+    }
+}
