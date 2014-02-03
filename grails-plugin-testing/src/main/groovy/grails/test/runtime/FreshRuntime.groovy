6d2bb7f18dce668a932bb409e54fec2cabcfbb95
Lari Hotari <lhotari@gopivotal.com>
2014-02-03 18:55:23 +0200

    GRAILS-11003 Add javadoc to FreshRuntime annotation

--- grails-plugin-testing/src/main/groovy/grails/test/runtime/FreshRuntime.groovy
+++ grails-plugin-testing/src/main/groovy/grails/test/runtime/FreshRuntime.groovy
@@ -5,6 +5,18 @@ import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
 import java.lang.annotation.Target
 
+/**
+ * Annotation to be used in Junit tests and Spock specifications.
+ * Tests annotated with this annotation will get a fresh TestRuntime instance.
+ * If the annotation is added to class level, it will get used for all tests in the class. 
+ *
+ * The usage of this annotation is required for enabling doWithSpring/doWithConfig callbacks 
+ * that work with the test instance.
+ *  
+ * @author Lari Hotari
+ * @since 2.4
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target([ElementType.TYPE, ElementType.METHOD])
 public @interface FreshRuntime {
