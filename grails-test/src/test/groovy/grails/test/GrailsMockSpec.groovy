40c2cf2abbf3e4107bfeda0e171b911bdd79d140
Jeff Scott Brown <jbrown@gopivotal.com>
2014-02-04 16:24:59 -0600

    GRAILS-11075 - add @Issue

--- grails-test/src/test/groovy/grails/test/GrailsMockSpec.groovy
+++ grails-test/src/test/groovy/grails/test/GrailsMockSpec.groovy
@@ -1,9 +1,11 @@
 package grails.test
 
+import spock.lang.Issue
 import spock.lang.Specification
 
 class GrailsMockSpec extends Specification {
 
+    @Issue('GRAILS-11075')
     void 'Test that verify() can be called in the then block of a Spock spec'() {
         given:
         def mock = new GrailsMock(String)
