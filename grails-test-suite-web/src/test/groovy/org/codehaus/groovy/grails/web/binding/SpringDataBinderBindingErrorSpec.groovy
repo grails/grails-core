3a53102cda2d1ffd3b48cf35bff46c07efb7b1a2
Jeff Scott Brown <jbrown@gopivotal.com>
2014-02-05 23:27:15 -0600

    GRAILS-11044 - simplify unit test

--- grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/binding/SpringDataBinderBindingErrorSpec.groovy
+++ grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/binding/SpringDataBinderBindingErrorSpec.groovy
@@ -1,45 +1,42 @@
 package org.codehaus.groovy.grails.web.binding
 
-import grails.test.mixin.TestMixin
-import grails.test.mixin.support.GrailsUnitTestMixin
-import grails.util.GrailsWebUtil
-import grails.validation.Validateable
+import grails.artefact.Artefact
+import grails.test.mixin.TestFor
 
 import org.codehaus.groovy.grails.commons.GrailsApplication
-import org.codehaus.groovy.grails.support.MockApplicationContext
 import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
-import org.springframework.web.context.request.RequestContextHolder
 
 import spock.lang.Issue
 import spock.lang.Specification
 
 
-@TestMixin(GrailsUnitTestMixin)
+@TestFor(CommandController)
 class SpringDataBinderBindingErrorSpec extends Specification {
 
     @Issue('GRAILS-11044')
     void 'Test binding errors when using the Spring binder'() {
         given: 'The spring binder is being used'
-        def appCtx = new MockApplicationContext()
-        appCtx.registerMockBean(GrailsApplication.APPLICATION_ID, grailsApplication)
-        appCtx.getServletContext().setAttribute(GrailsApplicationAttributes.APPLICATION_CONTEXT, appCtx)
-        GrailsWebUtil.bindMockWebRequest(appCtx)
         grailsApplication.config.grails.databinding.useSpringBinder=true
         grailsApplication.configChanged()
         
         when: 'a binding error occurs'
-        RequestContextHolder.getRequestAttributes().currentRequest.setParameter 'number', 'forty two'
-        def house = new House()
-        DataBindingUtils.bindObjectToInstance(house, null)
+        // this needs to be request.setParameter... params.number='forty two' does not trigger the relevant code
+        request.setParameter 'number', 'forty two'
+        controller.index()
         
         then: ' the non-domain class object has the expected binding error associated with it'
-        house.hasErrors()
-        house.errors.errorCount == 1
-        house.errors.getFieldError('number').code == 'typeMismatch'
+        response.contentAsString == 'Error Count: 1'
     }
 }
 
-@Validateable
+@Artefact('Controller')
+class CommandController {
+    
+    def index(House house) {
+        render "Error Count: ${house.errors.errorCount}"
+    }
+}
+
 class House {
     Integer number
 }
