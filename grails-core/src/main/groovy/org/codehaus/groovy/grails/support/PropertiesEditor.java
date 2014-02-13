4b6d7a3299533ef1cc900a0c1136af1caec9a278
Lari Hotari <lhotari@gopivotal.com>
2014-02-13 16:15:05 +0200

    GRAILS-11112 implement PropertiesEditor that converts all values and keys to Strings from an input java.util.Map

---
+++ grails-core/src/main/groovy/org/codehaus/groovy/grails/support/PropertiesEditor.java
@@ -0,0 +1,31 @@
+package org.codehaus.groovy.grails.support;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Fixed version of Spring's PropertiesEditor that converts all keys and values to String values
+ * 
+ * java.util.Properties should only contain String keys and values
+ * 
+ * @author Lari Hotari
+ * @since 2.3.6
+ *
+ */
+public class PropertiesEditor extends org.springframework.beans.propertyeditors.PropertiesEditor {
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    public void setValue(Object value) {
+        if (!(value instanceof Properties) && value instanceof Map) {
+            Properties props = new Properties();
+            for(Map.Entry entry : (Set<Map.Entry>)((Map)value).entrySet()) {
+                props.put(String.valueOf(entry.getKey()), entry.getValue() != null ? String.valueOf(entry.getValue()) : null);
+            }
+            super.setValue(props);
+        }
+        else {
+            super.setValue(value);
+        }
+    }
+}
