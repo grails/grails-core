c76500bcb0688414fb5cdaef188c9a6d4729d750
Jeff Scott Brown <jbrown@gopivotal.com>
2014-02-14 11:01:35 -0600

    Use scaffolding 2.0.2

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -57,7 +57,7 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.50"
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.1"
+        compile ":scaffolding:2.0.2"
         compile ':cache:1.1.1'
 
         // plugins needed at runtime but not for compilation
