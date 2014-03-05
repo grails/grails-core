22525234d7f65929dfaade910a77a1a119052724
Lari Hotari <lhotari@gopivotal.com>
2014-03-05 16:55:39 +0200

    GRAILS-11177 GRAILS-10063 Add configuration options for configuring read-only attribute passing to Hibernate in default Config.groovy - adds also OSIV singleSession mode config option to default DataSource.groovy

--- grails-resources/src/grails/grails-app/conf/Config.groovy
+++ grails-resources/src/grails/grails-app/conf/Config.groovy
@@ -83,6 +83,12 @@ grails.exceptionresolver.params.exclude = ['password']
 // configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
 grails.hibernate.cache.queries = false
 
+// configure passing transaction's read-only attribute to Hibernate session, queries and criterias
+// set "singleSession = false" OSIV mode in hibernate configuration after enabling
+grails.hibernate.pass.readonly = false
+// configure passing read-only to OSIV session by default, requires "singleSession = false" OSIV mode
+grails.hibernate.osiv.readonly = false
+
 environments {
     development {
         grails.logging.jul.usebridge = true
