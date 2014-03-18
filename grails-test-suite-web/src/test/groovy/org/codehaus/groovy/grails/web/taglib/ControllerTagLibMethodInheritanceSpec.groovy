ab827a4ed4a3aa0cc7cb836c213470fbfaa765ca
Jeff Scott Brown <jbrown@gopivotal.com>
2014-03-18 05:16:31 -0500

    GRAILS-10031 - rearrange unit test

--- grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/taglib/ControllerTagLibMethodInheritanceSpec.groovy
+++ grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/taglib/ControllerTagLibMethodInheritanceSpec.groovy
@@ -2,24 +2,22 @@ package org.codehaus.groovy.grails.web.taglib
 
 import grails.artefact.Artefact
 import grails.test.mixin.Mock
-import grails.test.mixin.TestFor
+import grails.test.mixin.TestMixin
+import grails.test.mixin.web.GroovyPageUnitTestMixin
 import spock.lang.Issue
 import spock.lang.Specification
 
-@TestFor(FirstTagLib)
-@Mock([SecondTagLib])
+@Mock([FirstTagLib, SecondTagLib])
+@TestMixin(GroovyPageUnitTestMixin)
 class ControllerTagLibMethodInheritanceSpec extends Specification {
 
     @Issue('GRAILS-10031')
     void 'Test calling an inherited tag which invokes a method overridden in the subclass'() {
-        when:
-        def content = applyTemplate('${second.myTag()}')
-
-        then:
-        content == 'SecondTagLib.doSomething()'
+        expect:
+        'FirstTagLib.doSomething()' == applyTemplate('${first.myTag()}')
+        'SecondTagLib.doSomething()' == applyTemplate('${second.myTag()}')
     }
 }
-
 
 @Artefact("TagLibrary")
 class FirstTagLib {
@@ -37,6 +35,7 @@ class FirstTagLib {
 
 @Artefact("TagLibrary")
 class SecondTagLib extends FirstTagLib {
+
     static namespace = 'second'
     
     protected String doSomething() {
