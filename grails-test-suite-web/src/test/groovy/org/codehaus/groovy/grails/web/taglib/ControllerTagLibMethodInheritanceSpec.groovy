695a82c41a97f255d8f66b51864c7f1f54e0fc4d
Jeff Scott Brown <jbrown@gopivotal.com>
2014-03-17 21:40:26 -0500

    GRAILS-10031 - add a failing test
    currently marked with @Ignore

---
+++ grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/taglib/ControllerTagLibMethodInheritanceSpec.groovy
@@ -0,0 +1,47 @@
+package org.codehaus.groovy.grails.web.taglib
+
+import grails.artefact.Artefact
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import spock.lang.Ignore
+import spock.lang.Issue
+import spock.lang.Specification
+
+@TestFor(FirstTagLib)
+@Mock([SecondTagLib])
+class ControllerTagLibMethodInheritanceSpec extends Specification {
+
+    @Issue('GRAILS-10031')
+    @Ignore
+    void 'Test calling an inherited tag which invokes a method overridden in the subclass'() {
+        when:
+        def content = applyTemplate('${second.myTag()}')
+
+        then:
+        content == 'SecondTagLib.doSomething()'
+    }
+}
+
+
+@Artefact("TagLibrary")
+class FirstTagLib {
+
+    static namespace = 'first'
+    
+    protected String doSomething() {
+        'FirstTagLib.doSomething()'
+    }
+
+    def myTag = { attrs ->
+        out << doSomething()
+    }
+}
+
+@Artefact("TagLibrary")
+class SecondTagLib extends FirstTagLib {
+    static namespace = 'second'
+    
+    protected String doSomething() {
+        'SecondTagLib.doSomething()'
+    }
+}
