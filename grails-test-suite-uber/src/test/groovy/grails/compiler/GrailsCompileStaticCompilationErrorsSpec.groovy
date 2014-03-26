30a144fa89724eddaad116ca19a6c9aa1625230d
Jeff Scott Brown <jbrown@gopivotal.com>
2014-03-25 19:03:50 -0500

    GRAILS-11056 - simplify test
    For the cases where we expect the compiler to succeed there is no need to have a unit test to verify that.  This change pulls the code out into classes.  If the compile fails for those, the test suite would reflect that without needing to run unit tests.

--- grails-test-suite-uber/src/test/groovy/grails/compiler/GrailsCompileStaticCompilationErrorsSpec.groovy
+++ grails-test-suite-uber/src/test/groovy/grails/compiler/GrailsCompileStaticCompilationErrorsSpec.groovy
@@ -1,5 +1,6 @@
 package grails.compiler
 import grails.persistence.Entity
+import groovy.transform.TypeCheckingMode
 
 import org.codehaus.groovy.control.MultipleCompilationErrorsException
 
@@ -9,32 +10,6 @@ import spock.lang.Specification
 
 
 class GrailsCompileStaticCompilationErrorsSpec extends Specification {
-
-    @Issue('GRAILS-11056')
-    void 'Test compiling valid dynamic finder calls'() {
-        given:
-        def gcl = new GroovyClassLoader()
-
-        when: 'a class marked with @GrailsCompileStatic invokes valid dynamic finders'
-        def c = gcl.parseClass('''
-package grails.compiler
-
-@GrailsCompileStatic
-class SomeClass {
-
-    def someMethod() {
-        List<Person> people = Person.findAllByName('William')
-        people = Person.listOrderByName('William')
-        int number = Person.countByName('William')
-        Person person = Person.findByName('William')
-        person = Person.findOrCreateByName('William')
-        person = Person.findOrSaveByName('William')
-    }
-}
-''')
-        then: 'no errors are thrown'
-        c
-    }
 
     @Ignore
     @Issue(['GRAILS-11056', 'GRAILS-11057'])
@@ -56,7 +31,6 @@ class SomeClass {
         then: 'an error is thrown'
         MultipleCompilationErrorsException e = thrown()
         e.message.contains 'Cannot find matching method grails.compiler.Person#findAllByName'
-
     }
 
     @Issue('GRAILS-11056')
@@ -89,59 +63,6 @@ class SomeClass {
         e.message.contains 'Cannot find matching method grails.compiler.SomeClass#findByName'
         e.message.contains 'Cannot find matching method grails.compiler.SomeClass#findOrCreateByName'
         e.message.contains 'Cannot find matching method grails.compiler.SomeClass#findOrSaveByName'
-    }
-
-    @Issue('GRAILS-11056')
-    void 'Test compiling invalid dynamic finder calls in a method marked with TypeCheckingMode.SKIP'() {
-        given:
-        def gcl = new GroovyClassLoader()
-
-        when: 'a class marked with @GrailsCompileStatic invokes dynamic finders on a non-domain class inside of a method marked with TypeCheckingMode.SKIP'
-        def c = gcl.parseClass('''
-package grails.compiler
-
-import groovy.transform.TypeCheckingMode
-
-@GrailsCompileStatic
-class SomeClass {
-
-    @GrailsCompileStatic(TypeCheckingMode.SKIP)
-    def someMethod() {
-        List<SomeClass> people = SomeClass.findAllByName('William')
-        people = SomeClass.listOrderByName('William')
-        int number = SomeClass.countByName('William')
-        SomeClass person = SomeClass.findByName('William')
-        person = SomeClass.findOrCreateByName('William')
-        person = SomeClass.findOrSaveByName('William')
-    }
-}
-''')
-        then: 'no errors are thrown'
-        c
-    }
-    
-    @Issue('GRAILS-11242')
-    void 'Test compiling @Validateable'() {
-        given:
-        def gcl = new GroovyClassLoader()
-
-        when: 'a class marked with @GrailsCompileStatic invokes dynamic finders on a non-domain class inside of a method marked with TypeCheckingMode.SKIP'
-        def c = gcl.parseClass('''
-package grails.compiler
-
-import groovy.transform.TypeCheckingMode
-
-@GrailsCompileStatic
-@grails.validation.Validateable
-class SomeClass {
-    String name
-    static constraints = {
-        name matches: /[A-Z].*/
-    }
-}
-''')
-        then: 'no errors are thrown'
-        c
     }
     
     @Issue('GRAILS-11242')
@@ -178,3 +99,45 @@ class SomeClass {
 class Person {
     String name
 }
+
+@GrailsCompileStatic
+class SomeClassWithValidDynamicFinders {
+
+    def someMethod() {
+        List<Person> people = Person.findAllByName('William')
+        people = Person.listOrderByName('William')
+        int number = Person.countByName('William')
+        Person person = Person.findByName('William')
+        person = Person.findOrCreateByName('William')
+        person = Person.findOrSaveByName('William')
+    }
+}
+
+class SomeNonDomainClass {
+    String name
+}
+
+@GrailsCompileStatic
+class SomeClassWithBogusDynamicFinders {
+
+    @GrailsCompileStatic(TypeCheckingMode.SKIP)
+    def someMethod() {
+        List<SomeNonDomainClass> people = SomeNonDomainClass.findAllByName('William')
+        people = SomeNonDomainClass.listOrderByName('William')
+        int number = SomeNonDomainClass.countByName('William')
+        SomeNonDomainClass person = SomeNonDomainClass.findByName('William')
+        person = SomeNonDomainClass.findOrCreateByName('William')
+        person = SomeNonDomainClass.findOrSaveByName('William')
+    }
+}
+
+@GrailsCompileStatic
+@grails.validation.Validateable
+class SomeValidateableClass {
+    String name
+    static constraints = {
+        name matches: /[A-Z].*/
+    }
+}
+
+
