9e170b56324035c7887b7145555329d79b13fe9b
Graeme Rocher <graeme.rocher@gmail.com>
2014-04-08 14:23:39 +0200

    allow redirects to be issues when using 'then' in returned promises

--- grails-plugin-async/src/main/groovy/org/codehaus/groovy/grails/plugins/web/async/mvc/AsyncActionResultTransformer.groovy
+++ grails-plugin-async/src/main/groovy/org/codehaus/groovy/grails/plugins/web/async/mvc/AsyncActionResultTransformer.groovy
@@ -16,15 +16,18 @@
 package org.codehaus.groovy.grails.plugins.web.async.mvc
 
 import grails.async.Promise
+import grails.async.PromiseList
 import groovy.transform.CompileStatic
 import org.codehaus.groovy.grails.plugins.web.async.GrailsAsyncContext
 import org.codehaus.groovy.grails.web.errors.GrailsExceptionResolver
 import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
 import org.codehaus.groovy.grails.web.servlet.mvc.ActionResultTransformer
 import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.web.servlet.ModelAndView
+
+import javax.servlet.AsyncContext
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 /**
  * Handles an Async response from a controller
@@ -50,18 +53,24 @@ class AsyncActionResultTransformer implements ActionResultTransformer {
 
             asyncContext.start {
                 Promise p = (Promise) actionResult
-                p.onComplete {
-                    if (it instanceof Map) {
-                        def modelAndView = new ModelAndView(viewName, it)
-                        asyncContext.getRequest().setAttribute(GrailsApplicationAttributes.MODEL_AND_VIEW, modelAndView);
+                if (p instanceof PromiseList) {
+                    p.onComplete { List results ->
+                        handleComplete(request, response, asyncContext)
+                    }
+                } else {
+                    p.onComplete {
+                        if (it instanceof Map) {
+                            def modelAndView = new ModelAndView(viewName, it)
+                            asyncContext.getRequest().setAttribute(GrailsApplicationAttributes.MODEL_AND_VIEW, modelAndView);
 
-                        asyncContext.dispatch()
-                    } else {
-                        final modelAndView = asyncContext.getRequest().getAttribute(GrailsApplicationAttributes.MODEL_AND_VIEW)
-                        if (modelAndView) {
                             asyncContext.dispatch()
                         } else {
-                            asyncContext.complete()
+                            final modelAndView = asyncContext.getRequest().getAttribute(GrailsApplicationAttributes.MODEL_AND_VIEW)
+                            if (modelAndView) {
+                                asyncContext.dispatch()
+                            } else {
+                                handleComplete(request, response, asyncContext)
+                            }
                         }
                     }
                 }
@@ -79,6 +88,10 @@ class AsyncActionResultTransformer implements ActionResultTransformer {
         return actionResult;
     }
 
+    protected void handleComplete(HttpServletRequest request, HttpServletResponse response, AsyncContext asyncContext) {
+        asyncContext.complete()
+    }
+
     private GrailsExceptionResolver createExceptionResolver(GrailsWebRequest webRequest) {
         if (!exceptionResolver) {
             exceptionResolver = new GrailsExceptionResolver()
@@ -86,7 +99,7 @@ class AsyncActionResultTransformer implements ActionResultTransformer {
             exceptionResolver.grailsApplication = webRequest.attributes.grailsApplication
             exceptionResolver.mappedHandlers = [this] as Set
             def properties = new Properties()
-            properties['java.lang.Exception'] =  '/error'
+            properties['java.lang.Exception'] = '/error'
             exceptionResolver.exceptionMappings = properties
         }
         return exceptionResolver
