73090433bf719aff8a0684f8615d92d40a43836c
Lari Hotari <lhotari@gopivotal.com>
2014-04-22 12:29:22 +0300

    GRAILS-11331 Add test for both naming formats of plugin name

--- grails-web/src/test/groovy/org/codehaus/groovy/grails/web/pages/GroovyPageConfigSpec.groovy
+++ grails-web/src/test/groovy/org/codehaus/groovy/grails/web/pages/GroovyPageConfigSpec.groovy
@@ -4,23 +4,27 @@ import org.codehaus.groovy.grails.plugins.GrailsPluginInfo
 
 import spock.lang.Issue
 import spock.lang.Specification
+import spock.lang.Unroll;
 
 class GroovyPageConfigSpec extends Specification {
 
     @Issue("GRAILS-11331")
-    def "per plugin settings should be supported"() {
+    @Unroll
+    def "per plugin settings should be supported - #pluginNameInConfig"() {
         given:
-        def flatConfig = ['platformUi.grails.views.gsp.codecs.expression': 'none']
+        def flatConfig = [("${pluginNameInConfig}.grails.views.gsp.codecs.expression".toString()): 'none']
         def groovyPageConfig = new GroovyPageConfig(flatConfig)
         def grailsPluginInfo = Mock(GrailsPluginInfo)
         when:
         def codecSettings=groovyPageConfig.getCodecSettings(grailsPluginInfo, "expression")
         then:
         grailsPluginInfo.getName() >> {
-            'platformUi'
+            'platform-ui'
         }
         0 * grailsPluginInfo._
         codecSettings == 'none'
+        where:
+        pluginNameInConfig << ['platform-ui','platformUi']
     }
 
 }
