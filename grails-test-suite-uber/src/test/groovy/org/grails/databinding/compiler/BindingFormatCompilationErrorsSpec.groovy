5d1d9cbcc0f357d45136d3189bfffeea1dde6417
Jeff Scott Brown <jbrown@gopivotal.com>
2014-04-23 10:10:15 -0500

    GRAILS-11321 - the value() attribute of @BindingAnnotation should not be required
    This only worked before because of http://jira.codehaus.org/browse/GROOVY-6705

---
+++ grails-test-suite-uber/src/test/groovy/org/grails/databinding/compiler/BindingFormatCompilationErrorsSpec.groovy
@@ -0,0 +1,62 @@
+package org.grails.databinding.compiler
+
+import org.codehaus.groovy.control.MultipleCompilationErrorsException
+
+import spock.lang.Issue
+import spock.lang.Specification
+
+class BindingFormatCompilationErrorsSpec extends Specification {
+
+	@Issue('GRAILS-11321')
+	void 'Test compiling @BindingFormat with no code and no value'() {
+		given:
+		def gcl = new GroovyClassLoader()
+		
+		when:
+		gcl.parseClass '''
+package com.demo
+
+class SomeClass {
+		@org.grails.databinding.BindingFormat
+		String someProperty
+}
+'''
+		then:
+		MultipleCompilationErrorsException e = thrown()
+		e.message.contains 'The @BindingFormat annotation on the field [someProperty] in class [com.demo.SomeClass] must provide a value for either the value() or code() attribute.'
+	}
+
+	void 'Test compiling @BindingFormat with code'() {
+		given:
+		def gcl = new GroovyClassLoader()
+		
+		when:
+		def c = gcl.parseClass '''
+package com.demo
+
+class SomeClass {
+		@org.grails.databinding.BindingFormat(code='foo')
+		String someProperty
+}
+'''
+		then:
+		c
+	}
+
+	void 'Test compiling @BindingFormat with value'() {
+		given:
+		def gcl = new GroovyClassLoader()
+		
+		when:
+		def c = gcl.parseClass '''
+package com.demo
+
+class SomeClass {
+		@org.grails.databinding.BindingFormat('foo')
+		String someProperty
+}
+'''
+		then:
+		c
+	}
+}
