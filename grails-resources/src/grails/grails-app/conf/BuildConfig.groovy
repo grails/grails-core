6bd173f55a73a69d5a090a287ad26a1f5e1b7125
Graeme Rocher <graeme.rocher@gmail.com>
2014-04-24 12:18:37 +0200

    update to cache plugin 1.1.3

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -58,7 +58,7 @@ grails.project.dependency.resolution = {
 
         // plugins for the compile step
         compile ":scaffolding:2.1.0"
-        compile ':cache:1.1.2'
+        compile ':cache:1.1.3'
         compile ":asset-pipeline:1.8.3"
 
         // plugins needed at runtime but not for compilation
