17319ba219d74798139433bbc79b793f3b372620
Bobby Warner <bobbywarner@gmail.com>
2014-05-03 09:23:20 -0500

    Updated jQuery plugin

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -63,7 +63,7 @@ grails.project.dependency.resolution = {
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.13" // or ":hibernate4:4.3.5.1"
         runtime ":database-migration:1.4.0"
-        runtime ":jquery:1.11.0.2"
+        runtime ":jquery:1.11.1"
         runtime ":resources:1.2.7"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
