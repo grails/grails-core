01764433615b154ddc4c920ddb41801d9d0f8bca
Jeff Scott Brown <jbrown@gopivotal.com>
2014-05-20 07:08:57 -0500

    Use :tomcat:7.0.53

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -54,7 +54,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.52.1"
+        build ":tomcat:7.0.53"
 
         // plugins for the compile step
         compile ":scaffolding:2.0.3"
