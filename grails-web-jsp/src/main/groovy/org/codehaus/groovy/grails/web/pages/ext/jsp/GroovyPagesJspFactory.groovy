0d6956c7ae616277369806bda19fe031e2722f55
Graeme Rocher <graeme.rocher@gmail.com>
2014-05-28 11:18:15 +0200

    remove support for JSP 2.0 (2.1 or above required)

--- grails-web-jsp/src/main/groovy/org/codehaus/groovy/grails/web/pages/ext/jsp/GroovyPagesJspFactory.groovy
+++ grails-web-jsp/src/main/groovy/org/codehaus/groovy/grails/web/pages/ext/jsp/GroovyPagesJspFactory.groovy
@@ -16,8 +16,10 @@
 package org.codehaus.groovy.grails.web.pages.ext.jsp
 
 import javax.servlet.Servlet
+import javax.servlet.ServletContext
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
+import javax.servlet.jsp.JspApplicationContext
 import javax.servlet.jsp.JspEngineInfo
 import javax.servlet.jsp.JspFactory
 import javax.servlet.jsp.PageContext
@@ -26,7 +28,7 @@ import javax.servlet.jsp.PageContext
  * @author Graeme Rocher
  * @since 1.0
  */
-abstract class GroovyPagesJspFactory extends JspFactory {
+class GroovyPagesJspFactory extends JspFactory {
 
     PageContext getPageContext(Servlet servlet, ServletRequest servletRequest, ServletResponse servletResponse, String s, boolean b, int i, boolean b1) {
          throw new UnsupportedOperationException()
@@ -40,5 +42,19 @@ abstract class GroovyPagesJspFactory extends JspFactory {
         return { getSpecificationVersion() } as JspEngineInfo
     }
 
-    protected abstract String getSpecificationVersion()
+    protected String getSpecificationVersion() { "2.1" }
+
+    JspApplicationContext getJspApplicationContext(ServletContext servletContext) {
+        def jspCtx = servletContext.getAttribute(GroovyPagesJspApplicationContext.getName())
+
+        if (!jspCtx) {
+            synchronized (servletContext) {
+                if (!servletContext.getAttribute(GroovyPagesJspApplicationContext.getName())) {
+                    jspCtx = new GroovyPagesJspApplicationContext()
+                    servletContext.setAttribute(GroovyPagesJspApplicationContext.getName(), jspCtx)
+                }
+            }
+        }
+        return jspCtx
+    }
 }
