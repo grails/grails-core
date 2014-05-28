db54e122478834d39d63624e1f4b9732c6285c62
Graeme Rocher <graeme.rocher@gmail.com>
2014-05-28 11:45:25 +0200

    cleanup imports

--- grails-web-boot/src/test/groovy/grails/boot/EmbeddedContainerWithGrailsSpec.groovy
+++ grails-web-boot/src/test/groovy/grails/boot/EmbeddedContainerWithGrailsSpec.groovy
@@ -2,41 +2,14 @@ package grails.boot
 
 import grails.artefact.Artefact
 import grails.boot.config.GrailsConfiguration
-import org.codehaus.groovy.grails.commons.DefaultGrailsApplication
-import org.codehaus.groovy.grails.commons.GrailsApplication
-import org.codehaus.groovy.grails.commons.spring.DefaultRuntimeSpringConfiguration
-import org.codehaus.groovy.grails.plugins.DefaultGrailsPluginManager
-import org.codehaus.groovy.grails.plugins.GrailsPluginManager
-import org.codehaus.groovy.grails.web.servlet.mvc.GrailsDispatcherServlet
-import org.springframework.beans.BeansException
-import org.springframework.beans.factory.ListableBeanFactory
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
-import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader
-import org.springframework.beans.factory.support.BeanDefinitionRegistry
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor
 import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory
-import org.springframework.boot.context.embedded.ServletRegistrationBean
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory
-import org.springframework.context.ApplicationContext
-import org.springframework.context.ApplicationContextAware
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
-import org.springframework.context.annotation.ImportBeanDefinitionRegistrar
-import org.springframework.core.type.AnnotationMetadata
-import org.springframework.http.HttpMethod
-import org.springframework.http.client.ClientHttpRequest
-import org.springframework.http.client.ClientHttpResponse
-import org.springframework.http.client.SimpleClientHttpRequestFactory
-import org.springframework.util.StreamUtils
-import org.springframework.web.servlet.DispatcherServlet
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import spock.lang.Specification
-
-import java.nio.charset.Charset
-
-import static org.junit.Assert.assertThat
 
 /**
  * Created by graemerocher on 28/05/14.
