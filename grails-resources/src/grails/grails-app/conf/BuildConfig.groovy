33495517bc74536c5e4e23bdf46c761126ff32fb
Bobby Warner <bobbywarner@gmail.com>
2014-06-28 10:55:01 -0500

    Updated Asset-Pipeline plugin

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -60,7 +60,7 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.7'
-        compile ":asset-pipeline:1.8.11"
+        compile ":asset-pipeline:1.9.0"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.5.4" // or ":hibernate:3.6.10.16"
@@ -68,9 +68,9 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.11.1"
 
         // Uncomment these to enable additional asset-pipeline capabilities
-        //compile ":sass-asset-pipeline:1.7.4"
-        //compile ":less-asset-pipeline:1.7.0"
-        //compile ":coffee-asset-pipeline:1.7.0"
+        //compile ":sass-asset-pipeline:1.8.0"
+        //compile ":less-asset-pipeline:1.8.0"
+        //compile ":coffee-asset-pipeline:1.8.0"
         //compile ":handlebars-asset-pipeline:1.3.0.3"
     }
 }
