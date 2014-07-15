999bfc2cc88b1ac94363f24225c1d3eb46e234bb
Lari Hotari <lhotari@gopivotal.com>
2014-07-15 14:09:28 +0300

    GRAILS-11447 add support for namespace attribute in g:form tag

--- grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/taglib/FormTagLibTests.groovy
+++ grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/taglib/FormTagLibTests.groovy
@@ -2,8 +2,10 @@ package org.codehaus.groovy.grails.web.taglib
 
 import grails.util.MockRequestDataValueProcessor
 
+import org.codehaus.groovy.grails.commons.AbstractGrailsClass
+import org.codehaus.groovy.grails.commons.GrailsUrlMappingsClass
+import org.codehaus.groovy.grails.commons.UrlMappingsArtefactHandler
 import org.codehaus.groovy.grails.plugins.web.taglib.FormTagLib
-import org.codehaus.groovy.grails.support.MockApplicationContext
 
 /**
  * Tests for the FormTagLib.groovy file which contains tags to help with the                                         l
@@ -17,6 +19,42 @@ class FormTagLibTests extends AbstractGrailsTagTests {
     protected void setUp() {
         super.setUp()
         appCtx.getBean(FormTagLib.name).requestDataValueProcessor = new MockRequestDataValueProcessor()
+    }
+    
+    @Override
+    protected void onInit() {
+        def mappingsClosure = {
+            "/admin/books"(controller:'books', namespace:'admin')
+            "/books"(controller:'books')
+        }
+        grailsApplication.addArtefact(UrlMappingsArtefactHandler.TYPE, new MockGrailsUrlMappingsClass(mappingsClosure));
+    }
+    
+    void testFormNamespace() {
+        def template = '<g:form controller="books" namespace="admin"></g:form>'
+        assertOutputEquals('<form action="/admin/books" method="post" ><input type="hidden" name="requestDataValueProcessorHiddenName" value="hiddenValue" />\n</form>', template)
+    }
+    
+    void testFormNoNamespace() {
+        def template = '<g:form controller="books"></g:form>'
+        assertOutputEquals('<form action="/books" method="post" ><input type="hidden" name="requestDataValueProcessorHiddenName" value="hiddenValue" />\n</form>', template)
+    }
+    
+    private static final class MockGrailsUrlMappingsClass extends AbstractGrailsClass implements GrailsUrlMappingsClass {
+        Closure mappingClosure;
+        public MockGrailsUrlMappingsClass(Closure mappingClosure) {
+            super(this.getClass(), "UrlMappings");
+            this.mappingClosure = mappingClosure;
+        }
+        @Override
+        public Closure getMappingsClosure() {
+            return mappingClosure;
+        }
+        
+        @Override
+        public List getExcludePatterns() {
+            return null;
+        }
     }
 
     void testFormTagWithAlternativeMethod() {
