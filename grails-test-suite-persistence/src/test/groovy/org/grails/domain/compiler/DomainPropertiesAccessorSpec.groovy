993f9df27fc6ddcc39caef85c6405b59e4518753
Jeff Scott Brown <jbrown@gopivotal.com>
2014-07-17 16:09:31 -0500

    simplify test of domain class setProperties and getProperties

---
+++ grails-test-suite-persistence/src/test/groovy/org/grails/domain/compiler/DomainPropertiesAccessorSpec.groovy
@@ -0,0 +1,43 @@
+package org.grails.domain.compiler
+
+import grails.persistence.Entity
+
+import org.grails.plugins.web.controllers.api.ControllersDomainBindingApi
+
+import spock.lang.Specification
+
+
+class DomainPropertiesAccessorSpec extends Specification {
+
+    void "Test binding constructor adding via AST"() {
+        when:
+            def test = new TestDomain(age:"10")
+
+        then:
+            test.age == 10
+    }
+
+    void "Test setProperties method added via AST"() {
+        when:
+            def test = new TestDomain()
+            test.properties = [age:"10"]
+
+        then:
+            test.age == 10
+    }
+
+    void "Test getProperties method added via AST"() {
+        when:
+            def test = new TestDomain()
+            test.properties['age', 'name'] = [age:"10"]
+
+        then:
+            test.age == 10
+    }
+}
+
+@Entity
+class TestDomain {
+    Integer age
+}
+
