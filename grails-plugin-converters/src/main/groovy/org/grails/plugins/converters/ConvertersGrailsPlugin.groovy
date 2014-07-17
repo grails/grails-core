bbd298e9e216e3da3ba064b8d673393425ba3d13
Jeff Scott Brown <jbrown@gopivotal.com>
2014-07-17 13:45:56 -0500

    Revert "remove ConvertersControllerstransformer and ConvertersControllersApi"
    This reverts commit f93d5d6830f56bd0216207f7ac2422f546d26940.

--- grails-plugin-converters/src/main/groovy/org/grails/plugins/converters/ConvertersGrailsPlugin.groovy
+++ grails-plugin-converters/src/main/groovy/org/grails/plugins/converters/ConvertersGrailsPlugin.groovy
@@ -21,6 +21,7 @@ import grails.core.GrailsApplication
 import grails.core.support.GrailsApplicationAware
 import grails.util.GrailsUtil
 
+import org.grails.plugins.converters.api.ConvertersControllersApi
 import org.grails.web.converters.configuration.ConvertersConfigurationInitializer
 import org.grails.web.converters.configuration.ObjectMarshallerRegisterer
 import org.grails.web.converters.marshaller.json.ValidationErrorsMarshaller as JsonErrorsMarshaller
@@ -66,6 +67,8 @@ class ConvertersGrailsPlugin implements GrailsApplicationAware {
             marshaller = { JsonErrorsMarshaller om -> }
             converterClass = JSON
         }
+
+        instanceConvertersControllersApi(ConvertersControllersApi)
     }
 
     def doWithDynamicMethods = {applicationContext ->
---
+++ grails-plugin-converters/src/main/groovy/org/grails/plugins/converters/api/ConvertersControllersApi.groovy
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2011 SpringSource
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.grails.plugins.converters.api
+
+import grails.converters.JSON
+
+import org.grails.web.converters.Converter
+import org.codehaus.groovy.grails.web.json.JSONArray
+import org.codehaus.groovy.grails.web.json.JSONObject
+import org.grails.web.servlet.mvc.GrailsWebRequest
+
+/**
+ * Additional API extensions provided to controllers via the converters plugin.
+ *
+ * @author Graeme Rocher
+ * @since 2.0
+ */
+class ConvertersControllersApi {
+
+    /**
+     * Method used to render out converted responses. Used for render foo as XML
+     *
+     * @param controller The controller
+     * @param converter The converter instance
+     */
+    void render(controller, Converter converter) {
+        converter.render(controller.response)
+
+        // Prevent Grails from looking for a view if this method is used.
+        GrailsWebRequest.lookup().renderView = false
+    }
+
+    /**
+     * Used to render out X-JSON header
+     *
+     * @param controller The controller
+     * @param value the value
+     */
+    void jsonHeader(controller, value) {
+        if (!value) {
+            return
+        }
+
+        def json = (value instanceof JSON || value instanceof JSONObject || value instanceof JSONArray ||
+                    value instanceof String) ? value : new JSON(value)
+        controller.response?.setHeader("X-JSON", json.toString())
+    }
+}
