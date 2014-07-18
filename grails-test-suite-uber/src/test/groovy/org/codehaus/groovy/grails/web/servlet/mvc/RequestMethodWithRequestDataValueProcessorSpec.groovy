fa685f6db90e332815bc09517e23ce3b789f2f00
Jeff Scott Brown <jbrown@gopivotal.com>
2014-07-17 20:58:23 -0500

    Improve testing of redirects with a RequestDataValueProcessor

---
+++ grails-test-suite-uber/src/test/groovy/org/codehaus/groovy/grails/web/servlet/mvc/RequestMethodWithRequestDataValueProcessorSpec.groovy
@@ -0,0 +1,89 @@
+package org.codehaus.groovy.grails.web.servlet.mvc
+
+import grails.test.mixin.TestFor
+import grails.util.MockRequestDataValueProcessor
+
+import org.codehaus.groovy.grails.web.servlet.HttpHeaders
+
+import spock.lang.Specification
+
+@TestFor(RedirectController)
+class RequestMethodWithRequestDataValueProcessorSpec extends Specification {
+    
+    def doWithSpring = {
+        requestDataValueProcessor MockRequestDataValueProcessor
+    }
+    
+    void 'test redirect in controller with all upper class class name'() {
+        when:
+        controller.index()
+        
+        then:
+        "/redirect/list?requestDataValueProcessorParamName=paramValue" == response.redirectedUrl
+    }
+    
+    void 'test permanent redirect'() {
+        when:
+        controller.toActionPermanent()
+        
+        then:
+        "http://localhost:8080/redirect/foo?requestDataValueProcessorParamName=paramValue" == response.getHeader(HttpHeaders.LOCATION)
+        301 == response.status
+    }
+    
+    void 'test redirect to controller with duplicate params'() {
+        when:
+        controller.toControllerWithDuplicateParams()
+        
+        then:
+        "/test/foo?one=two&one=three&requestDataValueProcessorParamName=paramValue" == response.redirectedUrl
+    }
+
+    void 'test redirect with fragment'() {
+        when:
+        controller.toControllerAndActionWithFragment()
+        
+        then:
+        "/test/foo?requestDataValueProcessorParamName=paramValue#frag" == response.redirectedUrl
+    }
+    
+    void 'test redirect to default action of another controller'() {
+        when:
+        controller.redirectToDefaultAction()
+        
+        then:
+        "/redirect/toAction?requestDataValueProcessorParamName=paramValue" == response.redirectedUrl
+    }
+
+    void 'test redirect to action'() {
+        when:
+        controller.toAction()
+        
+        then:
+        "/redirect/foo?requestDataValueProcessorParamName=paramValue" == response.redirectedUrl
+    }
+    
+    void 'test redirect to controller'() {
+        when:
+        controller.toController()
+        
+        then:
+        "/test?requestDataValueProcessorParamName=paramValue" == response.redirectedUrl
+    }
+
+    void 'test redirect to controller with params'() {
+        when:
+        controller.toControllerWithParams()
+        
+        then:
+        "/test/foo?one=two&two=three&requestDataValueProcessorParamName=paramValue" == response.redirectedUrl
+    }
+
+    void 'test redirect to controller with duplicate array params'() {
+        when:
+        controller.toControllerWithDuplicateArrayParams()
+        
+        then:
+        "/test/foo?one=two&one=three&requestDataValueProcessorParamName=paramValue" == response.redirectedUrl
+    }
+}
