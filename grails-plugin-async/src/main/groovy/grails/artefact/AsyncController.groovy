00807b302cc0f841cb49278ffa0b4bc31953a6f5
Jeff Scott Brown <jbrown@pivotal.io>
2014-08-05 09:53:31 -0500

    introduce AsyncController trait

---
+++ grails-plugin-async/src/main/groovy/grails/artefact/AsyncController.groovy
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package grails.artefact
+
+import javax.servlet.AsyncContext
+import javax.servlet.http.HttpServletRequest
+
+import org.grails.plugins.web.async.GrailsAsyncContext
+import grails.web.util.GrailsApplicationAttributes
+import org.grails.web.servlet.mvc.GrailsWebRequest
+import org.springframework.web.context.request.RequestContextHolder
+
+/**
+ * 
+ * @author Jeff Brown
+ * @since 3.0
+ *
+ */
+trait AsyncController {
+
+    /**
+     * Raw access to the Servlet 3.0 startAsync method
+     *
+     * @return a new {@link javax.servlet.AsyncContext}
+     */
+    AsyncContext startAsync() {
+        GrailsWebRequest webRequest = RequestContextHolder.currentRequestAttributes()
+        HttpServletRequest request = webRequest.currentRequest
+        def ctx = request.startAsync(request, webRequest.currentResponse)
+        request.setAttribute(GrailsApplicationAttributes.ASYNC_STARTED, true)
+        new GrailsAsyncContext(ctx, webRequest)
+    }
+}
