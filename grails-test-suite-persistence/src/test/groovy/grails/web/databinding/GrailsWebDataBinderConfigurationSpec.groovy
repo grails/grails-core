34d2fd35e5b071f8478e6f1fe94fb89b2966db63
Jeff Scott Brown <jbrown@pivotal.io>
2014-08-18 12:48:38 -0500

    move tests out of org.codehaus.* package

--- grails-test-suite-persistence/src/test/groovy/grails/web/databinding/GrailsWebDataBinderConfigurationSpec.groovy
+++ grails-test-suite-persistence/src/test/groovy/grails/web/databinding/GrailsWebDataBinderConfigurationSpec.groovy
@@ -1,4 +1,4 @@
-package org.codehaus.groovy.grails.orm
+package grails.web.databinding
 
 import grails.databinding.SimpleMapDataBindingSource;
 import grails.test.mixin.Mock
--- grails-test-suite-persistence/src/test/groovy/grails/web/databinding/GrailsWebDataBinderSpec.groovy
+++ grails-test-suite-persistence/src/test/groovy/grails/web/databinding/GrailsWebDataBinderSpec.groovy
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.codehaus.groovy.grails.orm
+package grails.web.databinding
 
 import grails.databinding.DataBindingSource;
 import grails.databinding.SimpleMapDataBindingSource;
