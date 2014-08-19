fa384f4beedb202308363e3faaaecd369d6a3ae4
Jeff Scott Brown <jbrown@pivotal.io>
2014-08-19 10:19:46 -0500

    move code out of org.codehaus.*

--- grails-plugin-testing/src/main/groovy/org/grails/compiler/injection/test/DirtiesRuntimeTransformation.java
+++ grails-plugin-testing/src/main/groovy/org/grails/compiler/injection/test/DirtiesRuntimeTransformation.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.codehaus.groovy.grails.compiler.injection.test;
+package org.grails.compiler.injection.test;
 
 import grails.test.runtime.FreshRuntime;
 
