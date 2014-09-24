c4453b9497088e0f9736dca9acb18af0ead87439
Lari Hotari <lhotari@pivotal.io>
2014-09-24 15:29:59 +0300

    use ExpectIt to implement basic cli tests

--- grails-shell/src/test/groovy/org/grails/cli/GrailsCliSpec.groovy
+++ grails-shell/src/test/groovy/org/grails/cli/GrailsCliSpec.groovy
@@ -1,18 +1,22 @@
 package org.grails.cli
 
+import static net.sf.expectit.matcher.Matchers.*
+import grails.build.logging.GrailsConsole;
+
 import java.lang.reflect.Field
+import java.util.concurrent.TimeUnit
 
 import jnr.posix.POSIXFactory
 import net.sf.expectit.Expect
 import net.sf.expectit.ExpectBuilder
+import net.sf.expectit.Result
 
+import org.codehaus.groovy.runtime.InvokerHelper
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 
 import spock.lang.Shared
 import spock.lang.Specification
-
-import static net.sf.expectit.matcher.Matchers.*
 
 class GrailsCliSpec extends Specification {
     @Rule
@@ -22,11 +26,35 @@ class GrailsCliSpec extends Specification {
     @Shared File previousUserDir
     @Shared Map originalStreams
     
+    PipedOutputStream expectCommandsPipe
+    PipedOutputStream expectSystemOutPipe
+    
     def setup() {
+        GrailsConsole.removeInstance()
         cli = new GrailsCli(ansiEnabled: false)
         chdir(tempFolder.getRoot())
     }
 
+    def setupSpec() {
+        originalStreams = [out: System.out, in: System.in, err: System.err]
+        previousUserDir = new File("").absoluteFile
+        disableFileCanonCaches()
+        System.setProperty("jansi.passthrough", "true")
+        System.setProperty("jline.terminal", "jline.UnsupportedTerminal")
+    }
+    
+    def cleanup() {
+        expectCommandsPipe?.close()
+        expectSystemOutPipe?.close()
+
+        GrailsConsole.removeInstance()
+                
+        System.setIn(originalStreams.in)
+        System.setOut(originalStreams.out)
+        System.setErr(originalStreams.err)
+        chdir(previousUserDir)
+    }
+    
     private static void chdir(File newDir) {
         String path = newDir.absolutePath
         System.setProperty("user.dir", path)
@@ -45,21 +73,38 @@ class GrailsCliSpec extends Specification {
             field.setBoolean(null, false)
         }
     }
-    
-    def setupSpec() {
-        originalStreams = [out: System.out, in: System.in, err: System.err]
-        previousUserDir = new File("").absoluteFile
-        disableFileCanonCaches()
-        System.setProperty("jansi.passthrough", "true")
-    }
-    
-    def cleanupSpec() {
-        System.setIn(originalStreams.in)
-        System.setOut(originalStreams.out)
-        System.setErr(originalStreams.err)
-        chdir(previousUserDir)
+
+    private Result expectPrompt(Expect expect) {
+        expect.expect(contains("grails> "))
     }
 
+    void println(Object value) {
+        def out = originalStreams.out ?: System.out
+        out.println(InvokerHelper.toString(value))
+    }
+    
+    private ExpectBuilder createExpectsBuilderWithSystemInOut() {
+        PipedInputStream emulatedSystemIn = new PipedInputStream()
+        expectCommandsPipe = new PipedOutputStream(emulatedSystemIn)
+        System.setIn(emulatedSystemIn)
+
+        PipedInputStream systemOutInput = new PipedInputStream()
+        expectSystemOutPipe = new PipedOutputStream(systemOutInput)
+        PrintStream emulatedSystemOut = new PrintStream(expectSystemOutPipe, true)
+        System.setOut(emulatedSystemOut)
+        System.setErr(emulatedSystemOut)
+
+        ExpectBuilder expectBuilder = new ExpectBuilder().withInputs(systemOutInput).withOutput(expectCommandsPipe).withTimeout(5, TimeUnit.SECONDS)
+        expectBuilder.withEchoOutput(originalStreams.err).withEchoInput(originalStreams.out)
+        expectBuilder
+    }
+
+    private File createApp() {
+        cli.execute('create-app','newapp','--profile=web')
+        File appdir = new File(tempFolder.getRoot(), 'newapp')
+        return appdir
+    }
+    
     def "should create new application"() {
         when:
         File appdir = createApp()
@@ -68,12 +113,6 @@ class GrailsCliSpec extends Specification {
         assert new File(appdir, "application.properties").exists()
     }    
 
-    private File createApp() {
-        cli.execute('create-app','newapp','--profile=web')
-        File appdir = new File(tempFolder.getRoot(), 'newapp')
-        return appdir
-    }
-    
     def "should start and exit interactive mode"() {
         when:
         File appdir = createApp()
@@ -84,9 +123,7 @@ class GrailsCliSpec extends Specification {
         Thread cliThread = new Thread({-> retval=cli.execute()} as Runnable)
         cliThread.start()
         Expect expect = expectBuilder.build()
-        expect.sendLine("help")
-        def grailsPrompt = ~/.+/
-        expect.expect(matches(grailsPrompt))
+        expectPrompt(expect)
         expect.sendLine("exit")
         expect.close()
         cliThread.join()
@@ -94,7 +131,7 @@ class GrailsCliSpec extends Specification {
         retval == 0
     }
     
-    def "should provide help in interactive mode"() {
+    def "should provide help for all commands in interactive mode"() {
         when:
         File appdir = createApp()
         assert new File(appdir, "application.properties").exists()
@@ -104,24 +141,19 @@ class GrailsCliSpec extends Specification {
         Thread cliThread = new Thread({-> retval=cli.execute()} as Runnable)
         cliThread.start()
         Expect expect = expectBuilder.build()
+        expectPrompt(expect)
         expect.sendLine("help")
+        def helpContent = expectPrompt(expect).before
         expect.sendLine("exit")
         expect.close()
         cliThread.join()
         then:
         retval == 0
-    }
-
-    private ExpectBuilder createExpectsBuilderWithSystemInOut() {
-        PipedInputStream emulatedSystemIn = new PipedInputStream()
-        PipedOutputStream commandsOutput = new PipedOutputStream(emulatedSystemIn)
-        System.setIn(emulatedSystemIn)
-
-        PipedInputStream systemOutInput = new PipedInputStream()
-        PrintStream emulatedSystemOut = new PrintStream(new PipedOutputStream(systemOutInput), true)
-        System.setOut(emulatedSystemOut)
-        System.setErr(emulatedSystemOut)
-
-        new ExpectBuilder().withInputs(systemOutInput).withOutput(commandsOutput)
+        helpContent == '''create-controller\tCreates a controller
+create-domain\tCreates a domain class
+create-service\tCreates a service
+create-taglib\tCreates a tag library
+detailed usage with help [command]
+'''
     }
 }
