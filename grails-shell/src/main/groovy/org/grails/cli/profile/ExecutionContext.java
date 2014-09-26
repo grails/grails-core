73fdd7776cf6e472f48bd71f30461e9c825f260b
Lari Hotari <lhotari@pivotal.io>
2014-09-26 16:51:30 +0300

    add baseDir to execution context

--- grails-shell/src/main/groovy/org/grails/cli/profile/ExecutionContext.java
+++ grails-shell/src/main/groovy/org/grails/cli/profile/ExecutionContext.java
@@ -2,9 +2,12 @@ package org.grails.cli.profile;
 
 import grails.build.logging.GrailsConsole;
 
+import java.io.File;
+
 import org.codehaus.groovy.grails.cli.parsing.CommandLine;
 
 public interface ExecutionContext {
     CommandLine getCommandLine();
     GrailsConsole getConsole();
+    File getBaseDir();
 }
