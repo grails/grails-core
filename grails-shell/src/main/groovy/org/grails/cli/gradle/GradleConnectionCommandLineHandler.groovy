02d20048fb79cffc590ce8750effa5e6fc17f4bd
Lari Hotari <lhotari@pivotal.io>
2014-10-01 12:29:39 +0300

    implement listing of all available Gradle task selectors with multi-project support - could be used for task selector completion in interactive shell

--- grails-shell/src/main/groovy/org/grails/cli/gradle/GradleConnectionCommandLineHandler.groovy
+++ grails-shell/src/main/groovy/org/grails/cli/gradle/GradleConnectionCommandLineHandler.groovy
@@ -1,50 +1,78 @@
 package org.grails.cli.gradle
 
+import groovy.transform.CompileDynamic
+import groovy.transform.CompileStatic
+
 import org.gradle.tooling.GradleConnector
-import org.gradle.tooling.ModelBuilder
-import org.gradle.tooling.ProgressEvent
-import org.gradle.tooling.ProgressListener
 import org.gradle.tooling.ProjectConnection
-import org.gradle.tooling.model.GradleProject
-import org.gradle.tooling.model.GradleTask
+import org.grails.cli.gradle.FetchAllTaskSelectorsBuildAction.AllTasksModel
 import org.grails.cli.profile.CommandDescription
 import org.grails.cli.profile.CommandLineHandler
 import org.grails.cli.profile.ExecutionContext
 import org.grails.cli.profile.ProjectContext
 
+@CompileStatic
 class GradleConnectionCommandLineHandler implements CommandLineHandler {
-    ProjectConnection projectConnection
 
-    public void initialize(File baseDir) {
+    private ProjectConnection openGradleConnection(File baseDir) {
         SystemOutErrCapturer.doWithCapturer {
-            projectConnection = GradleConnector.newConnector().forProjectDirectory(baseDir).connect()
+            GradleConnector.newConnector().forProjectDirectory(baseDir).connect()
+        }
+    }
+    
+    private <T> T withProjectConnection(File baseDir, boolean suppressOutput=true, Closure<T> closure) {
+        ProjectConnection projectConnection=openGradleConnection(baseDir)
+        try {
+            if(suppressOutput) {
+                SystemOutErrCapturer.doWithCapturer {
+                    closure(projectConnection)
+                }
+            } else {
+                closure(projectConnection)
+            }
+        } finally {
+            projectConnection.close()
         }
     }
 
     @Override
     public boolean handleCommand(ExecutionContext context) {
-        if(projectConnection==null) {
-            initialize(context.baseDir)
+        if(context.commandLine.commandName == 'gradle') {
+            
         }
         return false;
     }
 
+    @CompileDynamic
     @Override
     public List<CommandDescription> listCommands(ProjectContext context) {
-        def sysout = System.out
-        if(projectConnection==null) {
-            initialize(context.getBaseDir())
+        [new CommandDescription("gradle", "run gradle build", "usage: gradle [task]")]
+    }
+    
+    private static final Closure<String> taskNameFormatter = { String projectPath, String taskName ->
+        if(projectPath == ':') {
+            ":$taskName".toString()
+        } else {
+            "$projectPath:$taskName".toString()
         }
-        SystemOutErrCapturer.doWithCapturer { 
-            ModelBuilder<GradleProject> builder = projectConnection.model(GradleProject.class);
-            //builder.withArguments("--quiet")
-            builder.addProgressListener({ ProgressEvent event ->  
-                //sysout.println "event: $event.description"
-            } as ProgressListener)
-            GradleProject project = builder.get();
-            project.getTasks().collect { GradleTask task ->
-                new CommandDescription(task.name, task.description, null)
+    }
+    
+    public Set<String> listAllTaskSelectors(ProjectContext context) {
+        AllTasksModel allTasksModel = ((AllTasksModel)withProjectConnection(context.getBaseDir()) { ProjectConnection projectConnection ->
+            (AllTasksModel)projectConnection.action(new FetchAllTaskSelectorsBuildAction(context.getBaseDir())).run()
+        })
+        Set<String> allTaskSelectors=[] as Set
+        
+        if (allTasksModel.currentProject) {
+            allTaskSelectors.addAll(allTasksModel.allTaskSelectors.get(allTasksModel.currentProject))
+        }
+        
+        allTasksModel.projectPaths.each { String projectName, String projectPath ->
+            allTasksModel.allTasks.get(projectName).each { String taskName ->
+                allTaskSelectors.add(taskNameFormatter(projectPath, taskName))
             }
         }
+        
+        allTaskSelectors
     }
 }
