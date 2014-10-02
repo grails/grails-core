e2d4dd2b7fbde566e779f407604c5bc49e03d5c6
Lari Hotari <lhotari@pivotal.io>
2014-10-02 13:20:26 +0300

    refactor RenderCommandStep

--- grails-shell/src/main/groovy/org/grails/cli/profile/simple/RenderCommandStep.groovy
+++ grails-shell/src/main/groovy/org/grails/cli/profile/simple/RenderCommandStep.groovy
@@ -1,17 +1,55 @@
 package org.grails.cli.profile.simple
 
-import java.io.File;
-
 import org.grails.cli.profile.ExecutionContext
 
 class RenderCommandStep extends SimpleCommandStep {
-
     @Override
     public boolean handleStep(ExecutionContext context) {
+        String nameAsArgument = context.getCommandLine().getRemainingArgs()[0]
+        String artifactName
+        String artifactPackage 
+        (artifactName, artifactPackage) = resolveNameAndPackage(context, nameAsArgument)
+        Map<String, String> variables = createVariables(artifactPackage, artifactName)
+        
+        File destination = resolveDestination(context, variables)
+        
+        String relPath = relativePath(context.baseDir, destination)
+        context.console.info("Creating $relPath")
+        
+        renderToDestination(destination, variables)
+        
+        return true
+    }
+
+    protected renderToDestination(File destination, Map variables) {
         File profileDir = command.profile.profileDir
         File templateFile = new File(profileDir, commandParameters.template)
-        
-        String nameAsArgument = context.getCommandLine().getRemainingArgs()[0]
+        destination.text = doReplacements(templateFile.text, variables)
+    }
+
+    private File resolveDestination(ExecutionContext context, Map variables) {
+        String destinationName = doReplacements(commandParameters.destination, variables)
+        File destination = new File(context.baseDir, destinationName).absoluteFile
+
+        if(destination.exists()) {
+            throw new RuntimeException("$destination already exists.")
+        }
+        if(!destination.getParentFile().exists()) {
+            destination.getParentFile().mkdirs()
+        }
+        return destination
+    }
+
+    private Map createVariables(String artifactPackage, String artifactName) {
+        Map<String, String> variables = [:]
+        variables['artifact.package.name'] = artifactPackage
+        variables['artifact.package.path'] = artifactPackage.replace('.','/')
+        variables['artifact.package'] = "package $artifactPackage\n"
+        variables['artifact.name'] = artifactName
+        return variables
+    }
+    
+    protected List<String> resolveNameAndPackage(ExecutionContext context, String nameAsArgument) {
         List<String> parts = nameAsArgument.split(/\./) as List
         
         String artifactName
@@ -25,29 +63,8 @@ class RenderCommandStep extends SimpleCommandStep {
             artifactPackage = parts[0..-2].join('.')
         }
         
-        Map<String, String> variables = [:]
-        variables['artifact.package.name'] = artifactPackage
-        variables['artifact.package.path'] = artifactPackage.replace('.','/')
-        variables['artifact.package'] = "package $artifactPackage\n"
-        variables['artifact.name'] = artifactName
-        
-        String destinationName = doReplacements(commandParameters.destination, variables)
-        File destination = new File(context.baseDir, destinationName).absoluteFile
-        
-        if(destination.exists()) {
-            throw new RuntimeException("$destination already exists.") 
-        }
-        if(!destination.getParentFile().exists()) {
-            destination.getParentFile().mkdirs()
-        }
-        
-        String relPath = relativePath(context.baseDir, destination)
-        context.console.info("Creating $relPath")
-        
-        destination.text = doReplacements(templateFile.text, variables)
-        
-        return true
-    }
+        [artifactName, artifactPackage]
+    } 
     
     protected String relativePath(File relbase, File file) {
         def pathParts = []
@@ -59,7 +76,7 @@ class RenderCommandStep extends SimpleCommandStep {
         pathParts.reverse().join('/')
     }
     
-    private String doReplacements(String source, Map<String, String> variables) {
+    protected String doReplacements(String source, Map<String, String> variables) {
         String result = source
         variables.each { k, v ->
             result = result.replace("@${k}@".toString(), v)
