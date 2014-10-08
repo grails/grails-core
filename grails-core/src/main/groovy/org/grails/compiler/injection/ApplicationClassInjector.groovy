c4320e2724cd4501140c2a89652a014b8149e30a
Graeme Rocher <graeme.rocher@gmail.com>
2014-10-08 12:35:39 +0200

    fix transform not enabling auto configuration and remove test for ivy which will be removed

--- grails-core/src/main/groovy/org/grails/compiler/injection/ApplicationClassInjector.groovy
+++ grails-core/src/main/groovy/org/grails/compiler/injection/ApplicationClassInjector.groovy
@@ -67,17 +67,24 @@ class ApplicationClassInjector implements GrailsArtefactClassInjector {
     @CompileDynamic
     void performInjectionOnAnnotatedClass(SourceUnit source, ClassNode classNode) {
         if(applicationArtefactHandler.isArtefact(classNode)) {
+            println "APPLYING APPLICATION TRANSFORM"
             def objectId = Integer.valueOf( System.identityHashCode(classNode) )
             if(!transformedInstances.contains(objectId)) {
                 transformedInstances << objectId
+
+                println "ADDING AGENT"
                 def enableAgentMethodCall = new MethodCallExpression(new ClassExpression(ClassHelper.make(Support)), "enableAgentIfNotPresent", GrailsASTUtils.ZERO_ARGUMENTS)
                 def methodCallStatement = new ExpressionStatement(enableAgentMethodCall)
                 List<Statement> statements = [ methodCallStatement ]
                 classNode.addStaticInitializerStatements(statements, false)
 
-                def classLoader = Thread.currentThread().contextClassLoader
+                def classLoader = getClass().classLoader
                 if(ClassUtils.isPresent('org.springframework.boot.autoconfigure.EnableAutoConfiguration', classLoader) ) {
+                    println "ADDING EnableAuto annotaiton"
                     GrailsASTUtils.addAnnotationIfNecessary(classNode, classLoader.loadClass('org.springframework.boot.autoconfigure.EnableAutoConfiguration'))
+                }
+                else {
+                    println "NOT BOOT CLASSES"
                 }
             }
         }
