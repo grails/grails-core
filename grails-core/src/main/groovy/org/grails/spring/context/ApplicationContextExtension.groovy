b44e57d7c9e0bc40b76f9a406e7117e143afa60c
Graeme Rocher <graeme.rocher@gmail.com>
2014-10-17 10:51:27 +0200

    Implement Groovy extension module to allow property access to the ApplicationContext instance like in Grails 2.x

---
+++ grails-core/src/main/groovy/org/grails/spring/context/ApplicationContextExtension.groovy
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2014 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.grails.spring.context
+
+import groovy.transform.CompileStatic
+import org.springframework.context.ApplicationContext
+
+
+
+/**
+ * A Groovy extension module that adds additional methods to the Spring {@link org.springframework.context.ApplicationContext} interface
+ *
+ * @author Graeme Rocher
+ * @since 3.0
+ */
+@CompileStatic
+class ApplicationContextExtension {
+
+    /**
+     * Adds the ability to get beans via the dot operator
+     *
+     * @param applicationContext The ApplicationContext instance
+     * @param name The bean name
+     * @return
+     */
+    public static Object propertyMissing(ApplicationContext applicationContext, String name ) {
+        if(applicationContext.containsBean(name)) {
+            return applicationContext.getBean(name);
+        }
+        else {
+            return null;
+        }
+    }
+
+    /**
+     * Adds the ability to use the subscript operator to obtain beans
+     *
+     * @param applicationContext The ApplicationContext instance
+     * @param name The bean name
+     * @return A bean or null
+     */
+    public static Object getAt(ApplicationContext applicationContext, String name) {
+        return propertyMissing(applicationContext, name);
+    }
+}
---
+++ grails-core/src/test/groovy/org/grails/spring/context/ApplicationContextExtensionSpec.groovy
@@ -0,0 +1,40 @@
+package org.grails.spring.context
+
+import org.springframework.context.support.GenericApplicationContext
+import spock.lang.Specification
+
+/*
+ * Copyright 2014 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author graemerocher
+ */
+class ApplicationContextExtensionSpec extends Specification {
+
+    void "Test that beans can be accessed via the dot operator thanks to the extension"() {
+        given:"An application context instance"
+            def ctx = new GenericApplicationContext()
+            def myBean = new Object()
+            ctx.beanFactory.registerSingleton("myBean", myBean)
+            ctx.refresh()
+
+        expect:"That the bean can be accessed by property access or the subscript operator"
+            ctx.myBean != null
+            ctx.myBean.is(myBean)
+            ctx['myBean'] != null
+            ctx['myBean'].is(myBean)
+    }
+}
