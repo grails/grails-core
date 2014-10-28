58ebd00007f63fcaac928c8b77f7cbd42abf1eef
Jeff Scott Brown <jbrown@pivotal.io>
2014-10-28 17:58:27 -0500

    GRAILS-11670 - support readOnly criteria queries in unit tests
    There is already a TestGormEnhancer in grails-data-mapping.  It is in the grails-datastore-gorm-test subproject.  To use that here we would need to either add a dependency on that module or move the class to a module that is already depended on.

---
+++ grails-test-suite-persistence/src/test/groovy/grails/gorm/criteri/WithCriteriaReadOnlySpec.groovy
@@ -0,0 +1,30 @@
+package grails.gorm.criteri
+
+import grails.persistence.Entity
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(Person)
+class WithCriteriaReadOnlySpec extends Specification {
+
+    void 'test that the readOnly criteria method is available in a unit test'() {
+        given:
+        ['Jeff', 'Betsy', 'Jake', 'Zack'].each { name ->
+            new Person(name: name).save()
+        }
+        
+        when:
+        def results = Person.withCriteria {
+            readOnly true
+            like 'name', 'J%'
+        }
+        
+        then:
+        results.size() == 2
+    }
+}
+
+@Entity
+class Person {
+    String name
+}
