47f691af73fba59be0577bfbaf0acdef0d00796f
Lari Hotari <lhotari@pivotal.io>
2014-10-28 13:08:45 +0200

    use org.grails:grails-datastore-test-support:1.0.2-grails-2.3

--- grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
+++ grails-resources/src/grails/grails-app/conf/BuildConfig.groovy
@@ -50,7 +50,7 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         // runtime 'mysql:mysql-connector-java:5.1.27'
         // runtime 'org.postgresql:postgresql:9.3-1100-jdbc41'
-        test "org.grails:grails-datastore-test-support:1.0.1-grails-2.3"
+        test "org.grails:grails-datastore-test-support:1.0.2-grails-2.3"
     }
 
     plugins {
