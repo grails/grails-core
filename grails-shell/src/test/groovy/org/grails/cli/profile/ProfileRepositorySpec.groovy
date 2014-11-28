98d3d255b67800a42199bd70646bcfa590cacdb0
Lari Hotari <lhotari@pivotal.io>
2014-11-28 10:28:08 +0200

    fix ProfileRepositorySpec

--- grails-shell/src/test/groovy/org/grails/cli/profile/ProfileRepositorySpec.groovy
+++ grails-shell/src/test/groovy/org/grails/cli/profile/ProfileRepositorySpec.groovy
@@ -1,13 +1,20 @@
 package org.grails.cli.profile
 
+import org.grails.cli.GrailsCliSpec
 import org.grails.cli.profile.git.GitProfileRepository
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+
 import spock.lang.Specification
 
 class ProfileRepositorySpec extends Specification {
     GitProfileRepository profileRepository
+    @Rule
+    TemporaryFolder tempFolder = new TemporaryFolder()
     
     def setup() {
-        profileRepository = new GitProfileRepository(profilesDirectory: new File('src/test/resources/profiles-repository'))
+        profileRepository = new GitProfileRepository()
+        GrailsCliSpec.setupProfileRepositoryForTesting(profileRepository, new File(tempFolder.newFolder(), "repository"), new File("").absoluteFile)
     }
     
     def "should return profile"() {
