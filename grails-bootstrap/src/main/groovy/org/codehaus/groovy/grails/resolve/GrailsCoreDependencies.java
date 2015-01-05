3aa34e2c3e7ffa4317f6fac526b861c57518879b
Lari Hotari <lhotari@pivotal.io>
2015-01-05 23:00:15 +0200

    use groovy 2.3.9

--- grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
+++ grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
@@ -31,7 +31,7 @@ public class GrailsCoreDependencies {
 
     public final String grailsVersion;
     public final String servletVersion;
-    protected final String groovyVersion = "2.3.7";
+    protected final String groovyVersion = "2.3.9";
     protected final String springVersion = "4.0.9.RELEASE";
     protected final String log4jVersion = "1.2.17";
     protected final String h2Version = "1.3.176";
