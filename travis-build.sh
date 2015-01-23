fbfc4e7487c65120bed32e0c299fe7fe8c58de60
Lari Hotari <lhotari@pivotal.io>
2015-01-23 10:37:57 -0500

    add integrationTest task to Travis build

--- travis-build.sh
+++ travis-build.sh
@@ -8,7 +8,12 @@ echo "Project Version: '$grailsVersion'"
 
 EXIT_STATUS=0
 ./gradlew --stop
+echo "Executing tests"
 ./gradlew --no-daemon --stacktrace test || EXIT_STATUS=$?
+echo "Done."
+echo "Executing integration tests"
+./gradlew --no-daemon --stacktrace integrationTest || EXIT_STATUS=$?
+echo "Done."
 
 if [[ $TRAVIS_BRANCH =~ ^master|2\.[34]\.x$ && $TRAVIS_REPO_SLUG == "grails/grails-core" 
 	&& $TRAVIS_PULL_REQUEST == 'false' 
@@ -17,6 +22,7 @@ if [[ $TRAVIS_BRANCH =~ ^master|2\.[34]\.x$ && $TRAVIS_REPO_SLUG == "grails/grai
     echo "Publishing archives"
     upload_skip_tasks="-x :grails-dependencies:assemble -x :grails-dependencies:install -x :grails-dependencies:uploadPublished -x :grails-bom:assemble -x :grails-bom:install -x :grails-bom:uploadPublished"
     ./gradlew -PartifactoryPublishUsername=travis-grails-core $upload_skip_tasks upload || EXIT_STATUS=$?
+    echo "Done."
 fi
 
 exit $EXIT_STATUS
