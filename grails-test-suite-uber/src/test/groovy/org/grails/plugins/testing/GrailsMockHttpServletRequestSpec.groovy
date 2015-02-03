a28194b360a46ea5b94a0b243bd9546b407783f3
Jeff Scott Brown <jbrown@pivotal.io>
2015-02-02 20:18:02 -0600

    GRAILS-11483 - Improve handling of GrailsMockHttpServletRequest.inputStream
    don't allow request.inputStream to be read multiple times

--- grails-test-suite-uber/src/test/groovy/org/grails/plugins/testing/GrailsMockHttpServletRequestSpec.groovy
+++ grails-test-suite-uber/src/test/groovy/org/grails/plugins/testing/GrailsMockHttpServletRequestSpec.groovy
@@ -21,4 +21,24 @@ class GrailsMockHttpServletRequestSpec extends Specification {
         then:
             request.inputStream.getText('UTF-8') == '<?xml version="1.0" encoding="UTF-8"?><map><entry key="a">1</entry><entry key="b">2</entry><entry key="c">3</entry></map>'
     }
+
+    @Issue('GRAILS-11483')
+    def 'test that the inputStream may not be read multiple times'() {
+        given: 'a mock request which contains some content'
+        def req = new GrailsMockHttpServletRequest(content: 'some content')
+
+        when: 'the inputStream is read'
+        def reader = new InputStreamReader(req.inputStream)
+        def result = reader.readLine()
+
+        then: 'the inputStream contents are returned'
+        'some content' == result
+
+        when: 'the inputStream is read again'
+        reader = new InputStreamReader(req.inputStream)
+        result = reader.readLine()
+
+        then: 'the content is no longer available'
+        null == result
+    }
 }
