8a63ed72a571b70eceb53641a5fd209942603417
Lari Hotari <lhotari@pivotal.io>
2015-02-02 19:08:09 -0500

    Add ConfigFileApplicationInitializer that processes application.yml

--- grails-plugin-testing/src/main/groovy/grails/test/runtime/GrailsTestRuntimeConfigurator.java
+++ grails-plugin-testing/src/main/groovy/grails/test/runtime/GrailsTestRuntimeConfigurator.java
@@ -2,13 +2,22 @@ package grails.test.runtime;
 
 import grails.core.GrailsApplication;
 import org.grails.web.servlet.context.support.GrailsRuntimeConfigurator;
+import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 public class GrailsTestRuntimeConfigurator extends GrailsRuntimeConfigurator {
     public GrailsTestRuntimeConfigurator(GrailsApplication application, ApplicationContext parent) {
         super(application, parent);
     }
-    
+
+    @Override
+    protected void initializeContext(ApplicationContext mainContext) {
+        ConfigFileApplicationContextInitializer contextInitializer = new ConfigFileApplicationContextInitializer();
+        contextInitializer.initialize((ConfigurableApplicationContext)mainContext);
+        super.initializeContext(mainContext);
+    }
+
     @Override
     protected void initializePluginManager() {
         this.pluginManager = new NoOpGrailsPluginManager();
