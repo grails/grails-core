cd2b5812080d5a2bc0a8f7aeece01158cb0c1fb6
graemerocher <graeme.rocher@gmail.com>
2015-03-09 15:22:24 +0100

    fix Travis publish arguments

--- travis-build.sh
+++ travis-build.sh
@@ -19,11 +19,11 @@ if [[ $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0 ]]; then
     echo "Running Gradle publish for branch $TRAVIS_BRANCH"
 
     if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
-        ./gradlew -Dsigning.key="$SIGNING_KEY" -Dsigning.password="$SIGNING_PASSPHRASE" -Dsigning.secretKeyRingFile="$USER/.gnupg/secring.gpg uploadArchives publish || EXIT_STATUS=$?
+        ./gradlew -Dsigning.key=$SIGNING_KEY -Dsigning.password=$SIGNING_PASSPHRASE -Dsigning.secretKeyRingFile=$USER/.gnupg/secring.gpg uploadArchives publish || EXIT_STATUS=$?
         ./gradlew assemble || EXIT_STATUS=$?
     elif [[ $TRAVIS_BRANCH =~ ^(master|2.5.x|2.4.x)$ ]]; then
 
-        ./gradlew -Dsigning.key="$SIGNING_KEY" -Dsigning.password="$SIGNING_PASSPHRASE" -Dsigning.secretKeyRingFile="$USER/.gnupg/secring.gpg uploadArchives publish || EXIT_STATUS=$?
+        ./gradlew -Dsigning.key=$SIGNING_KEY -Dsigning.password=$SIGNING_PASSPHRASE -Dsigning.secretKeyRingFile=$USER/.gnupg/secring.gpg uploadArchives publish || EXIT_STATUS=$?
     fi
 
 fi
