22e6c10f4830f6e94796b3107460fb3652aa585e
graemerocher <graeme.rocher@gmail.com>
2015-03-09 15:21:10 +0100

    Debug travis publish

--- travis-build.sh
+++ travis-build.sh
@@ -16,6 +16,8 @@ if [[ $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0 ]]; then
 
     gpg --keyserver keyserver.ubuntu.com --recv-key $SIGNING_KEY
 
+    echo "Running Gradle publish for branch $TRAVIS_BRANCH"
+
     if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
         ./gradlew -Dsigning.key="$SIGNING_KEY" -Dsigning.password="$SIGNING_PASSPHRASE" -Dsigning.secretKeyRingFile="$USER/.gnupg/secring.gpg uploadArchives publish || EXIT_STATUS=$?
         ./gradlew assemble || EXIT_STATUS=$?
