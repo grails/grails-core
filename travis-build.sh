ebb24debec427ddada13261ed579fee6d44cb82d
graemerocher <graeme.rocher@gmail.com>
2015-03-09 17:02:57 +0100

    Travis publish take 5

--- travis-build.sh
+++ travis-build.sh
@@ -19,10 +19,10 @@ if [[ $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0 ]]; then
     echo "Running Gradle publish for branch $TRAVIS_BRANCH"
 
     if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
-        ./gradlew -Psigning.keyId=$SIGNING_KEY -Psigning.password=$SIGNING_PASSPHRASE -PsigningFile="${TRAVIS_BUILD_DIR}/local.secring.gpg" uploadArchives publish || EXIT_STATUS=$?
+        ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -PsigningFile="${TRAVIS_BUILD_DIR}/local.secring.gpg" uploadArchives publish || EXIT_STATUS=$?
         ./gradlew assemble || EXIT_STATUS=$?
     elif [[ $TRAVIS_BRANCH =~ ^(master|2.5.x|2.4.x)$ ]]; then
-        ./gradlew -Psigning.keyId=$SIGNING_KEY -Psigning.password=$SIGNING_PASSPHRASE -PsigningFile="${TRAVIS_BUILD_DIR}/local.secring.gpg" uploadArchives publish || EXIT_STATUS=$?
+        ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -PsigningFile="${TRAVIS_BUILD_DIR}/local.secring.gpg" uploadArchives publish || EXIT_STATUS=$?
     fi
 
 fi
