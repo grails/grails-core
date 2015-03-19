e8acd53206a1f2f733563a5df90bc612a4e060d6
Lari Hotari <lhotari@users.noreply.github.com>
2015-03-19 06:39:35 -0400

    use Spring 4.1.5.RELEASE

--- grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
+++ grails-bootstrap/src/main/groovy/org/codehaus/groovy/grails/resolve/GrailsCoreDependencies.java
@@ -32,7 +32,7 @@ public class GrailsCoreDependencies {
     public final String grailsVersion;
     public final String servletVersion;
     protected final String groovyVersion = "2.4.1";
-    protected final String springVersion = "4.1.4.RELEASE";
+    protected final String springVersion = "4.1.5.RELEASE";
     protected final String log4jVersion = "1.2.17";
     protected final String h2Version = "1.3.176";
     protected final String jaxbVersion = "2.0";
