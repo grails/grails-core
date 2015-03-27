66db29d7a0472f767e391a96e175184a23e19ac3
graemerocher <graeme.rocher@gmail.com>
2015-03-27 12:14:40 +0100

    Potential fix for GRAILS-12123 until better solution found

--- grails-core/src/main/groovy/org/grails/config/EnvironmentAwarePropertySource.java
+++ grails-core/src/main/groovy/org/grails/config/EnvironmentAwarePropertySource.java
@@ -46,7 +46,10 @@ public class EnvironmentAwarePropertySource extends EnumerablePropertySource<Pro
             Environment env = Environment.getCurrent();
             String key = "environments." + env.getName();
             for(PropertySource propertySource : source) {
-                if((propertySource != this) && propertySource instanceof EnumerablePropertySource) {
+
+                if((propertySource != this) &&
+                        !propertySource.getName().contains("plugin") && // plugin default configuration is not allowed to be environment aware (GRAILS-12123)
+                            propertySource instanceof EnumerablePropertySource) {
                     EnumerablePropertySource enumerablePropertySource = (EnumerablePropertySource)propertySource;
 
                     for(String propertyName : enumerablePropertySource.getPropertyNames()) {
