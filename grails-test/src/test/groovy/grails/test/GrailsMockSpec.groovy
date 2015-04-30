371d7c9f1afa0cf5fc3f6f52b321b64eceff496b
Ronald Dahlgren <ronald.dahlgren@gmail.com>
2015-04-30 11:45:58 -0400

    Update GrailsMock tests to cover ExplicitDemand

--- grails-test/src/test/groovy/grails/test/GrailsMockSpec.groovy
+++ grails-test/src/test/groovy/grails/test/GrailsMockSpec.groovy
@@ -8,17 +8,72 @@ class GrailsMockSpec extends Specification {
     @Issue('GRAILS-11075')
     void 'Test that verify() can be called in the then block of a Spock spec'() {
         given:
-        def mock = new GrailsMock(String)
-        mock.demand.trim(1..1) { ->
-            'trimmed string'
-        }
-        def str = mock.createMock()
+            def mock = new GrailsMock(String)
+            mock.demand.trim(1..1) { ->
+                'trimmed string'
+            }
+            def str = mock.createMock()
         
         when:
-        def result = str.trim()
+            def result = str.trim()
         
         then: 
-        result == 'trimmed string'
-        mock.verify()
+            result == 'trimmed string'
+            mock.verify()
+    }
+
+    void 'Test that ExplicitDemand correctly detects missing methods (fail case)'() {
+        given:
+            def mock = new GrailsMock(String)
+        when:
+            mock.demandExplicit.thisMethodDoesNotExist(1..1) { ->
+                'will never be called'
+            }
+        then: 
+            thrown(ExplicitDemandException)
+    }
+
+    void 'Test that ExplicitDemand correctly detects missing methods (success case)'() {
+        given:
+            def mock = new GrailsMock(String)
+        when:
+            mock.demandExplicit.trim(1..1) { ->
+                'trimmed string'
+            }
+            def str = mock.createMock()
+            def result = str.trim()
+        then: 
+            result == 'trimmed string'
+            mock.verify()
+    }
+
+    class ClassWithStaticMethod {
+        static String staticStringThing() {
+            return 'Here is a string'
+        }
+    }
+
+    void 'Test that ExplicitDemand correctly detects missing static methods (fail case)'() {
+        given:
+            def mock = new GrailsMock(ClassWithStaticMethod)
+        when:
+            mock.demandExplicit.static.thisStaticMethodDoesNotExist(1..1) { ->
+                'will never be called'
+            }
+        then: 
+            thrown(ExplicitDemandException)
+    }
+
+    void 'Test that ExplicitDemand correctly detects missing static methods (success case)'() {
+        given:
+            def mock = new GrailsMock(ClassWithStaticMethod)
+            mock.demandExplicit.static.staticStringThing(1..1) { ->
+                return 'different string'
+            }
+        when:
+            def result = ClassWithStaticMethod.staticStringThing()
+        then: 
+            result == 'different string'
+            mock.verify()
     }
 }
