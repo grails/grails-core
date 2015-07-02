d9392333959c794b97d88a839077b8efcbc47a5a
graemerocher <graeme.rocher@gmail.com>
2015-07-02 18:02:49 +0200

    Fixes run-app fails with BeanDefinitionStoreException - Fixes #671

--- grails-core/src/main/groovy/org/grails/spring/context/support/GrailsPlaceholderConfigurer.java
+++ grails-core/src/main/groovy/org/grails/spring/context/support/GrailsPlaceholderConfigurer.java
@@ -15,9 +15,18 @@
  */
 package org.grails.spring.context.support;
 
+import grails.config.Config;
+import grails.core.support.GrailsConfigurationAware;
+import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionVisitor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.util.StringValueResolver;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -26,10 +35,13 @@ import java.util.Properties;
  * @author Graeme Rocher
  * @since 1.0
  */
-public class GrailsPlaceholderConfigurer extends PropertySourcesPlaceholderConfigurer {
+public class GrailsPlaceholderConfigurer extends PropertySourcesPlaceholderConfigurer implements GrailsConfigurationAware {
 
 
-    private final Properties properties;
+    private Properties properties;
+    private String beanName;
+    private BeanFactory beanFactory;
+    private Config config;
 
     public GrailsPlaceholderConfigurer(String placeHolderPrefix, Properties properties) {
         this.properties = properties;
@@ -37,10 +49,71 @@ public class GrailsPlaceholderConfigurer extends PropertySourcesPlaceholderConfi
         setIgnoreUnresolvablePlaceholders(true);
     }
 
+    public GrailsPlaceholderConfigurer() {
+        setIgnoreUnresolvablePlaceholders(true);
+    }
+
     @Override
     protected void loadProperties(Properties props) throws IOException {
-        if (properties != null) {
+        if (config != null) {
+            props.putAll(config.toProperties());
+        }
+        else if(this.properties != null) {
             props.putAll(properties);
         }
+        this.properties = props;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    @Override
+    public void setBeanName(String beanName) {
+        super.setBeanName(beanName);
+        this.beanName = beanName;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) {
+        super.setBeanFactory(beanFactory);
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    protected void doProcessProperties(ConfigurableListableBeanFactory beanFactoryToProcess, StringValueResolver valueResolver) {
+        BeanDefinitionVisitor visitor = new BeanDefinitionVisitor(valueResolver) {
+            @Override
+            protected void visitMap(Map<?, ?> mapVal) {
+                if(mapVal instanceof Config) return;
+                super.visitMap(mapVal);
+            }
+        };
+
+        String[] beanNames = beanFactoryToProcess.getBeanDefinitionNames();
+        for (String curName : beanNames) {
+            // Check that we're not parsing our own bean definition,
+            // to avoid failing on unresolvable placeholders in properties file locations.
+            if (!(curName.equals(this.beanName) && beanFactoryToProcess.equals(this.beanFactory))) {
+                BeanDefinition bd = beanFactoryToProcess.getBeanDefinition(curName);
+                try {
+                    visitor.visitBeanDefinition(bd);
+                }
+                catch (Exception ex) {
+                    throw new BeanDefinitionStoreException(bd.getResourceDescription(), curName, ex.getMessage(), ex);
+                }
+            }
+        }
+
+        // New in Spring 2.5: resolve placeholders in alias target names and aliases as well.
+        beanFactoryToProcess.resolveAliases(valueResolver);
+
+        // New in Spring 3.0: resolve placeholders in embedded values such as annotation attributes.
+        beanFactoryToProcess.addEmbeddedValueResolver(valueResolver);
+    }
+
+    @Override
+    public void setConfiguration(Config co) {
+        this.config = co;
     }
 }
