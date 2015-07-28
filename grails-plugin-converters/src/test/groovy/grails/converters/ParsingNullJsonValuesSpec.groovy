069bc12167de3f091653d2a23dc18b6ba3d7d727
Jeff Scott Brown <jeff@jeffandbetsy.net>
2015-07-28 13:48:02 -0500

    Add @Issue tag
    #9129

--- grails-plugin-converters/src/test/groovy/grails/converters/ParsingNullJsonValuesSpec.groovy
+++ grails-plugin-converters/src/test/groovy/grails/converters/ParsingNullJsonValuesSpec.groovy
@@ -1,15 +1,18 @@
 package grails.converters
 
+import spock.lang.Issue
 import spock.lang.Specification
 
 class ParsingNullJsonValuesSpec extends Specification {
 
+    @Issue('grails/grails-core#9129')
     def "test parsing null value"() {
         expect:
         JSON.parse("{'myList':null}").myList == null
         JSON.parse("{'myList':null}").get('myList') == null
     }
 
+    @Issue('grails/grails-core#9129')
     def "test the remove method returns null, not JSONObject.Null"() {
         when:
         def obj = JSON.parse("{'myList':null}")
