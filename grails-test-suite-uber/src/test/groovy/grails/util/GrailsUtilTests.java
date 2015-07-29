82c59da7eb2596defc051d7441dc35e0afb748a3
Graeme Rocher <graeme.rocher@gmail.com>
2015-07-29 08:30:43 +0200

    Back to snapshot

--- grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-test-suite-uber/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("2.5.1", GrailsUtil.getGrailsVersion());
+        assertEquals("2.5.2.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @Override
