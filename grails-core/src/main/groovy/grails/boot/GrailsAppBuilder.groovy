b6e799c3aaad66f95bf6f69f3a50518063587b8c
Graeme Rocher <graeme.rocher@gmail.com>
2015-09-07 08:49:53 +0200

    Fixes #9226 - SpringBoot Profile not set when deployed as war

---
+++ grails-core/src/main/groovy/grails/boot/GrailsAppBuilder.groovy
@@ -0,0 +1,22 @@
+package grails.boot
+
+import groovy.transform.CompileStatic
+import groovy.transform.InheritConstructors
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+
+/**
+ * Fluent API for constructing GrailsApp instances. Simple extension of {@link SpringApplicationBuilder}.
+ *
+ * @author Graeme Rocher
+ * @since 3.0.6
+ */
+@CompileStatic
+@InheritConstructors
+class GrailsAppBuilder extends SpringApplicationBuilder {
+
+    @Override
+    protected SpringApplication createSpringApplication(Object... sources) {
+        return new GrailsApp(sources)
+    }
+}
