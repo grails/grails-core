8b9f279b57f6f1821e0092598832edc9ddbf7d83
graemerocher <graeme.rocher@gmail.com>
2015-11-20 16:18:23 +0100

    Upgrade to latest dependencies

--- grails-plugin-testing/src/main/groovy/grails/test/mixin/domain/DomainClassUnitTestMixin.groovy
+++ grails-plugin-testing/src/main/groovy/grails/test/mixin/domain/DomainClassUnitTestMixin.groovy
@@ -16,26 +16,16 @@
 package grails.test.mixin.domain
 
 import grails.artefact.Enhanced
-import grails.gorm.TestCriteriaBuilder
+import grails.core.GrailsDomainClass
 import grails.test.mixin.support.GrailsUnitTestMixin
 import groovy.transform.CompileStatic
-
 import org.grails.core.artefact.DomainClassArtefactHandler
-import grails.util.GrailsClassUtils
-import grails.core.GrailsDomainClass
-import org.grails.plugins.domain.DomainClassGrailsPlugin
-import org.grails.plugins.web.controllers.ControllersGrailsPlugin
-import org.grails.validation.ConstraintEvalUtils
 import org.grails.datastore.gorm.GormEnhancer
-import org.grails.datastore.gorm.GormStaticApi
-import org.grails.datastore.gorm.finders.FinderMethod
-import org.grails.datastore.mapping.core.Datastore
 import org.grails.datastore.mapping.model.PersistentEntity
-import org.grails.datastore.mapping.query.api.BuildableCriteria
 import org.grails.datastore.mapping.simple.SimpleMapDatastore
+import org.grails.validation.ConstraintEvalUtils
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.validation.Validator
-
 /**
  * <p>A mixin that can be applied to JUnit or Spock tests to add testing support
  * to a users test classes. Can be used in combination with {@link grails.test.mixin.web.ControllerUnitTestMixin}
@@ -90,7 +80,7 @@ class DomainClassUnitTestMixin extends GrailsUnitTestMixin {
             Validator validator = registerDomainClassValidator(domain)
             simpleDatastore.mappingContext.addEntityValidator(entity, validator)
         }
-        def enhancer = new TestGormEnhancer(simpleDatastore, transactionManager)
+        def enhancer = new GormEnhancer(simpleDatastore, transactionManager)
         final failOnError = getFailOnError()
         enhancer.failOnError = failOnError instanceof Boolean ? (Boolean)failOnError : false
 
@@ -173,23 +163,4 @@ class DomainClassUnitTestMixin extends GrailsUnitTestMixin {
     }
 }
 
-class TestGormEnhancer extends GormEnhancer {
-    TestGormEnhancer(Datastore datastore, PlatformTransactionManager transactionManager) {
-        super(datastore, transactionManager)
-    }
 
-    protected <D> GormStaticApi<D> getStaticApi(Class<D> cls) {
-        return new TestGormStaticApi(cls, datastore, getFinders(), transactionManager)
-    }
-}
-
-class TestGormStaticApi extends GormStaticApi {
-    
-    TestGormStaticApi(Class persistentClass, Datastore datastore, List<FinderMethod> finders, PlatformTransactionManager transactionManager) {
-        super(persistentClass, datastore, finders, transactionManager)
-    }
-    
-    BuildableCriteria createCriteria() {
-        new TestCriteriaBuilder(persistentClass, datastore.currentSession)
-    }
-}
