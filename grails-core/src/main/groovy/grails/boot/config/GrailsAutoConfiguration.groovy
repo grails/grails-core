bae8b34177dd0d96ad5ae9b50dc358d7629e3215
graemerocher <graeme.rocher@gmail.com>
2016-01-11 16:02:27 +0100

    Further limit classpath scanning to only classes relative to the Application class in order to improve startup

--- grails-core/src/main/groovy/grails/boot/config/GrailsAutoConfiguration.groovy
+++ grails-core/src/main/groovy/grails/boot/config/GrailsAutoConfiguration.groovy
@@ -1,8 +1,7 @@
 package grails.boot.config
-
 import grails.config.Settings
 import grails.core.GrailsApplicationClass
-import grails.core.GrailsApplicationLifeCycle
+import grails.io.IOUtils
 import groovy.transform.CompileStatic
 import groovy.transform.InheritConstructors
 import groovy.transform.Memoized
@@ -14,17 +13,14 @@ import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
 import org.springframework.context.ResourceLoaderAware
 import org.springframework.context.annotation.Bean
+import org.springframework.core.io.DefaultResourceLoader
 import org.springframework.core.io.Resource
-import org.springframework.core.io.ResourceLoader
-import org.springframework.core.io.UrlResource
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.core.io.support.ResourcePatternResolver
 import org.springframework.core.type.classreading.CachingMetadataReaderFactory
 import org.springframework.util.ClassUtils
-import org.springframework.util.ResourceUtils
 
 import java.lang.reflect.Field
-
 /**
  * A base class for configurations that bootstrap a Grails application
  *
@@ -33,9 +29,10 @@ import java.lang.reflect.Field
  *
  */
 @CompileStatic
-class GrailsAutoConfiguration implements GrailsApplicationClass, ResourceLoaderAware, ApplicationContextAware {
+class GrailsAutoConfiguration implements GrailsApplicationClass, ApplicationContextAware {
 
     private static final String APC_PRIORITY_LIST_FIELD = "APC_PRIORITY_LIST";
+    private static final List DEFAULT_IGNORED_ROOT_PACKAGES = ['com', 'org', 'net', 'co', 'java', 'javax', 'groovy']
 
     static {
         try {
@@ -52,7 +49,7 @@ class GrailsAutoConfiguration implements GrailsApplicationClass, ResourceLoaderA
         }
     }
 
-    ResourcePatternResolver resourcePatternResolver = new GrailsClasspathIgnoringResourceResolver()
+    ResourcePatternResolver resourcePatternResolver = new GrailsClasspathIgnoringResourceResolver(getClass())
 
     ApplicationContext applicationContext
 
@@ -73,6 +70,9 @@ class GrailsAutoConfiguration implements GrailsApplicationClass, ResourceLoaderA
         Collection<Class> classes = new HashSet()
         for (pkg in packages) {
             if(pkg == null) continue
+            if(ignoredRootPackages().contains(pkg)) {
+               continue
+            }
             String pattern = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX +
                     ClassUtils.convertClassNameToResourcePath(pkg) + Settings.CLASS_RESOURCE_PATTERN;
 
@@ -96,6 +96,23 @@ class GrailsAutoConfiguration implements GrailsApplicationClass, ResourceLoaderA
     }
 
     /**
+     * @return The root packages to ignore by default
+     */
+    protected List ignoredRootPackages() {
+        DEFAULT_IGNORED_ROOT_PACKAGES
+    }
+
+    /**
+     * Whether classpath scanning should be limited to the application and not dependent JAR files. Users can override this method to enable more broad scanning
+     * at the cost of startup time.
+     *
+     * @return True if scanning should be limited to the application and should not include dependant JAR files
+     */
+    protected boolean limitScanningToApplication() {
+        return true
+    }
+
+    /**
      * @return The packages to scan
      */
     Collection<Package> packages() {
@@ -110,18 +127,14 @@ class GrailsAutoConfiguration implements GrailsApplicationClass, ResourceLoaderA
         packages().collect { Package p -> p.name }
     }
 
-    @Override
-    void setResourceLoader(ResourceLoader resourceLoader) {
-        this.resourcePatternResolver = new GrailsClasspathIgnoringResourceResolver(resourceLoader)
-    }
 
     private Collection<Class> scanUsingPattern(String pattern, CachingMetadataReaderFactory readerFactory) {
-        def resources = this.resourcePatternResolver.getResources(pattern)
+        def resources = limitScanningToApplication() ?  this.resourcePatternResolver.getResources(pattern) : new PathMatchingResourcePatternResolver(applicationContext).getResources(pattern)
         def classLoader = Thread.currentThread().contextClassLoader
         Collection<Class> classes = []
         for (Resource res in resources) {
             // ignore closures / inner classes
-            if(!res.filename.contains('$')) {
+            if(!res.filename.contains('$') && !res.filename.startsWith("gsp_")) {
                 def reader = readerFactory.getMetadataReader(res)
                 def metadata = reader.annotationMetadata
                 if (metadata.annotationTypes.any() { String annotation -> annotation.startsWith('grails.') }) {
@@ -164,7 +177,12 @@ class GrailsAutoConfiguration implements GrailsApplicationClass, ResourceLoaderA
     @InheritConstructors
     @Slf4j
     private static class GrailsClasspathIgnoringResourceResolver extends PathMatchingResourcePatternResolver {
-        @Override
+
+        GrailsClasspathIgnoringResourceResolver(Class applicationClass) {
+            super(new DefaultResourceLoader(new ApplicationRelativeClassLoader(applicationClass)))
+        }
+
+        @Memoized(maxCacheSize = 20)
         protected Set<Resource> doFindAllClassPathResources(String path) throws IOException {
             Set<Resource> result = new LinkedHashSet<Resource>(16)
             ClassLoader cl = getClassLoader()
@@ -181,7 +199,6 @@ class GrailsAutoConfiguration implements GrailsApplicationClass, ResourceLoaderA
             return result
         }
 
-
         @Memoized
         protected Resource[] findAllClassPathResources(String location) throws IOException {
             return super.findAllClassPathResources(location)
@@ -191,7 +208,27 @@ class GrailsAutoConfiguration implements GrailsApplicationClass, ResourceLoaderA
         protected Resource[] findPathMatchingResources(String locationPattern) throws IOException {
             return super.findPathMatchingResources(locationPattern)
         }
+
     }
+    private static class ApplicationRelativeClassLoader extends URLClassLoader {
+
+        final URL rootResource
+
+        ApplicationRelativeClassLoader(Class applicationClass) {
+            super([ IOUtils.findRootResource(applicationClass)] as URL[])
+
+            this.rootResource = getURLs()[0]
+            def urlStr = rootResource.toString()
+            def withoutBang = new URL("${urlStr.substring(0, urlStr.length() - 2)}/")
+            addURL(withoutBang)
+        }
+
+        @Override
+        Enumeration<URL> getResources(String name) throws IOException {
+            return super.findResources(name)
+        }
+    }
+
 
 
 }
