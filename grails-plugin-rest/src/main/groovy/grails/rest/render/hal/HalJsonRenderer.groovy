f27355dbae3a682fff079e49f4ab5ff0e8b33eaf
graemerocher <graeme.rocher@gmail.com>
2016-02-22 17:51:44 +0100

    Revert "Upgrade to Groovy 2.4.6 and remove forked StreamingJsonBuilder"
    This reverts commit 695a11caa4025e91bdbf5094c0b6324cf5b22075.

--- grails-plugin-rest/src/main/groovy/grails/rest/render/hal/HalJsonRenderer.groovy
+++ grails-plugin-rest/src/main/groovy/grails/rest/render/hal/HalJsonRenderer.groovy
@@ -19,7 +19,6 @@ import grails.rest.Link
 import grails.rest.render.RenderContext
 import grails.rest.render.util.AbstractLinkingRenderer
 import groovy.json.JsonOutput
-import groovy.json.StreamingJsonBuilder
 import groovy.transform.CompileStatic
 import org.grails.web.databinding.bindingsource.DataBindingSourceRegistry
 import org.grails.web.databinding.bindingsource.HalJsonDataBindingSourceCreator
@@ -28,6 +27,7 @@ import org.grails.datastore.mapping.model.MappingFactory
 import org.grails.datastore.mapping.model.PersistentEntity
 import org.grails.datastore.mapping.model.types.Association
 import org.grails.datastore.mapping.model.types.ToOne
+import org.grails.web.json.StreamingJsonBuilder
 import org.springframework.beans.PropertyAccessorFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.convert.converter.Converter
