ac8825316ed7df9f5ff0332d9910bea2e589d576
graemerocher <graeme.rocher@gmail.com>
2016-04-28 14:50:54 +0200

    Release 3.0.17

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("3.0.17.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("3.0.17", GrailsUtil.getGrailsVersion());
     }
 
     @Override
