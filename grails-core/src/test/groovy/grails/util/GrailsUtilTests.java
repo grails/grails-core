0270eea26743811d003680fa028a9119f18b2510
Graeme Rocher <graeme.rocher@gmail.com>
2016-07-21 17:40:39 +0200

    Back to snapshot

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("3.2.0.M2", GrailsUtil.getGrailsVersion());
+        assertEquals("3.2.0.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @Override
