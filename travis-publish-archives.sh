36d75b8ebac91bc82fe4c206ac6e4504d41a1585
Graeme Rocher <graeme.rocher@gmail.com>
2016-09-01 15:52:15 +0200

    Release Grails 3.2.0.RC1

--- travis-publish-archives.sh
+++ travis-publish-archives.sh
@@ -9,18 +9,6 @@ set -x
 echo "Project Version: '$grailsVersion'"
 echo "EXIT STATUS of build: '$EXIT_STATUS'"
 
-# use travis_after_all.py for publishing only after all builds are successful.
-if [[ "$BUILD_LEADER" == "YES" ]]; then
-  if [[ "$BUILD_AGGREGATE_STATUS" != "others_succeeded" ]]; then
-    echo "Some builds failed, not publishing."
-    exit 0
-  fi
-else
-  # not build leader, exit
-  echo "Not build leader, exiting"
-  exit 0
-fi
-
 
 if [[ $TRAVIS_PULL_REQUEST == 'false'
     && $TRAVIS_REPO_SLUG == grails/grails-core
