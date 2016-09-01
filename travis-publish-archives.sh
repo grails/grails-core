cfe4f51bfea5da2a3a523a0e0bb5611b60832d27
Graeme Rocher <graeme.rocher@gmail.com>
2016-09-01 16:36:53 +0200

    Back to snapshot

--- travis-publish-archives.sh
+++ travis-publish-archives.sh
@@ -34,9 +34,54 @@ if [[ $TRAVIS_PULL_REQUEST == 'false'
     echo "Running Gradle publish for branch $TRAVIS_BRANCH"
 
     if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
-        ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" grails-dependencies:uploadArchives grails-bom:uploadArchives || EXIT_STATUS=$?
+        ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" publish uploadArchives -x grails-bom:uploadArchives -x grails-dependencies:uploadArchives || EXIT_STATUS=$?
         ./gradlew closeAndPromoteRepository
-        ./gradlew assemble || EXIT_STATUS=$?
+
+        if [[ $EXIT_STATUS == 0 ]]; then
+            ./gradlew --stop
+            # wait 30 seconds to ensure the previous promotion completes
+            sleep 30
+            ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" grails-dependencies:uploadArchives grails-bom:uploadArchives || EXIT_STATUS=$?
+            ./gradlew closeAndPromoteRepository
+        fi
+
+        if [[ $EXIT_STATUS == 0 ]]; then
+            ./gradlew assemble || EXIT_STATUS=$?
+        fi
+
+        # Configure GIT
+        git config --global credential.helper "store --file=~/.git-credentials"
+        echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
+        git config --global user.name "$GIT_NAME"
+        git config --global user.email "$GIT_EMAIL"
+
+        # Tag and release the docs
+        git clone https://${GH_TOKEN}@github.com/grails/grails-doc.git grails-doc
+        cd grails-doc
+
+        echo "grails.version=${TRAVIS_TAG:1}" > gradle.properties
+        git add gradle.properties
+        git commit -m "Release $TRAVIS_TAG docs"
+        git tag $TRAVIS_TAG
+        git push --tags
+        git push
+        cd ..
+
+        # Update the website
+        git clone https://${GH_TOKEN}@github.com/grails/grails-static-website.git
+        cd grails-static-website
+        echo -e "${TRAVIS_TAG:1}" >> generator/src/main/resources/versions
+        git add generator/src/main/resources/versions
+        git commit -m "Release Grails $TRAVIS_TAG"
+        git push
+        cd ..
+
+        # Rebuild Artifactory index
+        curl -H "X-Api-Key:$ARTIFACTORY_API_KEY" -X POST "http://repo.grails.org/grails/api/maven?repos=libs-releases-local,plugins-releases-local,plugins3-releases-local,core&force=1"
+
+    elif [[ $TRAVIS_BRANCH =~ ^master|[23]\..\.x$ ]]; then
+        ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" publish || EXIT_STATUS=$?
     fi
 
 fi
