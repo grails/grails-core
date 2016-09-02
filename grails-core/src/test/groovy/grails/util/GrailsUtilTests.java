452d3412894099eb64072eb448d4dad70926924a
Graeme Rocher <graeme.rocher@gmail.com>
2016-09-02 10:47:05 +0200

    Release Grails 3.1.11

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -35,7 +35,7 @@ import org.xml.sax.SAXException;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("3.1.11.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("3.1.11", GrailsUtil.getGrailsVersion());
     }
 
     @Override
