fccaaeaf3f0803f490270ab750381eb27ccb2744
Graeme Rocher <graeme.rocher@gmail.com>
2016-09-30 11:14:02 +0200

    Optimize classpath scanning, by not scanning more than is necessary

--- grails-bootstrap/src/main/groovy/grails/io/ResourceUtils.groovy
+++ grails-bootstrap/src/main/groovy/grails/io/ResourceUtils.groovy
@@ -47,7 +47,8 @@ class ResourceUtils extends GrailsResourceUtils {
             rootDir.eachDir { File dir ->
                 def dirName = dir.name
                 if (!dir.hidden && !dirName.startsWith('.') && !['conf', 'i18n', 'assets', 'views'].contains(dirName)) {
-                    populatePackages(dir, packageNames, "")
+                    File[] files = dir.listFiles()
+                    populatePackages(dir,files, packageNames, "")
                 }
             }
         }
@@ -55,13 +56,28 @@ class ResourceUtils extends GrailsResourceUtils {
         return packageNames
     }
 
-    protected static populatePackages(File rootDir, Collection<String> packageNames, String prefix) {
-        rootDir.eachDir { File dir ->
-            def dirName = dir.name
-            if (!dir.hidden && !dirName.startsWith('.')) {
-                packageNames << "${prefix}${dirName}".toString()
+    protected static populatePackages(File rootDir, File[] files, Collection<String> packageNames, String prefix) {
 
-                populatePackages(dir, packageNames, "${prefix}${dirName}.")
+        if(files != null) {
+            for(dir in files ) {
+                if(dir.isDirectory()) {
+                    String dirName = dir.name
+                    if (!dir.hidden && !dirName.startsWith('.')) {
+                        def dirFiles = dir.listFiles()
+                        if(dirFiles != null) {
+                            boolean hasGroovySources = dirFiles?.find() { File f -> f.name.endsWith('.groovy') }
+                            if(hasGroovySources) {
+                                // if there are Groovy sources stop here, no need to add child packages
+                                packageNames.add "${prefix}${dirName}".toString()
+                            }
+                            else {
+                                // otherwise recurse into a child package
+                                populatePackages(dir, dirFiles, packageNames, "${prefix}${dirName}.")
+                            }
+                        }
+
+                    }
+                }
             }
         }
     }
