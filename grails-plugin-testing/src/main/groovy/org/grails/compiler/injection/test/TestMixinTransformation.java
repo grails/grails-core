2604f2b016b6c7fac12e06e7b714383a0b65a462
Graeme Rocher <graeme.rocher@gmail.com>
2016-10-04 11:58:19 +0200

    Don't override methods that already exist in unit test mixins. Fixes #10201

--- grails-plugin-testing/src/main/groovy/org/grails/compiler/injection/test/TestMixinTransformation.java
+++ grails-plugin-testing/src/main/groovy/org/grails/compiler/injection/test/TestMixinTransformation.java
@@ -217,7 +217,7 @@ public class TestMixinTransformation implements ASTTransformation{
         
         boolean implementsTestRuntimeAwareMixin = GrailsASTUtils.findInterface(mixinClassNode, ClassHelper.make(TestRuntimeAwareMixin.class)) != null;
         
-        FieldNode mixinFieldNode = null;
+        FieldNode mixinFieldNode;
         if(!implementsTestRuntimeAwareMixin) { 
             mixinFieldNode = addLegacyMixinFieldIfNonExistent(classNode, mixinClassNode, fieldName);
         } else {
@@ -395,7 +395,7 @@ public class TestMixinTransformation implements ASTTransformation{
     }
 
     static protected boolean hasDeclaredMethod(ClassNode classNode, MethodNode mixinMethod) {
-        return classNode.hasDeclaredMethod(mixinMethod.getName(), mixinMethod.getParameters());
+        return classNode.hasMethod(mixinMethod.getName(), mixinMethod.getParameters());
     }
 
     static protected boolean hasAnnotation(MethodNode mixinMethod, Class<?> beforeClass) {
