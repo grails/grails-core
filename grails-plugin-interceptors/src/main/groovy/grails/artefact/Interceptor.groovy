3a053bb307c4f251d5583cef77cfba3ca26c629f
Jeff Scott Brown <brownj@ociweb.com>
2016-10-18 19:38:50 -0500

    Remove TraitInjector impls in favor of @Enhances

--- grails-plugin-interceptors/src/main/groovy/grails/artefact/Interceptor.groovy
+++ grails-plugin-interceptors/src/main/groovy/grails/artefact/Interceptor.groovy
@@ -50,6 +50,7 @@ import java.util.regex.Pattern
  * @since 3.0
  */
 @CompileStatic
+@Enhances('Interceptor')
 trait Interceptor implements ResponseRenderer, ResponseRedirector, RequestForwarder, DataBinder, WebAttributes, ServletAttributes, Ordered {
 
     /**
