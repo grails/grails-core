3f431c8108f553b147183f80fd4ba84cecee0323
graemerocher <graeme.rocher@gmail.com>
2016-11-10 15:03:05 +0100

    Release Grails 3.2.3

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -25,7 +25,7 @@ import junit.framework.TestCase;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("3.2.3.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("3.2.3", GrailsUtil.getGrailsVersion());
     }
 
     @Override
