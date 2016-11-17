97981d591273de24b82acb9780300bbab224ed01
Lari Hotari <lari@hotari.net>
2016-11-17 10:57:52 +0200

    Simple test for GSP model and compile static
    - test passes since model is ignored initially

---
+++ grails-gsp/src/test/groovy/org/grails/gsp/GspCompileStaticSpec.groovy
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.grails.gsp
+
+import spock.lang.Specification
+
+
+class GspCompileStaticSpec extends Specification {
+    GroovyPagesTemplateEngine gpte
+
+    def setup() {
+        gpte = new GroovyPagesTemplateEngine()
+        gpte.afterPropertiesSet()
+    }
+
+    def "should support static compilation"() {
+        given:
+        def template = '''<%@page model="Date date"%>${date.time}'''
+        def date = new Date(123L)
+        when:
+        def rendered = renderTemplate(template, [date: date])
+        then:
+        rendered == '123'
+    }
+
+    def renderTemplate(templateSource, model) {
+        def t = gpte.createTemplate(templateSource, "template${templateSource.hashCode()}")
+        def w = t.make(model)
+        def sw = new StringWriter()
+        def pw = new PrintWriter(sw, true)
+        w.writeTo(pw)
+        sw.toString()
+    }
+}
