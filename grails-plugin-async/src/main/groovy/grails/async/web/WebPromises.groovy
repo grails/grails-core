34daeed6a3ce6dbd2a7e09b362c211218c423c2a
graemerocher <graeme.rocher@gmail.com>
2017-02-21 13:59:37 +0100

    Restore previous behaviour of Promises, now that WebPromises exists

--- grails-plugin-async/src/main/groovy/grails/async/web/WebPromises.groovy
+++ grails-plugin-async/src/main/groovy/grails/async/web/WebPromises.groovy
@@ -1,9 +1,13 @@
 package grails.async.web
 
 import grails.async.Promise
+import grails.async.PromiseFactory
 import grails.async.Promises
 import grails.async.decorator.PromiseDecorator
 import groovy.transform.CompileStatic
+import org.grails.async.factory.SynchronousPromiseFactory
+import org.grails.async.factory.gpars.GparsPromiseFactory
+import org.grails.plugins.web.async.AsyncWebRequestPromiseDecoratorLookupStrategy
 import org.grails.plugins.web.async.WebRequestPromiseDecoratorLookupStrategy
 
 import java.util.concurrent.TimeUnit
@@ -17,51 +21,65 @@ import java.util.concurrent.TimeUnit
 @CompileStatic
 class WebPromises {
 
-    private static final WebRequestPromiseDecoratorLookupStrategy DECORATOR_LOOKUP = new WebRequestPromiseDecoratorLookupStrategy()
+    private static final AsyncWebRequestPromiseDecoratorLookupStrategy DECORATOR_LOOKUP = new AsyncWebRequestPromiseDecoratorLookupStrategy()
+
+    static PromiseFactory promiseFactory
+
+    static {
+        if (GparsPromiseFactory.isGparsAvailable()) {
+            promiseFactory = new GparsPromiseFactory()
+        }
+        else {
+            promiseFactory = new SynchronousPromiseFactory()
+        }
+    }
+
+    private WebPromises() {
+    }
     /**
      * @see grails.async.PromiseFactory#waitAll(grails.async.Promise[])
      */
     static<T> List<T> waitAll(Promise<T>...promises) {
-        return Promises.waitAll(promises)
+        return promiseFactory.waitAll(promises)
     }
 
     /**
      * @see grails.async.PromiseFactory#waitAll(java.util.List)
      */
     static<T> List<T> waitAll(List<Promise<T>> promises) {
-        return Promises.waitAll(promises)
+        return promiseFactory.waitAll(promises)
     }
 
     /**
      * @see grails.async.PromiseFactory#waitAll(java.util.List)
      */
     static<T> List<T> waitAll(List<Promise<T>> promises, final long timeout, final TimeUnit units) {
-        return Promises.waitAll(promises, timeout, units)
+        return promiseFactory.waitAll(promises, timeout, units)
     }
 
     /**
      * @see grails.async.PromiseFactory#onComplete(java.util.List, groovy.lang.Closure)
      */
     static<T> Promise<List<T>> onComplete(List<Promise<T>> promises, Closure<?> callable ) {
-        return Promises.onComplete(promises, callable)
+        return promiseFactory.onComplete(promises, callable)
     }
     /**
      * @see grails.async.PromiseFactory#onError(java.util.List, groovy.lang.Closure)
      */
     static<T> Promise<List<T>> onError(List<Promise<T>> promises, Closure<?> callable ) {
-        return Promises.onError(promises, callable)
+        return promiseFactory.onError(promises, callable)
     }
     /**
      * @see grails.async.PromiseFactory#createPromise(java.util.Map)
      */
     static<K,V> Promise<Map<K,V>> createPromise(Map<K, V> map) {
-        return Promises.createPromise(map, DECORATOR_LOOKUP.findDecorators())
+        return promiseFactory.createPromise(map, DECORATOR_LOOKUP.findDecorators())
     }
     /**
      * @see grails.async.PromiseFactory#createPromise(groovy.lang.Closure[])
      */
     static<T> Promise<List<T>> createPromise(Closure<T>... c) {
-        return Promises.createPromise(Arrays.asList(c), DECORATOR_LOOKUP.findDecorators())
+        return promiseFactory.createPromise(Arrays.asList(c), DECORATOR_LOOKUP.findDecorators())
     }
 
     /**
@@ -74,7 +92,7 @@ class WebPromises {
      * @see grails.async.PromiseFactory#createPromise(groovy.lang.Closure[])
      */
     static<T> Promise<T> task(Closure<T> c) {
-        return Promises.createPromise(c, DECORATOR_LOOKUP.findDecorators())
+        return promiseFactory.createPromise(c, DECORATOR_LOOKUP.findDecorators())
     }
     /**
      * @see grails.async.PromiseFactory#createPromise(groovy.lang.Closure[])
@@ -86,46 +104,46 @@ class WebPromises {
      * @see grails.async.PromiseFactory#createPromise(groovy.lang.Closure[])
      */
     static<T> Promise<List<T>> tasks(List<Closure<T>> closures) {
-        return Promises.createPromise(closures, DECORATOR_LOOKUP.findDecorators())
+        return promiseFactory.createPromise(closures, DECORATOR_LOOKUP.findDecorators())
     }
 
     /**
      * @see grails.async.PromiseFactory#createPromise()
      */
     static Promise<Object> createPromise() {
-        return Promises.createPromise()
+        return promiseFactory.createPromise()
     }
 
     /**
      * @see grails.async.PromiseFactory#createPromise(Class)
      */
     static<T> Promise<T> createPromise(Class<T> returnType) {
-        return Promises.createPromise(returnType)
+        return promiseFactory.createPromise(returnType)
     }
 
     /**
      * @see grails.async.PromiseFactory#createPromise(groovy.lang.Closure, java.util.List)
      */
     static<T> Promise<T> createPromise(Closure<T> c, List<PromiseDecorator> decorators) {
-        return Promises.createPromise(c, DECORATOR_LOOKUP.findDecorators())
+        return promiseFactory.createPromise(c, DECORATOR_LOOKUP.findDecorators())
     }
     /**
      * @see grails.async.PromiseFactory#createPromise(java.util.List, java.util.List)
      */
     static<T> Promise<List<T>> createPromise(List<Closure<T>> closures, List<PromiseDecorator> decorators) {
-        return Promises.createPromise(closures, DECORATOR_LOOKUP.findDecorators())
+        return promiseFactory.createPromise(closures, DECORATOR_LOOKUP.findDecorators())
     }
     /**
      * @see grails.async.PromiseFactory#createPromise(grails.async.Promise[])
      */
     static <T> Promise<List<T>> createPromise(Promise<T>...promises) {
-        return Promises.createPromise(promises)
+        return promiseFactory.createPromise(promises)
     }
 
     /**
      * @see grails.async.PromiseFactory#createBoundPromise(java.lang.Object)
      */
     static<T> Promise<T> createBoundPromise(T value) {
-        return Promises.createBoundPromise(value)
+        return promiseFactory.createBoundPromise(value)
     }
 }
