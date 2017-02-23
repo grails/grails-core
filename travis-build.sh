77bc187957726d40c96b9330494fc8b9639ae4fb
graemerocher <graeme.rocher@gmail.com>
2017-02-23 17:32:46 +0100

    Attempt to restore build matrix

--- travis-build.sh
+++ travis-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 # Configure GIT
 git config --global user.name "$GIT_NAME"
@@ -15,15 +16,14 @@ grailsVersion="${grailsVersion#*=}"
 grailsVersion="${grailsVersion//[[:blank:]\'\"]/}"
 
 echo "Project Version: '$grailsVersion'"
-
-git config --global credential.helper "store --file=~/.git-credentials"
-echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+echo "Gradle command to be run: '$GRADLE_CMD'"
 
 EXIT_STATUS=0
 ./gradlew --stop
 
 if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
     echo "Tagged Release Skipping Tests for Publish"
+    ./travis-publish-archives.sh
 else
     echo "Executing tests"
     ./gradlew --stacktrace test  || EXIT_STATUS=$?
@@ -36,85 +36,12 @@ else
 fi
 
 
-if [[ $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0
-    && $TRAVIS_REPO_SLUG == grails/grails-core && ( $TRAVIS_TAG =~ ^v[[:digit:]] || $TRAVIS_BRANCH =~ ^master|[23]\..\.x$ )  ]]; then
-    # files encrypted with 'openssl aes-256-cbc -in <INPUT FILE> -out <OUTPUT_FILE> -pass pass:$SIGNING_PASSPHRASE'
-    openssl aes-256-cbc -pass pass:$SIGNING_PASSPHRASE -in secring.gpg.enc -out secring.gpg -d
-    openssl aes-256-cbc -pass pass:$SIGNING_PASSPHRASE -in pubring.gpg.enc -out pubring.gpg -d
-    openssl aes-256-cbc -pass pass:$SIGNING_PASSPHRASE -in settings.xml.enc -out settings.xml -d
-    mkdir -p ~/.m2
-    cp settings.xml ~/.m2/settings.xml
-
-    mv ~/.gradle/gradle.properties{,.orig}
-    echo "org.gradle.jvmargs=-XX\:MaxPermSize\=1024m -Xmx1500m -Dfile.encoding\=UTF-8 -Duser.country\=US -Duser.language\=en -Duser.variant" >> ~/.gradle/gradle.properties
-    echo "org.gradle.daemon=true" >> ~/.gradle/gradle.properties
-    ./gradlew --stop
-    #./gradlew groovydoc
-    mv ~/.gradle/gradle.properties{.orig,}
-
-    echo "Publishing archives"
-
-    gpg --keyserver keyserver.ubuntu.com --recv-key $SIGNING_KEY
-
-    echo "Running Gradle publish for branch $TRAVIS_BRANCH"
-
-    if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
-        ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" publish uploadArchives -x grails-bom:uploadArchives -x grails-dependencies:uploadArchives || EXIT_STATUS=$?
-        ./gradlew closeAndPromoteRepository
-
-        if [[ $EXIT_STATUS == 0 ]]; then
-            ./gradlew --stop
-            # wait 30 seconds to ensure the previous promotion completes
-            sleep 30
-            ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" grails-dependencies:uploadArchives grails-bom:uploadArchives || EXIT_STATUS=$?
-            ./gradlew closeAndPromoteRepository
-        fi
-
-        if [[ $EXIT_STATUS == 0 ]]; then
-            ./gradlew assemble || EXIT_STATUS=$?
-        fi
-
-        # Tag and release the docs
-        cd ..
-        git clone -b 3.2.x https://${GH_TOKEN}@github.com/grails/grails-doc.git grails-doc
-        cd grails-doc
-
-        echo "grails.version=${TRAVIS_TAG:1}" > gradle.properties
-        git add gradle.properties
-        git commit -m "Release $TRAVIS_TAG docs"
-        git tag $TRAVIS_TAG
-        git push --tags
-        git push
-        cd ..
-
-        # Update the website
-        git clone https://${GH_TOKEN}@github.com/grails/grails-static-website.git
-        cd grails-static-website
-        echo -e "${TRAVIS_TAG:1}" >> generator/src/main/resources/versions
-        git add generator/src/main/resources/versions
-        git commit -m "Release Grails $TRAVIS_TAG"
-        git push
-        cd ..
-
-        # Rebuild Artifactory index
-        curl -H "X-Api-Key:$ARTIFACTORY_API_KEY" -X POST "http://repo.grails.org/grails/api/maven?repos=libs-releases-local,plugins-releases-local,plugins3-releases-local,core&force=1"
-
-    elif [[ $TRAVIS_BRANCH =~ ^master|[23]\..\.x$ ]]; then
-        ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" publish || EXIT_STATUS=$?
-        cd ..
-        # Trigger the functional tests
-        git clone -b master https://${GH_TOKEN}@github.com/grails/grails3-functional-tests.git functional-tests
-        cd functional-tests
-        echo "$(date)" > .snapshot
-        git add .snapshot
-        git commit -m "New Core Snapshot: $(date)"
-        git push
-    fi
-
-fi
-
-#if [[ $EXIT_STATUS == 0 ]]; then
-#    ./gradlew travisciTrigger -i
-#fi
+export EXIT_STATUS
+export grailsVersion
+# done. after build, then after_success will run if EXIT_STATUS is 0
+#
+# This also means that the later modifications to EXIT_STATUS will not affect
+# the build status: Build could now be success, even though the later steps will
+# change EXIT_STATUS variable>>>>>>> parent of 19b4737... Restore previous build since matrix changes broke release process
 
 exit $EXIT_STATUS
