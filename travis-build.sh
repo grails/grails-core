92515bcf279213d45c348695bce75f6d58e37f64
graemerocher <graeme.rocher@gmail.com>
2017-02-23 17:34:07 +0100

    Attempt to restore build matrix

--- travis-build.sh
+++ travis-build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -x
 
-# Configure GIT
-git config --global user.name "$GIT_NAME"
-git config --global user.email "$GIT_EMAIL"
-git config --global credential.helper "store --file=~/.git-credentials"
-
 # Set Gradle daemon JVM args
 mkdir ~/.gradle
 echo "org.gradle.jvmargs=-XX\:MaxPermSize\=512m -Xmx1024m -Dfile.encoding\=UTF-8 -Duser.country\=US -Duser.language\=en -Duser.variant" >> ~/.gradle/gradle.properties
@@ -23,18 +18,11 @@ EXIT_STATUS=0
 
 if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
     echo "Tagged Release Skipping Tests for Publish"
-    ./travis-publish-archives.sh
 else
     echo "Executing tests"
-    ./gradlew --stacktrace test  || EXIT_STATUS=$?
+    ./gradlew $GRADLE_FLAGS $GRADLE_CMD || EXIT_STATUS=$?
     echo "Done."
-    if [[ $EXIT_STATUS == 0 ]]; then
-      echo "Executing integration tests"
-      ./gradlew --stacktrace --info integrationTest || EXIT_STATUS=$?
-      echo "Done."
-    fi
 fi
-
 
 export EXIT_STATUS
 export grailsVersion
@@ -42,6 +30,6 @@ export grailsVersion
 #
 # This also means that the later modifications to EXIT_STATUS will not affect
 # the build status: Build could now be success, even though the later steps will
-# change EXIT_STATUS variable>>>>>>> parent of 19b4737... Restore previous build since matrix changes broke release process
+# change EXIT_STATUS variable
 
 exit $EXIT_STATUS
