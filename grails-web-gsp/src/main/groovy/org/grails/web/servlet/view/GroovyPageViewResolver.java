72185061de46e3a48a86eb19887a214d728176db
jameskleeh <james.kleeh@gmail.com>
2017-02-27 11:53:53 -0500

    Fixed defaultAction in controllers to be url converter aware. Removed addViewPrefix that was not url converter aware

--- grails-web-gsp/src/main/groovy/org/grails/web/servlet/view/GroovyPageViewResolver.java
+++ grails-web-gsp/src/main/groovy/org/grails/web/servlet/view/GroovyPageViewResolver.java
@@ -82,11 +82,6 @@ public class GroovyPageViewResolver extends InternalResourceViewResolver impleme
     public void setGroovyPageLocator(GrailsConventionGroovyPageLocator groovyPageLocator) {
         this.groovyPageLocator = groovyPageLocator;
     }
-    
-    @Override
-    public View resolveViewName(String viewName, Locale locale) throws Exception {
-        return super.resolveViewName(WebUtils.addViewPrefix(viewName), locale);
-    }
 
     @Override
     protected View loadView(String viewName, Locale locale) throws Exception {
