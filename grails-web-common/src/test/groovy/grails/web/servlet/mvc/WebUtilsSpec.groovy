8e7ce5cc126436ab309aff05e6057f6f555c3dca
sudhir nimavat <sudhir@nimavat.me>
2017-03-23 20:00:52 +0530

    #10545 Handle parameters with dot when generating query string

---
+++ grails-web-common/src/test/groovy/grails/web/servlet/mvc/WebUtilsSpec.groovy
@@ -0,0 +1,26 @@
+package grails.web.servlet.mvc
+
+import org.grails.web.util.WebUtils
+import spock.lang.Issue
+import spock.lang.Specification
+
+/**
+ * @Author Sudhir Nimavat
+ */
+class WebUtilsSpec extends Specification {
+
+	@Issue("https://github.com/grails/grails-core/issues/10545")
+	def testToQueryString() {
+		given:
+		Map params = ["name":"sudhir-nimavat", "address.zip":"12345"]
+
+		when:
+		String result = WebUtils.toQueryString(params)
+
+		then:
+		result.startsWith("?")
+		def tokens = result[1..-1].split('&')
+		tokens.find({ it == "name=sudhir-nimavat"}) != null
+		tokens.find({ it == "address.zip=12345"}) != null
+	}
+}
