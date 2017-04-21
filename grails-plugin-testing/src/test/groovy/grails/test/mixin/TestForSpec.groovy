2deddca997be1cb5bb705a1e2807e75f20bd0e91
graemerocher <graeme.rocher@gmail.com>
2017-04-21 15:25:12 +0200

    Remove test that tests JUnit 3 (now deprecated)

--- grails-plugin-testing/src/test/groovy/grails/test/mixin/TestForSpec.groovy
+++ grails-plugin-testing/src/test/groovy/grails/test/mixin/TestForSpec.groovy
@@ -17,16 +17,6 @@ class TestForSpec extends Specification{
             test.retrieveLog() instanceof Logger
     }
 
-    void "Test junit 3 test doesn't get annotation"() {
-        when:
-            def test = junit3Test
-
-        then:
-            test != null
-            test.getClass().getDeclaredMethod("testIndex", null).getAnnotation(Test.class) == null
-            test.retrieveLog() instanceof Logger
-    }
-
     void "Test spock test doesn't get annotation"() {
         when:
             def test = spockTest
@@ -88,28 +78,4 @@ class ControllerTestForTests {
 ''').newInstance()
     }
 
-    def getJunit3Test() {
-        final gcl = new GroovyClassLoader()
-        gcl.parseClass('''
-class SimpleController {
-    def index = {
-        render "Hello"
-    }
-}
-''')
-        gcl.parseClass('''
-import grails.test.mixin.*
-
-@TestFor(SimpleController)
-class ControllerTestForTests extends GroovyTestCase {
-
-    void testIndex() {
-        controller.index()
-        assert response.text == 'Hello'
-    }
-
-    def retrieveLog() { log }
-}
-''').newInstance()
-    }
 }
