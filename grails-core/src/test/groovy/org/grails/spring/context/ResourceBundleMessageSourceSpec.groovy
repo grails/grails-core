666fc3f43aff1545497f1639153be47f95f3dcb2
Dennie de Lange <dennie@tkvw.nl>
2017-05-10 10:37:25 +0200

    Added test for retrieving bundle codes per messagebundle.

---
+++ grails-core/src/test/groovy/org/grails/spring/context/ResourceBundleMessageSourceSpec.groovy
@@ -0,0 +1,53 @@
+package org.grails.spring.context
+
+import org.grails.spring.context.support.ReloadableResourceBundleMessageSource
+import org.springframework.core.io.FileSystemResourceLoader
+import org.springframework.core.io.Resource
+import spock.lang.Specification
+
+class ResourceBundleMessageSourceSpec extends Specification {
+    File resourceFolder
+    
+    void setup(){
+        resourceFolder = new File(System.getProperty('java.io.tmpdir'),'resources')
+        resourceFolder.mkdirs()
+        def messages = new File(resourceFolder,'messages.properties')
+        messages.text = '''\
+            foo=bar
+        '''.stripIndent()
+        def other = new File(resourceFolder,'other.properties')
+        other.text = '''\
+            bar=foo
+        '''.stripIndent()
+    }
+    
+    void cleanup(){
+        resourceFolder.deleteDir()
+    }
+    
+    
+    void 'Check method to retrieve bundle codes per messagebundle'(){
+        given:
+            def messageSource = new ReloadableResourceBundleMessageSource(
+                resourceLoader: new TempResourceLoader(resourceFolder:resourceFolder)
+            )
+            messageSource.setBasenames('messages','other')
+            def locale = Locale.default
+        expect:
+            messageSource.getBundleCodes(locale,'messages') == ['foo']
+            messageSource.getBundleCodes(locale,'other') == ['bar']
+            messageSource.getBundleCodes(locale,'messages','other') == ['foo','bar']
+    }
+    
+}
+
+class TempResourceLoader extends FileSystemResourceLoader{
+    File resourceFolder
+    
+    @Override
+    protected Resource getResourceByPath(String path) {
+        path = new File(resourceFolder,path).absolutePath
+        return super.getResourceByPath(path)
+    }
+}
+
