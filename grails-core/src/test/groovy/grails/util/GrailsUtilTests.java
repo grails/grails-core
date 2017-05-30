719733c5510a5885163ae932f22f499464a20885
graemerocher <graeme.rocher@gmail.com>
2017-05-30 12:49:00 +0200

    Release Grails 3.2.10

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -25,8 +25,8 @@ import junit.framework.TestCase;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-       assertEquals("3.2.10.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
-        // assertEquals("3.2.8", GrailsUtil.getGrailsVersion());
+       // assertEquals("3.2.10.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("3.2.10", GrailsUtil.getGrailsVersion());
     }
 
     @Override
