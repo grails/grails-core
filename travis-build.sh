b129d72fcf30c077bdbcdc2b7394982589115b02
graemerocher <graeme.rocher@gmail.com>
2017-05-30 13:09:10 +0200

    back to snapshot

--- travis-build.sh
+++ travis-build.sh
@@ -64,14 +64,14 @@ if [[ $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0
 
     if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
         ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" publish uploadArchives -x grails-bom:uploadArchives -x grails-dependencies:uploadArchives || EXIT_STATUS=$?
-        ./gradlew closeAndPromoteRepository
+        ./gradlew closeAndReleaseRepository
 
         if [[ $EXIT_STATUS == 0 ]]; then
             ./gradlew --stop
             # wait 30 seconds to ensure the previous promotion completes
             sleep 30
             ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSPHRASE" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" grails-dependencies:uploadArchives grails-bom:uploadArchives || EXIT_STATUS=$?
-            ./gradlew closeAndPromoteRepository
+            ./gradlew closeAndReleaseRepository
         fi
 
         if [[ $EXIT_STATUS == 0 ]]; then
