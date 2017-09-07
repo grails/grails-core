b312193dcfcbe725ce35e66236088530ac1a0b4e
jameskleeh <james.kleeh@gmail.com>
2017-09-07 14:30:11 -0400

    Return the super class in the factory bean

--- grails-plugin-domain-class/src/main/groovy/org/grails/plugins/domain/support/DefaultConstraintEvaluatorFactoryBean.groovy
+++ grails-plugin-domain-class/src/main/groovy/org/grails/plugins/domain/support/DefaultConstraintEvaluatorFactoryBean.groovy
@@ -33,7 +33,7 @@ class DefaultConstraintEvaluatorFactoryBean implements FactoryBean<ConstraintsEv
 
     @Override
     Class<?> getObjectType() {
-        DefaultConstraintEvaluator
+        ConstraintsEvaluator
     }
 
     @Override
