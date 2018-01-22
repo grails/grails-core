7e2b4249edd9ecfc00947214981370af0b127a63
Iván López <lopez.ivan@gmail.com>
2018-01-22 17:26:48 +0100

    Revert "Use the gradle wrapper existing in the grails directory instead of the default selected in SDKMan"
    This reverts commit bb883b59e8a56fb3fa43df2a7ff6f1418aa1ec3b.

--- grails-shell/src/main/groovy/org/grails/cli/gradle/GradleUtil.groovy
+++ grails-shell/src/main/groovy/org/grails/cli/gradle/GradleUtil.groovy
@@ -49,8 +49,15 @@ class GradleUtil {
         GradleConnector gradleConnector = GradleConnector.newConnector().forProjectDirectory(baseDir)
         if (System.getenv("GRAILS_GRADLE_HOME")) {
             gradleConnector.useInstallation(new File(System.getenv("GRAILS_GRADLE_HOME")))
-        } else {
-            gradleConnector.forProjectDirectory(baseDir)
+        }
+        else {
+            def userHome = System.getProperty("user.home")
+            if(userHome) {
+                File sdkManGradle = new File("$userHome/.sdkman/candidates/gradle/current")
+                if(sdkManGradle.exists()) {
+                    gradleConnector.useInstallation(sdkManGradle)
+                }
+            }
         }
 
         gradleConnector.connect()
