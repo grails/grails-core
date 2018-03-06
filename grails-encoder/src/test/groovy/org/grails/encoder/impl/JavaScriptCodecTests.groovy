6a7f25081b09d4ec501ef56e9dbe8dff2d348fac
Ben Jones <benatwork99@gmail.com>
2018-03-06 13:00:56 +1100

    JavaScript encoder should replace CRLF with LF for https://github.com/grails/grails-core/issues/10956

--- grails-encoder/src/test/groovy/org/grails/encoder/impl/JavaScriptCodecTests.groovy
+++ grails-encoder/src/test/groovy/org/grails/encoder/impl/JavaScriptCodecTests.groovy
@@ -11,6 +11,18 @@ class JavaScriptCodecTests extends GroovyTestCase {
         assertEquals("\\u0027\\u0027", codec.encode("''"))
         assertEquals('\\u005c', codec.encode('\\'))
     }
+
+    void testEncodeNewlines() {
+        // CRLF should be collapsed to LF
+        assertEquals("\n", codec.encode("\r\n"))
+
+        // All other combinations should pass through
+        assertEquals("\r", codec.encode("\r"))
+        assertEquals("\n", codec.encode("\n"))
+        assertEquals("\r\r", codec.encode("\r\r"))
+        assertEquals("\n\n", codec.encode("\n\n"))
+        assertEquals("\n\r", codec.encode("\n\r"))
+    }
     
     void testEncodeSeparators() {
         String separators="\u2028\u2029"; // http://timelessrepo.com/json-isnt-a-javascript-subset
