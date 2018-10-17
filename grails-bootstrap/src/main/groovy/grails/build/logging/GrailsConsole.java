08c2626cbb499355424f83afe7a561c23fd3d370
Graeme Rocher <graeme.rocher@gmail.com>
2018-10-17 17:09:41 +0200

    Catch all jline related exceptions

--- grails-bootstrap/src/main/groovy/grails/build/logging/GrailsConsole.java
+++ grails-bootstrap/src/main/groovy/grails/build/logging/GrailsConsole.java
@@ -373,7 +373,7 @@ public class GrailsConsole implements ConsoleLogger {
             try {
                 ((Flushable)history).flush();
             }
-            catch (IOException e) {
+            catch (Throwable e) {
                 // ignore exception
             }
         }
