e7b563c4ef852dcb6428f3acad4a0d6f628a70e9
sdelamo <sergio.delamo@softamo.com>
2018-11-30 15:46:15 +0100

    Use types
    Add type

--- grails-gradle-plugin/src/main/groovy/org/grails/gradle/plugin/core/GrailsGradlePlugin.groovy
+++ grails-gradle-plugin/src/main/groovy/org/grails/gradle/plugin/core/GrailsGradlePlugin.groovy
@@ -85,7 +85,7 @@ class GrailsGradlePlugin extends GroovyPlugin {
     // NOTE: mongodb, neo4j etc. should NOT be included here so they can be independently versioned
     protected static final List<String> CORE_GORM_PLUGINS = ['hibernate4','hibernate5']
 
-    List<Class<Plugin>> basePluginClasses = [ProvidedBasePlugin, IntegrationTestGradlePlugin]
+    List<Class<Plugin>> basePluginClasses = [ProvidedBasePlugin, IntegrationTestGradlePlugin] as List< Class<Plugin> >
     List<String> excludedGrailsAppSourceDirs = ['migrations', 'assets']
     List<String> grailsAppResourceDirs = ['views', 'i18n', 'conf']
     private final ToolingModelBuilderRegistry registry
