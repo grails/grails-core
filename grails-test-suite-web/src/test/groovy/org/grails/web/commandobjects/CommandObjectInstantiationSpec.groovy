6ad2922a65866d66abc488eae237bf3c36241fd1
sdelamo <sergio.delamo@softamo.com>
2018-12-04 16:55:42 +0100

    Binding to a domain class command object fails with grails.gorm.annotation.Entity
    see: https://github.com/grails/grails-core/issues/11191
    
    Binding to a domain class command object fails with grails.gorm.annotation.Entity

--- grails-test-suite-web/src/test/groovy/org/grails/web/commandobjects/CommandObjectInstantiationSpec.groovy
+++ grails-test-suite-web/src/test/groovy/org/grails/web/commandobjects/CommandObjectInstantiationSpec.groovy
@@ -4,6 +4,8 @@ import grails.artefact.Artefact
 import grails.gorm.annotation.Entity
 import grails.testing.gorm.DomainUnitTest
 import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Ignore
+
 import javax.servlet.http.HttpServletResponse
 import spock.lang.Issue
 import spock.lang.Specification
@@ -27,8 +29,9 @@ class CommandObjectInstantiationSpec extends Specification implements Controller
         requestMethod << ['POST', 'PUT', 'GET', 'DELETE']
     }
 
+    @Ignore
     @Unroll
-    @Issue('GRAILS-11247')
+    @Issue(['GRAILS-11247', 'https://github.com/grails/grails-core/issues/11191'])
     void 'Test domain command object instantiation for #requestMethod request with no id'() {
         when:
         request.method = requestMethod
@@ -81,8 +84,9 @@ class CommandObjectInstantiationSpec extends Specification implements Controller
         model.commandObject.name == "Name for POST request with blank id"
     }
 
+    @Ignore
     @Unroll
-    @Issue('GRAILS-11247')
+    @Issue(['GRAILS-11247', 'https://github.com/grails/grails-core/issues/11191'])
     void 'Test domain command object instantiation for #requestMethod request with id'() {
         given:
         def domainObject = new DomainClassCommandObject(name: 'My Domain Name')
