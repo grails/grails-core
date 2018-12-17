5afbaa45f1711d6af5d04e3f371dc327362a2237
graemerocher <graeme.rocher@gmail.com>
2018-12-17 13:53:22 +0100

    Revert "workaround to failures in compileGroovyPages"
    This reverts commit ca84086bcb1f9df71f08ee052051a3ac759cc89f.

--- grails-bootstrap/src/main/groovy/grails/build/logging/GrailsConsole.java
+++ grails-bootstrap/src/main/groovy/grails/build/logging/GrailsConsole.java
@@ -316,8 +316,7 @@ public class GrailsConsole implements ConsoleLogger {
      * handle it and the wrapped stream will not pass the ansi chars on to Eclipse).
      */
     protected OutputStream ansiWrap(OutputStream out) {
-        //TODO https://github.com/grails/grails-core/issues/11199
-        return out; // return AnsiConsole.wrapOutputStream(out);
+        return AnsiConsole.wrapOutputStream(out);
     }
 
     public boolean isWindows() {
