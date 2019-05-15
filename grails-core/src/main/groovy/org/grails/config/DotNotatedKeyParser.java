fa8580bab06970c137ae8818aa86d84bb382ef0c
Dean Del Ponte <dean.delponte@gmail.com>
2019-05-15 10:06:12 -0500

    Issue #11042: NavigableMapConfig.getProperty does not resolve environmental variables

---
+++ grails-core/src/main/groovy/org/grails/config/DotNotatedKeyParser.java
@@ -0,0 +1,44 @@
+package org.grails.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class DotNotatedKeyParser {
+
+    /**
+     * Resolves dot notated getProperty calls on a config object so that injected environmental variables
+     * are properly resolved the same as Groovy's dot notation.
+     * @param configMap NavigableMap
+     * @param key identifies NavigableMap value to retrieve
+     * @return the property value associated with the key
+     */
+    public static Object getValueWithDotNotatedKeySupport(NavigableMap configMap, String key) {
+        if (key == null || configMap == null) {
+            return null;
+        }
+
+        List<String> keys = convertTokensIntoArrayList(new StringTokenizer(key, "."));
+        if (keys.size() == 0) {
+            return null;
+        }
+
+        Object value = null;
+        for (int i = 0; i < keys.size(); i++) {
+            if (i == 0) {
+                value = configMap.get(keys.get(i));
+            } else if (value instanceof NavigableMap) {
+                value = ((NavigableMap) value).get(keys.get(i));
+            }
+        }
+        return value;
+    }
+
+    private static List<String> convertTokensIntoArrayList(StringTokenizer st) {
+        List<String> elements = new ArrayList<String>();
+        while (st.hasMoreTokens()) {
+            elements.add(st.nextToken());
+        }
+        return elements;
+    }
+}
