9549a27644028ccfaaf268ff5e4702ec6d1a4b20
jameskleeh <james.kleeh@gmail.com>
2019-06-20 12:31:03 -0400

    Test shared constraint without dataspec

--- grails-plugin-validation/src/main/groovy/grails/validation/Validateable.groovy
+++ grails-plugin-validation/src/main/groovy/grails/validation/Validateable.groovy
@@ -23,6 +23,7 @@ import org.grails.datastore.gorm.validation.constraints.eval.DefaultConstraintEv
 import org.grails.datastore.gorm.validation.constraints.registry.DefaultConstraintRegistry
 import org.grails.datastore.mapping.keyvalue.mapping.config.KeyValueMappingContext
 import org.grails.validation.ConstrainedDelegate
+import org.grails.validation.ConstraintEvalUtils
 import org.springframework.context.ApplicationContext
 import org.springframework.context.MessageSource
 import org.springframework.context.support.StaticMessageSource
@@ -210,10 +211,12 @@ trait Validateable {
             return evaluator
         } catch (Throwable e) {
             MessageSource messageSource = Holders.findApplicationContext() ?: new StaticMessageSource()
+            Map<String, Object> defaultConstraints = Holders.findApplication() ?
+                    ConstraintEvalUtils.getDefaultConstraints(Holders.grailsApplication.config) : Collections.<String, Object>emptyMap()
             return new DefaultConstraintEvaluator(
                     new DefaultConstraintRegistry(messageSource),
                     new KeyValueMappingContext(""),
-                    Collections.emptyMap()
+                    defaultConstraints
             )
         }
     }
