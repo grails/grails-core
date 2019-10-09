68084d85838945e3229e397bf99e818c13b0bbde
Eric Helgeson <eric@agileorbit.com>
2019-10-09 13:39:05 -0500

    Initial Spring-boot 2.2 support

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -26,7 +26,7 @@ public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
         // assertEquals("3.3.1", GrailsUtil.getGrailsVersion());
-        assertEquals("4.0.1.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("4.1.0.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @Override
