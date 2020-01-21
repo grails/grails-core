cb9e09e14839d138897e62dc2d8c6f4d4d91f725
Puneet Behl <behl4485@gmail.com>
2020-01-22 02:58:45 +0530

    #11462 Test that DataBindingListenerAdapter is called

---
+++ grails-test-suite-persistence/src/test/groovy/grails/web/databinding/GrailsWebDataBindingListenerSpec.groovy
@@ -0,0 +1,37 @@
+package grails.web.databinding
+
+import grails.databinding.SimpleMapDataBindingSource
+import grails.databinding.events.DataBindingListenerAdapter
+import org.grails.spring.beans.factory.InstanceFactoryBean
+import org.grails.testing.GrailsUnitTest
+import spock.lang.Specification
+
+class GrailsWebDataBindingListenerSpec extends Specification implements GrailsUnitTest {
+
+    DataBindingListenerAdapter dataBindingListenerAdapter = Mock()
+
+    Closure doWithSpring() { { ->
+            testWidgetDataBindingListener(InstanceFactoryBean, dataBindingListenerAdapter, DataBindingListenerAdapter)
+        }
+    }
+
+    void "test that DataBindingListener is added to GrailsWebDataBinder"() {
+
+        given:
+        GrailsWebDataBinder binder = grailsApplication.mainContext.getBean(DataBindingUtils.DATA_BINDER_BEAN_NAME)
+        TestWidget testWidget = new TestWidget()
+
+        when:
+        binder.bind(testWidget, ["name": "Clock"] as SimpleMapDataBindingSource)
+
+        then:
+        1 * dataBindingListenerAdapter.supports(TestWidget) >> true
+        1 * dataBindingListenerAdapter.beforeBinding(testWidget, _)
+        1 * dataBindingListenerAdapter.afterBinding(testWidget, _)
+    }
+
+    static class TestWidget {
+        String name
+    }
+}
+
