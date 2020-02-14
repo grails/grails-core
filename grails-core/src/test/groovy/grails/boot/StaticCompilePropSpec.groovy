1f6be87f34cc65dd49f89685c665a820476a69f9
Puneet Behl <behl4485@gmail.com>
2020-02-14 16:03:08 +0530

    Set StaticCompile prop to false from GrailsApp (#11484)
    * #11423 Set StaticCompile prop to false from GrailsApp
    
    * #11423 Check if the original System property is set

---
+++ grails-core/src/test/groovy/grails/boot/StaticCompilePropSpec.groovy
@@ -0,0 +1,20 @@
+package grails.boot
+
+import org.springframework.core.env.ConfigurableEnvironment
+import spock.lang.Specification
+
+class StaticCompilePropSpec extends Specification {
+
+    void "test that compileStatic system property is set to avoid static compilation exception with application.groovy"() {
+
+        when:
+        GrailsApp app = new GrailsApp(GrailsTestConfigurationClass.class)
+        app.run()
+        ConfigurableEnvironment environment = app.getConfiguredEnvironment()
+
+        then:
+        environment
+        environment.systemProperties['micronaut.groovy.config.compileStatic'] == "false"
+        System.getProperty('micronaut.groovy.config.compileStatic') == "false"
+    }
+}
