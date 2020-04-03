96552682b2a1926c49854da66d7280fdaae535db
Puneet Behl <behl4485@gmail.com>
2020-04-04 01:59:26 +0530

    Release Grails 4.0.3

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -25,8 +25,8 @@ import junit.framework.TestCase;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-//        assertEquals("4.0.3", GrailsUtil.getGrailsVersion());
-        assertEquals("4.0.3.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("4.0.3", GrailsUtil.getGrailsVersion());
+//        assertEquals("4.0.3.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @Override
