c6080f1f8e5b31768c9818aee665801da3dec6ac
Puneet Behl <behl4485@gmail.com>
2020-05-23 02:28:43 +0530

    Fixed SoftwareVersion

--- grails-docs/src/main/groovy/grails/doc/dropdown/SoftwareVersion.groovy
+++ grails-docs/src/main/groovy/grails/doc/dropdown/SoftwareVersion.groovy
@@ -16,11 +16,16 @@ class SoftwareVersion implements Comparable<SoftwareVersion> {
         if (parts.length >= 3) {
             softVersion = new SoftwareVersion()
             softVersion.versionText = version
-            if (parts.length > 3) {
-                softVersion.snapshot = new Snapshot(parts[3])
-            }
             softVersion.major = parts[0].toInteger()
             softVersion.minor = parts[1].toInteger()
+            if (parts.length > 3) {
+                softVersion.snapshot = new Snapshot(parts[3])
+            } else if (parts[2].contains('-')) {
+                String[] subparts = parts[2].split("-")
+                softVersion.patch = subparts.first() as int
+                softVersion.snapshot = new Snapshot(subparts[1..-1].join("-"))
+                return softVersion
+            }
             softVersion.patch = parts[2].toInteger()
         }
         softVersion
@@ -56,5 +61,16 @@ class SoftwareVersion implements Comparable<SoftwareVersion> {
         } else {
             return 0
         }
+    }
+
+    @Override
+    public String toString() {
+        return "SoftwareVersion{" +
+                "major=" + major +
+                ", minor=" + minor +
+                ", patch=" + patch +
+                ", snapshot=" + snapshot +
+                ", versionText='" + versionText + '\'' +
+                '}';
     }
 }
