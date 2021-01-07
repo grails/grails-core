afbcab1e0a67908d95af7378fdd4482bd32a6ecf
Puneet Behl <behlp@objectcomputing.com>
2021-01-07 18:00:49 +0530

    Created custom pre-release action to also update the version in Update GrailsUtilTests.java

---
+++ .github/actions/pre-release/Dockerfile
@@ -0,0 +1,10 @@
+# Container image that runs your code
+FROM alpine:3
+
+RUN apk add --no-cache curl jq git bash
+
+# Copies your code file from your action repository to the filesystem path `/` of the container
+COPY entrypoint.sh /entrypoint.sh
+
+# Code file to execute when the docker container starts up (`entrypoint.sh`)
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ .github/actions/pre-release/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# $1 == GH_TOKEN
+
+echo -n "Determining release version: "
+release_version=${GITHUB_REF:11}
+echo $release_version
+
+if [ -n "$MICRONAUT_BUILD_EMAIL" ]; then
+    GIT_USER_EMAIL=$MICRONAUT_BUILD_EMAIL
+fi
+
+if [ -z "$GIT_USER_NAME" ]; then
+   GIT_USER_NAME="micronaut-build"
+fi
+
+echo "Configuring git"
+git config --global user.email "$GIT_USER_EMAIL"
+git config --global user.name "$GIT_USER_NAME"
+git fetch
+
+echo -n "Determining target branch: "
+target_branch=`cat $GITHUB_EVENT_PATH | jq '.release.target_commitish' | sed -e 's/^"\(.*\)"$/\1/g'`
+echo $target_branch
+git checkout $target_branch
+
+echo "Setting release version in gradle.properties"
+sed -i "s/^projectVersion.*$/projectVersion\=${release_version}/" gradle.properties
+sed -i "s/assertEquals(\".*$/assertEquals(\"${release_version}\", GrailsUtil.getGrailsVersion());/" grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+sed -n "/assertEquals(\".*/p" grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+cat gradle.properties
+
+echo "Pushing release version and recreating v${release_version} tag"
+git add gradle.properties
+git add grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+git commit -m "Release v${release_version}"
+git push origin $target_branch
+git push origin :refs/tags/v${release_version}
+git tag -fa v${release_version} -m "Release v${release_version}"
+git push origin $target_branch --tags
+
+echo "Closing again the release after updating the tag"
+release_url=`cat $GITHUB_EVENT_PATH | jq '.release.url' | sed -e 's/^"\(.*\)"$/\1/g'`
+echo $release_url
+curl -s --request PATCH -H "Authorization: Bearer $1" -H "Content-Type: application/json" $release_url --data "{\"draft\": false}"
