423233daa15e200d6a26def73dcf0690602def1e
Puneet Behl <behlp@objectcomputing.com>
2021-01-14 17:37:34 +0530

    Customize post-release Github action
    to automate expected version update in GrailsUtilTests.java

---
+++ .github/actions/post-release/increment_version.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Increment a version string using Semantic Versioning (SemVer) terminology.
+# Source: https://github.com/fmahnke/shell-semver
+
+# Parse command line options.
+
+while getopts ":Mmp" Option
+do
+  case $Option in
+    M ) major=true;;
+    m ) minor=true;;
+    p ) patch=true;;
+  esac
+done
+
+shift $(($OPTIND - 1))
+
+version=$1
+
+# Build array from version string.
+
+a=( ${version//./ } )
+
+# Increment version numbers as requested.
+
+if [ ! -z $major ]
+then
+  ((a[0]++))
+  a[1]=0
+  a[2]=0
+fi
+
+if [ ! -z $minor ]
+then
+  ((a[1]++))
+  a[2]=0
+fi
+
+if [ ! -z $patch ] && ! [[ "${a[3]}" =~ ^M.*|^RC.* ]]
+then
+  ((a[2]++))
+fi
+
+echo "${a[0]}.${a[1]}.${a[2]}"
+
