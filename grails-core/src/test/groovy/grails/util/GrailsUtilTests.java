f42035d2ac64b526bea2a9c9ff204aa3972b5698
puneetbehl <behlp@objectcomputing.com>
2021-03-11 21:01:27 +0000

    Back to 4.0.10.BUILD-SNAPSHOT

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -25,7 +25,7 @@ import junit.framework.TestCase;
 public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
-        assertEquals("4.0.9", GrailsUtil.getGrailsVersion());
+        assertEquals("4.0.9.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @Override
