872e6235f34eba158909a3575009b41fd3c8c683
Puneet Behl <behlp@objectcomputing.com>
2021-06-29 20:26:56 +0530

    Updated to 3.3.13-SNAPSHOT

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -26,7 +26,7 @@ public class GrailsUtilTests extends TestCase {
 
     public void testGrailsVersion() {
 //        assertEquals("3.3.13", GrailsUtil.getGrailsVersion());
-        assertEquals("3.3.13.BUILD-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("3.3.13-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @Override
