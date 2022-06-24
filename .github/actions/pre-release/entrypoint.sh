dd25216fd0d5ec8703e84ffccae459a68b88a8b1
Puneet Behl <behlp@objectcomputing.com>
2022-06-24 07:54:03 +0530

    Mark /github/workspace as safe directory

--- .github/actions/pre-release/entrypoint.sh
+++ .github/actions/pre-release/entrypoint.sh
@@ -16,6 +16,7 @@ fi
 echo "Configuring git"
 git config --global user.email "$GIT_USER_EMAIL"
 git config --global user.name "$GIT_USER_NAME"
+git config --global --add safe.directory /github/workspace
 git fetch
 
 echo -n "Determining target branch: "
