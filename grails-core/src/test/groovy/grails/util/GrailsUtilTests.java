ef1589ea238abdd06cfbb4b1b8b9fc0157a945b0
Puneet Behl <behlp@objectcomputing.com>
2022-07-18 12:03:41 +0530

    Update GrailsUtilTest
    Signed-off-by: Puneet Behl <behlp@objectcomputing.com>

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -29,7 +29,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("5.1.8-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("5.1.9-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
