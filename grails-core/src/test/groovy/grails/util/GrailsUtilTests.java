84dded49ee00d60ad71b239399a0662da582ebc8
puneetbehl <behlp@objectcomputing.com>
2022-11-09 13:10:29 +0000

    Back to 5.2.6-SNAPSHOT

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -29,7 +29,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("5.2.5", GrailsUtil.getGrailsVersion());
+        assertEquals("5.2.6-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
