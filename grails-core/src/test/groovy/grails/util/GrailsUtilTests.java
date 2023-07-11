7763c0f4cd379487ee62dcaee5e17bbef88cf7b8
puneetbehl <behlp@objectcomputing.com>
2023-07-11 10:05:03 +0000

    Release v5.3.3

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -29,7 +29,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("5.3.3-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("5.3.3", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
