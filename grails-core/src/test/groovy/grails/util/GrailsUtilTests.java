380df5277808d736896dce38363bd19aa9729d83
puneetbehl <behlp@unityfoundation.io>
2023-11-03 16:01:56 +0000

    Release v6.1.0

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -29,7 +29,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("6.1.0-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("6.1.0", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
