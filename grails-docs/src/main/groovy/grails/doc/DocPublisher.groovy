213a4a12f410dae70d47adbc2ff080ada7c3fc41
Guillermo Calvo <guillermo@guillermo.dev>
2023-12-07 08:08:56 +0100

    Replace deprecated constructor calls (#13271)

--- grails-docs/src/main/groovy/grails/doc/DocPublisher.groovy
+++ grails-docs/src/main/groovy/grails/doc/DocPublisher.groovy
@@ -24,7 +24,9 @@ import org.apache.commons.logging.LogFactory
 import org.radeox.api.engine.WikiRenderEngine
 import org.radeox.engine.context.BaseInitialRenderContext
 import org.radeox.engine.context.BaseRenderContext
+import org.yaml.snakeyaml.LoaderOptions
 import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.constructor.SafeConstructor
 
 import java.util.regex.Pattern
 
@@ -264,7 +266,7 @@ class DocPublisher {
         def legacyLinks = [:]
         if (legacyLinksFile.exists()) {
             legacyLinksFile.withInputStream { input ->
-                legacyLinks = new Yaml().load(input)
+                legacyLinks = new Yaml(new SafeConstructor(new LoaderOptions())).load(input)
             }
         }
 
@@ -536,7 +538,7 @@ class DocPublisher {
             }
             else if(propertiesFile.name.endsWith('.yml')) {
                 propertiesFile.withInputStream { input ->
-                    def ymls = new Yaml().loadAll(input)
+                    def ymls = new Yaml(new SafeConstructor(new LoaderOptions())).loadAll(input)
                     for(yml in ymls) {
                         if(yml instanceof Map) {
                             def config = yml.grails?.doc
--- grails-shell/src/main/groovy/org/grails/cli/gradle/cache/MapReadingCachedGradleOperation.groovy
+++ grails-shell/src/main/groovy/org/grails/cli/gradle/cache/MapReadingCachedGradleOperation.groovy
@@ -20,7 +20,10 @@ import groovy.transform.CompileStatic
 import groovy.transform.InheritConstructors
 import org.gradle.tooling.ProjectConnection
 import org.yaml.snakeyaml.DumperOptions
+import org.yaml.snakeyaml.LoaderOptions
 import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.constructor.SafeConstructor
+import org.yaml.snakeyaml.representer.Representer
 
 
 /**
@@ -35,7 +38,7 @@ abstract class MapReadingCachedGradleOperation <V> extends CachedGradleOperation
     @Override
     Map<String, V> readFromCached(File f) {
         def map = (Map<String, Object>) f.withReader { BufferedReader r ->
-            new Yaml().load(r)
+            new Yaml(new SafeConstructor(new LoaderOptions())).load(r)
         }
         Map<String, V> newMap = [:]
 
@@ -59,7 +62,7 @@ abstract class MapReadingCachedGradleOperation <V> extends CachedGradleOperation
                 return [(key):val.toString()]
             }
         }
-        new Yaml(options).dump(toWrite, writer)
+        new Yaml(new SafeConstructor(new LoaderOptions()), new Representer(options), options).dump(toWrite, writer)
     }
 
 }
