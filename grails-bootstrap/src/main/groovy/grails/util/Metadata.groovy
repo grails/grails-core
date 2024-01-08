1f5cdf7239b4a4e922d5b12347b8a14254899e5f
Guillermo Calvo <guillermo@guillermo.dev>
2024-01-08 10:45:38 +0100

    Revert SnakeYAML changes
    Revert "Override Spring BOM for SnakeYAML to force version 2.2  (#13255)"
    - This reverts commit 13737502d06f2f8b877c5b4956c90e14e6ae8cbd.
    
    Revert "Upgrade to SnakeYaml 2.2 and use SafeContructor to prevent accessing the classpath (#13207)"
    - This reverts commit dd02741aa931455f28678f78b5b440c821e4a70e.
    - This reverts commit 980d835b9707c20c4a1147670d0f4d024f95c2dd.
    - This reverts commit 64a28067b68b9b7be38ef21176520597345845ae.

--- grails-bootstrap/src/main/groovy/grails/util/Metadata.groovy
+++ grails-bootstrap/src/main/groovy/grails/util/Metadata.groovy
@@ -22,7 +22,6 @@ import org.grails.config.NavigableMap
 import org.grails.io.support.FileSystemResource
 import org.grails.io.support.Resource
 import org.grails.io.support.UrlResource
-import org.yaml.snakeyaml.LoaderOptions
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.SafeConstructor
 
@@ -169,7 +168,7 @@ class Metadata extends NavigableMap implements ConfigMap  {
     }
 
     private Object loadYml(InputStream input) {
-        Yaml yaml = new Yaml(new SafeConstructor(new LoaderOptions()))
+        Yaml yaml = new Yaml(new SafeConstructor())
         def loadedYaml = yaml.loadAll(input)
         List result = []
         for(Object yamlObject : loadedYaml) {
