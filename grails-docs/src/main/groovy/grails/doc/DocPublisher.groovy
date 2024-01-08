184c6ffcc456d0328d543f093971bea0bb15893f
Guillermo Calvo <guillermo@guillermo.dev>
2024-01-08 12:28:18 +0100

    Revert SnakeYAML changes (#13337)
    * Revert SnakeYAML changes
    
    Revert "Add SnakeYAML BOM (#13245)"
    - This reverts commit 002557cc2ec6e9790fe5620aedf653b68f50ae73.
    
    Revert "Upgrade snakeyaml, micronaut, spring and spring boot (#13194)"
    - This reverts commit 0c86bf79213245c1912feffe6463de20f76bc696.
    - This reverts commit b047374d2b7f4eff8b33455e51f65234e2c2a29a.
    
    * Fix Groovy artifactory snapshots repository URL

--- grails-docs/src/main/groovy/grails/doc/DocPublisher.groovy
+++ grails-docs/src/main/groovy/grails/doc/DocPublisher.groovy
@@ -24,7 +24,6 @@ import org.apache.commons.logging.LogFactory
 import org.radeox.api.engine.WikiRenderEngine
 import org.radeox.engine.context.BaseInitialRenderContext
 import org.radeox.engine.context.BaseRenderContext
-import org.yaml.snakeyaml.LoaderOptions
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.SafeConstructor
 
@@ -266,7 +265,7 @@ class DocPublisher {
         def legacyLinks = [:]
         if (legacyLinksFile.exists()) {
             legacyLinksFile.withInputStream { input ->
-                legacyLinks = new Yaml(new SafeConstructor(new LoaderOptions())).load(input)
+                legacyLinks = new Yaml(new SafeConstructor()).load(input)
             }
         }
 
@@ -538,7 +537,7 @@ class DocPublisher {
             }
             else if(propertiesFile.name.endsWith('.yml')) {
                 propertiesFile.withInputStream { input ->
-                    def ymls = new Yaml(new SafeConstructor(new LoaderOptions())).loadAll(input)
+                    def ymls = new Yaml(new SafeConstructor()).loadAll(input)
                     for(yml in ymls) {
                         if(yml instanceof Map) {
                             def config = yml.grails?.doc
