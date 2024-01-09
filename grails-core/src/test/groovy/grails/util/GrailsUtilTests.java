ae13540701c95d028c04cf9f8eb31789b72add14
puneetbehl <behlp@unityfoundation.io>
2024-01-09 12:28:12 +0000

    Back to 5.3.7-SNAPSHOT

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -29,7 +29,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("5.3.6", GrailsUtil.getGrailsVersion());
+        assertEquals("5.3.7-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
