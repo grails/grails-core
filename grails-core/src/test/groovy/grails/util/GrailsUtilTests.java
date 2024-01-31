30485259ff330f95403c4e165d4c4a80d764ad6b
grails-build <puneetbehl@users.noreply.github.com>
2024-01-31 09:33:14 +0000

    Back to 6.1.3-SNAPSHOT

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -29,7 +29,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("6.1.2-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("6.1.3-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
