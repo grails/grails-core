e408c922c6d5e977719ea5cc5c8e3c77ebe9ea63
James Fredley <jamesfredley@users.noreply.github.com>
2024-09-29 15:18:01 -0400

    Restore original contentType for 3 tests

--- grails-test-suite-web/src/test/groovy/grails/rest/web/RespondMethodSpec.groovy
+++ grails-test-suite-web/src/test/groovy/grails/rest/web/RespondMethodSpec.groovy
@@ -73,7 +73,7 @@ class RespondMethodSpec extends Specification implements ControllerUnitTest<Book
 
         then:"A modelAndView and view is produced"
             result == null
-            response.contentType == 'application/xml;charset=UTF-8'
+            response.contentType == 'text/xml;charset=UTF-8'
             response.xml.title.text() == 'The Stand'
     }
 
@@ -87,7 +87,7 @@ class RespondMethodSpec extends Specification implements ControllerUnitTest<Book
 
         then:"A modelAndView and view is produced"
             result == null
-            response.contentType == 'application/xml;charset=UTF-8'
+            response.contentType == 'text/xml;charset=UTF-8'
     }
 
     void "Test that the respond method produces errors XML for a domain instance that has errors and a content type of XML"() {
@@ -102,7 +102,7 @@ class RespondMethodSpec extends Specification implements ControllerUnitTest<Book
 
         then:"A modelAndView and view is produced"
             result == null
-            response.contentType == 'application/xml;charset=UTF-8'
+            response.contentType == 'text/xml;charset=UTF-8'
             response.xml.error.message.text() == 'Property [title] of class [class grails.rest.web.Book] cannot be null'
     }
 
